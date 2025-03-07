4f00da3fc4e5c329763e8fbb8f73cd7b10addb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 06:05:07 +0000

    Add package anaconda-assistant-sdk/0.3.7/anaconda_assistant_sdk-0.3.7.tar.gz

---
+++ packages/anaconda-assistant-sdk/anaconda_assistant_sdk-0.3.7.tar.gz/anaconda_assistant_sdk-0.3.7/src/anaconda_assistant/config.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from anaconda_cli_base.config import AnacondaBaseSettings
+
+
+class AssistantConfig(AnacondaBaseSettings, plugin_name="assistant"):
+    domain: str = "assistant.anaconda.cloud"
+    client_source: str = "anaconda-cli-prod"
+    api_version: str = "v3"
+    accepted_terms: Optional[bool] = None
+    data_collection: Optional[bool] = None
