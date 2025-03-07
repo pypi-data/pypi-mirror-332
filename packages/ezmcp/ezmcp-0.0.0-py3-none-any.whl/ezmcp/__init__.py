36064af39405ad4c7c911cfea3ddae0df3b94a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:04:34 +0000

    Add package ezmcp/0.0.0/ezmcp-0.0.0-py3-none-any.whl

---
+++ packages/ezmcp/ezmcp-0.0.0-py3-none-any.whl/ezmcp/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.1"
+
+from ezmcp.app import ezmcp
+from ezmcp.types import EmbeddedResource, ImageContent, Response, TextContent, Tool
+
+__all__ = ["ezmcp", "Tool", "Response", "TextContent", "ImageContent", "EmbeddedResource"]
