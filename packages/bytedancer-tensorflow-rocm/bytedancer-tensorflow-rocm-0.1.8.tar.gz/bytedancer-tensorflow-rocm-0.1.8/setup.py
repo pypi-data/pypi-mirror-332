b3145e471cc25229e681e6b43919436595273111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:54 +0000

    Add package bytedancer-tensorflow-rocm/0.1.8/bytedancer-tensorflow-rocm-0.1.8.tar.gz

---
+++ packages/bytedancer-tensorflow-rocm/bytedancer-tensorflow-rocm-0.1.8.tar.gz/bytedancer-tensorflow-rocm-0.1.8/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+setup(
+    name="bytedancer-tensorflow-rocm",
+    version="0.1.8",
+    packages=["bytedancer-tensorflow-rocm"],
+    description="A simple package named bytedancer-tensorflow-rocm",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/bytedancer-tensorflow-rocm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
