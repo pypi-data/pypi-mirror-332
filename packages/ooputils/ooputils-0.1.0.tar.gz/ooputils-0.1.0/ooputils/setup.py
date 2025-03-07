0f9e86c8a21e3459ca9d567ecb5a9f2b72f0a824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:05:12 +0000

    Add package ooputils/0.1.0/ooputils-0.1.0.tar.gz

---
+++ packages/ooputils/ooputils-0.1.0.tar.gz/ooputils-0.1.0/ooputils/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="ooputils",
+    version="0.1.0",
+    description="Library for OOP utilities",
+    author="Rafael da Rocha Ferreira",
+    author_email="programadorlhama@gmail.com",
+    packages=["ooputils"],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
