05f3f553fb1d5bbe206ab92787c44b6fe209469f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:04:52 +0000

    Add package gru/0.0.1rc26.dev21/gru-0.0.1rc26.dev21.tar.gz

---
+++ packages/gru/gru-0.0.1rc26.dev21.tar.gz/gru-0.0.1rc26.dev21/gru/components/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+class Broker(BaseModel):
+    type: str
+    name: str
+
+class CheckpointDB(BaseModel):
+    type: str
+    name: str
+    size: str
+
+class TaskServer(BaseModel):
+    type: str
+    name: str
+    replicas: int
+    concurrency_per_replica: int
+    broker_name: str
+    aws_role_arn: str | None = None
+
+class ResourceSetupRequest(BaseModel):
+    cluster_name: str = None
+    broker: Broker = None
+    checkpoint_db: CheckpointDB = None
+    task_server: TaskServer = None
