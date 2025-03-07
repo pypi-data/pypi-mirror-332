1304f98c1fb030ab400417f1de3889acf48f307a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 17:05:09 +0000

    Add package mns-common/1.3.2.3/mns_common-1.3.2.3-py3-none-any.whl

---
+++ packages/mns-common/mns_common-1.3.2.3-py3-none-any.whl/mns_common/api/k_line/__init__.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 16
+project_path = file_path[0:end]
+sys.path.append(project_path)
+import akshare as ak
+
+stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20170301", end_date='20240528', adjust="")
+print(stock_zh_a_hist_df)
