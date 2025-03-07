9ef16d28c52f546322009d64e162bf932696b951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:11:59 +0000

    Add package cmr-connectors-lib/0.2/cmr_connectors_lib-0.2.tar.gz

---
+++ packages/cmr-connectors-lib/cmr_connectors_lib-0.2.tar.gz/cmr_connectors_lib-0.2/README.md
@@ -0,0 +1,11 @@
+## CMR Connectors Library
+This library returns connectors to use in your projects from different databases.
+
+
+## Push to pypi
+```bash
+pip install wheel
+python setup.py sdist bdist_wheel
+pip install twine
+twine upload dist/*
+```
---
+++ packages/cmr-connectors-lib/cmr_connectors_lib-0.2.tar.gz/cmr_connectors_lib-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cmr_connectors_lib',
+    version='0.2',
+    packages=find_packages(),
+    # Add dependencies here
+    install_requires=[
+    "libkrb5-dev",
+    "libsasl2-dev",
+    "pymssql",
+    "pyodbc",
+    "psycopg2",
+    "sqlalchemy",
+    "cx_oracle"
+    ],
+    description='CMR Connectors Library',
+    author='Soufiane',
+    author_email='soufiane.amghar@berexia.com',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
