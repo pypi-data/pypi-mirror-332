ccc0e3f11a7b90f0ee64a21a835c023ab4780198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 06:05:06 +0000

    Add package hermes-stream-nodes/1.0.2/hermes_stream_nodes-1.0.2-py3-none-any.whl

---
+++ packages/hermes-stream-nodes/hermes_stream_nodes-1.0.2-py3-none-any.whl/hermes_stream_nodes/__init__.py
@@ -0,0 +1,5 @@
+from .generic_stream import GenericStreamTransport
+from .serial_stream import SerialPortInfo, SerialStream
+__all__ = ["GenericStreamTransport", "SerialStream", "SerialPortInfo"]
+
+NODES = [SerialStream]
---
+++ packages/hermes-stream-nodes/hermes_stream_nodes-1.0.2-py3-none-any.whl/hermes_stream_nodes/stream_counter.py
@@ -0,0 +1,46 @@
+import time
+from dataclasses import dataclass
+
+
+@dataclass
+class StreamStatistics:
+    tx_bytes: int = 0
+    rx_bytes: int = 0
+    tx_rate: float = 0  # in bits per second
+    rx_rate: float = 0  # in bits per second
+
+
+class StreamStatisticsManager:
+    def __init__(self):
+        self.tx_bytes = 0
+        self.rx_bytes = 0
+        self.last_rate_update = time.time()
+        self.last_stats = StreamStatistics()
+
+    def get_rates(self) -> StreamStatistics:
+        now = time.time()
+
+        elapsed = now - self.last_rate_update
+
+        if elapsed < 0.1:
+            return self.last_stats
+
+        tx_rate = (self.tx_bytes - self.last_stats.tx_bytes) * 8 / elapsed
+        rx_rate = (self.rx_bytes - self.last_stats.rx_bytes) * 8 / elapsed
+
+        self.last_stats = StreamStatistics(
+            tx_bytes=self.tx_bytes,
+            rx_bytes=self.rx_bytes,
+            tx_rate=tx_rate,
+            rx_rate=rx_rate,
+        )
+
+        self.last_rate_update = now
+
+        return self.last_stats
+    
+    def register_tx_bytes(self, bytes:int):
+        self.tx_bytes += bytes
+
+    def register_rx_bytes(self, bytes:int):
+        self.rx_bytes += bytes
