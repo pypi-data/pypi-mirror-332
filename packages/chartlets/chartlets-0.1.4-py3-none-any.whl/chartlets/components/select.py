f5e99402b78b85d3f6a8a2892a33675cc1569757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:11:42 +0000

    Add package chartlets/0.1.4/chartlets-0.1.4-py3-none-any.whl

---
+++ packages/chartlets/chartlets-0.1.4-py3-none-any.whl/chartlets/components/select.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+
+from chartlets import Component
+
+
+OptionValue = str | int | float
+SelectOption = OptionValue | tuple[OptionValue, str]
+"""A select option is a number or text value or a (value, label) pair."""
+
+
+@dataclass(frozen=True)
+class Select(Component):
+    """Select components are used for collecting user provided
+    information from a list of options."""
+
+    options: list[SelectOption] = field(default_factory=list)
+    """The options given as a list of number or text values or a list
+    of (value, label) pairs.
+    """
+
+    tooltip: str | None = None
+    """Tooltip title. Optional."""
