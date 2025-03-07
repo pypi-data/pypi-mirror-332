7e57d278ebc55ebf2f14928ce402749e182e001d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:05:10 +0000

    Add package test-cosmic/0.0.1/test_cosmic-0.0.1.tar.gz

---
+++ packages/test-cosmic/test_cosmic-0.0.1.tar.gz/test_cosmic-0.0.1/README.md
@@ -0,0 +1,23 @@
+
+
+
+**Create a virtual environment**:
+   ```bash
+   python -m venv env
+   source env/bin/activate  # On Windows use `env\Scripts\activate`
+   ```
+
+**Install dependencies:**
+
+   Use pip to install the required packages:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+
+**Run the application:**
+
+```bash
+	python3 main.py
+```
---
+++ packages/test-cosmic/test_cosmic-0.0.1.tar.gz/test_cosmic-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_cosmic",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    # author="Your Name",
+    # author_email="your.email@example.com",
+    # description="A short description of your package",
+    # url="https://github.com/yourusername/your-repo",
+)
