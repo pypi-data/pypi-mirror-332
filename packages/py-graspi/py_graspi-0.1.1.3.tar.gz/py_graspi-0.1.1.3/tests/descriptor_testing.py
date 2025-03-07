11a1dd147b74737274e299186dbeea0488f95cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:04:30 +0000

    Add package py-graspi/0.1.1.3/py_graspi-0.1.1.3.tar.gz

---
+++ packages/py-graspi/py_graspi-0.1.1.3.tar.gz/py_graspi-0.1.1.3/tests/descriptor_testing.py
@@ -0,0 +1,16 @@
+# from py_graspi import igraph_testing as ig
+# from py_graspi import descriptors as d
+import os
+import sys
+
+sys.path.append(os.path.abspath('../src'))
+import src.igraph_testing as ig
+import src.descriptors as d
+
+
+graph_data = ig.generateGraphAdj(sys.argv[1])
+
+dic = d.descriptors(graph_data, sys.argv[1])
+
+for key, value in dic.items():
+    print(key, value)
