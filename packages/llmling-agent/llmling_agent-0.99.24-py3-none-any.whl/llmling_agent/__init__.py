a3d42daac8aa920708d737138e79e3734f90f21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:04 +0000

    Add package llmling-agent/0.99.24/llmling_agent-0.99.24-py3-none-any.whl

---
+++ packages/llmling-agent/llmling_agent-0.99.24-py3-none-any.whl/llmling_agent/__init__.py
@@ -0,0 +1,31 @@
+"""Agent configuration and creation."""
+
+from llmling_agent.models import AgentsManifest, AgentConfig
+from llmling_agent.agent import Agent, StructuredAgent, AnyAgent, AgentContext
+
+from llmling_agent.delegation import AgentPool, Team, TeamRun, BaseTeam
+from dotenv import load_dotenv
+from llmling_agent.messaging.messages import ChatMessage
+from llmling_agent.tools import Tool, ToolCallInfo
+from llmling_agent.messaging.messagenode import MessageNode
+
+__version__ = "0.99.24"
+
+load_dotenv()
+
+__all__ = [
+    "Agent",
+    "AgentConfig",
+    "AgentContext",
+    "AgentPool",
+    "AgentsManifest",
+    "AnyAgent",
+    "BaseTeam",
+    "ChatMessage",
+    "MessageNode",
+    "StructuredAgent",
+    "Team",
+    "TeamRun",
+    "Tool",
+    "ToolCallInfo",
+]
