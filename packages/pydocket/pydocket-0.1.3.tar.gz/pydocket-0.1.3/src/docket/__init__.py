bd1e8e405150658bd59ee83a55f24c259ed4f1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:37 +0000

    Add package pydocket/0.1.3/pydocket-0.1.3.tar.gz

---
+++ packages/pydocket/pydocket-0.1.3.tar.gz/pydocket-0.1.3/src/docket/__init__.py
@@ -0,0 +1,38 @@
+"""
+docket - A distributed background task system for Python functions.
+
+docket focuses on scheduling future work as seamlessly and efficiently as immediate work.
+"""
+
+from importlib.metadata import version
+
+__version__ = version("pydocket")
+
+from .annotations import Logged
+from .dependencies import (
+    CurrentDocket,
+    CurrentExecution,
+    CurrentWorker,
+    ExponentialRetry,
+    Retry,
+    TaskKey,
+    TaskLogger,
+)
+from .docket import Docket
+from .execution import Execution
+from .worker import Worker
+
+__all__ = [
+    "Docket",
+    "Worker",
+    "Execution",
+    "CurrentDocket",
+    "CurrentWorker",
+    "CurrentExecution",
+    "TaskKey",
+    "TaskLogger",
+    "Retry",
+    "ExponentialRetry",
+    "Logged",
+    "__version__",
+]
