5f5aa97e21ca65f7ed26a45141c4968d61afaf9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 05:04:09 +0000

    Add package example-palacio1212/1.0/example_palacio1212-1.0.tar.gz

---
+++ packages/example-palacio1212/example_palacio1212-1.0.tar.gz/example_palacio1212-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="example_palacio1212",
+    version="1.0",
+    description="Example package",
+    author="Your Name",
+    packages=["example"],
+)
