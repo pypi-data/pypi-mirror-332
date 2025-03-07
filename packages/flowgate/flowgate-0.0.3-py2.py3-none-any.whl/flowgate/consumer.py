2b07d56f4d1b9061224e97d118a9c77ade4e5231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:56 +0000

    Add package flowgate/0.0.3/flowgate-0.0.3-py2.py3-none-any.whl

---
+++ packages/flowgate/flowgate-0.0.3-py2.py3-none-any.whl/flowgate/consumer.py
@@ -0,0 +1,14 @@
+from flowgate.handler import Handler
+from flowgate.messagebus import MessageBus
+
+
+class Consumer:
+    """
+    Helper to consume and handle messages from the bus.
+    """
+    def __init__(self, messagebus: MessageBus, handler: Handler) -> None:
+        self._messagebus = messagebus
+        self._handler = handler
+
+    def consume(self) -> None:
+        self._messagebus.consume(handler=self._handler.handle)
