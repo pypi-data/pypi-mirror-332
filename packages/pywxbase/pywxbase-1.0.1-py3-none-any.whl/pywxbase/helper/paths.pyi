8c06b9e205424b293c076e4dd490ba8f96345931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:04:36 +0000

    Add package pywxbase/1.0.1/pywxbase-1.0.1-py3-none-any.whl

---
+++ packages/pywxbase/pywxbase-1.0.1-py3-none-any.whl/pywxbase/helper/paths.pyi
@@ -0,0 +1,12 @@
+# Stub file for type hints
+
+import os
+import sys
+from pathlib import Path as _Path
+
+class Paths:
+    @staticmethod
+    def root_dir(*args, **kwargs): ...
+    @staticmethod
+    def create_file_dirs(*args, **kwargs): ...
+
