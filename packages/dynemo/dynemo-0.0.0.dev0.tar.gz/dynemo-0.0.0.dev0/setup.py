127c147750062cdf369d314f3f769078de80e975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:04:37 +0000

    Add package dynemo/0.0.0.dev0/dynemo-0.0.0.dev0.tar.gz

---
+++ packages/dynemo/dynemo-0.0.0.dev0.tar.gz/dynemo-0.0.0.dev0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="dynemo",
+    version="0.0.0.dev0",
+    description="Reserved package for upcoming dynemo project",
+    long_description="",
+    author="NVIDIA Inc.",
+    author_email="sw-dl-dynemo@nvidia.com",
+    packages=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    python_requires='>=3.10',
+)
