7bf284f0727de69823a6451a9ba799c487bf4dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:29 +0000

    Add package rumusmatrev/0.1.0/rumusmatrev-0.1.0.tar.gz

---
+++ packages/rumusmatrev/rumusmatrev-0.1.0.tar.gz/rumusmatrev-0.1.0/src/rumusMatRev.py
@@ -0,0 +1,19 @@
+class rumusMatRev:
+    def persegi(x):
+        return x*x
+    
+    def luasPersegiPanjang(x, y):
+        return x*y
+    
+    def tambah (x, y):
+        return x+y
+    
+    def kurang (x, y):
+        return x-y
+    
+    def kali (x, y):
+        return x*y
+    
+    def bagi (x, y):
+        return x/y
+    
