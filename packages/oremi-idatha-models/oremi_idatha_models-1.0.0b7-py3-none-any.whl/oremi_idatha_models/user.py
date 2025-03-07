12778a4fab71f712e8a33bc7ccd7d8978024f48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:30 +0000

    Add package oremi-idatha-models/1.0.0b7/oremi_idatha_models-1.0.0b7-py3-none-any.whl

---
+++ packages/oremi-idatha-models/oremi_idatha_models-1.0.0b7-py3-none-any.whl/oremi_idatha_models/user.py
@@ -0,0 +1,40 @@
+# Copyright 2024-2025 Sébastien Demanou. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from typing import Literal
+
+from pydantic import BaseModel
+from pydantic import Field
+
+from .document import BaseMeta
+from .document import Document
+
+
+class User(BaseModel):
+  email: str
+  firstname: str
+  lastname: str | None = None
+  gender: Literal['male', 'female'] | None = None
+  birthdate: str | None = None
+  phone: str | None = None
+
+
+class UserData(User, BaseModel):
+  devices: list[str] = Field(default_factory=list)
+
+
+UserDocumentType = Document[UserData, None, BaseMeta]
+UserDocument = UserDocumentType.create_alias_from_base_meta(
+  data_cls=UserData,
+)
