aa2e750d463387fcbef47d39a87bb19202402d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 05:04:06 +0000

    Add package bluecore-models/0.2.0/bluecore_models-0.2.0-py3-none-any.whl

---
+++ packages/bluecore-models/bluecore_models-0.2.0-py3-none-any.whl/bluecore/models/__init__.py
@@ -0,0 +1,13 @@
+
+from bluecore.models.base import Base
+from bluecore.models.resource import ResourceBase
+from bluecore.models.bf_classes import BibframeClass, ResourceBibframeClass
+from bluecore.models.instance import Instance
+from bluecore.models.version import Version
+from bluecore.models.work import Work
+from bluecore.models.other_resource import OtherResource, BibframeOtherResources
+
+
+
+
+
