b5c08e55c310fb677af202a473f8e26af108ad4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 17:05:16 +0000

    Add package tobiko-cloud-helpers/202510.39.0/tobiko_cloud_helpers-202510.39.0.tar.gz

---
+++ packages/tobiko-cloud-helpers/tobiko_cloud_helpers-202510.39.0.tar.gz/tobiko_cloud_helpers-202510.39.0/tobikodata/helpers/tcloud_locations.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+DEFAULT_PYTHON_VERSION = "3.12"
+TCLOUD_PYTHON_PATH_ENV_VAR = "TCLOUD_PYTHON_PATH"
+TCLOUD_PATH = Path(os.environ.get("TCLOUD_HOME", Path.home() / ".tcloud"))
+
+
+def get_unpack_path(
+    version: str,
+    tcloud_path: Path = TCLOUD_PATH,
+    python_version: str = DEFAULT_PYTHON_VERSION,
+) -> Path:
+    return Path(tcloud_path) / "artifacts" / version / "executors" / python_version
+
+
+def get_unpacked_site_packages_path(
+    version: str,
+    tcloud_path: Path = TCLOUD_PATH,
+    python_version: str = DEFAULT_PYTHON_VERSION,
+) -> Path:
+    path = get_unpack_path(version, tcloud_path, python_version)
+    return path / "lib" / f"python{python_version}" / "site-packages"
