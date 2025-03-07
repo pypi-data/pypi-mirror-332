8e5b291515af09d2fcfe8c9fbb08c4abaaba3ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:04:00 +0000

    Add package 3lc/2.12.1/3lc-2.12.1-cp312-cp312-win_amd64.whl

---
+++ packages/3lc/3lc-2.12.1-cp312-cp312-win_amd64.whl/tlc/integration/__init__.pyi
@@ -0,0 +1,7 @@
+from .detectron2 import DetectronMetricsCollectionHook as DetectronMetricsCollectionHook, MetricsCollectionHook as MetricsCollectionHook, UMAPReduceEmbeddingsHook as UMAPReduceEmbeddingsHook, register_coco_instances as register_coco_instances
+from .pytorch_lightning import lightning_module as lightning_module
+from _typeshed import Incomplete
+
+logger: Incomplete
+TLCTrainer: Incomplete
+Trainer: Incomplete
