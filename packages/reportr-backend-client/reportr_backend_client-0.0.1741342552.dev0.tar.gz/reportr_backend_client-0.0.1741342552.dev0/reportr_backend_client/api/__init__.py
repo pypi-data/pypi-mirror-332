55ea71ae7711098031240d293de68bd092c865a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 17:05:15 +0000

    Add package reportr-backend-client/0.0.1741342552.dev0/reportr_backend_client-0.0.1741342552.dev0.tar.gz

---
+++ packages/reportr-backend-client/reportr_backend_client-0.0.1741342552.dev0.tar.gz/reportr_backend_client-0.0.1741342552.dev0/reportr_backend_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from reportr_backend_client.api.events_api import EventsApi
+from reportr_backend_client.api.news_api import NewsApi
+from reportr_backend_client.api.status_api import StatusApi
+
