adc888fbccfe7c52e7c83a0c6dca4520236b4845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:05:08 +0000

    Add package foundry-etl-sdk/0.1.6/foundry_etl_sdk-0.1.6.tar.gz

---
+++ packages/foundry-etl-sdk/foundry_etl_sdk-0.1.6.tar.gz/foundry_etl_sdk-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="foundry_etl_sdk",
+    version="0.1.6",
+    packages=find_packages("src"),
+    package_dir={'': 'src'},  
+    install_requires=requirements,
+)
