8a3fcec2666bedd5fd416d59f545a27dee9790fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:04:02 +0000

    Add package 3lc/2.12.1/3lc-2.12.1-cp39-cp39-win_amd64.whl

---
+++ packages/3lc/3lc-2.12.1-cp39-cp39-win_amd64.whl/tlc/core/objects/tables/from_python_object/table_from_pandas.pyi
@@ -0,0 +1,12 @@
+import pandas as pd
+from tlc.client.sample_type import SampleType as SampleType
+from tlc.core.object_registry import ObjectTypeRegistry as ObjectTypeRegistry
+from tlc.core.objects.tables.in_memory_rows_table import _InMemoryRowsTable
+from tlc.core.schema import Schema as Schema
+from tlc.core.url import Url as Url
+from tlc.utils.decorators import disallow_positional_arguments as disallow_positional_arguments
+from typing import Any
+
+class TableFromPandas(_InMemoryRowsTable):
+    """A table populated from a Pandas Dataframe object"""
+    def __init__(self, *, url: Url | None = None, created: str | None = None, description: str | None = None, row_cache_url: Url | None = None, row_cache_populated: bool | None = None, override_table_rows_schema: Schema | None = None, init_parameters: Any | None = None, dataframe: pd.DataFrame | None = None, input_tables: list[Url] | None = None) -> None: ...
