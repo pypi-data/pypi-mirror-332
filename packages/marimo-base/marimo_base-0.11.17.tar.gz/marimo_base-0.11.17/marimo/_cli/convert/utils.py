199f1a5cad9048fefad9899d58b8a792e8d4f724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 05:04:33 +0000

    Add package marimo-base/0.11.17/marimo_base-0.11.17.tar.gz

---
+++ packages/marimo-base/marimo_base-0.11.17.tar.gz/marimo_base-0.11.17/marimo/_cli/convert/utils.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import urllib.request
+
+from marimo._cli.file_path import get_github_src_url, is_github_src
+
+
+def load_external_file(file_path: str, ext: str) -> str:
+    notebook: str = ""
+    if is_github_src(file_path, ext=ext):
+        notebook = (
+            urllib.request.urlopen(get_github_src_url(file_path))
+            .read()
+            .decode("utf-8")
+        )
+    else:
+        with open(file_path, encoding="utf-8") as f:
+            notebook = f.read()
+
+    return notebook
---
+++ packages/marimo-base/marimo_base-0.11.17.tar.gz/marimo_base-0.11.17/marimo/_utils/paths.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import os
+from typing import Any
+
+
+def import_files(filename: str) -> Any:
+    from importlib.resources import files as importlib_files
+
+    return importlib_files(filename)
+
+
+def pretty_path(filename: str) -> str:
+    """
+    If it's an absolute path, shorten to relative path if
+    we don't go outside the current directory.
+    Otherwise, return the filename as is.
+    """
+    if os.path.isabs(filename):
+        try:
+            relpath = os.path.relpath(filename)
+        except ValueError:
+            # Windows: relpath doesn't work if filename is on a different drive
+            # than current drive
+            return filename
+        if not relpath.startswith(".."):
+            return relpath
+    return filename
+
+
+def maybe_make_dirs(filepath: str) -> None:
+    """
+    Create directories if they don't exist.
+    """
+    dirname = os.path.dirname(filepath)
+    if dirname:
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
