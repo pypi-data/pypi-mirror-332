bef467c8e049dcb77d738809b68ca5f49b7bbb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 14:04:22 +0000

    Add package fibonacci-even-sum/1.0.2/fibonacci_even_sum-1.0.2-py3-none-any.whl

---
+++ packages/fibonacci-even-sum/fibonacci_even_sum-1.0.2-py3-none-any.whl/fibonacci_even_sum/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Fibonacci serisinin belirli bir sayıya kadar olan çift sayılarının
+toplamını hesaplayan paket.
+"""
+
+__version__ = "1.0.2"
+__author__ = "Hüseyin ASLIM"
+
+# Dışa aktarılan fonksiyonlar - noqa ile uyarıları bastırıyoruz
+# çünkü bu importlar paket API'sinin bir parçası
+from .fibonacci_even_sum import (  # noqa
+    fibonacci_even_sum,
+    fibonacci_even_sum_original,
+    fibonacci_even_sum_direct,
+    fibonacci_even_sum_formula,
+    main
+)
