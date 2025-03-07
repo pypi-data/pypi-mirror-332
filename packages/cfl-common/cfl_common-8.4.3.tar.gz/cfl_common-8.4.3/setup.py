6c2e1c77a6d81c38306b168a6ab8898cb6efdace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 05:04:58 +0000

    Add package cfl-common/8.4.3/cfl_common-8.4.3.tar.gz

---
+++ packages/cfl-common/cfl_common-8.4.3.tar.gz/cfl_common-8.4.3/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import re
+
+from setuptools import find_packages, setup
+
+with open("../portal/__init__.py", "r") as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE,
+    ).group(1)
+
+setup(
+    name="cfl-common",
+    long_description="Common package for Code for Life",
+    packages=find_packages(),
+    version=version,
+    include_package_data=True,
+    install_requires=[
+        "django==4.2.18",
+        "django-countries==7.6.1",
+        "django-csp==3.8",
+        "django-import-export==4.2.0",
+        "django-pipeline==3.1.0",
+        "django-two-factor-auth==1.17.0",
+        "djangorestframework==3.15.2",
+        "libsass==0.23.0",
+        "more-itertools==8.7.0",
+        "pgeocode==0.4.0",
+        "pyjwt==2.6.0",
+    ],
+    tests_require=[],
+    test_suite="tests",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
