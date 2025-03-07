33a387d4061cb7331450a3b2adeba81ea0d26ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 05:04:13 +0000

    Add package ommlx/0.0.0.dev241/ommlx-0.0.0.dev241.tar.gz

---
+++ packages/ommlx/ommlx-0.0.0.dev241.tar.gz/ommlx-0.0.0.dev241/ommlx/minichain/__init__.py
@@ -0,0 +1,83 @@
+from .chat import (  # noqa
+    ChatHistory,
+    ChatHistoryModel,
+    ListChatHistory,
+
+    AiMessage,
+    Chat,
+    Message,
+    SystemMessage,
+    ToolExecResultMessage,
+    UserMessage,
+
+    AiChoice,
+    AiChoices,
+    ChatInput,
+    ChatModel,
+    ChatOutput,
+    ChatRequest,
+    ChatResponse,
+
+    ChatOption,
+    ChatOptions,
+
+    Tool,
+    ToolDtype,
+    ToolExecRequest,
+    ToolParam,
+    ToolSpec,
+)
+
+from .content import (  # noqa
+    Content,
+    ExtendedContent,
+
+    Image,
+)
+
+from .models import (  # noqa
+    FinishReason,
+
+    TokenUsage,
+
+    Model,
+    ModelRequest,
+    ModelOption,
+    ModelResponse,
+)
+
+from .options import (  # noqa
+    DuplicateUniqueOptionError,
+    Option,
+    Options,
+    ScalarOption,
+    UniqueOption,
+)
+
+from .prompts import (  # noqa
+    PromptInput,
+    PromptModel,
+    PromptNew,
+    PromptOutput,
+    PromptRequest,
+    PromptOptions,
+    PromptResponse,
+)
+
+from .services import (  # noqa
+    Service,
+    ServiceOption,
+    ServiceRequest,
+    ServiceResponse,
+)
+
+from .vectors import (  # noqa
+    EmbeddingModel,
+    EmbeddingOptions,
+    EmbeddingOutput,
+    EmbeddingRequest,
+    EmbeddingResponse,
+
+    Vector,
+    Vectorable,
+)
---
+++ packages/ommlx/ommlx-0.0.0.dev241.tar.gz/ommlx-0.0.0.dev241/ommlx/tools/git.py
@@ -0,0 +1,133 @@
+"""
+TODO:
+ - exclude @omlish-amalg-output files
+"""
+import abc
+import dataclasses as dc
+import os.path
+import typing as ta
+import urllib.request
+
+from omdev.home.secrets import load_secrets
+from omdev.tools.git.messages import GitMessageGenerator
+from omlish import check
+from omlish import lang
+from omlish.configs.classes import Configurable
+from omlish.subprocesses.sync import subprocesses
+
+from ..minichain.backends.openai import OpenaiChatModel
+from ..minichain.chat import UserMessage
+from ..minichain.generative import MaxTokens
+
+
+GitAiBackendConfigT = ta.TypeVar('GitAiBackendConfigT', bound='GitAiBackend.Config')
+
+
+##
+
+
+class GitAiBackend(Configurable[GitAiBackendConfigT], lang.Abstract):
+    @dc.dataclass(frozen=True)
+    class Config(Configurable.Config):
+        max_tokens: int | None = 128
+
+    @abc.abstractmethod
+    def run_prompt(self, prompt: str) -> str:
+        raise NotImplementedError
+
+
+#
+
+
+class OpenaiGitAiBackend(GitAiBackend['OpenaiGitAiBackend.Config']):
+    @dc.dataclass(frozen=True)
+    class Config(GitAiBackend.Config):
+        pass
+
+    def __init__(self, config: Config = Config()) -> None:
+        super().__init__(config)
+
+    def run_prompt(self, prompt: str) -> str:
+        # FIXME:
+        for key in ['OPENAI_API_KEY']:
+            if (sec := load_secrets().try_get(key.lower())) is not None:
+                os.environ[key] = sec.reveal()
+
+        llm = OpenaiChatModel()
+
+        resp = llm(
+            [UserMessage(prompt)],
+            *((MaxTokens(self._config.max_tokens),) if self._config.max_tokens is not None else ()),
+        )
+        return check.non_empty_str(resp.v[0].m.s)
+
+
+#
+
+
+class LocalhostHttpPostGitAiBackend(GitAiBackend['LocalhostHttpPostGitAiBackend.Config']):
+    @dc.dataclass(frozen=True)
+    class Config(GitAiBackend.Config):
+        port: int = 5067
+        path: str = ''
+
+    def __init__(self, config: Config = Config()) -> None:
+        super().__init__(config)
+
+    @property
+    def config(self) -> Config:
+        return self._config
+
+    def run_prompt(self, prompt: str) -> str:
+        with urllib.request.urlopen(urllib.request.Request(
+                f'http://localhost:{self._config.port}/{self._config.path}',
+                data=prompt.encode('utf-8'),
+        )) as resp:
+            return resp.read().decode('utf-8')
+
+
+##
+
+
+# @omlish-manifest omdev.tools.git.messages.GitMessageGeneratorManifest(name='ai')
+class AiGitMessageGenerator(GitMessageGenerator):
+    def __init__(
+            self,
+            *,
+            backend: GitAiBackend | None = None,
+    ) -> None:
+        super().__init__()
+
+        if backend is None:
+            backend = self.DEFAULT_BACKEND
+        self._backend = backend
+
+    DEFAULT_BACKEND: ta.ClassVar[GitAiBackend] = (
+        OpenaiGitAiBackend()
+        # LocalhostHttpPostGitAiBackend()
+    )
+
+    def generate_commit_message(
+            self,
+            args: GitMessageGenerator.GenerateCommitMessageArgs,
+    ) -> GitMessageGenerator.GenerateCommitMessageResult:
+        diff = subprocesses.check_output(
+            'git',
+            'diff',
+            'HEAD',
+            '--',
+            ':(exclude)**/.manifests.json',  # TODO: configurable
+            cwd=args.cwd,
+        ).decode()
+
+        prompt = '\n\n'.join([
+            'Write a short git commit message for the following git diff:',
+            f'```\n{diff}\n```',
+        ])
+
+        msg = self._backend.run_prompt(prompt)
+
+        return GitMessageGenerator.GenerateCommitMessageResult(
+            msg=msg,
+            confirm=True,
+        )
