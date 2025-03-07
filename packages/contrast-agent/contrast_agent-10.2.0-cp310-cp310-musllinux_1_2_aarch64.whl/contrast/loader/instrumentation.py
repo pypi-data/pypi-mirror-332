50bf6d98f85c7127f439856ea57a62402bdd12fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:03:53 +0000

    Add package contrast-agent/10.2.0/contrast_agent-10.2.0-cp310-cp310-musllinux_1_2_aarch64.whl

---
+++ packages/contrast-agent/contrast_agent-10.2.0-cp310-cp310-musllinux_1_2_aarch64.whl/contrast/loader/instrumentation.py
@@ -0,0 +1,48 @@
+# Copyright © 2025 Contrast Security, Inc.
+# See https://www.contrastsecurity.com/enduser-terms-0317a for more details.
+
+from contrast.agent import scope
+from contrast_rewriter import rewrite_for_pytest
+from contrast.agent.patch_controller import enable_assess_patches
+from contrast.agent.policy.rewriter import apply_rewrite_policy
+from contrast.configuration.agent_config import AgentConfig
+from contrast.patches import (
+    register_chaining_monkeypatches,
+    register_automatic_middleware_monkeypatches,
+)
+
+
+@scope.contrast_scope()
+def preinstrument():
+    """
+    preinstrument applies a subset of instrumentation during site loading.
+
+    Currently, only instrumentation that needs to be applied as early as possible
+    is applied here. Eventually, it would be simpler to apply all instrumentation,
+    but that will require more refactoring.
+    """
+
+    if rewrite_for_pytest():
+        apply_rewrite_policy(override_config=True)
+        return
+
+    config = AgentConfig()
+
+    # Policy-based rewrites need to be applied prior to any policy patches.
+    # Policy patches can be layered on top of rewritten functions. So that
+    # means we need to make sure that the "original" function called by the
+    # policy patch is the *rewritten* one.
+    if config.should_apply_policy_rewrites:
+        apply_rewrite_policy()
+
+    if config.enable_automatic_middleware:
+        register_automatic_middleware_monkeypatches()
+
+    if config.assess_enabled:
+        # NOTE: policy is currently loaded/generated on import. It is applied explicitly
+        # in policy/applicator.py later
+        from contrast import policy  # noqa: F401
+
+        enable_assess_patches()
+
+    register_chaining_monkeypatches()
