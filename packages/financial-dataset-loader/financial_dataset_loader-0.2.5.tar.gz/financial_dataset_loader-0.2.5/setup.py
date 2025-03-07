7e5c2cc558d03a87521fb9e3ed754e1c381ddd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:04:23 +0000

    Add package financial-dataset-loader/0.2.5/financial_dataset_loader-0.2.5.tar.gz

---
+++ packages/financial-dataset-loader/financial_dataset_loader-0.2.5.tar.gz/financial_dataset_loader-0.2.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='financial_dataset_loader',
+    version='0.2.5',
+    packages=find_packages(),
+    install_requires=[
+        'aws-s3-controller>=0.7.4',
+        'string_date_controller>=0.1.1',
+        'shining_pebbles',
+    ],
+    author='June Young Park',
+    author_email='juneyoungpaak@gmail.com',
+    description='A Python module for loading financial datasets from various sources',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nailen1/financial_dataset_loader.git',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    keywords='financial data loader dataset aws s3',
+)
