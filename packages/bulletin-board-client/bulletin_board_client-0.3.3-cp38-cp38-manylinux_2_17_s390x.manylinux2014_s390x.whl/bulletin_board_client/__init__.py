6d44e7f19c39093a5493f1b18fbb5fcaa96ffd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:11:58 +0000

    Add package bulletin-board-client/0.3.3/bulletin_board_client-0.3.3-cp38-cp38-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/bulletin-board-client/bulletin_board_client-0.3.3-cp38-cp38-manylinux_2_17_s390x.manylinux2014_s390x.whl/bulletin_board_client/__init__.py
@@ -0,0 +1,1 @@
+from .helper import set_addr, set_timeout, post, read, relabel, client_version, server_version, status, log, view_board, get_info, clear_revisions, remove, archive, load, list_archive, rename_archive, delete_archive, delete_archive, dump, restore, reset_server, terminate_server
