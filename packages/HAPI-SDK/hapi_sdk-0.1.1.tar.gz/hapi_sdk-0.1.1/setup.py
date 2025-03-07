0a815c3a6b12ff035b01a1325f055faa941e6703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 06:05:00 +0000

    Add package HAPI-SDK/0.1.1/hapi_sdk-0.1.1.tar.gz

---
+++ packages/HAPI-SDK/hapi_sdk-0.1.1.tar.gz/hapi_sdk-0.1.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HAPI_SDK",
+    version="0.1.1",
+    description="A plug-and-play SDK for de-hallucinating outputs from LLMs",
+    author="True",
+    author_email="cyprienseydoux@gmail.com",
+    url="https://github.com/cyprienn967/HAPI_SDK",
+    packages=find_packages(),
+    install_requires=[
+        "torch", 
+        "transformers", 
+        "accelerate", 
+        "pyarrow >=14.0.0", 
+        "flask", 
+        "numpy", 
+        "evaluate", 
+        "datasets", 
+        "pandas", 
+        "tqdm", 
+        "tokenizers", 
+        "openai", 
+        "tiktoken", 
+        "nltk", 
+        "tenacity", 
+        "safetensors", 
+        "azure-identity", 
+        "azure-keyvault-secrets", 
+        "azure-ai-textanalytics", 
+        "requests", 
+        "werkzeug", 
+        "aws-wsgi",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # or your chosen license
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
