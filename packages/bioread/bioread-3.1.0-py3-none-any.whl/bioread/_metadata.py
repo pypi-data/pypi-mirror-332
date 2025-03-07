1adeae800b38603434a16b348a1aace64e06258b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 05:04:10 +0000

    Add package bioread/3.1.0/bioread-3.1.0-py3-none-any.whl

---
+++ packages/bioread/bioread-3.1.0-py3-none-any.whl/bioread/_metadata.py
@@ -0,0 +1,20 @@
+# coding: utf8
+# Part of the bioread package for reading BIOPAC data.
+#
+# Copyright (c) 2025 Board of Regents of the University of Wisconsin System
+#
+# Written Nate Vack <njvack@wisc.edu> with research from John Ollinger
+# at the Waisman Laboratory for Brain Imaging and Behavior, University of
+# Wisconsin-Madison
+# Project home: http://github.com/njvack/bioread
+
+# NOTE: This file must not import anything, or it will break installation.
+
+__version__ = "3.1.0"
+version_description = "bioread {0}".format(__version__)
+
+author = "Nate Vack"
+author_email = "njvack@wisc.edu"
+license = "MIT"
+copyright = "Copyright (c) 2025 Boards of Regent of the University of Wisconsin System"  # noqa
+url = "https://github.com/uwmadison-chm/bioread"
