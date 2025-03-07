3d7b2b57166e00c12e2fcc0238378b0826370a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 06:04:09 +0000

    Add package qnp/0.1.8/qnp-0.1.8-py3-none-any.whl

---
+++ packages/qnp/qnp-0.1.8-py3-none-any.whl/qnp/Physics/Constants.py
@@ -0,0 +1,12 @@
+class Plancks_Constant:
+    JS = 6.62607015e-34
+    eVS = 4.135667696e-15
+    bar_JS = 1.054571817e-34
+    bar_eVS = 6.582119569e-16
+
+
+class Speed_of_Light:
+    MperS = 299792458
+
+
+pi = 3.1415926535
