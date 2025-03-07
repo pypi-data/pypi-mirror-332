65768b008c79d030cc3ea2b88017311227edac9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:11:56 +0000

    Add package kasper/0.1.1/kasper-0.1.1.tar.gz

---
+++ packages/kasper/kasper-0.1.1.tar.gz/kasper-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kasper",
+    version="0.1.1",
+    author="Kasper Arfman",
+    author_email="kasper.arf@gmail.com",
+    description="A simple hello world package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kasper-Arfman/kasper",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "kasper=kasper.main:hello",
+        ],
+    },
+)
