2692f504b1c6b78ef1d38ad1a79b40f00f10f6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:11:53 +0000

    Add package example2410/0.1.47/example2410-0.1.47.tar.gz

---
+++ packages/example2410/example2410-0.1.47.tar.gz/example2410-0.1.47/example2410/ex_model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from example2410.helper import helper
+
+class sum_tf:
+    def __init__(self, numbers):
+        print("HELLO")
+        for i in numbers:
+            helper.checknumber(i)
+        self.sum = tf.reduce_sum(numbers)
+
+
+from seededpf import SPF
+from seededpf import SPF
+import numpy as np
+
+import tensorflow as tf
+tf.__version__
+
+np.__version__
