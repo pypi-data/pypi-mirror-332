1d2c76afa7830024893b3386ea4610af5a5b25ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 17:05:10 +0000

    Add package xrst/2025.0.4/xrst-2025.0.4-py3-none-any.whl

---
+++ packages/xrst/xrst-2025.0.4-py3-none-any.whl/xrst/temp.1.py
@@ -0,0 +1,5 @@
+# {xrst_comment_ch #}
+#
+# #.  data_in must contain '\\n{xrst@before_title}' @python_breakpoint@
+#     which is referred to as the command below. @python_breakpoint@
+# 345678901234567890
