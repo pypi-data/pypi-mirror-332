1f3303f24093f0b0c029a91f6b7f234b1a0b907f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:51 +0000

    Add package rclone-api/1.3.20/rclone_api-1.3.20.tar.gz

---
+++ packages/rclone-api/rclone_api-1.3.20.tar.gz/rclone_api-1.3.20/src/rclone_api/remote.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+
+class Remote:
+    """Remote (root) directory."""
+
+    def __init__(self, name: str, rclone: Any) -> None:
+        from rclone_api.rclone import Rclone
+
+        if ":" in name:
+            raise ValueError("Remote name cannot contain ':'")
+
+        assert isinstance(rclone, Rclone)
+        self.name = name
+        self.rclone: Rclone = rclone
+
+    def __str__(self) -> str:
+        return f"{self.name}:"
+
+    def __repr__(self) -> str:
+        return f"Remote({self.name!r})"
