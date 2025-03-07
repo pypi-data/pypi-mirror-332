cd6a5fd97f2e45519c0410550f1842cca2e996db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:03 +0000

    Add package ez/11.5.5/ez-11.5.5.tar.gz

---
+++ packages/ez/ez-11.5.5.tar.gz/ez-11.5.5/ez/__init__.py
@@ -0,0 +1,9 @@
+
+from . ez import *
+from . firefox import *
+
+__all__=[
+'Firefox', 'BY', 'KEYS',
+'JDict', 'TimeStamp', 'Moment', 'GSheet', 'GCal',
+'fire', 'requests', 'sqlite3', 'json', 'io', 'os', 'sys', 'platform', 'string', 'random', 're', 'datetime', 'tzlocal', 'pd', 'np', 'urlparse', 'BeautifulSoup', 
+]
