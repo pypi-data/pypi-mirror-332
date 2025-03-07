576693e214062d9e87d8b74215492cb6631830ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:11:46 +0000

    Add package logictools/0.1.0/logictools-0.1.0.tar.gz

---
+++ packages/logictools/logictools-0.1.0.tar.gz/logictools-0.1.0/logictools/__init__.py
@@ -0,0 +1,2 @@
+from .string_utils import convert_to_uppercase, reverse_string, word_count
+from .math_utils import factorial
---
+++ packages/logictools/logictools-0.1.0.tar.gz/logictools-0.1.0/logictools/string_utils.py
@@ -0,0 +1,8 @@
+def convert_to_uppercase(text):
+    return text.upper()        #Converts a string to uppercase.
+
+def reverse_string(text):
+    return text[::-1]          #Reverses a given string.
+
+def word_count(text):
+    return len(text)           #Gives the length of the string.
