7ad337f838a01c101fba8d11114a1473b1a97448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:04:00 +0000

    Add package plain.admin/0.21.1/plain_admin-0.21.1-py3-none-any.whl

---
+++ packages/plain.admin/plain_admin-0.21.1-py3-none-any.whl/plain/admin/urls.py
@@ -0,0 +1,43 @@
+from plain.http import ResponseRedirect
+from plain.urls import RouterBase, include, path, register_router
+
+from .impersonate import urls as impersonate_urls
+from .querystats import urls as querystats_urls
+from .views.base import AdminView
+from .views.registry import registry
+
+
+class AdminIndexView(AdminView):
+    template_name = "admin/index.html"
+    title = "Dashboard"
+
+    def get(self):
+        # Slight hack to redirect to the first view that doesn't
+        # require any url params...
+        if views := registry.get_searchable_views():
+            return ResponseRedirect(list(views)[0].get_view_url())
+
+        return super().get()
+
+
+class AdminSearchView(AdminView):
+    template_name = "admin/search.html"
+    title = "Search"
+
+    def get_template_context(self):
+        context = super().get_template_context()
+        context["searchable_views"] = registry.get_searchable_views()
+        context["global_search_query"] = self.request.GET.get("query", "")
+        return context
+
+
+@register_router
+class Router(RouterBase):
+    namespace = "admin"
+    urls = [
+        path("search/", AdminSearchView, name="search"),
+        include("impersonate/", impersonate_urls),
+        include("querystats/", querystats_urls),
+        include("", registry.get_urls()),
+        path("", AdminIndexView, name="index"),
+    ]
