81eafebafa336daa01912b410daee0e4b91702d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 17:05:15 +0000

    Add package almanak-src/0.0.4/almanak_src-0.0.4-py3-none-any.whl

---
+++ packages/almanak-src/almanak_src-0.0.4-py3-none-any.whl/src/strategy/utils/base_model.pyi
@@ -0,0 +1,12 @@
+from src.strategy.utils.utils import DataFormat as DataFormat, DataSource as DataSource
+from typing import Any
+
+class BaseModel:
+    def __init__(self, data) -> None: ...
+
+class ModelConfig(BaseModel):
+    method: str
+    params: dict[str, Any]
+    data_source: DataSource
+    data_format: DataFormat
+    def model_dump(self, **kwargs): ...
