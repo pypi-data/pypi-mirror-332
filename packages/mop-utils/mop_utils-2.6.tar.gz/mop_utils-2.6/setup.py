e259c8798273adcca4fc8d8728e37954ed683e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:08 +0000

    Add package mop-utils/2.6/mop_utils-2.6.tar.gz

---
+++ packages/mop-utils/mop_utils-2.6.tar.gz/mop_utils-2.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mop_utils',
+    version='2.6',
+    description='Utility SDK for RAI Model Onboarding Pipeline (MOP) model contributor.',
+    packages=['mop_utils'],
+    package_dir={'mop_utils': './mop_utils'},
+    python_requires='>=3.8.0',
+    install_requires=[
+        'pyraisdk >= 0.5',
+        'PyYAML >= 6.0',
+        'numpy >= 1.22.0',
+    ]
+)
