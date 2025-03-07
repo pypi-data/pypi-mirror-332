7d81795ac342d7c3a13fe2b2d8bef4a913eff325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 05:04:03 +0000

    Add package structifyai/1.105.0/structifyai-1.105.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.105.0-py3-none-any.whl/structify/types/job_get_response.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+from .execution_step import ExecutionStep
+
+__all__ = ["JobGetResponse", "Job"]
+
+
+class Job(BaseModel):
+    id: str
+
+    creation_time: datetime
+
+    status: Literal["Queued", "Running", "Completed", "Failed"]
+
+    message: Optional[str] = None
+
+    reason: Optional[str] = None
+
+    report_on_complete: Optional[bool] = None
+
+    run_started_time: Optional[datetime] = None
+    """What time did the job start running?"""
+
+
+class JobGetResponse(BaseModel):
+    job: Job
+
+    steps: List[ExecutionStep]
