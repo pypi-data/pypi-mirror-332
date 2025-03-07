f439a18bdb9adbc4112e24f4a0d27c74cab99f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:04:58 +0000

    Add package autokattis/2.1.3/autokattis-2.1.3-py3-none-any.whl

---
+++ packages/autokattis/autokattis-2.1.3-py3-none-any.whl/autokattis/__init__.py
@@ -0,0 +1,6 @@
+from .api.openkattis import OpenKattis
+from .api.nuskattis import NUSKattis
+from .api.utils import suppress_warnings
+
+Kattis = OpenKattis # backwards compatibility :)
+suppress_warnings()
