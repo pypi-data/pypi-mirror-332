63f6a69dd6281a0e03a5275420853ffb309f77e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:12:01 +0000

    Add package vptq/0.0.5.post1/vptq-0.0.5.post1-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/vptq/vptq-0.0.5.post1-cp38-cp38-manylinux1_x86_64.whl/vptq/__init__.py
@@ -0,0 +1,15 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+import importlib.metadata
+
+from vptq.layers import AutoModelForCausalLM, VQuantLinear
+
+__version__ = importlib.metadata.version("vptq")
+
+__all__ = [
+    "AutoModelForCausalLM",
+    "VQuantLinear",
+]
