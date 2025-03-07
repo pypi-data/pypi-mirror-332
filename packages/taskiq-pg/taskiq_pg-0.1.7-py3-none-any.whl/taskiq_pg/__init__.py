fb05a37bb6edb519dd494523db4cee1e4391b0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:12:09 +0000

    Add package taskiq-pg/0.1.7/taskiq_pg-0.1.7-py3-none-any.whl

---
+++ packages/taskiq-pg/taskiq_pg-0.1.7-py3-none-any.whl/taskiq_pg/__init__.py
@@ -0,0 +1,9 @@
+from taskiq_pg.broker import AsyncpgBroker
+from taskiq_pg.result_backend import AsyncpgResultBackend
+
+__version__ = "0.1.7"
+
+__all__ = [
+    "AsyncpgBroker",
+    "AsyncpgResultBackend",
+]
