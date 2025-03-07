515100400a1debea2350a4a96202118f7f59eb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 04:03:50 +0000

    Add package foundry-db-temp/0.1.9/foundry_db_temp-0.1.9-py3-none-any.whl

---
+++ packages/foundry-db-temp/foundry_db_temp-0.1.9-py3-none-any.whl/foundry_db/tables/__init__.py
@@ -0,0 +1,42 @@
+from .base_table import BaseTable, IDColumn
+from .category import CategoryTable
+from .company import CompanyTable
+from .datapoints import DataPointsTable
+from .dates import DatesTable
+from .flags import FlagsTable, FlagNames
+from .price_discounts import PriceDiscountsTable
+from .product_categories import ProductCategoriesTable
+from .products import ProductsTable
+from .regions import RegionsTable, RegionTypes
+from .sales import SalesTable
+from .sku_table import SkuTable
+from .stores import StoresTable
+from .time_region_features_company import TimeRegionFeaturesCompanyTable
+from .time_region_features_description import TimeRegionFeaturesDescriptionTable
+from .time_region_features import TimeRegionFeaturesTable
+from .time_store_features_description import TimeStoreFeaturesDescriptionTable
+from .time_store_features import TimeStoreFeaturesTable
+
+__all__ = [
+    "BaseTable",
+    "IDColumn",
+    "CategoryTable",
+    "CompanyTable",
+    "DataPointsTable",
+    "DatesTable",
+    "FlagsTable",
+    "FlagNames",
+    "PriceDiscountsTable",
+    "ProductCategoriesTable",
+    "ProductsTable",
+    "RegionsTable",
+    "RegionTypes",
+    "SalesTable",
+    "SkuTable",
+    "StoresTable",
+    "TimeRegionFeaturesCompanyTable",
+    "TimeRegionFeaturesDescriptionTable",
+    "TimeRegionFeaturesTable",
+    "TimeStoreFeaturesDescriptionTable",
+    "TimeStoreFeaturesTable",
+]
