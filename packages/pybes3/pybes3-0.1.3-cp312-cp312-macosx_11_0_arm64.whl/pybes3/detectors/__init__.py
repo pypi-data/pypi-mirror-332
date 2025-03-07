07bf025446b74be6bbff217575cc77af6b26d276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 16:04:30 +0000

    Add package pybes3/0.1.3/pybes3-0.1.3-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/pybes3/pybes3-0.1.3-cp312-cp312-macosx_11_0_arm64.whl/pybes3/detectors/__init__.py
@@ -0,0 +1,13 @@
+from . import identify
+from .identify import (
+    get_cgem_id,
+    get_emc_id,
+    get_mdc_id,
+    get_muc_id,
+    get_tof_id,
+    parse_cgem_id,
+    parse_emc_id,
+    parse_mdc_id,
+    parse_muc_id,
+    parse_tof_id,
+)
