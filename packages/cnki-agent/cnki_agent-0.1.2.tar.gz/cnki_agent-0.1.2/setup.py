c804930c2c7cc505da98307f414d655c05ce8ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 03:11:41 +0000

    Add package cnki-agent/0.1.2/cnki_agent-0.1.2.tar.gz

---
+++ packages/cnki-agent/cnki_agent-0.1.2.tar.gz/cnki_agent-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cnki-agent",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        "selenium",
+    ],
+    author="zstar",
+    author_email="zstar1003@163.com",
+    description="A Python package for scraping CNKI papers.",
+    url="https://github.com/zstar1003/cnki-agent",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
