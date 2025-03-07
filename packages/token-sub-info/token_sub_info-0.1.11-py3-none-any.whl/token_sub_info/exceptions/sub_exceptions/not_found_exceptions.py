9f32c288ed487739eb343f2f2b6cf6ef939c8fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 17:05:05 +0000

    Add package token-sub-info/0.1.11/token_sub_info-0.1.11-py3-none-any.whl

---
+++ packages/token-sub-info/token_sub_info-0.1.11-py3-none-any.whl/token_sub_info/exceptions/sub_exceptions/not_found_exceptions.py
@@ -0,0 +1,6 @@
+from ..main_exceptions import NotFoundException
+
+
+class ProfileIDNotFoundException(NotFoundException):
+    def __init__(self):
+        super().__init__(message="Токен не содержит айди профиля.")
