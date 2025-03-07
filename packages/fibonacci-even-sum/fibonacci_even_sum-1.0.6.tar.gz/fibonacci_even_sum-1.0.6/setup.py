a1a9e7fa46c8ad8dc1a1aa2cf7eef38705521b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 15:05:57 +0000

    Add package fibonacci-even-sum/1.0.6/fibonacci_even_sum-1.0.6.tar.gz

---
+++ packages/fibonacci-even-sum/fibonacci_even_sum-1.0.6.tar.gz/fibonacci_even_sum-1.0.6/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fibonacci-even-sum",
+    version="1.0.6",
+    author="Hüseyin ASLIM",
+    author_email="founder@codev.com.tr",
+    description="Fibonacci serisinin çift sayılarının toplamını hesaplayan bir Python CLI uygulaması",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/huseyinaslim/fibonacci-even-sum",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "fibonacci-even-sum=fibonacci_even_sum:main",
+        ],
+    },
+    license="MIT",
+) 
