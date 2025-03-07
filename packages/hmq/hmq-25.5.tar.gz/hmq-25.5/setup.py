a43cf41de536b7dd329a85fad7f0771894fbbf38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-07 14:04:09 +0000

    Add package hmq/25.5/hmq-25.5.tar.gz

---
+++ packages/hmq/hmq-25.5.tar.gz/hmq-25.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="hmq",
+    version="25.5",
+    description="Distributed computing.",
+    url="https://github.com/NablaChem/hummingqueue",
+    author="Guido Falk von Rudorff",
+    author_email="guido@vonrudorff.de",
+    license="AGPL-3.0",
+    packages=["hmq"],
+    zip_safe=False,
+    install_requires=[
+        "dnspython",
+        "requests",
+        "cloudpickle",
+        "pynacl",
+        "tqdm",
+        "rq",
+        "toml",
+        "numpy",
+        "pandas",
+    ],
+    entry_points={
+        "console_scripts": ["hmq=hmq:cli"],
+    },
+)
