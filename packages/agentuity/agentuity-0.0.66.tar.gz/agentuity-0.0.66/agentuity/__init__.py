1b6e3670c852e09fd64460f2397f222ea78c762e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:25 +0000

    Add package agentuity/0.0.66/agentuity-0.0.66.tar.gz

---
+++ packages/agentuity/agentuity-0.0.66.tar.gz/agentuity-0.0.66/agentuity/__init__.py
@@ -0,0 +1,8 @@
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0-dev"
+
+from agentuity.server import AgentRequest, AgentResponse, AgentContext
