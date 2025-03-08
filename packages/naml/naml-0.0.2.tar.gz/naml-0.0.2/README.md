db395fed5ffab5cc5bea6cb51237935661575d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:04:54 +0000

    Add package naml/0.0.2/naml-0.0.2.tar.gz

---
+++ packages/naml/naml-0.0.2.tar.gz/naml-0.0.2/README.md
@@ -0,0 +1,11 @@
+Not Another Machine Learning library
+---
+Graphing and visualization tools for PyTorch (Hence the name!)
+
+# Installation
+```bash
+pip install naml
+```
+
+# References
+- https://zh-v2.d2l.ai/
---
+++ packages/naml/naml-0.0.2.tar.gz/naml-0.0.2/naml/modules/__init__.py
@@ -0,0 +1,13 @@
+from typing import List, Tuple, Dict, Any, Union, Generator, TypeVar
+import torch
+
+T = TypeVar("T")
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+import torch.functional as F
+import torch.nn as nn
+import torch.optim as optim
+
+from tqdm.notebook import tqdm
