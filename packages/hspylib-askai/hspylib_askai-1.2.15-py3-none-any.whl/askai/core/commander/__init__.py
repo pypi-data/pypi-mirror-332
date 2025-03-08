995a10d45dd35135122718a42a5861b0df0d4fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:24 +0000

    Add package hspylib-askai/1.2.15/hspylib_askai-1.2.15-py3-none-any.whl

---
+++ packages/hspylib-askai/hspylib_askai-1.2.15-py3-none-any.whl/askai/core/commander/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-askai v1.2.15
+#
+# Package: main.askai.core.commander
+"""Package initialization."""
+
+__all__ = [
+    'commander', 
+    'commands'
+]
+__version__ = '1.2.15'
---
+++ packages/hspylib-askai/hspylib_askai-1.2.15-py3-none-any.whl/askai/core/component/multimedia/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-askai v1.2.15
+#
+# Package: main.askai.core.component.multimedia
+"""Package initialization."""
+
+__all__ = [
+    'audio_player', 
+    'camera', 
+    'recorder'
+]
+__version__ = '1.2.15'
