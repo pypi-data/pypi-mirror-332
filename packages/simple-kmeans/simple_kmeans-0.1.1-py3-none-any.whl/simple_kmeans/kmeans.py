bf5e38eba5d5320ca002a232807de50d1da249b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 16:04:09 +0000

    Add package simple-kmeans/0.1.1/simple_kmeans-0.1.1-py3-none-any.whl

---
+++ packages/simple-kmeans/simple_kmeans-0.1.1-py3-none-any.whl/simple_kmeans/kmeans.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+class KMeans:
+    def __init__(self, k: int = 3, max_iters: int = 100, tol:float = 1e-4):
+        self.k = k
+        self.max_iters = max_iters
+        self.tol = tol
+        self.centroids = None
+    
+    def _compute_distance(self, X, centroids):
+        """
+        Computes distance between each data point and the centroids using
+        the appropriate metric based on dimensionality.
+
+        Parameters:
+        X (numpy array): Data points.
+        centroids (numpy array): Cluster centroids.
+
+        Returns:
+        numpy array: Distance matrix.
+        """
+
+        n_dim = X.shape[1]
+
+        if n_dim == 1:
+            # Manhattan Distance
+            distances  = np.abs(X[:, np.newaxis] - centroids)
+        elif n_dim == 1:
+            # Euclidean Distance
+            distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=2)
+        else:
+            # Minkowski Distance with p=3
+            distances = np.sum(np.abs(X[:, np.newaxis] - centroids) ** 3, axis=2) ** (1/3)
+        
+        return distances
+
+    def _assign_clusters(self, X):
+        """
+        Assigns each data point to the closest centroid.
+
+        Parameters:
+        X (numpy array): Data points.
+
+        Returns:
+        numpy array: Cluster assignments.
+        """
+        distances = self._compute_distance(X, self.centroids)
+        return np.argmin(distances, axis=1)
+
+    def fit(self, X):
+        """
+        Fits the K-Means algorithm to the dataset.
+
+        Parameters:
+        X (numpy array): Dataset of shape (n_samples, n_features).
+        """
+        np.random.seed(42)
+        self.centroids = X[np.random.choice(X.shape[0], self.k, replace=False)]
+
+        for _ in range(self.max_iters):
+            clusters = self._assign_clusters(X)
+            new_centroids = np.array([X[clusters == i].mean(axis=0) for i in range(self.k)]) # recalculate centroids
+
+            if np.linalg.norm(self.centroids - new_centroids) < self.tol:
+                break
+
+            self.centroids = new_centroids
+    
+    def predict(self, X):
+        """
+        Predicts cluster labels for new data points.
+
+        Parameters:
+        X (numpy array): Dataset to predict.
+
+        Returns:
+        numpy array: Cluster labels.
+        """
+        return self._assign_clusters(X)
+
+    def fit_predict(self, X):
+        """
+        Performs both fitting and prediction.
+
+        Parameters:
+        X (numpy array): Dataset.
+
+        Returns:
+        numpy array: Cluster labels.
+        """
+        self.fit(X)
+        return self.predict(X)
+
