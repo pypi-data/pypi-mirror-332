3d679fdf1d79c7d37cb6ee94ffca436c9d1ac5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:11 +0000

    Add package grid-api/1.0.0rc1/grid_api-1.0.0rc1.tar.gz

---
+++ packages/grid-api/grid_api-1.0.0rc1.tar.gz/grid_api-1.0.0rc1/src/grid_api/types/workbook_list_response.py
@@ -0,0 +1,54 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["WorkbookListResponse", "Item", "Pagination"]
+
+
+class Item(BaseModel):
+    id: str
+    """A workbook's unique identifier"""
+
+    created: datetime
+    """The date/time the workbook was created"""
+
+    defect: Literal[
+        "",
+        "too_big",
+        "converted_workbook_too_big",
+        "unrecognized_format",
+        "cannot_fetch_from_remote",
+        "processing_timeout",
+        "conversion_error",
+    ]
+    """The defect that was found in the most recent version of the workbook, if any"""
+
+    filename: str
+    """The original filename of the uploaded workbook"""
+
+    modified: datetime
+    """The date/time the workbook was last modified"""
+
+    state: Literal["processing", "ready", "error"]
+    """The current state of the most recent version of the workbook"""
+
+    version: int
+    """The most recent version of the workbook"""
+
+    latest_ready_version: Optional[int] = None
+    """The latest version of the workbook that has a 'ready' state"""
+
+
+class Pagination(BaseModel):
+    next_cursor: Optional[str] = None
+    """The cursor to pass on as query parameter for the next batch of items, if any"""
+
+
+class WorkbookListResponse(BaseModel):
+    items: List[Item]
+
+    pagination: Pagination
