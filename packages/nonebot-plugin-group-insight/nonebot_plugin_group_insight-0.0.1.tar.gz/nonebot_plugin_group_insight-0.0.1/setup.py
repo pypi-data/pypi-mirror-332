b455858d2f52ceb70c2d99b50ce706fe0df1c929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:05:30 +0000

    Add package nonebot-plugin-group-insight/0.0.1/nonebot_plugin_group_insight-0.0.1.tar.gz

---
+++ packages/nonebot-plugin-group-insight/nonebot_plugin_group_insight-0.0.1.tar.gz/nonebot_plugin_group_insight-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nonebot-plugin-group-insight",
+    version="0.0.1",
+    description="NoneBot2 插件：群聊消息统计与排行榜",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="wohaokunr",
+    author_email="wohaokunr@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "nonebot2",
+        "nonebot-plugin-apscheduler",
+        "sqlalchemy",
+        "aiosqlite",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
