d623b475c94bb71c88a29eec0d19ac370e150337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:37 +0000

    Add package MeUtils/2025.3.7.22.33.48/meutils-2025.3.7.22.33.48.tar.gz

---
+++ packages/MeUtils/meutils-2025.3.7.22.33.48.tar.gz/meutils-2025.3.7.22.33.48/meutils/caches/c.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from collections import namedtuple
+import redis.asyncio as redis
+
+from aiocache import cached
+from aiocache import RedisCache
+from aiocache.serializers import PickleSerializer
+
+Result = namedtuple('Result', "content, status")
+
+cache = RedisCache(namespace="main", client=redis.Redis(), serializer=PickleSerializer())
+
+
+@cached(cache, ttl=10, key_builder=lambda *args, **kw: "key")
+async def cached_call():
+    return Result("content", 200)
+
+
+async def test_cached():
+    async with cache:
+        await cached_call()
+        exists = await cache.exists("key")
+        assert exists is True
+        await cache.delete("key")
+
+
+if __name__ == "__main__":
+    asyncio.run(test_cached())
