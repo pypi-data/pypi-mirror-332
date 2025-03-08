5f227b3327b6d33e66e30afbf8caec149b20e6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:04:45 +0000

    Add package face-cropping/0.1.0/face_cropping-0.1.0.tar.gz

---
+++ packages/face-cropping/face_cropping-0.1.0.tar.gz/face_cropping-0.1.0/setup.py
@@ -0,0 +1,40 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '0.1.0'
+DESCRIPTION = 'face_cropping'
+LONG_DESCRIPTION = 'face_cropping'
+
+# Setting up
+setup(
+    name="face_cropping",
+    version=VERSION,
+    author="ren3016",
+    author_email="ren3016@qq.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'dlib'
+    ],
+    keywords=['face_cropping', 'photo'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
