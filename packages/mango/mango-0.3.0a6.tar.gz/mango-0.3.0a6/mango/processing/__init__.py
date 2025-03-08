43bdcdd4cd6a5f5b89d06e168544e582c1691f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:04:18 +0000

    Add package mango/0.3.0a6/mango-0.3.0a6.tar.gz

---
+++ packages/mango/mango-0.3.0a6.tar.gz/mango-0.3.0a6/mango/processing/__init__.py
@@ -0,0 +1,33 @@
+from .date_functions import *
+from .file_functions import (
+    load_json,
+    list_files_directory,
+    is_json_file,
+    is_excel_file,
+    load_excel,
+    load_excel_light,
+    load_excel_sheet,
+    load_csv,
+    load_csv_light,
+    write_json,
+    write_excel,
+    write_excel_light,
+    write_csv,
+    write_csv_light,
+    is_excel_file,
+    is_json_file,
+)
+from .data_imputer import DataImputer
+from .object_functions import (
+    pickle_copy,
+    unique,
+    reverse_dict,
+    cumsum,
+    lag_list,
+    lead_list,
+    row_number,
+    flatten,
+    df_to_list,
+    df_to_dict,
+    as_list,
+)
