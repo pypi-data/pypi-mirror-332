61664ed14fd88296e5ada9c80f2167fdc6846fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:04:44 +0000

    Add package murasame-pp-py/1.0.2/murasame_pp_py-1.0.2.tar.gz

---
+++ packages/murasame-pp-py/murasame_pp_py-1.0.2.tar.gz/murasame_pp_py-1.0.2/README.md
@@ -0,0 +1,1 @@
+# murasame-pp-py
---
+++ packages/murasame-pp-py/murasame_pp_py-1.0.2.tar.gz/murasame_pp_py-1.0.2/src/lib.rs
@@ -0,0 +1,38 @@
+#![allow(unexpected_cfgs)]
+#![deny(clippy::all, nonstandard_style, rust_2018_idioms, unused, warnings)]
+
+use self::{
+    beatmap::PyBeatmap,
+    calculator::PyCalculator,
+    diff_attrs::PyDifficultyAttributes,
+    error::{KwargsError, ParseError},
+    map_attrs::PyBeatmapAttributes,
+    perf_attrs::PyPerformanceAttributes,
+};
+
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
+use strains::PyStrains;
+
+mod beatmap;
+mod calculator;
+mod diff_attrs;
+mod error;
+mod map_attrs;
+mod perf_attrs;
+mod strains;
+
+#[pymodule]
+fn murasame_pp_py(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyBeatmap>()?;
+    m.add_class::<PyCalculator>()?;
+
+    m.add_class::<PyBeatmapAttributes>()?;
+    m.add_class::<PyDifficultyAttributes>()?;
+    m.add_class::<PyPerformanceAttributes>()?;
+    m.add_class::<PyStrains>()?;
+
+    m.add("ParseError", py.get_type::<ParseError>())?;
+    m.add("KwargsError", py.get_type::<KwargsError>())?;
+
+    Ok(())
+}
