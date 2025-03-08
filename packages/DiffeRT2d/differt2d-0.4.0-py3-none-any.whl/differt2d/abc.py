152ed39a97de20d7bcd3095e56468278e3dbaadf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:04:05 +0000

    Add package DiffeRT2d/0.4.0/differt2d-0.4.0-py3-none-any.whl

---
+++ packages/DiffeRT2d/differt2d-0.4.0-py3-none-any.whl/differt2d/abc.py
@@ -0,0 +1,266 @@
+"""Abstract classes to be implemented by the user."""
+
+__all__ = (
+    "Interactable",
+    "Loc",
+    "Object",
+    "Plottable",
+)
+
+from abc import ABC, abstractmethod
+from collections.abc import MutableSequence
+from typing import Any, Literal, Optional
+
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+from beartype import beartype as typechecker
+from jaxtyping import Array, Float, PRNGKeyArray, jaxtyped
+from matplotlib.artist import Artist
+from matplotlib.axes import Axes
+
+from ._typing import ScalarFloat
+from .defaults import DEFAULT_PATCH
+from .logic import Truthy
+
+Loc = Literal["N", "E", "S", "W", "C", "NE", "NW", "SE", "SW"]
+"""Literal type for all valid locations."""
+
+
+class Plottable(ABC):
+    """Abstract class for any object that can be plotted using matplotlib."""
+
+    @abstractmethod
+    def plot(self, ax: Axes, *args: Any, **kwargs: Any) -> MutableSequence[Artist]:
+        """
+        Plot this object on the given axes and returns the results.
+
+        :param ax: The axes to plot on.
+        :param args: Arguments passed to the plot function.
+        :param kwargs: Keyword arguments passed to the plot
+            function.
+        :return: The artist(s).
+        """
+        # pragma: no cover
+
+    @abstractmethod
+    def bounding_box(self) -> Float[Array, "2 2"]:
+        """
+        Returns the bounding box of this object.
+
+        This is: :python:`[[min_x, min_y], [max_x, max_y]]`.
+
+        :return: The min. and max. coordinates of this object.
+        """
+        # pragma: no cover
+
+    @eqx.filter_jit
+    @jaxtyped(typechecker=typechecker)
+    def grid(
+        self,
+        m: int = 50,
+        n: Optional[int] = None,
+    ) -> tuple[Float[Array, "n_or_m {m}"], Float[Array, "n_or_m {m}"]]:
+        """
+        Returns a (mesh) grid that overlays the current object.
+
+        :param m: The number of sample along x dimension.
+        :param n: The number of sample along y dimension,
+            defaults to ``m`` is left unspecified.
+        :return: A tuple of (X, Y) coordinates.
+        """
+        bounding_box = self.bounding_box()
+
+        if n is None:
+            n = m
+
+        x = jnp.linspace(bounding_box[0, 0], bounding_box[1, 0], m)
+        y = jnp.linspace(bounding_box[0, 1], bounding_box[1, 1], n)
+
+        X, Y = jnp.meshgrid(x, y)
+        return X, Y
+
+    @eqx.filter_jit
+    @jaxtyped(typechecker=typechecker)
+    def center(self) -> Float[Array, "2"]:
+        """
+        Returns the center coordinates of this object.
+
+        This is: :python:`[avg_x, avg_y]`.
+
+        :return: The average coordinates of this object.
+        """
+        bounding_box = self.bounding_box()
+
+        return 0.5 * (bounding_box[0, :] + bounding_box[1, :])
+
+    @eqx.filter_jit
+    @jaxtyped(typechecker=typechecker)
+    def get_location(self, location: Loc) -> Float[Array, "2"]:
+        """
+        Returns the relative location within this object's extents.
+
+        'N', 'E', 'S', 'W', 'C' stand, respectively for North, East,
+        South, West, and center. You can also combine two letters to
+        define one of the four corners.
+
+        :param location: A literal referring to the location.
+        :return: The location coordinates within this object's extents.
+        """
+        (xmin, ymin), (xmax, ymax) = self.bounding_box()
+        xavg = 0.5 * (xmin + xmax)
+        yavg = 0.5 * (ymin + ymax)
+
+        x, y = {
+            "N": (xavg, ymax),
+            "E": (xmax, yavg),
+            "S": (xavg, ymin),
+            "W": (xmin, yavg),
+            "C": (xavg, yavg),
+            "NE": (xmax, ymax),
+            "NW": (xmin, ymax),
+            "SE": (xmax, ymin),
+            "SW": (xmin, ymin),
+        }[location]
+
+        return jnp.array([x, y])
+
+
+class Interactable(ABC):
+    """Abstract class for any object that a ray path can interact with."""
+
+    @staticmethod
+    @abstractmethod
+    def parameters_count() -> int:
+        """
+        Returns how many parameters (s, t, ...) are needed to define an interaction point on this object.
+
+        Typically, this equals to one for 2D surfaces.
+
+        :return: The number of parameters.
+        """
+        # pragma: no cover
+
+    @abstractmethod
+    def parametric_to_cartesian(
+        self,
+        param_coords: Float[Array, " parameters_counts"],  # type: ignore[reportUndefinedVariable]
+    ) -> Float[Array, "2"]:
+        """
+        Converts parametric coordinates to cartesian coordinates.
+
+        :param param_coords: Parametric coordinates.
+        :return: Cartesian coordinates.
+        """
+        # pragma: no cover
+
+    @eqx.filter_jit
+    @jaxtyped(typechecker=typechecker)
+    def sample(self, key: PRNGKeyArray) -> Float[Array, "2"]:
+        """
+        Samples a random point on this object.
+
+        :param key: The random key to be used.
+        :return: The cartesian coordinates of the point.
+
+        :Examples:
+
+        >>> from differt2d.geometry import Wall
+        >>> import jax
+        >>> import jax.numpy as jnp
+        >>> key = jax.random.PRNGKey(1234)
+        >>> wall = Wall(xys=jnp.array([[0.0, 0.0], [3.0, 4.0]]))
+        >>> wall.sample(key=key)  # doctest: +SKIP
+        Array([0.88359046, 1.1781206 ], dtype=float32)
+        """
+        return self.parametric_to_cartesian(
+            jax.random.uniform(key=key, shape=(self.parameters_count(),)),
+        )
+
+    @abstractmethod
+    def cartesian_to_parametric(
+        self,
+        carte_coords: Float[Array, "2"],
+    ) -> Float[Array, " parameters_counts"]:  # type: ignore[reportUndefinedVariable]
+        """
+        Converts cartesian coordinates to parametric coordinates.
+
+        :param carte_coords: Cartesian coordinates.
+        :return: Parametric coordinates.
+        """
+        # pragma: no cover
+
+    @abstractmethod
+    def contains_parametric(
+        self,
+        param_coords: Float[Array, " parameters_counts"],  # type: ignore[reportUndefinedVariable]
+        approx: Optional[bool] = None,
+        **kwargs: Any,
+    ) -> Truthy:
+        """
+        Checks if the given coordinates are within the object.
+
+        :param param_coords: Parametric coordinates.
+        :param approx: Whether approximation is enabled or not.
+        :param kwargs: Keyword arguments passed to
+            :func:`activation<differt2d.logic.activation>`.
+        :return: True if object contains these coordinates.
+        """
+        # pragma: no cover
+
+    @abstractmethod
+    def intersects_cartesian(
+        self,
+        ray: Float[Array, "2 2"],
+        patch: ScalarFloat = DEFAULT_PATCH,
+        approx: Optional[bool] = None,
+        **kwargs: Any,
+    ) -> Truthy:
+        """
+        Ray intersection test on the current object.
+
+        :param ray: Ray coordinates.
+        :param patch: The patch ratio, to virtually resize the object
+            prior to intersection check. A ``patch`` value greater than ``1``
+            indicates that the object is enlarged, and a value between ``0`` and
+            ``1`` indicates that the object is compressed. Patching the object
+            size can be useful when combined with :python:`approx=True`, because
+            smoothing objects can virtually reduce this object's size, so using
+            a ``patch`` value greater than ``1`` can compensate this effect.
+        :param approx: Whether approximation is enabled or not.
+        :param kwargs: Keyword arguments passed to
+            :func:`activation<differt2d.logic.activation>`.
+        :return: True if it intersects.
+        """
+        # pragma: no cover
+
+    @abstractmethod
+    def evaluate_cartesian(self, ray_path: Float[Array, "3 2"]) -> Float[Array, " "]:
+        """
+        Evaluates the given interaction triplet.
+
+        Evaluation is performed such that:
+
+        * incident vector is defined as :code:`v_in = b - a`;
+        * bouncing vector is defined as :code:`v_out = c - b`;
+
+        with :code:`a, b, c = ray_path` and :code:`b` lies on the current object.
+
+        A return value of 0 indicates that the interaction is successful.
+
+        The returned value cannot be negative.
+
+        :param ray_path: Ray path coordinates.
+        :return: Interaction score.
+        """
+        # pragma: no cover
+
+
+class Object(Plottable, Interactable):
+    """
+    Abstract class for any object implementing both :class:`Plottable` and :class:`Interactable`.
+
+    This type is actually needed to please Python type checkers, since
+    using :python:`typing.Union[Plottable, Interactable]` is understood
+    as implementing one of either classes, not both.
+    """
---
+++ packages/DiffeRT2d/differt2d-0.4.0-py3-none-any.whl/differt2d/optimize.py
@@ -0,0 +1,182 @@
+"""
+Optimization toolbox.
+
+All the functions present in this toolbox support
+Just-in-time compilation with :func:`jax.jit`.
+
+Examples
+--------
+>>> from differt2d.optimize import minimize
+>>> import chex
+>>> import jax
+>>> import jax.numpy as jnp
+>>> @jax.jit
+... def parabola_min(a, b, c):
+...     def f(x):
+...         x = a * (x + b) + c
+...         return jnp.dot(x, x)
+...
+...     return minimize(f, jnp.array(0.0))
+>>>
+>>> x, y = parabola_min(2.0, 1.0, 1.0)
+>>> chex.assert_trees_all_close(x, -1.5, rtol=1e-2)
+>>> chex.assert_trees_all_close(y, +0.0, atol=1e-3)
+
+"""
+
+import sys
+from typing import Any, Callable, Optional
+
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+import optax
+from jaxtyping import Array, Float, PRNGKeyArray, jaxtyped
+
+if sys.version_info >= (3, 11):
+    from typing import TypeVarTuple, Unpack
+else:  # pragma: no cover
+    from typing_extensions import TypeVarTuple, Unpack
+
+Ts = TypeVarTuple("Ts")
+
+
+@eqx.filter_jit
+@jaxtyped(typechecker=None)
+def minimize(
+    fun: Callable[[Float[Array, " n"], *Ts], Float[Array, " "]],
+    x0: Float[Array, " n"],
+    args: tuple[Unpack[Ts]] = (),
+    steps: int = 100,
+    optimizer: Optional[optax.GradientTransformation] = None,
+) -> tuple[Float[Array, " n"], Float[Array, " "]]:
+    """
+    Minimizes a scalar function of one or more variables.
+
+    :param fun: The objective function to be minimized.
+    :param x0: The initial guess.
+    :param args:
+        Positional arguments passed to ``fun``.
+    :param steps: The number of steps to perform.
+    :param optimizer: The optimizer to use. If not provided,
+        uses :func:`optax.adam` with a learning rate of ``0.1``.
+    :return: The solution array and the corresponding loss.
+
+    :Examples:
+
+    >>> from differt2d.optimize import minimize
+    >>> import chex
+    >>> import jax.numpy as jnp
+    >>> def f(x, offset=1.0):
+    ...     x = x - offset
+    ...     return jnp.dot(x, x)
+    >>>
+    >>> x, y = minimize(f, jnp.zeros(10))
+    >>> chex.assert_trees_all_close(x, jnp.ones(10), rtol=1e-2)
+    >>> chex.assert_trees_all_close(y, 0.0, atol=1e-4)
+    >>>
+    >>> # It is also possible to pass positional arguments
+    >>> x, y = minimize(f, jnp.zeros(10), args=(2.0,))
+    >>> chex.assert_trees_all_close(x, 2.0 * jnp.ones(10), rtol=1e-2)
+    >>> chex.assert_trees_all_close(y, 0.0, atol=1e-3)
+    """
+    optimizer = optimizer or optax.adam(learning_rate=0.1)
+
+    f_and_df = jax.value_and_grad(fun)
+    opt_state = optimizer.init(x0)
+
+    def f(carry, _):
+        x, opt_state = carry
+        loss, grads = f_and_df(x, *args)
+        updates, opt_state = optimizer.update(grads, opt_state)
+        x = x + updates
+        carry = (x, opt_state)
+        return carry, loss
+
+    (x, _), losses = jax.lax.scan(f, init=(x0, opt_state), xs=None, length=steps)
+    return x, losses[-1]
+
+
+@eqx.filter_jit
+@jaxtyped(typechecker=None)
+def minimize_random_uniform(
+    fun: Callable[[Float[Array, " {n}"], *Ts], Float[Array, " "]],
+    key: PRNGKeyArray,
+    n: int,
+    **kwargs: Any,
+) -> tuple[Float[Array, " {n}"], Float[Array, " "]]:
+    """
+    Minimizes a scalar function of one or more variables, with initial guess drawn randomly from a uniform distribution.
+
+    :param fun: The objective function to be minimized.
+    :param key: The random key used to generate the initial guess.
+    :param n: The size of the random vector to generate.
+    :param kwargs:
+        Keyword arguments passed to :func:`minimize`.
+    :return: The solution array and the corresponding loss.
+
+    :Examples:
+
+    >>> from differt2d.optimize import minimize_random_uniform
+    >>> import chex
+    >>> import jax
+    >>> import jax.numpy as jnp
+    >>> def f(x):
+    ...     x = x - 1.0
+    ...     return jnp.dot(x, x)
+    >>>
+    >>> x, y = minimize_random_uniform(f, jax.random.PRNGKey(1234), 10)
+    >>> chex.assert_trees_all_close(x, jnp.ones(10), rtol=1e-2)
+    >>> chex.assert_trees_all_close(y, 0.0, atol=1e-3)
+    """
+    x0 = jax.random.uniform(key, shape=(n,))
+    return minimize(fun, x0, **kwargs)
+
+
+@eqx.filter_jit
+@jaxtyped(typechecker=None)
+def minimize_many_random_uniform(
+    fun: Callable[[Float[Array, " {n}"], *Ts], Float[Array, " "]],
+    key: PRNGKeyArray,
+    n: int,
+    many: int = 10,
+    **kwargs: Any,
+) -> tuple[Float[Array, " {n}"], Float[Array, " "]]:
+    """
+    Minimizes many times a scalar function of one or more variables, with initial guess drawn randomly from a uniform distribution, and returns the best minimum out of the :code:`many` trials.
+
+    :param fun: The objective function to be minimized.
+    :param key: The random key used to generate the initial guesses.
+    :param n: The size of the random vector to generate.
+    :param many:
+        How many times the minimization should be performed.
+    :param kwargs:
+        Keyword arguments passed to :func:`minimize_random_uniform`.
+    :return: The solution array and the corresponding loss.
+
+    :Examples:
+
+    >>> from differt2d.optimize import minimize_many_random_uniform
+    >>> import chex
+    >>> import jax
+    >>> import jax.numpy as jnp
+    >>> def f(x):
+    ...     x = x - 1.0
+    ...     return jnp.dot(x, x)
+    >>>
+    >>> x, y = minimize_many_random_uniform(f, jax.random.PRNGKey(1234), 10)
+    >>> chex.assert_trees_all_close(x, jnp.ones(10), rtol=1e-2)
+    >>> chex.assert_trees_all_close(y, 0.0, atol=1e-4)
+    """
+    if many == 1:
+        return minimize_random_uniform(fun, key, n, **kwargs)
+
+    keys = jax.random.split(key, num=many)
+
+    xs, losses = jax.vmap(lambda key: minimize_random_uniform(fun, key, n, **kwargs))(
+        keys
+    )
+
+    i_min = jnp.argmin(losses)
+
+    return xs[i_min, :], losses[i_min]
