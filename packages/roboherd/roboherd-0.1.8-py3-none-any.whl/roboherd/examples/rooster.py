bd3346b65839839b844a67d775d34d4eace03b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:05:10 +0000

    Add package roboherd/0.1.8/roboherd-0.1.8-py3-none-any.whl

---
+++ packages/roboherd/roboherd-0.1.8-py3-none-any.whl/roboherd/examples/rooster.py
@@ -0,0 +1,16 @@
+from roboherd import RoboCow, PublishObject, ObjectFactory
+
+from .meta import meta_information
+
+bot = RoboCow.create(
+    handle="rooster",
+    name="The crowing rooster 🐓",
+    meta_information=meta_information,
+)
+
+
+@bot.cron("42 * * * *")
+async def crow(publisher: PublishObject, object_factory: ObjectFactory):
+    await publisher(
+        object_factory.note(content="cock-a-doodle-doo").as_public().build()  # type: ignore
+    )
