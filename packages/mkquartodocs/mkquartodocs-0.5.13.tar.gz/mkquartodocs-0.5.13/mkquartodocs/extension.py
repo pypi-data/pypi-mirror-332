8ae2f593c0da5776d31c91a9ebfd859a4a04619a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 14:04:46 +0000

    Add package mkquartodocs/0.5.13/mkquartodocs-0.5.13.tar.gz

---
+++ packages/mkquartodocs/mkquartodocs-0.5.13.tar.gz/mkquartodocs-0.5.13/mkquartodocs/extension.py
@@ -0,0 +1,90 @@
+import re
+from typing import Final
+
+from markdown import Markdown
+from markdown.extensions import Extension
+from markdown.extensions.admonition import AdmonitionProcessor
+from markdown.preprocessors import Preprocessor
+
+from .logging import get_logger
+
+log = get_logger(__name__)
+
+# TODO: implement ways to actually use the information ...
+
+
+class AdmotionCellDataPreprocessor(Preprocessor):
+
+    CELL_REGEX: Final = re.compile(r"^(:{3,}) \{\.cell .*}\s*$")
+    CELL_END: Final = re.compile(r"^(:{3,})$")
+    CELL_ELEM_REGEX: Final = re.compile(
+        r"^(:{3,}) \{(.cell-\w+)\s?(\.cell-[\w-]+)?( execution_count=\"\d+\")?\}$"
+    )
+    CODEBLOCK_REGEX: Final = re.compile(r"^(`{3,}){\.(\w+) .*}")
+
+    # https://squidfunk.github.io/mkdocs-material/reference/admonitions/#supported-types
+    TYPE_MAPPING: Final = {
+        ".cell-output-stdout": '!!! note "output"',
+        ".cell-output-stderr": '!!! warning "stderr"',
+        ".cell-output-error": '!!! danger "error"',
+        ".cell-output-display": '!!! note "Display"',
+    }
+
+    def run(self, lines):
+        log.info(f"Running {self}")
+        outs = [self._process_line(x) for x in lines]
+        log.debug(f"Removing {sum(1 for x in outs if x is None)} lines")
+        out = [x for x in outs if x is not None]
+        return out
+
+    def _process_line(self, line):
+        if sr := self.CELL_REGEX.search(line):
+            log.debug(f"Matched Cell start: {line}")
+            out = "\n\n"
+
+        elif sr := self.CELL_END.search(line):
+            log.debug(f"Matched Cell end: {line}")
+            out = "\n\n"
+
+        elif sr := self.CELL_ELEM_REGEX.search(line):
+            grp = sr.groups()
+            log.debug(f"Matched Cell element: {line}, groups: {grp}")
+            output_type = grp[2]
+            out = self.TYPE_MAPPING[output_type]
+
+        elif sr := self.CODEBLOCK_REGEX.search(line):
+            grp = sr.groups()
+            log.debug(f"Matched codeblock: {line}, groups: {grp}")
+            lang = grp[2]
+            nesting_level = len(grp[1])
+            out = f"{'`' * nesting_level}{lang}"
+        else:
+            out = line
+
+        if line != out:
+            log.debug(f"Transformed {line} -> {out}")
+        return out
+
+
+# TODO consider implemeting this ...
+# class CellBLockProcessor(BlockProcessor):
+
+
+class QuartoCellDataExtension(Extension):
+    def extendMarkdown(
+        self, md: Markdown
+    ) -> None:  # noqa: N802 (casing: parent method's name)
+        """Register the extension.
+
+        Adds an instance of the Processor to the Markdown instance.
+            md: A `markdown.Markdown` instance.
+        """
+        md.registerExtension(self)
+        md.preprocessors.register(
+            AdmotionCellDataPreprocessor(),
+            name="QuatoCellData",
+            priority=106,  # Right before Admonition
+        )
+        md.parser.blockprocessors.register(
+            AdmonitionProcessor(md.parser), "admonition", 105
+        )
