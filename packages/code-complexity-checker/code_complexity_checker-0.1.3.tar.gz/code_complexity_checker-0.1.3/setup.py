394d1dcc124d5dedbd7dc03e235c8004e7b00169
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:26 +0000

    Add package code-complexity-checker/0.1.3/code_complexity_checker-0.1.3.tar.gz

---
+++ packages/code-complexity-checker/code_complexity_checker-0.1.3.tar.gz/code_complexity_checker-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Read README.md
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='code-complexity-checker',  
+    version='0.1.3',  
+    packages=find_packages(),
+    install_requires=[
+        'radon>=5.1.0',  
+    ],
+    entry_points={
+        'console_scripts': [
+            'code-checker=checker.complexity:main',  
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple Python tool to calculate code complexity.',
+    url='https://github.com/sneha30404/code-complexity-checker',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
