f3269e9f1fb486d6c1919b71e5d309e80ab87f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:05:22 +0000

    Add package QA-automation-phone/0.1.0/qa_automation_phone-0.1.0-py3-none-any.whl

---
+++ packages/QA-automation-phone/qa_automation_phone-0.1.0-py3-none-any.whl/QA_automation_phone/config.py
@@ -0,0 +1,64 @@
+import subprocess
+import xml.etree.ElementTree as ET
+import uiautomator2 as u2
+from typing import Literal
+def run_command(command: str) -> dict:
+    process = subprocess.Popen(
+        command, 
+        shell=True, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate()
+    return {
+        'stdout': stdout.strip(),
+        'stderr': stderr.strip(),
+        'returncode': process.returncode
+    }
+def run_command_text(command: str) -> dict:
+    process = subprocess.Popen(
+        command, 
+        shell=True, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    stdout, stderr = process.communicate()
+    return {
+        'stdout': stdout.strip(),
+        'stderr': stderr.strip(),
+        'returncode': process.returncode
+    }
+
+def adb_click(device: str,x:int,y:int):
+    command = f"adb -s {device} shell input tap {x} {y}"
+    run_command(command=command)
+def adb_send(device: str,content: str):
+    command = f"adb -s {device} shell input text {content}"
+    run_command(command=command)
+def adb_click_send(device: str,x:int,y:int,content:str):
+    adb_click(device, x, y)
+    adb_send(device, content)
+def adb_keyevent(device: str,key: int):
+    command = f"adb -s {device} shell input keyevent {key}"
+    run_command(command=command)
+def scroll_height(device: str, x: int,y1: int, y2: int, distance: int=300):
+    command = f"adb -s {device} shell input swipe {x} {y1} {x} {y2} {distance}"
+    run_command(command=command)
+def scroll_width(device: str, x1: int, x2: int, y: int, distance: int=300):
+    command = f"adb -s {device} shell input swipe {x1} {y} {x2} {y} {distance}"
+    run_command(command=command)
+def scroll_up_down(device: str,type: Literal["up","down"], x: int, y1: int, y2: int, distance: int=300):
+    if type == "up":
+        scroll_height(device, x, y1, y2, distance)
+    else:
+        scroll_height(device, x, y2, y1, distance)
+def scroll_left_right(device: str,type: Literal["left","right"], x1: int, x2: int, y: int, distance: int=300):
+    if type == "left":
+        scroll_width(device, x1, x2, y, distance)
+    else:
+        scroll_width(device, x2, x1, y, distance)
+def long_press(device: str, x: int, y: int, duration: int=1000):
+    command = f"adb -s {device} shell input swipe {x} {y} {x} {y} {duration}"
+    run_command(command=command)
+
---
+++ packages/QA-automation-phone/qa_automation_phone-0.1.0-py3-none-any.whl/QA_automation_phone/coreapp.py
@@ -0,0 +1,78 @@
+from QA_automation_phone.config import *
+
+def get_xml_content(device: str)->str:
+    command = f"adb -s {device} exec-out uiautomator dump /dev/stdout"
+    result = run_command_text(command)
+    if result['returncode'] == 0:
+        return result['stdout'].replace('UI hierchary dumped to: /dev/stdout', "")  
+    else:
+        return result['stderr']
+def get_xml_content_uiautomator2(connect)->str:
+    if connect:
+        return connect.dump_hierarchy()
+    print("Not connected")
+    return None
+
+def wait_for_element(connect: u2.connect, type: Literal["text","content-desc", "resource-id"], value: str, loop: int=2)->str:
+    for _ in range(loop):
+        xml_content = get_xml_content_uiautomator2(connect)
+        if xml_content:
+            if type == "text":
+                if value in xml_content:
+                    return xml_content
+            elif type == "content-desc":
+                if value in xml_content:
+                    return xml_content
+            else:
+                if value in xml_content:
+                    return xml_content
+    return None
+def get_bounds(connect: u2.connect, type: Literal["text","content-desc", "resource-id"], value: str, index: int=0, loop: int=2)->str:
+    xml = wait_for_element(connect, type, value, loop)
+    if xml:
+        convert = ET.fromstring(xml)
+        elements =  [element for element in convert.iter() if value in element.attrib.get(type,"")]
+        if elements:
+            return elements[index].attrib.get('bounds','')
+    return None
+
+def center_point_bounds(connect: u2.connect, type: Literal["text","content-desc", "resource-id"], value: str, index: int=0, loop: int=2)->tuple:
+    bounds = get_bounds(connect, type, value, index, loop)
+    if bounds:
+        xy = eval(bounds.replace("][",","))
+        return (xy[0]+xy[2])//2, (xy[1]+xy[3])//2
+    return None
+def click_element(device: str, connect: u2.connect, type: Literal["text","content-desc", "resource-id"], value: str, index: int=0, loop: int=2):
+    xy = center_point_bounds(connect, type, value, index, loop)
+    if xy:
+        adb_click(device, xy[0], xy[1])
+        return xy
+    return None
+def tab_and_send_text_to_element(device: str, type: Literal["text","content-desc", "resource-id"], value: str, index: int=0, loop: int=2, content: str=""):
+    xy = center_point_bounds(device, type, value, index, loop)
+    if xy:
+        adb_click_send(device, xy[0], xy[1],content)
+        return xy
+    return None
+
+
+
+
+# devices = "R58NC2W4ZQK"
+# import time
+# start = time.time()
+# if click_element(device_id, "text", "Settings", 0, 2):
+#     print("Clicked")
+# else: 
+#     print("Not clicked")
+# print(time.time()-start)
+# def get_element(device: str, type: Literal["text","content-desc", "resource-id"], value: str)->str:
+#     xml_content = get_xml_content(device)
+#     if xml_content:
+#         if type == "text":
+#             element = f'"{value}"'
+#         elif type == "resourceId":
+#             element = f'"{value}"'
+#         print(element)
+# result = run_command("echo Hello World")
+# print(result)
