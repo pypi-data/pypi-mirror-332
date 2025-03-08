21cafcd9674c7a6ee650baf8b64ef04197c9b514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:04:51 +0000

    Add package fabricatio/0.2.4.dev3/fabricatio-0.2.4.dev3-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/fabricatio/fabricatio-0.2.4.dev3-cp312-cp312-manylinux_2_34_x86_64.whl/fabricatio/fs/__init__.py
@@ -0,0 +1,17 @@
+"""FileSystem manipulation module for Fabricatio."""
+
+from fabricatio.fs.curd import copy_file, create_directory, delete_directory, delete_file, dump_text, move_file, tree
+from fabricatio.fs.readers import magika, safe_json_read, safe_text_read
+
+__all__ = [
+    "copy_file",
+    "create_directory",
+    "delete_directory",
+    "delete_file",
+    "dump_text",
+    "magika",
+    "move_file",
+    "safe_json_read",
+    "safe_text_read",
+    "tree",
+]
