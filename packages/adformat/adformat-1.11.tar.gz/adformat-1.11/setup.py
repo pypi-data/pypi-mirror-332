3bef76e053c892b5854a3b6ea175d4fc063ed8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:05:02 +0000

    Add package adformat/1.11/adformat-1.11.tar.gz

---
+++ packages/adformat/adformat-1.11.tar.gz/adformat-1.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adformat",
+    version="1.11",
+    packages=find_packages(),
+    install_requires=[
+        "colorama",
+        "wmi",
+    ],
+    entry_points={
+        "console_scripts": [
+            "adformat=adformat.ADFormat:main",
+        ],
+    },
+    author="ARnoLD Digital",
+    author_email="arnold_digital@yandex.ru",
+    description="Formats multiply USB disks at once",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ARnoLD-Digital-Store/ADFormat",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires=">=3.6"
+)
