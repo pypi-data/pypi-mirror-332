31e9b4c4b4147925c33073be0e2fa712143ac4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:04:49 +0000

    Add package neuroslides/0.0.1/neuroslides-0.0.1.tar.gz

---
+++ packages/neuroslides/neuroslides-0.0.1.tar.gz/neuroslides-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neuroslides",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Muhammad Saboor Islam",
+    author_email="muhammadsaboor119@gmail.com",
+    description="This is a test package to reserve the name",
+    long_description="This package is uploaded for name reservation only.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Muhammad Saboor Islam/neuroslides",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
