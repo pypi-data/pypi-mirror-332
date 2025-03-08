7dfc42e631abc42d4ac5e9e7cd4d9fca3d285c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:04:47 +0000

    Add package sentence-generalized-pooling/0.9.0/sentence_generalized_pooling-0.9.0-py3-none-any.whl

---
+++ packages/sentence-generalized-pooling/sentence_generalized_pooling-0.9.0-py3-none-any.whl/sentence_generalized_pooling/__init__.py
@@ -0,0 +1,5 @@
+from .multihead_generalized_pooling import MultiHeadGeneralizedPooling
+from .generalized_sentence_transformer_maker import GeneralizedSentenceTransformerMaker
+
+# Define which functions and classes should be accessible in this subpackage
+__all__ = ['MultiHeadGeneralizedPooling', 'GeneralizedSentenceTransformerMaker']
