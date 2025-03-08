d2d2a16ae6ce6d1d6550d6dc658159d3f2262c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:06:42 +0000

    Add package morebuiltins/1.2.3/morebuiltins-1.2.3.tar.gz

---
+++ packages/morebuiltins/morebuiltins-1.2.3.tar.gz/morebuiltins-1.2.3/morebuiltins/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "1.2.3"
+__all__ = [
+    "morebuiltins.utils",
+    "morebuiltins.date",
+    "morebuiltins.functools",
+    "morebuiltins.ipc",
+    "morebuiltins.request",
+    "morebuiltins.download_python",
+    "morebuiltins.tk",
+    "morebuiltins.emails",
+    "morebuiltins.cmd.log_server",
+    "morebuiltins.cmd.proxy_checker",
+    "morebuiltins.cmd.ui",
+    "morebuiltins.cmd.parse_deps",
+    "morebuiltins.snippets.event",
+    "morebuiltins.snippets.sql",
+    "morebuiltins.cmd.systemd.service",
+    "morebuiltins.cmd.systemd.timer",
+    "morebuiltins.sqlite",
+]
