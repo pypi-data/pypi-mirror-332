a0540fe3749f05dada3e761720e03f04dd546bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:04:50 +0000

    Add package cloud-auth-tpm/0.6.6/cloud_auth_tpm-0.6.6-py3-none-any.whl

---
+++ packages/cloud-auth-tpm/cloud_auth_tpm-0.6.6-py3-none-any.whl/cloud_auth_tpm/policy/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from cloud_auth_tpm.policy.policy import PolicyEval
+from cloud_auth_tpm.policy.pcr import PCRPolicy
+from cloud_auth_tpm.policy.pcr_authvalue import PCRAuthValuePolicy
