3ad3108ba1f1664b90ab2a9355cbb8704f81b315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:03:57 +0000

    Add package memo/0.2.4/memo-0.2.4-py2.py3-none-any.whl

---
+++ packages/memo/memo-0.2.4-py2.py3-none-any.whl/memo/_grid.py
@@ -0,0 +1,55 @@
+import random
+import itertools as it
+
+
+def grid(shuffle: bool = True, progbar: bool = None, **kwargs):
+    """
+    Generates a grid of settings.
+
+    Arguments:
+        kwargs: the name of parameter is the key while the values represent items to iterate over
+
+    Example
+
+    ```python
+    from memo import grid
+
+    settings = grid(a=[1,2], b=[1, 2], shuffle=False)
+    expected = [
+        {'a': 1, 'b': 1},
+        {'a': 1, 'b': 2},
+        {'a': 2, 'b': 1},
+        {'a': 2, 'b': 2}
+    ]
+    assert settings == expected
+    ```
+    """
+    settings = [
+        dict(zip(kwargs.keys(), d)) for d in it.product(*[v for v in kwargs.values()])
+    ]
+    if progbar:
+        raise DeprecationWarning(
+            "`progbar` is deprecated, use a `from memo import Runner` to get a progbar."
+        )
+    if shuffle:
+        random.shuffle(settings)
+    return settings
+
+
+def random_grid(n: int = 30, **kwargs):
+    """
+    Generates a random grid settings.
+
+    Arguments:
+        kwargs: the name of parameter is the key while the values represent items to iterate over
+
+    Example
+
+    ```python
+    from memo import random_grid
+
+    settings = random_grid(n=30, a=[1,2], b=[1, 2])
+    assert len(settings) == 30
+    ```
+    """
+    return [{k: random.choice(v) for k, v in kwargs.items()} for _ in range(n)]
