e4ab1d965c8bc33b946adfbb98ae935a1d346116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 07:07:03 +0000

    Add package host-communication/1.0/host_communication-1.0.tar.gz

---
+++ packages/host-communication/host_communication-1.0.tar.gz/host_communication-1.0/README.md
@@ -0,0 +1,8 @@
+# Host Communication
+
+This package provides tools for enabling communication between two hosts using sockets.
+
+## Installation
+
+```bash
+pip install host_communication
---
+++ packages/host-communication/host_communication-1.0.tar.gz/host_communication-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="host_communication",
+    version="1.0",
+    author="Hakim Adiche",
+    author_email="adiche@kfupm.edu.sa",
+    description="A Python package for enabling communication between two hosts using sockets.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[],
+)
