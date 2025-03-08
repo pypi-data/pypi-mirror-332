7b23b2f7161bc1f16a4adfc2792e74e9edf45495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 06:05:31 +0000

    Add package blendpy/25.3.36/blendpy-25.3.36.tar.gz

---
+++ packages/blendpy/blendpy-25.3.36.tar.gz/blendpy-25.3.36/test/conftest.py
@@ -0,0 +1,11 @@
+import os
+from glob import glob
+
+LOGFILES = ['optimize.log', 'test_optimize.log']
+
+def pytest_sessionfinish(session, exitstatus):
+    print("\n\n  FINISHING TESTS")
+    for logfile in LOGFILES:
+        if os.path.exists(logfile):
+            os.remove(logfile)
+            print(f"    {logfile} has been deleted.")
