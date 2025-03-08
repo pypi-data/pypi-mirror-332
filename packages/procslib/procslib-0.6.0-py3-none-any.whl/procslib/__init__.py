56d5d642de6a6e029d56a0d3d19a1ff26893bec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 06:05:23 +0000

    Add package procslib/0.6.0/procslib-0.6.0-py3-none-any.whl

---
+++ packages/procslib/procslib-0.6.0-py3-none-any.whl/procslib/__init__.py
@@ -0,0 +1,11 @@
+"""procslib package.
+
+Multi-purpose processing library for downstream use
+"""
+
+from __future__ import annotations
+
+__all__: list[str] = []
+
+from .config import get_config, get_configs, set_config
+from .model_builder import get_model, get_model_keys
