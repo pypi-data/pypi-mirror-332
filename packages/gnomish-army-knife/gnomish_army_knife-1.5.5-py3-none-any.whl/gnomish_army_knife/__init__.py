3c5b009ea22cc6d3fe23f7a171818c3a9186e366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 16:04:09 +0000

    Add package gnomish-army-knife/1.5.5/gnomish_army_knife-1.5.5-py3-none-any.whl

---
+++ packages/gnomish-army-knife/gnomish_army_knife-1.5.5-py3-none-any.whl/gnomish_army_knife/__init__.py
@@ -0,0 +1,19 @@
+# =====================================
+# generator=datazen
+# version=3.2.0
+# hash=4bb634f5c35d0d705fd86337062ec86b
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "World of Warcraft polyfills and standardization."
+PKG_NAME = "gnomish-army-knife"
+VERSION = "1.5.5"
+
+# gnomish-army-knife-specific content.
+PKG_SLUG = PKG_NAME.replace("-", "_")
+PKG_ABBREV = "gak"
+DEFAULT_CONFIG = f"{PKG_ABBREV}.yaml"
+WOW_DIR = "World of Warcraft"
