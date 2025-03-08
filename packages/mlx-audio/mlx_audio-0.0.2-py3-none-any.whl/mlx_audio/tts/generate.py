2b1d50ee32913d9260a1fccc7eadc567ffe3e25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 14:04:18 +0000

    Add package mlx-audio/0.0.2/mlx_audio-0.0.2-py3-none-any.whl

---
+++ packages/mlx-audio/mlx_audio-0.0.2-py3-none-any.whl/mlx_audio/tts/generate.py
@@ -0,0 +1,121 @@
+import argparse
+import json
+import os
+import sys
+
+import mlx.core as mx
+import soundfile as sf
+
+from .audio_player import AudioPlayer
+from .utils import load_model
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="prince-canuma/Kokoro-82M",
+        help="Path or repo id of the model",
+    )
+    parser.add_argument(
+        "--text",
+        type=str,
+        default=None,
+        help="Text to generate (leave blank to input via stdin)",
+    )
+    parser.add_argument("--voice", type=str, default="af_heart", help="Voice name")
+    parser.add_argument("--speed", type=float, default=1.0, help="Speed of the audio")
+    parser.add_argument("--lang_code", type=str, default="a", help="Language code")
+    parser.add_argument(
+        "--file_prefix", type=str, default="audio", help="Output file name prefix"
+    )
+    parser.add_argument("--verbose", action="store_false", help="Print verbose output")
+    parser.add_argument(
+        "--join_audio", action="store_true", help="Join all audio files into one"
+    )
+    parser.add_argument("--play", action="store_true", help="Play the output audio")
+    args = parser.parse_args()
+
+    if args.text is None:
+        if not sys.stdin.isatty():
+            args.text = sys.stdin.read().strip()
+        else:
+            print("Please enter the text to generate:")
+            args.text = input("> ").strip()
+
+    return args
+
+
+def main():
+    args = parse_args()
+    try:
+        model = load_model(model_path=args.model)
+        print(
+            f"\n\033[94mModel:\033[0m {args.model}\n"
+            f"\033[94mText:\033[0m {args.text}\n"
+            f"\033[94mVoice:\033[0m {args.voice}\n"
+            f"\033[94mSpeed:\033[0m {args.speed}x\n"
+            f"\033[94mLanguage:\033[0m {args.lang_code}"
+        )
+        print("==========")
+        results = model.generate(
+            text=args.text,
+            voice=args.voice,
+            speed=args.speed,
+            lang_code=args.lang_code,
+            verbose=True,
+        )
+        print(
+            f"\033[92mAudio generated successfully, saving to\033[0m {args.file_prefix}!"
+        )
+
+        audio_list = []
+        for i, result in enumerate(results):
+            if args.join_audio or args.play:
+                audio_list.append(result.audio)
+            else:
+                sf.write(f"{args.file_prefix}_{i:03d}.wav", result.audio, 24000)
+
+            if args.verbose:
+                print("==========")
+                print(f"Duration:              {result.audio_duration}")
+                print(
+                    f"Samples/sec:           {result.audio_samples['samples-per-sec']:.1f}"
+                )
+                print(
+                    f"Prompt:                {result.token_count} tokens, {result.prompt['tokens-per-sec']:.1f} tokens-per-sec"
+                )
+                print(
+                    f"Audio:                 {result.audio_samples['samples']} samples, {result.audio_samples['samples-per-sec']:.1f} samples-per-sec"
+                )
+                print(f"Real-time factor:      {result.real_time_factor:.2f}x")
+                print(f"Processing time:       {result.processing_time_seconds:.2f}s")
+                print(f"Peak memory usage:     {result.peak_memory_usage:.2f}GB")
+
+        if args.join_audio:
+            print(f"Joining {len(audio_list)} audio files")
+            audio = mx.concatenate(audio_list, axis=0)
+            sf.write(f"{args.file_prefix}.wav", audio, 24000)
+
+        if args.play:
+            audio = mx.concatenate(audio_list, axis=0)
+
+            player = AudioPlayer()
+            player.queue_audio(audio)
+            player.wait_for_drain()
+            player.stop()
+    except ImportError as e:
+        print(f"Import error: {e}")
+        print(
+            "This might be due to incorrect Python path. Check your project structure."
+        )
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        import traceback
+
+        traceback.print_exc()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mlx-audio/mlx_audio-0.0.2-py3-none-any.whl/mlx_audio/tts/models/base.py
@@ -0,0 +1,48 @@
+import inspect
+from dataclasses import dataclass
+
+import mlx.core as mx
+
+
+@dataclass
+class BaseModelArgs:
+    @classmethod
+    def from_dict(cls, params):
+        return cls(
+            **{
+                k: v
+                for k, v in params.items()
+                if k in inspect.signature(cls).parameters
+            }
+        )
+
+
+def check_array_shape(arr):
+    shape = arr.shape
+
+    # Check if the shape has 4 dimensions
+    if len(shape) != 3:
+        return False
+
+    out_channels, kH, KW = shape
+
+    # Check if out_channels is the largest, and kH and KW are the same
+    if (out_channels >= kH) and (out_channels >= KW) and (kH == KW):
+        return True
+    else:
+        return False
+
+
+@dataclass
+class GenerationResult:
+    audio: mx.array
+    samples: int
+    segment_idx: int
+    token_count: int
+    audio_samples: int
+    audio_duration: str
+    real_time_factor: float
+    prompt: dict
+    audio_samples: dict
+    processing_time_seconds: float
+    peak_memory_usage: float
