fb285c5600db059a273cf15179af52ff5330d9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:05:11 +0000

    Add package maaranparottakadai/0.1.0/maaranparottakadai-0.1.0-py3-none-any.whl

---
+++ packages/maaranparottakadai/maaranparottakadai-0.1.0-py3-none-any.whl/maaranparottakadai/parotta.py
@@ -0,0 +1,15 @@
+"""
+Maaran Parotta Kadai
+
+Disclaimer:
+This package is purely for fun and has no connection to any real-world parotta shop.
+It is not intended for commercial use or branding purposes.
+
+Author: Balamurugan
+"""
+
+def parotta():
+    return "Parotanaley adhu maran parotta kadai dhan!\nMaaran parotta kadai, Kovil vaasal, Kazhugu Malai. \nMachans Marandhuradhinga...\n\nAnbu Rasigan Balamurugan!!"
+
+if __name__ == "__main__":
+    print(parotta())
