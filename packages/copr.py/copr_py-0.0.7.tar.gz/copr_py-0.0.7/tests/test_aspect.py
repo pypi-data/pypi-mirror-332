f15bb40688d24dc6cff6935c27cfc0bdc8f4847d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:06:07 +0000

    Add package copr.py/0.0.7/copr_py-0.0.7.tar.gz

---
+++ packages/copr.py/copr_py-0.0.7.tar.gz/copr_py-0.0.7/tests/test_aspect.py
@@ -0,0 +1,8 @@
+from copr import *
+
+def test_aspectSelection():
+  assert len(COPR.aspects(qualityKind='colour')) > 0
+
+def test_aspectClass():
+  for entity in COPR.aspects():
+    assert isinstance(entity, COPRAspect)
