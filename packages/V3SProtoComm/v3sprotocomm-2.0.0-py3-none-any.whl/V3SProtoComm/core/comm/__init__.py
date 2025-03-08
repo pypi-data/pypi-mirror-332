6340df28f141b087327f45709d8965a9d3de906e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 14:04:40 +0000

    Add package V3SProtoComm/2.0.0/v3sprotocomm-2.0.0-py3-none-any.whl

---
+++ packages/V3SProtoComm/v3sprotocomm-2.0.0-py3-none-any.whl/V3SProtoComm/core/comm/__init__.py
@@ -0,0 +1,10 @@
+from V3SProtoComm.core.comm.controls import ProtoControl
+from V3SProtoComm.core.comm.controls import ProtoControlThread
+from V3SProtoComm.core.comm.vision import ProtoVision
+from V3SProtoComm.core.comm.vision import ProtoVisionThread
+from V3SProtoComm.core.comm.referee import RefereeComm
+from V3SProtoComm.core.comm.thread_job import Job
+
+#ABC
+from V3SProtoComm.core.comm.transmitter import Transmitter
+from V3SProtoComm.core.comm.receiver import Receiver
