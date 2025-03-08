4bf28ef71dd375c39461af37f973244f19b1b422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 06:05:32 +0000

    Add package dingo-walk/0.1.3/dingo_walk-0.1.3.tar.gz

---
+++ packages/dingo-walk/dingo_walk-0.1.3.tar.gz/dingo_walk-0.1.3/dingo_walk/__main__.py
@@ -0,0 +1,10 @@
+# dingo_walk.walk : a python library for metabolic networks sampling and analysis
+# dingo_walk.walk is part of GeomScale project
+
+# Copyright (c) 2021 Apostolos Chalkis
+
+# Licensed under GNU LGPL.3, see LICENCE file
+
+from dingo_walk.walk import dingo_walk.walk_main
+
+dingo_walk.walk_main()
