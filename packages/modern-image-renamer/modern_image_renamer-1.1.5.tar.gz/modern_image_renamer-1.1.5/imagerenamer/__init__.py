16e097158459d26f91f96b03ce4cc90822789d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:07:21 +0000

    Add package modern-image-renamer/1.1.5/modern_image_renamer-1.1.5.tar.gz

---
+++ packages/modern-image-renamer/modern_image_renamer-1.1.5.tar.gz/modern_image_renamer-1.1.5/imagerenamer/__init__.py
@@ -0,0 +1,8 @@
+"""
+Image Renamer - Rename image files based on their creation date from EXIF metadata.
+"""
+
+__version__ = "1.1.5"
+__author__ = "Lars van der Niet"
+__license__ = "MIT"
+__url__ = "https://github.com/larsniet/image-renamer"
