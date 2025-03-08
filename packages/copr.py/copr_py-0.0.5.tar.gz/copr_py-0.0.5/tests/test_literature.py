99ccc4b1e5471ed134208d2d75d57b46b7a0e628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:06:29 +0000

    Add package copr.py/0.0.5/copr_py-0.0.5.tar.gz

---
+++ packages/copr.py/copr_py-0.0.5.tar.gz/copr_py-0.0.5/tests/test_literature.py
@@ -0,0 +1,13 @@
+from copr import *
+
+def test_literatureSelection():
+  assert len(COPR.literature(author='Mocnik')) > 0
+
+def test_literatureClass():
+  for literature in COPR.literature():
+    assert isinstance(literature, COPRLiterature)
+
+def test_literatureParameters():
+  for literature in COPR.literature():
+    assert literature.author() or literature.editor() or literature.institution()
+    assert literature.title()
