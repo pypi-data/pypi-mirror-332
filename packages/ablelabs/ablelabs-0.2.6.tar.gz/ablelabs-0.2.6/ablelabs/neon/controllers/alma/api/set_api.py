cff4a2740fbc8f64881cac15d098c6a5945f872b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:06:35 +0000

    Add package ablelabs/0.2.6/ablelabs-0.2.6.tar.gz

---
+++ packages/ablelabs/ablelabs-0.2.6.tar.gz/ablelabs-0.2.6/ablelabs/neon/controllers/alma/api/set_api.py
@@ -0,0 +1,15 @@
+import sys, os
+
+sys.path.append(os.path.abspath(os.curdir))
+from ablelabs.neon.utils.network.messenger import MessengerClient, run_server_func
+from ablelabs.neon.utils.network.tcp_client import TcpClient
+from ablelabs.neon.controllers.alma.api.robot_router import RobotRouter
+
+
+class SetAPI(MessengerClient):
+    def __init__(self, tcp_client: TcpClient) -> None:
+        super().__init__(tcp_client)
+
+    @run_server_func(RobotRouter.set_update_pipette_attrs)
+    async def update_pipette_attrs(self, value: dict[int, dict]):
+        pass
