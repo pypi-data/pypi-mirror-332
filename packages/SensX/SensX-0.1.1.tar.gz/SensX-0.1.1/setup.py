3ea9277137f335fa8c2cd6a26db25f55bca069c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 14:04:43 +0000

    Add package SensX/0.1.1/SensX-0.1.1.tar.gz

---
+++ packages/SensX/SensX-0.1.1.tar.gz/SensX-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SensX',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+)
