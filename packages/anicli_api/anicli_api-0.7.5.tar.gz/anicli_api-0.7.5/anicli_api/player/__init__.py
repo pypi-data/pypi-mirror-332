bab7d2d6c3db614e383f0f195e5ad854278d1075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:04 +0000

    Add package anicli_api/0.7.5/anicli_api-0.7.5.tar.gz

---
+++ packages/anicli_api/anicli_api-0.7.5.tar.gz/anicli_api-0.7.5/anicli_api/player/__init__.py
@@ -0,0 +1,7 @@
+from anicli_api.player.aniboom import Aniboom
+from anicli_api.player.csst import CsstOnline
+from anicli_api.player.kodik import Kodik
+from anicli_api.player.sibnet import SibNet
+from anicli_api.player.sovetromantica import SovietRomanticaPlayer
+
+ALL_DECODERS = (Kodik, Aniboom, SibNet, CsstOnline, SovietRomanticaPlayer)
