352efa39422666155ec1e9050d2cec85ccd4c3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 06:05:12 +0000

    Add package ducpy/0.0.4/ducpy-0.0.4.tar.gz

---
+++ packages/ducpy/ducpy-0.0.4.tar.gz/ducpy-0.0.4/src/ducpy/__init__.py
@@ -0,0 +1,20 @@
+"""Main module for duc_py package.
+
+Import usage examples:
+    import ducpy as duc
+    
+    # Access modules directly:
+    duc.classes.SomeClass
+    duc.parse.parse_function
+    duc.serialize.serialize_function
+    duc.utils.some_utility
+"""
+
+from . import Duc
+
+# Import modules for direct access
+from . import utils
+from . import parse
+from . import serialize
+from . import classes
+from . import tests
