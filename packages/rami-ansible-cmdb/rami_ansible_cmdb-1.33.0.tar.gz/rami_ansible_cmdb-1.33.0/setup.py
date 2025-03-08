27ff4adf0a08942ae149d9d7fc949a960ceb6325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:05:45 +0000

    Add package rami-ansible-cmdb/1.33.0/rami_ansible_cmdb-1.33.0.tar.gz

---
+++ packages/rami-ansible-cmdb/rami_ansible_cmdb-1.33.0.tar.gz/rami_ansible_cmdb-1.33.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import os
+import sys
+from setuptools import setup, find_namespace_packages
+
+def get_long_description():
+    path = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(path) as f:
+        return f.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    print('You should also add a git tag for this version:')
+    print(' git tag {0}'.format(get_version()))
+    print(' git push --tags')
+    sys.exit()
+
+setup(
+    name='rami-ansible-cmdb',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    license='GPLv3',
+    description='Generate host overview from ansible fact gathering output',
+    long_description=get_long_description(),
+    url='https://github.com/fboender/ansible-cmdb',
+
+    author='Ferry Boender',
+    author_email='ferry.boender@electricmonk.nl',
+
+    package_dir={'': 'src'},
+    packages=find_namespace_packages('src'),
+    package_data={
+        'ansiblecmdb.data': ['*.*'],
+        'ansiblecmdb.data.static.images': ['*.*'],
+        'ansiblecmdb.data.static.js': ['*.*'],
+        'ansiblecmdb.data.tpl': ['*.*']
+    },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['mako', 'pyyaml', 'ushlex', 'jsonxs'],
+    scripts=[
+        'src/ansible-cmdb',
+        'src/ansible-cmdb.py'
+    ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+)
---
+++ packages/rami-ansible-cmdb/rami_ansible_cmdb-1.33.0.tar.gz/rami_ansible_cmdb-1.33.0/test/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Python v2"
+python2 test.py
+python2 test_ansible_api.py
+
+echo "Python v3"
+python3 test.py
+python3 test_ansible_api.py
