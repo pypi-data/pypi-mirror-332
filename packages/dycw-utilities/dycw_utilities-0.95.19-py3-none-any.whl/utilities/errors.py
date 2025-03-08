6ea60be737f5c74fa5d43a09a98f01ee3b47773a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:34 +0000

    Add package dycw-utilities/0.95.19/dycw_utilities-0.95.19-py3-none-any.whl

---
+++ packages/dycw-utilities/dycw_utilities-0.95.19-py3-none-any.whl/utilities/errors.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from typing_extensions import override
+
+
+@dataclass(kw_only=True, slots=True)
+class ImpossibleCaseError(Exception):
+    case: list[str]
+
+    @override
+    def __str__(self) -> str:
+        desc = ", ".join(self.case)
+        return f"Case must be possible: {desc}."
+
+
+__all__ = ["ImpossibleCaseError"]
