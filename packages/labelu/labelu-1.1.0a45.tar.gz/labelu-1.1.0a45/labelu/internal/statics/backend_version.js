4b29922b12adb2d430c580ff7d1a0928d482ebca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:05:46 +0000

    Add package labelu/1.1.0a45/labelu-1.1.0a45.tar.gz

---
+++ packages/labelu/labelu-1.1.0a45.tar.gz/labelu-1.1.0a45/labelu/internal/statics/backend_version.js
@@ -0,0 +1,10 @@
+
+                  (function () {
+                    window.__backend = {
+  "version": "1.1.0-alpha.45",
+  "name": "labelu",
+  "build_time": "2025-03-07T11:48:48.261Z",
+  "commit": "7a94abe6e5ee97d40c04d8b51e83ae52e9ec89b4"
+};
+                  })();
+                
