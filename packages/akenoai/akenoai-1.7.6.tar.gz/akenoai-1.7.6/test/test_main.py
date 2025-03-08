13ec96725645a53110449b73e81fe4966260b4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 06:05:31 +0000

    Add package akenoai/1.7.6/akenoai-1.7.6.tar.gz

---
+++ packages/akenoai/akenoai-1.7.6.tar.gz/akenoai-1.7.6/test/test_main.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from akenoai import AkenoXJs
+
+js = AkenoXJs(is_akenox_fast=True).connect()
+
+async def test_main():
+    response = js.fast.create(
+        model="lu-sunda",
+        query="hello",
+        is_obj=True
+    )
+    print(response)
+
+
+asyncio.run(test_main())
