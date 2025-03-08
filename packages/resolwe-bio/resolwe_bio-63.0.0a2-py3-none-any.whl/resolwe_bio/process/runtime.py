b277f14b8c965cb681cb8e7a912576a94cac1e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:39 +0000

    Add package resolwe-bio/63.0.0a2/resolwe_bio-63.0.0a2-py3-none-any.whl

---
+++ packages/resolwe-bio/resolwe_bio-63.0.0a2-py3-none-any.whl/resolwe_bio/process/runtime.py
@@ -0,0 +1,32 @@
+"""Resolwe-bio process with KnowledgeBase extensions."""
+
+from typing import Any, Dict, List
+
+from resolwe.process.communicator import communicator
+from resolwe.process.models import Data
+from resolwe.process.runtime import Process
+
+from resolwe_bio.process.models import Feature, Mapping, Variant, VariantCall
+
+
+class ProcessBio(Process):
+    """Resolwe-bio process class."""
+
+    # Mark this process abstract ie the base class for real processes.
+    _abstract = True
+
+    def __init__(self, data: Data):
+        """Initialize."""
+        super().__init__(data)
+        self.feature = Feature
+        self.mapping = Mapping
+        self.variant = Variant
+        self.variant_call = VariantCall
+
+    def add_variants(self, data_source: str, variants: List[Dict[str, Any]]):
+        """Add variants to the database."""
+        return communicator.add_variants((data_source, variants))
+
+    def add_variants_annotations(self, variants: List[Dict[str, Any]]):
+        """Add variants annotations to the database."""
+        return communicator.add_variants_annotations(variants)
