d9550655c5582bcfffd98e03d5a4ca53ac5a0cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:05:18 +0000

    Add package spicecode/0.3.3/spicecode-0.3.3.tar.gz

---
+++ packages/spicecode/spicecode-0.3.3.tar.gz/spicecode-0.3.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt using utf-16 encoding to handle the BOM correctly
+with open("requirements.txt", encoding="utf-16") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="spicecode",
+    version="0.3.3",
+    packages=find_packages(exclude=["spicecode-venv", "spicecode.egg-info"]),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'spice = cli.main:main',  # This tells Python to run the main function in cli.main
+        ],
+    },
+
+)
