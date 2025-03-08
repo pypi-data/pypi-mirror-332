c9dbbfd94fc45dce5a67d73d59742504bd489dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:18 +0000

    Add package kurzaj/0.9.0/kurzaj-0.9.0.tar.gz

---
+++ packages/kurzaj/kurzaj-0.9.0.tar.gz/kurzaj-0.9.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kurzaj",
+    version="0.9.0",
+    packages=find_packages(),
+    install_requires=[
+        'Pillow'
+    ],
+    options= {
+        "kurzaj": {
+            "include_files": ["kurzaj/capybara.jpg"]
+        }},
+    author="meow",
+    description="nuh",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://example.com/",  # Replace with your GitHub
+    python_requires=">=3.6",
+)
