cc105ac3a4136e5d4a19e51910bb8802e11f9dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:05:08 +0000

    Add package secfsdstools/2.2.0/secfsdstools-2.2.0-py3-none-any.whl

---
+++ packages/secfsdstools/secfsdstools-2.2.0-py3-none-any.whl/secfsdstools/__init__.py
@@ -0,0 +1,15 @@
+"""
+main __init__.py
+
+Defines the version attribut of the library
+"""
+import logging
+import sys
+from importlib.metadata import version, PackageNotFoundError
+
+import secfsdstools.update
+
+try:
+    __version__ = version("secfsdstools")
+except PackageNotFoundError:
+    __version__ = "0.0.0"  # Fallback-Version, falls das Paket nicht installiert ist
