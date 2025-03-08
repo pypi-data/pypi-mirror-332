633b16b2570b3ceafe374ad085add68166f23b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:05:34 +0000

    Add package syntrend/0.2.1/syntrend-0.2.1.tar.gz

---
+++ packages/syntrend/syntrend-0.2.1.tar.gz/syntrend-0.2.1/tests/utils/test_exceptions.py
@@ -0,0 +1,37 @@
+from syntrend.utils import exc
+from pytest import mark
+from os import linesep
+
+
+@mark.unit
+def test_exception_output(monkeypatch):
+    reported_exception = [0]
+    reported_output = ['']
+
+    def _exit_code(code: int):
+        reported_exception[0] = code
+
+    def _error_output(content: str):
+        reported_output[0] = content
+
+    monkeypatch.setattr(exc.sys, 'exit', _exit_code)
+    monkeypatch.setattr(exc.sys.stderr, 'write', _error_output)
+    err = ValueError('Failed Expression', {'a': 1, 'b': 2, 'c': 3})
+    exc.process_exception(err)
+
+    assert reported_exception[0] == 1, (
+        'Reported error exit code should be 2 (Expression Error)'
+    )
+    assert (
+        reported_output[0]
+        == linesep.join(
+            [
+                'Error Encountered: (ValueError)',
+                '  | Failed Expression',
+                '  | a: 1',
+                '  | b: 2',
+                '  | c: 3',
+            ]
+        )
+        + linesep
+    ), 'Exception output format not meeting expected format'
