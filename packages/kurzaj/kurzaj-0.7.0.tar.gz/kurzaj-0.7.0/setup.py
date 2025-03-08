b6257b633648de154170a6b35e9e070cd9944a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 03:07:22 +0000

    Add package kurzaj/0.7.0/kurzaj-0.7.0.tar.gz

---
+++ packages/kurzaj/kurzaj-0.7.0.tar.gz/kurzaj-0.7.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kurzaj",
+    version="0.7.0",
+    packages=find_packages(),
+    install_requires=[
+        'Pillow'
+    ],
+    entry_points={
+        "console_scripts": [
+            "kurzaj=kurzajpackage.main:kurzaj",
+        ],
+    },
+    author="meow",
+    description="nuh",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://example.com/",  # Replace with your GitHub
+    python_requires=">=3.6",
+)
