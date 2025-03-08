867eb6c963f0ab69c7f6193a3bb8807153f7a89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:05:01 +0000

    Add package fitrequest/1.0.2/fitrequest-1.0.2.tar.gz

---
+++ packages/fitrequest/fitrequest-1.0.2.tar.gz/fitrequest-1.0.2/docs/source/developer_interface/utils.rst
@@ -0,0 +1,8 @@
+Utils
+=====
+
+.. autofunction:: fitrequest.utils.extract_method_params
+.. autofunction:: fitrequest.utils.string_varnames
+.. autofunction:: fitrequest.utils.format_url
+.. autofunction:: fitrequest.utils.extract_url_params
+.. autofunction:: fitrequest.utils.get_return_annotation
---
+++ packages/fitrequest/fitrequest-1.0.2.tar.gz/fitrequest-1.0.2/tests/demo_mix.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import Any
+from fitrequest.decorators import fit, retry
+from fitrequest.client import FitRequest
+
+
+class RestApiClient(FitRequest):
+    """Awesome class generated with fitrequest."""
+
+    client_name = 'rest_api'
+    base_url = 'https://test.skillcorner.fr'
+    method_docstring = 'Calling endpoint: {endpoint}'
+
+    method_config_list = [
+        {
+            'base_name': 'items',
+            'endpoint': '/items/',
+            'add_async_method': True,
+        },
+    ]
+
+    @retry(max_retries=3, on_status='500-600')
+    @fit(endpoint='/items/{item_id}')
+    def get_item(self, item_id: str) -> Any: ...
+
+    @retry(max_retries=2, on_status='507')
+    @fit(endpoint='/items/{item_id}/details/{detail_id}')
+    def get_item_details(self, item_id: str, detail_id: str) -> Any: ...
+
+
+client_mix = RestApiClient()
