8859e9345d4f4df015c4fafb4e2a7583cbd651fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 07:07:08 +0000

    Add package depoc/0.1.3a3/depoc-0.1.3a3-py3-none-any.whl

---
+++ packages/depoc/depoc-0.1.3a3-py3-none-any.whl/depoc/cli.py
@@ -0,0 +1,31 @@
+import click
+
+from .commands import account
+from .commands import login
+from .commands import logout
+from .commands import me
+from .commands import finance
+from .commands import contact
+from .commands import receivable
+from .commands import payable
+from .commands import report
+from .commands import customer
+from .commands import supplier
+
+@click.group()
+def main() -> None:
+   pass
+ 
+main.add_command(account)
+main.add_command(login)
+main.add_command(logout)
+main.add_command(me)
+main.add_command(finance.bank)
+main.add_command(finance.category)
+main.add_command(finance.transaction)
+main.add_command(contact)
+main.add_command(receivable)
+main.add_command(payable)
+main.add_command(report)
+main.add_command(customer)
+main.add_command(supplier)
