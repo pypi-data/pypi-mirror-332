ad67bd862a54d55ab8e58bd4e45e4f5b4a211d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 14:04:16 +0000

    Add package livekit-plugins-noise-cancellation/0.1.3/livekit_plugins_noise_cancellation-0.1.3-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/livekit-plugins-noise-cancellation/livekit_plugins_noise_cancellation-0.1.3-py3-none-manylinux_2_28_x86_64.whl/livekit/plugins/noise_cancellation/__init__.py
@@ -0,0 +1,24 @@
+import sys
+
+from .plugin import plugin_path, dependencies_path, NC
+
+
+__all__ = [
+    "NC",
+]
+
+_loaded = False
+
+def load():
+    global _loaded
+    if not _loaded:
+        _loaded = True
+
+        from livekit import rtc
+
+        module = sys.modules[__name__]
+        module_id = str(id(module))
+
+        plugin = rtc.AudioFilter(module_id, plugin_path(), dependencies_path())
+
+load()
