417ada5b32175ac9f3cec13bbcc5a4a84db5c4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 05:04:34 +0000

    Add package docling-serve/0.5.0/docling_serve-0.5.0.tar.gz

---
+++ packages/docling-serve/docling_serve-0.5.0.tar.gz/docling_serve-0.5.0/docling_serve/settings.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from typing import Optional, Union
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from docling_serve.datamodel.engines import AsyncEngine
+
+
+class UvicornSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="UVICORN_", env_file=".env", extra="allow"
+    )
+
+    host: str = "0.0.0.0"
+    port: int = 5001
+    reload: bool = False
+    root_path: str = ""
+    proxy_headers: bool = True
+    workers: Union[int, None] = None
+
+
+class DoclingServeSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_prefix="DOCLING_SERVE_",
+        env_file=".env",
+        env_parse_none_str="",
+        extra="allow",
+    )
+
+    enable_ui: bool = False
+    artifacts_path: Optional[Path] = None
+
+    eng_kind: AsyncEngine = AsyncEngine.LOCAL
+    eng_loc_num_workers: int = 2
+
+
+uvicorn_settings = UvicornSettings()
+docling_serve_settings = DoclingServeSettings()
