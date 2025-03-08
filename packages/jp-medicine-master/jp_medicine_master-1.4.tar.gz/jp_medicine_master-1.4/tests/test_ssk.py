85491a8be533da094e775aac0449ec7cec4722f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 15:04:55 +0000

    Add package jp-medicine-master/1.4/jp_medicine_master-1.4.tar.gz

---
+++ packages/jp-medicine-master/jp_medicine_master-1.4.tar.gz/jp_medicine_master-1.4/tests/test_ssk.py
@@ -0,0 +1,27 @@
+import pytest
+import jp_medicine_master as jpmed
+from jp_medicine_master.ssk import _get_url_ssk
+
+
+def test_get_url_ssk():
+    # 最新年度 == 2024
+    assert _get_url_ssk() == _get_url_ssk(2024)
+
+
+def test_read_ssk_y():
+    df = jpmed.read_ssk_y()
+
+    # ヘッダー行
+    assert len(df.columns) == 42
+    assert df.columns[0] == '変更区分'
+
+    # 行数 ~ 20,000
+    assert 18_000 <= len(df) <= 22_000
+
+
+def test_read_ssk_y_with_file_info():
+    df = jpmed.read_ssk_y(file_info=True)
+
+    # ヘッダー行
+    assert len(df.columns) == 43
+    assert df.columns[-1] == 'file'
