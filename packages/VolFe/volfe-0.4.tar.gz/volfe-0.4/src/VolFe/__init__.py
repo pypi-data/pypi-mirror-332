f58772ca9aabf3ee1c69d47ce66c9358d0f84761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 14:04:24 +0000

    Add package VolFe/0.4/volfe-0.4.tar.gz

---
+++ packages/VolFe/volfe-0.4.tar.gz/volfe-0.4/src/VolFe/__init__.py
@@ -0,0 +1,23 @@
+__author__ = "Ery Hughes, Pip Liggins, Penny Wieser"
+__version__ = "0.4"
+
+# functions for calculating properties of the melt and gas
+from .melt_gas import *
+
+# functions for variables that can be model depedent (e.g., solubility constants,
+# fugacity coefficients)
+from .model_dependent_variables import *
+
+# functions for calculating equilibrium speciation and concentration between melt ± gas
+# at given P and T
+from .equilibrium_equations import *
+from .differential_equations import *
+
+# functions to calculate equilibrium isotope fractionation given melt ± gas composition
+from .batch_calculations import *
+
+# functions to run a calculations
+from .calculations import *
+
+# functions to run calculations in batch mode
+from .batch_calculations import *
