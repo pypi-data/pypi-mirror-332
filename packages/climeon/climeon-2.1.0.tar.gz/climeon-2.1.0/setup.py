43f1b08cfa8e281c973c4f7ce2ddffdcd14f00ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 04:04:28 +0000

    Add package climeon/2.1.0/climeon-2.1.0.tar.gz

---
+++ packages/climeon/climeon-2.1.0.tar.gz/climeon-2.1.0/setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="climeon",
+    packages=find_packages(),
+    version="2.1.0",
+    license="MIT",
+    description="Climeon API client",
+    long_description="Climeon API client",
+    author="Emil Hjelm",
+    author_email="emil.hjelm@climeon.com",
+    keywords=["climeon", "REST", "API"],
+    python_requires=">=3.9",
+    install_requires=[
+        "plotly",
+        "plotly-resampler",
+        "msal",
+        "pandas",
+        "requests",
+        "dateparser"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent"
+    ]
+)
