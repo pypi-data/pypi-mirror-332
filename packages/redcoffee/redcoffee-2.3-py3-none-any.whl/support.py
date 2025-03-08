bf01a9f33d53e22690bc1dfde8d8ffb650171d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-08 16:04:09 +0000

    Add package redcoffee/2.3/redcoffee-2.3-py3-none-any.whl

---
+++ packages/redcoffee/redcoffee-2.3-py3-none-any.whl/support.py
@@ -0,0 +1,9 @@
+import random
+def pick_random_support_message():
+
+    support_messages=[
+        "💖 Hey friend! If you like this tool, why not give it a ⭐ on GitHub? It makes me super happy! (≧◡≦). Link: https://github.com/Anubhav9/redcoffee",
+        "🎉 You’re awesome! If this CLI made your life easier, drop a ⭐ on GitHub to show some love! 🌸. Link: https://github.com/Anubhav9/redcoffee",
+        "💕 This project runs on coffee & GitHub stars! ☕✨ Send some love by starring the repo! ⭐. Link: https://github.com/Anubhav9/redcoffee"
+    ]
+    return random.choice(support_messages)
