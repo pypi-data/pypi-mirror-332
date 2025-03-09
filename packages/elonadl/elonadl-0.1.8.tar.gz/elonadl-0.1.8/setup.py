eb0752fd0fb7a4a79adf3caeab52b04b8fb4d3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 14:05:20 +0000

    Add package elonadl/0.1.8/elonadl-0.1.8.tar.gz

---
+++ packages/elonadl/elonadl-0.1.8.tar.gz/elonadl-0.1.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="elonadl",
+    version="0.1.8",
+    author="vichmartins",
+    packages=["elonadl"],
+    description="ElonaPlusCGXDownload",
+    long_description="Installs the latest version of Elona+CGX (Windows Only Currently)",
+    python_requires='>=3.10',
+    install_requires=['pywin32', 'requests', 'winshell']
+)
