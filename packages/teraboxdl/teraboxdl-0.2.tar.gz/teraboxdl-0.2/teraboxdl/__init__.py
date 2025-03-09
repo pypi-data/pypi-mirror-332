8a95b77bf84d57a4c94742fc4c08fc59ab26a398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:49 +0000

    Add package teraboxdl/0.2/teraboxdl-0.2.tar.gz

---
+++ packages/teraboxdl/teraboxdl-0.2.tar.gz/teraboxdl-0.2/teraboxdl/__init__.py
@@ -0,0 +1,10 @@
+from teraboxdl.downloader import TeraBoxDownloader
+from teraboxdl.exceptions import TeraBoxError, DownloadError, LinkNotFoundError
+
+__all__ = ["TeraBoxDownloader"]
+__version__ = "0.2"
+__license__ = "GNU General Public License v3.0"
+__author__ = "Santhosh"
+print("TeraBoxDL package has been imported successfully.")
+print(f"Version: {__version__}")
+print(f"License: {__license__}")
