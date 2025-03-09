0770b548955b048dd78abb38210e2ccf2d22038d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:58 +0000

    Add package sarcharts/0.3.2/sarcharts-0.3.2-py3-none-any.whl

---
+++ packages/sarcharts/sarcharts-0.3.2-py3-none-any.whl/sarcharts/lib/events.py
@@ -0,0 +1,32 @@
+import csv
+
+from sarcharts.lib import util
+
+
+class Events:
+
+    def getCSVdata(args, charts):
+        if args.eventfile:
+            with open(args.eventfile) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                for row in csv_reader:
+                    if not row[0].startswith("#"):
+                        if row[1] not in charts.keys():
+                            util.debug(
+                                args, 'W',
+                                f"Host '{row[1]}' from '{args.eventfile}'"
+                                + " not in 'sar' data.")
+                            continue
+                        if util.in_date_range(args, row[0]):
+                            charts[row[1]]['xlabels'].append(row[0])
+                            if row[2] not in charts[row[1]]['events']:
+                                charts[row[1]]['events'][row[2]] = []
+                            charts[row[1]]['events'][row[2]].append(
+                                   {'date': util.valid_date(args, row[0]),
+                                    'description': (
+                                        row[3]
+                                        .replace(r'"', r'\"')
+                                        .replace(r'(', r'\(')
+                                        .replace(r')', r'\)'))
+                                    })
+        return charts
