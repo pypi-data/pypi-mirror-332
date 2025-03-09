efb177e2e056c9c157f5997a5360217d2308d3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:15 +0000

    Add package chpeck/0.0.1/chpeck-0.0.1.tar.gz

---
+++ packages/chpeck/chpeck-0.0.1.tar.gz/chpeck-0.0.1/chpeck.py
@@ -0,0 +1,47 @@
+# pip install beautifulsoup4 requests pandas
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class RecipeScraper:
+    def __init__(self):
+        self.soup = None
+        self.data_frame = None
+
+    def scrape_url(self, url: str) -> None:
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        self.soup = BeautifulSoup(response.text, 'html.parser')
+
+    def get_tables_data(self) -> pd.DataFrame:
+        if self.data_frame is not None:
+            return self.data_frame
+        tables = self.soup.find_all('table')[1:]
+        headers = self.soup.find_all('h2')
+        data_tables = []
+        for i, table in enumerate(tables):
+            table_rows = table.find_all('tr')[1:]
+            data_tables.append({
+                'table_name': headers[i].text,
+                'table_data': []
+            })
+            for row in table_rows:
+                cells = row.find_all('td')
+                data_tables[i]['table_data'].append({
+                    'name': cells[0].text,
+                    'ingredients': cells[1].text,
+                    'image': cells[2].find('img')['src'],
+                    'description': cells[3].text
+                })
+        self.data_frame = pd.DataFrame(data_tables)
+        return self.data_frame
+
+if __name__ == '__main__':
+    rs = RecipeScraper()
+    rs.scrape_url("https://www.minecraftcrafting.info/")
+    print(rs.get_tables_data())
+
+#py -m pip install --upgrade build
+#py -m pip install --upgrade twine
+#py -m build
+#py -m twine upload dist/*
