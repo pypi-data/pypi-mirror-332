57dc083bbe64c68d98fbb15a62e5d6b429cd87a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:06:18 +0000

    Add package bloyid/0.1/bloyid-0.1.tar.gz

---
+++ packages/bloyid/bloyid-0.1.tar.gz/bloyid-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bloyid",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="UkY",
+    description="Bloyid Bot Bibliothek",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ukyyyy/bloyid.py",
+)
