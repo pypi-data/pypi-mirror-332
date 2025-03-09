ca64383f25e6049dd7d2476bb067c896428a3b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:05:03 +0000

    Add package box2d-python/0.1.2/box2d_python-0.1.2-cp312-cp312-musllinux_1_2_i686.whl

---
+++ packages/box2d-python/box2d_python-0.1.2-cp312-cp312-musllinux_1_2_i686.whl/box2d/__init__.py
@@ -0,0 +1,7 @@
+from .world import World
+from .math import Vec2, Rot, Transform, AABB, Mat22, ScaledTransform
+from .body import Body, BodyBuilder
+from .shape import Box, Circle, Polygon
+from .joint import MouseJoint, WeldJoint, RevoluteJoint
+from .debug_draw import DebugDraw, Color
+from .collision_filter import CollisionFilter, CollisionCategoryRegistry
