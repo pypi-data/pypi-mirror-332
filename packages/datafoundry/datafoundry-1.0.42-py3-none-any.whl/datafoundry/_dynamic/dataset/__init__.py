4ad6b6f95e043656a59f9ee30c7c6210a3f25df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:46 +0000

    Add package datafoundry/1.0.42/datafoundry-1.0.42-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.42-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,14 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .datasettest_67ca9b4e72e1d762ef9be120 import DatasetTest
+
+from .hainv4test22121_67cc9020dfc1b464c7c61d41 import hainv4test22121
+
+
+__all__ = [
+    
+    "DatasetTest",
+    
+    "hainv4test22121"
+    
+]
