614d50ddd74ad1105fd6945e31099ff6ce2216d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:20 +0000

    Add package datafoundry/1.0.33/datafoundry-1.0.33-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.33-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,38 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .datasettest_67ca9b4e72e1d762ef9be120 import DatasetTest
+
+from .hainv4test22121_67cc9020dfc1b464c7c61d41 import hainv4test22121
+
+from .db21111_67cc9f36dfc1b464c7c61d44 import db21111
+
+from .db3_67cca0dfdfc1b464c7c61d45 import db3
+
+from .db4_67cca443dfc1b464c7c61d46 import db4
+
+from .db5_67cca631dfc1b464c7c61d47 import db5
+
+from .db6_67cca9c8dfc1b464c7c61d48 import db6
+
+from .db7_67ccab5cdfc1b464c7c61d49 import db7
+
+
+__all__ = [
+    
+    "DatasetTest",
+    
+    "hainv4test22121",
+    
+    "db21111",
+    
+    "db3",
+    
+    "db4",
+    
+    "db5",
+    
+    "db6",
+    
+    "db7"
+    
+]
---
+++ packages/datafoundry/datafoundry-1.0.33-py3-none-any.whl/datafoundry/_dynamic/dataset/db7_67ccab5cdfc1b464c7c61d49.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class db7(DatasetBase):
+    """
+    Database: db7
+    Description: None
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
