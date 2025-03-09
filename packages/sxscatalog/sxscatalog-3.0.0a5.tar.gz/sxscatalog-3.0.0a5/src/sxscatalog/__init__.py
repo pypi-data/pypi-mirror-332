209b1b1d910ff2dea64ce54a51a197ce505eead5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:53 +0000

    Add package sxscatalog/3.0.0a5/sxscatalog-3.0.0a5.tar.gz

---
+++ packages/sxscatalog/sxscatalog-3.0.0a5.tar.gz/sxscatalog-3.0.0a5/src/sxscatalog/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025-present Mike Boyle <michael.oliver.boyle@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .__about__ import __version__
+
+from . import utilities
+from . import metadata
+from . import simulations
