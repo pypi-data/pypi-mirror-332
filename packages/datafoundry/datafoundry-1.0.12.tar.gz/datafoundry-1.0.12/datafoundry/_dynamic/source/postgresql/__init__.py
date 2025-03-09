f495a291db73495cd21677793203b078191a2100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:02 +0000

    Add package datafoundry/1.0.12/datafoundry-1.0.12.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.12.tar.gz/datafoundry-1.0.12/datafoundry/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,10 @@
+# --- render code block -----
+
+from .hainv4testing import hainv4testing
+
+__all__ = [
+    
+    "hainv4testing"
+    
+]
+# --- render code block -----
---
+++ packages/datafoundry/datafoundry-1.0.12.tar.gz/datafoundry-1.0.12/datafoundry/ontology/datasource/postgresql.py
@@ -0,0 +1,10 @@
+import datafoundry._dynamic.source.postgresql as db
+
+## ----render code block----
+
+class hainv4testing(db.hainv4testing):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+## ----render code block----
