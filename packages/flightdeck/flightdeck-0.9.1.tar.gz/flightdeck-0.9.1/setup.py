3cb2a545216a62805c9f3df8ef3999d482c55a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:44 +0000

    Add package flightdeck/0.9.1/flightdeck-0.9.1.tar.gz

---
+++ packages/flightdeck/flightdeck-0.9.1.tar.gz/flightdeck-0.9.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='flightdeck',
+    version='0.9.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'flightdeck = main:main', 
+        ],
+    },
+    install_requires=[
+        'requests',
+        'rich',
+        'pyAesCrypt',
+        'pillow'
+    ],
+    author='Advait Contractor',
+    author_email='advait@advaitconty.com',
+    description='A multi-tool CLI for the masses, with a file encrypter.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/advaitconty/flightdeck',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
