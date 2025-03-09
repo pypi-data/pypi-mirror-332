bf0b81a95e338ce15145caf4b219e37fc60e2393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:53 +0000

    Add package labelu/1.1.0a46/labelu-1.1.0a46.tar.gz

---
+++ packages/labelu/labelu-1.1.0a46.tar.gz/labelu-1.1.0a46/labelu/internal/statics/frontend_version.js
@@ -0,0 +1,16 @@
+(function () {
+  window.__frontend = {
+    version: '5.7.0-alpha.17',
+    deps: {
+      '@labelu/audio-annotator-react': '1.8.0-alpha.10',
+      '@labelu/components-react': '1.8.0-alpha.7',
+      '@labelu/formatter': '1.0.2',
+      '@labelu/i18n': '1.1.0-alpha.6',
+      '@labelu/image': '1.4.0-alpha.5',
+      '@labelu/image-annotator-react': '2.4.0-alpha.7',
+      '@labelu/interface': '1.3.1',
+      '@labelu/video-annotator-react': '1.5.0-alpha.7',
+      '@labelu/video-react': '1.5.0-alpha.7',
+    },
+  };
+})();
