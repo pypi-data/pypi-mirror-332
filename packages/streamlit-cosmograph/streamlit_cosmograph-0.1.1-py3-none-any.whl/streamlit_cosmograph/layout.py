c8450b48122a3635ae64e94e01177777cd8399d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:05:04 +0000

    Add package streamlit-cosmograph/0.1.1/streamlit_cosmograph-0.1.1-py3-none-any.whl

---
+++ packages/streamlit-cosmograph/streamlit_cosmograph-0.1.1-py3-none-any.whl/streamlit_cosmograph/layout.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File: layout.py
+@Author: Wang Yang
+@Email: yangwang0222@163.com
+@Date:   2025/02/28 15:43 
+@Last Modified by: yangwang0222@163.com
+@Description : Supported layout types.
+'''
+
+class LayoutEnum:
+    BYLABEL = "ByLabel"
+    CIRCULAR = "Circular"
+    RANDOM = "Random"
+    ORIGIN = "Origin"
