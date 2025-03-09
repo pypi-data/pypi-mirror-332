c17a4650a4af8d0cb964bb4eb8a3698266ab711f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 14:05:24 +0000

    Add package example-package-LordDoH/0.0.1/example_package_lorddoh-0.0.1-py3-none-any.whl

---
+++ packages/example-package-LordDoH/example_package_lorddoh-0.0.1-py3-none-any.whl/example_package_LordDoH/stock.py
@@ -0,0 +1,6 @@
+def add_product(product_name, quantity):
+    print(f"Added {quantity} {product_name} to inventory...")
+
+
+def remove_product(product_name, quantity):
+    print(f"Removed {quantity} {product_name} from inventory...")
