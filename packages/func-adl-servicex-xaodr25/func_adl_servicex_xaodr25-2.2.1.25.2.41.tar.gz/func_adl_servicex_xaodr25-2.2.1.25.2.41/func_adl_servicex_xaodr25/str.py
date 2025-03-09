50062bc458da4b42a7581f848cbe29c598fdf1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:30 +0000

    Add package func-adl-servicex-xaodr25/2.2.1.25.2.41/func_adl_servicex_xaodr25-2.2.1.25.2.41.tar.gz

---
+++ packages/func-adl-servicex-xaodr25/func_adl_servicex_xaodr25-2.2.1.25.2.41.tar.gz/func_adl_servicex_xaodr25-2.2.1.25.2.41/func_adl_servicex_xaodr25/str.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+import ast
+from typing import Tuple, TypeVar, Iterable
+from func_adl import ObjectStream, func_adl_callback, func_adl_parameterized_call
+from enum import Enum
+import func_adl_servicex_xaodr25
+
+_method_map = {
+    'empty': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'string',
+        'method_name': 'empty',
+        'return_type': 'bool',
+    },
+    'append': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'string',
+        'method_name': 'append',
+        'return_type': 'string',
+    },
+    'assign': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'string',
+        'method_name': 'assign',
+        'return_type': 'string',
+    },
+    'c_str': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'string',
+        'method_name': 'c_str',
+        'return_type': 'const char *',
+    },
+    'data': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'string',
+        'method_name': 'data',
+        'return_type': 'const char *',
+    },
+    'compare': {
+        'metadata_type': 'add_method_type_info',
+        'type_string': 'string',
+        'method_name': 'compare',
+        'return_type': 'int',
+    },
+}
+
+_enum_map = {      
+}
+
+T = TypeVar('T')
+
+
+def _add_method_metadata(s: ObjectStream[T], a: ast.Call) -> Tuple[ObjectStream[T], ast.Call]:
+    '''Add metadata for a collection to the func_adl stream if we know about it
+    '''
+    assert isinstance(a.func, ast.Attribute)
+    if a.func.attr in _method_map:
+        s_update = s.MetaData(_method_map[a.func.attr])
+
+
+        for md in _enum_map.get(a.func.attr, []):
+            s_update = s_update.MetaData(md)
+        return s_update, a
+    else:
+        return s, a
+
+
+@func_adl_callback(_add_method_metadata)
+class str:
+    "A class"
+
+    class (unnamed)(Enum):
+        _S_local_capacity = 15
+
+
+    def empty(self) -> bool:
+        "A method"
+        ...
+
+    def append(self, __str: str) -> func_adl_servicex_xaodr25.str.str:
+        "A method"
+        ...
+
+    def assign(self, __str: str) -> func_adl_servicex_xaodr25.str.str:
+        "A method"
+        ...
+
+    def c_str(self) -> func_adl_servicex_xaodr25.str.str:
+        "A method"
+        ...
+
+    def data(self) -> func_adl_servicex_xaodr25.str.str:
+        "A method"
+        ...
+
+    def compare(self, __str: str) -> int:
+        "A method"
+        ...
