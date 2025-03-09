36945c3ed377804c8d91e703be1558bc0edf807c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:19 +0000

    Add package consol/0.2.1/consol-0.2.1.tar.gz

---
+++ packages/consol/consol-0.2.1.tar.gz/consol-0.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="consol",
+    version="0.2.1",
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv',
+        'scipy',
+        'pandas',
+        'langchain>=0.3.14',
+        'langchain-openai>=0.3.3',
+        'langchain-ollama>=0.2.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'consol=consol.main:main',
+        ],
+    },
+    author=["Jaeyeon Lee", "Hyun-Hwan Jeong"],
+    author_email=["Jaeyeon.Lee@bcm.edu", "hyun-hwan.jeong@bcm.edu"],
+    description="consol: Confident Solver to use LLM to solve various problems confidently and efficiently with a statistical approach.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LiuzLab/consol",
+    python_requires=">=3.11.0",
+)
