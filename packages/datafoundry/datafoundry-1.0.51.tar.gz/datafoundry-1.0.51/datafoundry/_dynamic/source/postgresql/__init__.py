937937e1758655999cac736ed681ffebed4e4981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:05:01 +0000

    Add package datafoundry/1.0.51/datafoundry-1.0.51.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.51.tar.gz/datafoundry-1.0.51/datafoundry/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,19 @@
+# --- render code block -----
+
+from .hainv4testing import hainv4testing
+from .sourcetest import sourcetest
+from .haitestsource import haitestsource
+from .gameanalyze import gameanalyze
+
+__all__ = [
+    
+    "hainv4testing",
+    
+    "sourcetest",
+    
+    "haitestsource",
+    
+    "gameanalyze"
+    
+]
+# --- render code block -----
