1b763d67311be55863fe9f59ce7b92b2744c29f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:52 +0000

    Add package datafoundry/1.0.45/datafoundry-1.0.45.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.45.tar.gz/datafoundry-1.0.45/datafoundry/_dynamic/dataset/gamemetric_67cd469c75d7d2c3e145214e.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class gamemetric(DatasetBase):
+    """
+    Database: gamemetric
+    Description: Game analyze data warehouse
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
