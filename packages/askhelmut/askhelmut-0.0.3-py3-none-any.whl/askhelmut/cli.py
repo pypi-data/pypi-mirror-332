2e73fa291b150c194f1311171038e7045e71d977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:32 +0000

    Add package askhelmut/0.0.3/askhelmut-0.0.3-py3-none-any.whl

---
+++ packages/askhelmut/askhelmut-0.0.3-py3-none-any.whl/askhelmut/cli.py
@@ -0,0 +1,55 @@
+"""Command line interface of askhelmut."""
+
+from typing import Annotated
+
+import typer
+from rich.console import Console
+
+from askhelmut import Service, __version__
+
+console = Console()
+
+cli = typer.Typer(name="Command Line Interface of askhelmut")
+
+
+@cli.command()
+def echo(
+    text: Annotated[
+        str, typer.Argument(help="The text to echo")
+    ] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    json: Annotated[
+        bool,
+        typer.Option(
+            help=("Print as JSON"),
+        ),
+    ] = False,
+) -> None:
+    """Echo the text."""
+    if json:
+        console.print_json(data={"text": text})
+    else:
+        console.print(text)
+
+
+@cli.command()
+def hello_world() -> None:
+    """Print hello world message and what's in the environment variable THE_VAR."""
+    console.print(Service.get_hello_world())
+
+
+def _apply_cli_settings(cli: typer.Typer, epilog: str) -> None:
+    """Add epilog to all typers in the tree and configure default behavior."""
+    cli.info.epilog = epilog
+    cli.info.no_args_is_help = True
+    for command in cli.registered_commands:
+        command.epilog = cli.info.epilog
+
+
+_apply_cli_settings(
+    cli,
+    f"🤖 askhelmut v{__version__} - built with love in Berlin 🐻",
+)
+
+
+if __name__ == "__main__":
+    cli()
