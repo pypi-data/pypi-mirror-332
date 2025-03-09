09bac0ab18a0c90b9a5ab6bd06598827aa8481c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:59 +0000

    Add package eseas/1.0.0/eseas-1.0.0-py3-none-any.whl

---
+++ packages/eseas/eseas-1.0.0-py3-none-any.whl/eseas/core/_options.py
@@ -0,0 +1,28 @@
+"""
+----------------------------------
+      Global options
+----------------------------------
+"""
+
+"""
+    middle_folder
+    .............
+      -middle_folder : 'test_output'
+      local_w_space / middle_folder / ...
+      # produced csv content will be placed here
+"""
+middle_folder = "test_output"
+"""
+    demetra_command_file_name
+    .............
+       e.g. :  demetra_commands
+"""
+demetra_command_file_name = "demetra_commands"
+"""
+    max_num
+    ...............
+      this number will be maximum number of xml files that can be picked
+      while searching for demetra xml files.
+"""
+
+max_num = 5_000
