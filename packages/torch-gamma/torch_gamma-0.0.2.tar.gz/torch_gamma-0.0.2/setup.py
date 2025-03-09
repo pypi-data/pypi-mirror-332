95a3100c57eecbee0bb8aed60707f957d47f8230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 14:05:06 +0000

    Add package torch-gamma/0.0.2/torch_gamma-0.0.2.tar.gz

---
+++ packages/torch-gamma/torch_gamma-0.0.2.tar.gz/torch_gamma-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torch-gamma',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'numpy',
+        'scikit-learn',
+    ],
+    author='KevinZonda',
+    author_email='realkevin@tutanota.com',
+    description='PyTorch γ is a high level library inspired by Ruby',
+    keywords="pytorch, machine learning",
+    url='https://github.com/KevinZonda/pytorch-gamma'
+)
