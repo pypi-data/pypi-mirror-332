a78eec9c9857de41b094d5237ab459bec258bcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:26 +0000

    Add package ctk-gui-components/0.1.2/ctk_gui_components-0.1.2.tar.gz

---
+++ packages/ctk-gui-components/ctk_gui_components-0.1.2.tar.gz/ctk_gui_components-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ctk-gui-components",  # Unique package name
+    version="0.1.2",    # Package version
+    author="Amit Kshirsagar",
+    author_email="devopsnextgenx@gmail.com",
+    description="A collection of custom GUI components for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/devopsnextgenx/ctk-gui-components",  # Project URL
+    packages=find_packages(),
+    install_requires=[
+        "customtkinter",
+        "tkinter",
+        "ttkbootstrap"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    license="SLA",
+    license_files=["LICENSE"],
+)
