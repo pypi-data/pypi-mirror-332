7f9637692e2455532b70884a9e3c3de71f170633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:22 +0000

    Add package datafoundry/1.0.31/datafoundry-1.0.31.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.31.tar.gz/datafoundry-1.0.31/datafoundry/_dynamic/dataset/hainv4test22121_67cc9020dfc1b464c7c61d41.py
@@ -0,0 +1,55 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class hainv4test22121(DatasetBase):
+    """
+    Database: hainv4test22121
+    Description: None
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class campaign(TableBase):
+        """
+        Database: hainv4test22121
+        Description: abc        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "hainv4test22121"
+
+        @classmethod
+        def get_table_name(cls):
+            return "campaign"
+    
+    class abc222(TableBase):
+        """
+        Database: hainv4test22121
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "hainv4test22121"
+
+        @classmethod
+        def get_table_name(cls):
+            return "abc222"
+    
+    class test22(TableBase):
+        """
+        Database: hainv4test22121
+        Description: 2        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "hainv4test22121"
+
+        @classmethod
+        def get_table_name(cls):
+            return "test22"
+    
+# --- gen code block ---
