ff8ff778512bbc6160f8d719d495740f52f65ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:37 +0000

    Add package project-prompt-generator/0.1.6/project_prompt_generator-0.1.6-py3-none-any.whl

---
+++ packages/project-prompt-generator/project_prompt_generator-0.1.6-py3-none-any.whl/outputs/__init__.py
@@ -0,0 +1,9 @@
+from .output_handler import OutputHandler
+from .single_file_handler import SingleFileOutputHandler
+from .json_handler import JSONOutputHandler
+
+__all__ = [
+    "OutputHandler",
+    "SingleFileOutputHandler",
+    "JSONOutputHandler",
+]
