afdd291671890757094321cae9b5abc312ea01cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:26 +0000

    Add package Abobabus/0.0.2/abobabus-0.0.2.tar.gz

---
+++ packages/Abobabus/abobabus-0.0.2.tar.gz/abobabus-0.0.2/Abobabus.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class RecipeScraper():
+    def __init__(self):
+        self.soup = None
+        self.data_frame = None
+
+    def scrape_url(self, url: str) -> None:
+        responce = requests.get(url)
+        responce.encoding = 'utf-8'
+        self.soup = BeautifulSoup(responce.text, 'html.parser')
+
+    def get_table_data(self) -> pd.DataFrame:
+        tables = self.soup.find_all('table')[1:]
+        headers = self.soup.find_all('h2')
+        data_tables = []
+        for i, table in enumerate(tables):
+            table_rows = table.find_all('tr')[1:]
+            data_tables.append({
+                'table_name': headers[i].text,
+                'table_data': []
+            })
+            for row in table_rows:
+                cells = row.find_all('td')
+                data_tables[i]['table_data'].append({
+                    'name': cells[0].text,
+                    'ingredients': cells[1].text,
+                    'image': cells[2].find('img')['src'],
+                    'description': cells[3].text
+                })
+        self.data_frame = pd.DataFrame(data_tables)
+        return self.data_frame
+
+rs = RecipeScraper()
+rs.scrape_url("https://www.minecraftcrafting.info/")
+print(rs.get_table_data())
