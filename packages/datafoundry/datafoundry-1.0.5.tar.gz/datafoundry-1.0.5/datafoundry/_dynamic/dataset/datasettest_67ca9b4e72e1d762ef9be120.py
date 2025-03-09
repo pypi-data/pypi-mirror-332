76e7f1c33142c67797a33f231b77e2e5f0a670bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:25 +0000

    Add package datafoundry/1.0.5/datafoundry-1.0.5.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.5.tar.gz/datafoundry-1.0.5/datafoundry/_dynamic/dataset/datasettest_67ca9b4e72e1d762ef9be120.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class DatasetTest(DatasetBase):
+    """
+    Database: DatasetTest
+    Description: lorem ipsum abc22
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
---
+++ packages/datafoundry/datafoundry-1.0.5.tar.gz/datafoundry-1.0.5/datafoundry/_dynamic/source/postgresql/sourceNew.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class sourceNew(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67c9718c04f1f10fbe916978"
+# --- render code block -----
