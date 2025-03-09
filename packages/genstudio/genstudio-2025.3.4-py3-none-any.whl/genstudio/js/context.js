eb1987072d53c039930595914324879df0295ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:40 +0000

    Add package genstudio/2025.3.4/genstudio-2025.3.4-py3-none-any.whl

---
+++ packages/genstudio/genstudio-2025.3.4-py3-none-any.whl/genstudio/js/context.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+const {createContext} = React
+
+export const $StateContext = window.$StateContext = (window.$StateContext || createContext());
+export const AUTOGRID_MIN = 165;
+export const CONTAINER_PADDING = 10;
