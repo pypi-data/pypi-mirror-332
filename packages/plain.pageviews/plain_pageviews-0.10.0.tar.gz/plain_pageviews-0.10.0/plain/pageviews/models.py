f10e69a9218d7bd9ad58062f0d1e5172e13974ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 14:05:44 +0000

    Add package plain.pageviews/0.10.0/plain_pageviews-0.10.0.tar.gz

---
+++ packages/plain.pageviews/plain_pageviews-0.10.0.tar.gz/plain_pageviews-0.10.0/plain/pageviews/models.py
@@ -0,0 +1,28 @@
+import uuid
+
+from plain import models
+
+
+@models.register_model
+class Pageview(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
+
+    url = models.URLField(max_length=1024, db_index=True)
+    timestamp = models.DateTimeField(db_index=True, auto_now_add=True)
+
+    title = models.CharField(max_length=512, required=False)
+    referrer = models.URLField(max_length=1024, required=False)
+
+    user_id = models.CharField(max_length=255, db_index=True, required=False)
+    session_key = models.CharField(max_length=255, db_index=True, required=False)
+
+    class Meta:
+        ordering = ["-timestamp"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["uuid"], name="plainpageviews_pageview_unique_uuid"
+            ),
+        ]
+
+    def __str__(self):
+        return self.url
