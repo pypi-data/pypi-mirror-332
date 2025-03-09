40f7d06e4f252d45bb2fdc1980c6a61e72827e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:36 +0000

    Add package sirio/0.1.28/sirio-0.1.28-py3-none-any.whl

---
+++ packages/sirio/sirio-0.1.28-py3-none-any.whl/sirio/event.py
@@ -0,0 +1,37 @@
+import json
+from typing import List
+
+class ObjEvent:
+    taskId = ''
+    taskPriority = ''
+    taskName = ''
+    processId = ''
+    domain = ''
+    businessKey = ''
+    owner = ''
+    internalBusinessKey = ''
+    processVariables=[]
+    def __init__(self):
+        self.files = []
+        self.processVariables = []
+
+def getObjEvent(event) -> List[ObjEvent]: 
+    objReturn = []
+    myList = list(event['Records'])
+    for elem in myList:
+        objEvent = ObjEvent()
+        objMsg = json.loads(elem["body"])
+
+        objEvent.taskId = objMsg["taskId"]
+        objEvent.taskPriority = objMsg["taskPriority"]
+        objEvent.taskName = objMsg["taskName"]
+        objEvent.processId = objMsg["processId"]
+        objEvent.domain = objMsg["domain"]
+        objEvent.businessKey = objMsg["businessKey"]
+        processVariables = objMsg["processVariables"]
+        objEvent.processVariables = processVariables
+        #objEvent.owner = processVariables["owner"]
+        objEvent.internalBusinessKey = processVariables["internalBusinessKey"]
+        objReturn.append(objEvent)
+
+    return objReturn
