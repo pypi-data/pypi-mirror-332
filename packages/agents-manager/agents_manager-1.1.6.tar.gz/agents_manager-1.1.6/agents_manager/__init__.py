7212da188f3180e41ee221575fa76fd173742e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:04:58 +0000

    Add package agents-manager/1.1.6/agents_manager-1.1.6.tar.gz

---
+++ packages/agents-manager/agents_manager-1.1.6.tar.gz/agents_manager-1.1.6/agents_manager/__init__.py
@@ -0,0 +1,6 @@
+
+from agents_manager.Agent import Agent
+from agents_manager.AgentManager import AgentManager
+from agents_manager.Model import Model
+
+__all__ = ["Agent", "AgentManager", "Model"]
