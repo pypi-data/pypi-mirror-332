034fc56f41c472cc56671c4fff5cf6ac1ba2320c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:34 +0000

    Add package tinyvecdb/0.2.2/tinyvecdb-0.2.2.tar.gz

---
+++ packages/tinyvecdb/tinyvecdb-0.2.2.tar.gz/tinyvecdb-0.2.2/src/tinyvec/models.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from .types import VectorInput
+
+
+@dataclass
+class IndexStats:
+    vector_count: int
+    dimensions: int
+
+
+@dataclass
+class SearchResult:
+    similarity: float
+    id: int
+    metadata: dict | None
+
+
+@dataclass
+class Insertion:
+    vector: VectorInput
+    metadata: dict | None
+
+
+@dataclass
+class ClientConfig:
+    dimensions: int | None = None
+
+
+@dataclass
+class SearchOptions:
+    filter: dict | None = None
+
+
+@dataclass
+class DeletionResult:
+    deleted_count: int
+    success: bool
