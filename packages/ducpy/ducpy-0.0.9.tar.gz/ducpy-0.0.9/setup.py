297bd8b9d8c7ad1045ce9d9bdf85da2e0d139493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:06:08 +0000

    Add package ducpy/0.0.9/ducpy-0.0.9.tar.gz

---
+++ packages/ducpy/ducpy-0.0.9.tar.gz/ducpy-0.0.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='ducpy',
+    version='0.0.9',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=['flatbuffers', 'nanoid'],
+    python_requires='>=3.7',
+    include_package_data=True,
+    package_data={'ducpy': ['Duc/*']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
