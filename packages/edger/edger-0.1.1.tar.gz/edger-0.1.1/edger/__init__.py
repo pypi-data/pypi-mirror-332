7c4f38456edc84c404340b90eca5b70ab63df50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:26 +0000

    Add package edger/0.1.1/edger-0.1.1.tar.gz

---
+++ packages/edger/edger-0.1.1.tar.gz/edger-0.1.1/edger/__init__.py
@@ -0,0 +1,14 @@
+"""Edger: Reclaim your browser choice by redirecting Microsoft Edge."""
+
+# Instead of importing main directly, we should import the module
+# This avoids a circular import issue
+import importlib.util
+import sys
+
+__version__ = "0.1.0"
+
+# Don't import directly to avoid circular imports
+def main():
+    """Entry point for the package."""
+    from .edger import main as edger_main
+    edger_main()
