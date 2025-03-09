67f405b9cc8c39ea654bd36f6cb9ebdbdbda5567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:26 +0000

    Add package py-module-injector/0.1.1/py_module_injector-0.1.1-py3-none-any.whl

---
+++ packages/py-module-injector/py_module_injector-0.1.1-py3-none-any.whl/python_module/__init__.py
@@ -0,0 +1,6 @@
+﻿from .module import Module
+from .injector_instance import Injector
+from .inject import Inject
+import python_module.typing
+
+__all__ = ['Module', 'Injector', 'Inject']
---
+++ packages/py-module-injector/py_module_injector-0.1.1-py3-none-any.whl/tests/tests_core.py
@@ -0,0 +1,56 @@
+﻿import unittest
+from python_module import Inject, Module, Injector
+
+class Provider:
+    _log = "Provider Injetado"
+
+    @classmethod
+    def set_log(cls, log):
+        cls._log = log
+        return cls
+
+    def get(self):
+        return self._log
+
+@Inject
+class Service:
+    def __init__(self, provider: Provider):
+        self.provider = provider
+
+    def get_log(self):
+        return self.provider.get()
+
+    def process(self):
+        return 'Service processed'
+
+@Module(
+    instances=[Provider.set_log("Log Alterado")]
+)
+class ModuleTest:
+    def __init__(self):
+        pass
+
+    def run(self):
+        service = Service()
+        return service.process()
+
+class TestDependencyInjection(unittest.TestCase):
+    def test_module_registration(self):
+        module = ModuleTest()
+        self.assertEqual(module.run(), 'Service processed')
+
+    def test_provider_injection(self):
+        provider = Provider()
+        self.assertEqual(provider.get(), "Log Alterado")
+
+    def test_service_injection(self):
+        service = Service()
+        self.assertEqual(service.get_log(), "Log Alterado")
+
+
+    def test_injector_registration(self):
+        ModuleTest()
+        self.assertIsInstance(Injector.get('ModuleTest', 'Provider'), Provider)
+
+if __name__ == '__main__':
+    unittest.main()
