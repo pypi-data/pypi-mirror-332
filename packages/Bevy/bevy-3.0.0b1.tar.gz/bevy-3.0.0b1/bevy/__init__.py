fe4a8a40271146f9b583cc9af6be84f14222069e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 14:05:50 +0000

    Add package Bevy/3.0.0b1/bevy-3.0.0b1.tar.gz

---
+++ packages/Bevy/bevy-3.0.0b1.tar.gz/bevy-3.0.0b1/bevy/__init__.py
@@ -0,0 +1,5 @@
+from bevy.dependencies import dependency
+from bevy.injections import inject
+from bevy.registries import get_registry, Registry
+
+__all__ = ["Registry", "get_registry", "dependency", "inject"]
