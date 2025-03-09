1e72c7eb6958de587e1431cf3d1bcbc8c4489415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 14:05:01 +0000

    Add package EasyCryptographer/2.4.1/EasyCryptographer-2.4.1-py3-none-any.whl

---
+++ packages/EasyCryptographer/EasyCryptographer-2.4.1-py3-none-any.whl/easy_cryptographer/abnormal/error_class.py
@@ -0,0 +1,8 @@
+class SecurityLevelError(Exception):
+    """Throw an exception when the security value is incorrect."""
+    pass
+
+
+class ImageError(Exception):
+    """Throw an exception when the image is incorrect."""
+    pass
