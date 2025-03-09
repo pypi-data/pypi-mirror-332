34a3d57ade780e19330612f0560a1b9afee2f27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:36 +0000

    Add package finmarketpy/0.11.15/finmarketpy-0.11.15.tar.gz

---
+++ packages/finmarketpy/finmarketpy-0.11.15.tar.gz/finmarketpy-0.11.15/install_extra_packages.bat
@@ -0,0 +1,6 @@
+REM this will install all of the dependencies used by chartpy, findatapy and finmarketpy (other than the actual libraries themselves)
+pip install numpy, pandas, pytz, request, multiprocess, pandas_datareader, quandl, redis, openpxyl, blosc, bokeh, plotly, matplotlib
+pip install git+https://github.com/cuemacro/chartpy.git
+pip install git+https://github.com/cuemacro/findatapy.git
+# self reference?
+# pip install git+https://github.com/cuemacro/finmarketpy.git
