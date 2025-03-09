96a9b56be1a058d3af007ec8e905ce1b868e520f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:14 +0000

    Add package causara/1.1.3/causara-1.1.3.tar.gz

---
+++ packages/causara/causara-1.1.3.tar.gz/causara-1.1.3/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="causara",
+    version="1.1.3",
+    author="causara UG",
+    author_email="support@causara.com",
+    description="This package provides several AI-features for building and working with Gurobi optimization models.\n"
+                "The currently supported features are:\n"
+                "   (1) Learning a Gurobi model from historical / synthetic data\n"
+                "   (2) Converting a plain Python function into a Gurobi model\n"
+                "   (3) Post-processing Gurobi results with Python functions\n"
+                "   (4) AI-Interface for end-users of Gurobi models (e.g. no-code modifications using natural language)\n"
+                "   (5) Finetuning a Gurobi Model on real-world data\n",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://www.causara.com",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["*"],
+    },
+    license="Proprietary",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9, <3.13",
+    install_requires=[
+        "torch",
+        "bcrypt",
+        "numpy<2",
+        "scipy",
+        "gurobipy",
+        "pandas",
+        "openpyxl",
+        "reportlab",
+        "psutil",
+        "sympy",
+        "tqdm",
+        "pywebview",
+        "cefpython3",
+        "rdkit",
+        "matplotlib",
+        "folium",
+        "pyshortcuts",
+        "orjson",
+        "pyyaml"
+    ],
+)
