06e99936d928a602ebf70773016f072d321a4196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:05:01 +0000

    Add package datafoundry/1.0.51/datafoundry-1.0.51-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.51-py3-none-any.whl/datafoundry/_dynamic/dataset/gamemetric_67cd469c75d7d2c3e145214e.py
@@ -0,0 +1,42 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class gamemetric(DatasetBase):
+    """
+    Database: gamemetric
+    Description: Game analyze data warehouse
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class catmetrics(TableBase):
+        """
+        Database: gamemetric
+        Description: Cate table        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "gamemetric"
+
+        @classmethod
+        def get_table_name(cls):
+            return "catmetrics"
+    
+    class statustable(TableBase):
+        """
+        Database: gamemetric
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "gamemetric"
+
+        @classmethod
+        def get_table_name(cls):
+            return "statustable"
+    
+# --- gen code block ---
