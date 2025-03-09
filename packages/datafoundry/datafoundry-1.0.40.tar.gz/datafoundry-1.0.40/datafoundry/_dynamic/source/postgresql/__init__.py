514c7b5c739e0dca633504cf33078e5409a14f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:43 +0000

    Add package datafoundry/1.0.40/datafoundry-1.0.40.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.40.tar.gz/datafoundry-1.0.40/datafoundry/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,16 @@
+# --- render code block -----
+
+from .hainv4testing import hainv4testing
+from .sourcetest import sourcetest
+from .haitestsource import haitestsource
+
+__all__ = [
+    
+    "hainv4testing",
+    
+    "sourcetest",
+    
+    "haitestsource"
+    
+]
+# --- render code block -----
