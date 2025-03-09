3521b98bae9d4bc0ab89d342272e2269840fb407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:05:04 +0000

    Add package packify/0.3.0/packify-0.3.0-py3-none-any.whl

---
+++ packages/packify/packify-0.3.0-py3-none-any.whl/packify/errors.py
@@ -0,0 +1,11 @@
+class UsageError(BaseException):
+    """Used with tressa as a replacement for assert and AssertionError."""
+    ...
+
+
+def tressa(condition: bool, error_message: str) -> None:
+    """Raises a UsageError with the given error_message if the condition
+        is False. Replacement for assert statements and AssertionError.
+    """
+    if not condition:
+        raise UsageError(error_message)
