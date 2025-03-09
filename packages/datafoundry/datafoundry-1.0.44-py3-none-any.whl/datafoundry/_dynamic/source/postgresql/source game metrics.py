9eb7076ee706014f5effe8ecc0005b4a47e5a482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:50 +0000

    Add package datafoundry/1.0.44/datafoundry-1.0.44-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.44-py3-none-any.whl/datafoundry/_dynamic/source/postgresql/source game metrics.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class source game metrics(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67cd463675d7d2c3e145214d"
+# --- render code block -----
