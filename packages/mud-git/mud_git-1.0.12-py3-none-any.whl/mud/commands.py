c4e64cccb93cd4596a52edde2f3da58f04e2d14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:06:07 +0000

    Add package mud-git/1.0.12/mud_git-1.0.12-py3-none-any.whl

---
+++ packages/mud-git/mud_git-1.0.12-py3-none-any.whl/mud/commands.py
@@ -0,0 +1,28 @@
+# Commands
+ADD = ['add', 'a']
+REMOVE = ['remove', 'rm']
+LOG = ['log', 'l']
+INFO = ['info', 'i']
+INIT = ['init']
+TAGS = ['tags', 'tag', 't']
+LABELS = ['labels', 'lb']
+STATUS = ['status', 'st']
+BRANCHES = ['branch', 'branches', 'br']
+REMOTE_BRANCHES = ['remote-branch', 'remote-branches', 'rbr']
+HELP = ['help', '--help', '-h']
+CONFIGURE = ['configure', 'config']
+GET_CONFIG = ['get-config']
+SET_GLOBAL = ['set-global']
+
+COMMANDS = [ADD, REMOVE, LOG, INFO, INIT, TAGS, LABELS, STATUS, BRANCHES, REMOTE_BRANCHES, HELP, CONFIGURE, SET_GLOBAL, GET_CONFIG]
+
+# Filters
+ASYNC_ATTR = '-a', '--async'
+TABLE_ATTR = '-t', '--table'
+MODIFIED_ATTR = '-m', '--modified'
+DIVERGED_ATTR = '-d', '--diverged'
+LABEL_PREFIX = '-l=', '--label='
+COMMAND_ATTR = '-c=', '--command='
+BRANCH_PREFIX = '-b=', '--branch='
+NOT_LABEL_PREFIX = '-nl=', '--not-label='
+NOT_BRANCH_PREFIX = '-nb=', '--not-branch='
