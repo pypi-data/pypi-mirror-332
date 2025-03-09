c801daf0f1ade9ca99d31cc0953595410d6612bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:22 +0000

    Add package geodesicpandas/0.1.1/geodesicpandas-0.1.1.tar.gz

---
+++ packages/geodesicpandas/geodesicpandas-0.1.1.tar.gz/geodesicpandas-0.1.1/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Installation script."""
+
+from setuptools import setup
+
+# see pyproject.toml for static project metadata
+setup(
+    name="geodesicpandas",
+    version="0.1.1",
+)
