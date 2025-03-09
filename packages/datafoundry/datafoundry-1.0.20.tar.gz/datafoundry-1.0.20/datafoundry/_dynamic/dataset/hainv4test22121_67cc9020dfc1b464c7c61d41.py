716e697d87c1dd8cf0f6eff0b304e15c258f6561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:21 +0000

    Add package datafoundry/1.0.20/datafoundry-1.0.20.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.20.tar.gz/datafoundry-1.0.20/datafoundry/_dynamic/dataset/hainv4test22121_67cc9020dfc1b464c7c61d41.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class hainv4test22121(DatasetBase):
+    """
+    Database: hainv4test22121
+    Description: None
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
