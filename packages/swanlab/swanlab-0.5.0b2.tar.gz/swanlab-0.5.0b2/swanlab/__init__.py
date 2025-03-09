3e593a41d9a7d64edbbfe3b8638f3d46b9f32de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:44 +0000

    Add package swanlab/0.5.0b2/swanlab-0.5.0b2.tar.gz

---
+++ packages/swanlab/swanlab-0.5.0b2.tar.gz/swanlab-0.5.0b2/swanlab/__init__.py
@@ -0,0 +1,43 @@
+# 导出初始化函数和log函数
+from .data import (
+    login,
+    init,
+    log,
+    finish,
+    Audio,
+    Image,
+    Text,
+    Run,
+    State,
+    get_run,
+    get_config,
+    config,
+    get_url,
+    get_project_url,
+)
+from .env import SwanLabEnv
+from .package import get_package_version
+from .sync import sync_wandb, sync_tensorboardX, sync_tensorboard_torch
+
+# 设置默认环境变量
+SwanLabEnv.set_default()
+# 检查当前需要检查的环境变量
+SwanLabEnv.check()
+
+__version__ = get_package_version()
+
+__all__ = [
+    "login",
+    "init",
+    "log",
+    "finish",
+    "Audio",
+    "Image",
+    "Text",
+    "Run",
+    "State",
+    "get_run",
+    "get_config",
+    "config",
+    "__version__",
+]
