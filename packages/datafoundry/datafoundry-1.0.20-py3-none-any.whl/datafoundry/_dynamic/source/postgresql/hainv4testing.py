c02b20ce35f316af784f3f07329038d5922033b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 04:05:21 +0000

    Add package datafoundry/1.0.20/datafoundry-1.0.20-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.20-py3-none-any.whl/datafoundry/_dynamic/source/postgresql/hainv4testing.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class hainv4testing(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67cc6deddfc1b464c7c61d3d"
+# --- render code block -----
