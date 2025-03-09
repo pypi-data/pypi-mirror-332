b84dd35401f1de7b4d7a00577f1601a2795bc862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 15:04:53 +0000

    Add package kbec-plugin/0.4/kbec_plugin-0.4.tar.gz

---
+++ packages/kbec-plugin/kbec_plugin-0.4.tar.gz/kbec_plugin-0.4/kbec_plugin/__init__.py
@@ -0,0 +1,2 @@
+from .kbec_plugin import KBecPlugin
+from .kbec_plugin_external import KBecPluginExternal
---
+++ packages/kbec-plugin/kbec_plugin-0.4.tar.gz/kbec_plugin-0.4/kbec_plugin/kbec_plugin_external.py
@@ -0,0 +1,6 @@
+from .kbec_plugin import KbecPlugin
+
+class KbecPluginExternal(KbecPlugin):
+    def __init__(self, config_path: str, kbec_client: object, ftp_manager: object):
+        super().__init__(config_path, kbec_client)
+        self._ftp_manager = ftp_manager
