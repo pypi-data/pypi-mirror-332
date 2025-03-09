e9c8e65869100b65e052122718a540a00e78a57d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:39 +0000

    Add package pyopenhardwaremonitor/0.2.4/pyopenhardwaremonitor-0.2.4.tar.gz

---
+++ packages/pyopenhardwaremonitor/pyopenhardwaremonitor-0.2.4.tar.gz/pyopenhardwaremonitor-0.2.4/setup.py
@@ -0,0 +1,34 @@
+import pkg_resources
+from setuptools import setup
+from pathlib import Path
+
+with Path("requirements.txt").open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+consts = {}
+exec((Path("pyopenhardwaremonitor") / "const.py").read_text(encoding="utf-8"), consts)  # noqa: S102
+
+setup(
+    name="pyopenhardwaremonitor",
+    packages=["pyopenhardwaremonitor"],
+    install_requires=install_requires,
+    version=consts["__version__"],
+    description="A python3 library to communicate with an Open Hardware Monitor remote server",
+    python_requires=">=3.11.0",
+    author="Peter Åslund",
+    author_email="peter@peteraslund.me",
+    url="https://github.com/lazytarget/pyOpenHardwareMonitor",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
