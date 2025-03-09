227f2783ff33b7ed7c0e9ed8ee9fdefc0457ae81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:37 +0000

    Add package ScriptsOfTribute/1.0.0/scriptsoftribute-1.0.0-py3-none-any.whl

---
+++ packages/ScriptsOfTribute/scriptsoftribute-1.0.0-py3-none-any.whl/ScriptsOfTribute/base_ai.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from ScriptsOfTribute.board import GameState
+from ScriptsOfTribute.enums import PatronId
+from ScriptsOfTribute.move import BasicMove
+
+class BaseAI:
+    def __init__(self, bot_name):
+        self.bot_name = bot_name
+
+    def pregame_prepare(self):
+        pass
+
+    def select_patron(self, available_patrons: List[PatronId]):
+        raise NotImplementedError
+
+    def play(self, game_state: GameState, possible_moves: List[BasicMove], remaining_time: int) -> BasicMove:
+        raise NotImplementedError
+
+    def game_end(self, final_state):
+        pass
