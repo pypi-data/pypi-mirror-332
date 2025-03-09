8818d1ea450f72e9ea7912c275367f52a43afa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 03:04:55 +0000

    Add package simplemaths/0.0.5/simplemaths-0.0.5.tar.gz

---
+++ packages/simplemaths/simplemaths-0.0.5.tar.gz/simplemaths-0.0.5/README.md
@@ -0,0 +1,1 @@
+<h1>simplemaths</h1> An example Python project to build a wheel distribution and upload to PyPI
---
+++ packages/simplemaths/simplemaths-0.0.5.tar.gz/simplemaths-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simplemaths",
+    version="0.0.5",
+    author="Babloo Kumar",
+    long_description="<h1>simplemaths</h1> An example Python project to build a wheel distribution and upload to PyPI",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[]
+)
