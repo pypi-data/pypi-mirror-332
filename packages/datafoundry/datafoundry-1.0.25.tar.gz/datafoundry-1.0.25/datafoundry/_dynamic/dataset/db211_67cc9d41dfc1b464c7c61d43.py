3bb1bf2bd390264ab64bac14bce446b50131dede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-09 05:04:38 +0000

    Add package datafoundry/1.0.25/datafoundry-1.0.25.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.25.tar.gz/datafoundry-1.0.25/datafoundry/_dynamic/dataset/db211_67cc9d41dfc1b464c7c61d43.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class db211(DatasetBase):
+    """
+    Database: db211
+    Description: test
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
---
+++ packages/datafoundry/datafoundry-1.0.25.tar.gz/datafoundry-1.0.25/datafoundry/ontology/datasource/postgresql.py
@@ -0,0 +1,20 @@
+import datafoundry._dynamic.source.postgresql as db
+
+## ----render code block----
+
+class hainv4testing(db.hainv4testing):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+class sourcetest(db.sourcetest):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+class haitestsource(db.haitestsource):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+## ----render code block----
