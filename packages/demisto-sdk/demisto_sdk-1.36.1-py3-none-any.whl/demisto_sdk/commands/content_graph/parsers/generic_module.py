8b8128c9d37d4fbf0d8d8af5c21811225322b6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:06:29 +0000

    Add package demisto-sdk/1.36.1/demisto_sdk-1.36.1-py3-none-any.whl

---
+++ packages/demisto-sdk/demisto_sdk-1.36.1-py3-none-any.whl/demisto_sdk/commands/content_graph/parsers/generic_module.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import List, Optional, Set
+
+from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.content_graph.common import ContentType
+from demisto_sdk.commands.content_graph.parsers.json_content_item import (
+    JSONContentItemParser,
+)
+from demisto_sdk.commands.content_graph.strict_objects.generic_module import (
+    StrictGenericModule,
+)
+
+
+class GenericModuleParser(
+    JSONContentItemParser, content_type=ContentType.GENERIC_MODULE
+):
+    def __init__(
+        self,
+        path: Path,
+        pack_marketplaces: List[MarketplaceVersions],
+        git_sha: Optional[str] = None,
+    ) -> None:
+        super().__init__(path, pack_marketplaces, git_sha=git_sha)
+        self.definition_ids = self.json_data.get("definitionIds")
+
+    @property
+    def supported_marketplaces(self) -> Set[MarketplaceVersions]:
+        return {
+            MarketplaceVersions.XSOAR,
+            MarketplaceVersions.XSOAR_SAAS,
+            MarketplaceVersions.XSOAR_ON_PREM,
+        }
+
+    @property
+    def strict_object(self):
+        return StrictGenericModule
---
+++ packages/demisto-sdk/demisto_sdk-1.36.1-py3-none-any.whl/demisto_sdk/commands/content_graph/parsers/list.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import List, Optional, Set
+
+from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.content_graph.common import ContentType
+from demisto_sdk.commands.content_graph.parsers.json_content_item import (
+    JSONContentItemParser,
+)
+from demisto_sdk.commands.content_graph.strict_objects.list import StrictList
+
+
+class ListParser(JSONContentItemParser, content_type=ContentType.LIST):
+    def __init__(
+        self,
+        path: Path,
+        pack_marketplaces: List[MarketplaceVersions],
+        git_sha: Optional[str] = None,
+    ) -> None:
+        super().__init__(path, pack_marketplaces, git_sha=git_sha)
+        self.type = self.json_data.get("type")
+
+    @property
+    def supported_marketplaces(self) -> Set[MarketplaceVersions]:
+        return set(MarketplaceVersions)
+
+    @property
+    def is_unified(self) -> bool:
+        return bool((data := self.json_data.get("data")) and data != "-")
+
+    @property
+    def strict_object(self):
+        return StrictList
