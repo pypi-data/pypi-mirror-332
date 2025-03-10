c4f358cce8230880ed91d0d8a2fa340146fdf341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:05:09 +0000

    Add package datafoundry/1.0.69/datafoundry-1.0.69.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.69.tar.gz/datafoundry-1.0.69/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,18 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .gamemetric_67cd469c75d7d2c3e145214e import gamemetric
+
+from .mktvng2025_67cd75faf87b225450e953a2 import mktvng2025
+
+from .newwwaa212_67cda4def87b225450e953a9 import newwwaa212
+
+
+__all__ = [
+    
+    "gamemetric",
+    
+    "mktvng2025",
+    
+    "newwwaa212"
+    
+]
---
+++ packages/datafoundry/datafoundry-1.0.69.tar.gz/datafoundry-1.0.69/datafoundry/_dynamic/dataset/newwwaa212_67cda4def87b225450e953a9.py
@@ -0,0 +1,29 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class newwwaa212(DatasetBase):
+    """
+    Database: newwwaa212
+    Description: abc
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class table43(TableBase):
+        """
+        Database: newwwaa212
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "newwwaa212"
+
+        @classmethod
+        def get_table_name(cls):
+            return "table43"
+    
+# --- gen code block ---
