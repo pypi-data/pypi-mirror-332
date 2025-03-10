34cbda896b6baabe067d7530fca858e7c9e08d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:05:11 +0000

    Add package datafoundry/1.0.64/datafoundry-1.0.64-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.64-py3-none-any.whl/datafoundry/_dynamic/dataset/gamemetric_67cd469c75d7d2c3e145214e.py
@@ -0,0 +1,55 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class gamemetric(DatasetBase):
+    """
+    Database: gamemetric
+    Description: Game analyze data warehouse
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class catmetrics(TableBase):
+        """
+        Database: gamemetric
+        Description: Cate table abc        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "gamemetric"
+
+        @classmethod
+        def get_table_name(cls):
+            return "catmetrics"
+    
+    class statustable(TableBase):
+        """
+        Database: gamemetric
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "gamemetric"
+
+        @classmethod
+        def get_table_name(cls):
+            return "statustable"
+    
+    class gamedataw(TableBase):
+        """
+        Database: gamemetric
+        Description: Game data        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "gamemetric"
+
+        @classmethod
+        def get_table_name(cls):
+            return "gamedataw"
+    
+# --- gen code block ---
---
+++ packages/datafoundry/datafoundry-1.0.64-py3-none-any.whl/datafoundry/ontology/datasource/postgresql.py
@@ -0,0 +1,30 @@
+import datafoundry._dynamic.source.postgresql as db
+
+## ----render code block----
+
+class hainv4testing(db.hainv4testing):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+class sourcetest(db.sourcetest):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+class haitestsource(db.haitestsource):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+class gameanalyze(db.gameanalyze):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+class hainv4testsource(db.hainv4testsource):
+    # TODO: cron generate DB description
+    def __init__(self) -> None:
+        pass
+
+## ----render code block----
