6cf89fa0ec1ba1aaafbeb9ce302f36861cbebe09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:56 +0000

    Add package datafoundry/1.0.89/datafoundry-1.0.89.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.89.tar.gz/datafoundry-1.0.89/datafoundry/_dynamic/dataset/ird_db_v1_67cdccf416e2eaa728b1882e.py
@@ -0,0 +1,29 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class ird_db_v1(DatasetBase):
+    """
+    Database: ird_db_v1
+    Description: abc
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class campaign_db_2(TableBase):
+        """
+        Database: ird_db_v1
+        Description: desc        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "ird_db_v1"
+
+        @classmethod
+        def get_table_name(cls):
+            return "campaign_db_2"
+    
+# --- gen code block ---
