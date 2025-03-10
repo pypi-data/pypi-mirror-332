1501ec605cf8a3d0e2cbbdf95f7fdcd530e07506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:47 +0000

    Add package flightdeck/0.1.3/flightdeck-0.1.3-py3-none-any.whl

---
+++ packages/flightdeck/flightdeck-0.1.3-py3-none-any.whl/flightdeck/files/handler.py
@@ -0,0 +1,16 @@
+import pathlib
+import flightdeck.files.picture as picture
+import flightdeck.files.markdown as markdown
+
+def read_file(file):
+    print(f"Reading file: {file}")
+    extension = pathlib.Path(file).suffix.lower()
+    print("Extension:", extension)
+
+    image = [".jpg", ".jpeg", ".png"]
+    
+    if extension in image:
+        picture.print_image(file)
+    elif extension == ".md":
+        markdown.print_markdown_fancy(file)
+
