a8e682afb874a807bd8eba00b6cdf89b8607b266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:27 +0000

    Add package datafoundry/1.0.96/datafoundry-1.0.96-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.96-py3-none-any.whl/datafoundry/_dynamic/dataset/mktvng2024_67cddc49c02ec2e05423809d.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class mktvng2024(DatasetBase):
+    """
+    Database: mktvng2024
+    Description: mkt vng 2024
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
