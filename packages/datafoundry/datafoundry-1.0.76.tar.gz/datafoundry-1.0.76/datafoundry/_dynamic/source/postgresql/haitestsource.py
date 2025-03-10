ff12f5cc208afec16db15b8686a3ebfca49200dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:57 +0000

    Add package datafoundry/1.0.76/datafoundry-1.0.76.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.76.tar.gz/datafoundry-1.0.76/datafoundry/_dynamic/source/postgresql/haitestsource.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class haitestsource(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67cc9c9ddfc1b464c7c61d42"
+# --- render code block -----
