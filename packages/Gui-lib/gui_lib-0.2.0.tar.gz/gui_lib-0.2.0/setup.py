ec1fc24ac03553662450a03ad8fe6fe7aa5e88fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:44 +0000

    Add package Gui-lib/0.2.0/gui_lib-0.2.0.tar.gz

---
+++ packages/Gui-lib/gui_lib-0.2.0.tar.gz/gui_lib-0.2.0/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="Gui_lib",
+    version="0.2.0",
+    packages=find_packages(),
+    description="A simple GUI library similar to tkinter",
+    author="OUBStudios",
+    author_email="oubdocs.main@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
