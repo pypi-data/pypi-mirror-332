35d083fcbbd9e11aaea8e93b06023eaca037882b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:29 +0000

    Add package datafoundry/1.0.98/datafoundry-1.0.98-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.98-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,18 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .infrastructure_db_67cdcc9916e2eaa728b1882d import infrastructure_db
+
+from .test_edit_db_67cdcf0716e2eaa728b1882f import test_edit_db
+
+from .mktvng2024_67cddc49c02ec2e05423809d import mktvng2024
+
+
+__all__ = [
+    
+    "infrastructure_db",
+    
+    "test_edit_db",
+    
+    "mktvng2024"
+    
+]
