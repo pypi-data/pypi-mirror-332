449e84a4cb87ec52c256c4749c7e8c30fd27c15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:46 +0000

    Add package datafoundry/1.0.58/datafoundry-1.0.58-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.58-py3-none-any.whl/datafoundry/_dynamic/dataset/mktvng2025_67cd75faf87b225450e953a2.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class mktvng2025(DatasetBase):
+    """
+    Database: mktvng2025
+    Description: mkt vng 2025
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
