35f29fd89a6d32f748152d5a719552f811310629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:49 +0000

    Add package fastcrawl/0.5.0/fastcrawl-0.5.0-py3-none-any.whl

---
+++ packages/fastcrawl/fastcrawl-0.5.0-py3-none-any.whl/fastcrawl/models/__init__.py
@@ -0,0 +1,6 @@
+from .crawler_settings import CrawlerSettings
+from .crawler_stats import CrawlerStats
+from .http_client_settings import HttpClientSettings
+from .log_settings import LogSettings
+from .request import Request
+from .response import Response
