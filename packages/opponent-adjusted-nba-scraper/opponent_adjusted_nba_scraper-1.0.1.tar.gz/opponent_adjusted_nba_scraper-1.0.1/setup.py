a3b1483a5a3232e6ed50776cb9c6bbea00c39183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 05:06:23 +0000

    Add package opponent-adjusted-nba-scraper/1.0.1/opponent_adjusted_nba_scraper-1.0.1.tar.gz

---
+++ packages/opponent-adjusted-nba-scraper/opponent_adjusted_nba_scraper-1.0.1.tar.gz/opponent_adjusted_nba_scraper-1.0.1/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="opponent_adjusted_nba_scraper",
+    version="1.0.1",
+    author="Oscar Gutierrez Altamirano",
+    author_email="oscargutierreza617@gmail.com",
+    description="A Python client for scraping data from stats.nba.com and opponent-adjusting players' stats.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+
+    install_requires=[
+    "pandas==2.1.0",
+    "requests==2.31.0",
+    "six==1.16.0",
+    "Unidecode==1.3.8"
+    ],
+ 
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    keywords=[
+        "nba",
+        "nba.com",
+        "stats.nba.com",
+        "sports",
+        "basketball",
+        ],
+)
