bb9d4acdacb0a196ea78474fd11bdc50e0404048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 05:06:16 +0000

    Add package datafoundry/1.0.107/datafoundry-1.0.107-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.107-py3-none-any.whl/datafoundry/_dynamic/dataset/TestEditDb_67cdcf0716e2eaa728b1882f.py
@@ -0,0 +1,42 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class TestEditDb(DatasetBase):
+    """
+    Database: TestEditDb
+    Description: None
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class TestEditTable(TableBase):
+        """
+        Database: TestEditDb
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "testeditdb"
+
+        @classmethod
+        def get_table_name(cls):
+            return "testedittable"
+    
+    class Tabletest(TableBase):
+        """
+        Database: TestEditDb
+        Description: abc        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "testeditdb"
+
+        @classmethod
+        def get_table_name(cls):
+            return "tabletest"
+    
+# --- gen code block ---
