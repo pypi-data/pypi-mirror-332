eb424b842e0577c38ab8ef91fa70d37ab5fbe415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:44 +0000

    Add package xpander-utils/0.0.14/xpander_utils-0.0.14-py3-none-any.whl

---
+++ packages/xpander-utils/xpander_utils-0.0.14-py3-none-any.whl/xpander_utils/generic.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generate_tool_call_id() -> str:
+    """
+    Generate a unique tool call ID.
+
+    The ID consists of a fixed prefix "call_" followed by a randomly 
+    generated alphanumeric string of length 24.
+
+    Returns:
+        str: A unique tool call ID in the format "call_<random_string>".
+    """
+    prefix = "call_"
+    characters = string.ascii_letters + string.digits
+    length = 24
+    random_string = ''.join(random.choices(characters, k=length))
+    
+    return prefix + random_string
