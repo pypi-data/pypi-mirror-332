6564ffe9ea6df42d4e3615b5d2d8380fa537162d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:42 +0000

    Add package datavns/0.1.2/datavns-0.1.2-py3-none-any.whl

---
+++ packages/datavns/datavns-0.1.2-py3-none-any.whl/datavns/headers.py
@@ -0,0 +1,27 @@
+
+
+class headers_list:
+    cafef_header =  {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
+    }
+
+
+
+    fireant_header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        'Origin':'https://fireant.vn',
+        'Referer':'https://fireant.vn/',
+        'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSIsImtpZCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4iLCJhdWQiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4vcmVzb3VyY2VzIiwiZXhwIjoxODg5NjIyNTMwLCJuYmYiOjE1ODk2MjI1MzAsImNsaWVudF9pZCI6ImZpcmVhbnQudHJhZGVzdGF0aW9uIiwic2NvcGUiOlsiYWNhZGVteS1yZWFkIiwiYWNhZGVteS13cml0ZSIsImFjY291bnRzLXJlYWQiLCJhY2NvdW50cy13cml0ZSIsImJsb2ctcmVhZCIsImNvbXBhbmllcy1yZWFkIiwiZmluYW5jZS1yZWFkIiwiaW5kaXZpZHVhbHMtcmVhZCIsImludmVzdG9wZWRpYS1yZWFkIiwib3JkZXJzLXJlYWQiLCJvcmRlcnMtd3JpdGUiLCJwb3N0cy1yZWFkIiwicG9zdHMtd3JpdGUiLCJzZWFyY2giLCJzeW1ib2xzLXJlYWQiLCJ1c2VyLWRhdGEtcmVhZCIsInVzZXItZGF0YS13cml0ZSIsInVzZXJzLXJlYWQiXSwianRpIjoiMjYxYTZhYWQ2MTQ5Njk1ZmJiYzcwODM5MjM0Njc1NWQifQ.dA5-HVzWv-BRfEiAd24uNBiBxASO-PAyWeWESovZm_hj4aXMAZA1-bWNZeXt88dqogo18AwpDQ-h6gefLPdZSFrG5umC1dVWaeYvUnGm62g4XS29fj6p01dhKNNqrsu5KrhnhdnKYVv9VdmbmqDfWR8wDgglk5cJFqalzq6dJWJInFQEPmUs9BW_Zs8tQDn-i5r4tYq2U8vCdqptXoM7YgPllXaPVDeccC9QNu2Xlp9WUvoROzoQXg25lFub1IYkTrM66gJ6t9fJRZToewCt495WNEOQFa_rwLCZ1QwzvL0iYkONHS_jZ0BOhBCdW9dWSawD6iF1SIQaFROvMDH1rg'
+    }
+
+
+    fmarket_headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJidWl0cnVvbmdhbmFkQGdtYWlsLmNvbSIsImF1ZGllbmNlIjoid2ViIiwiY3JlYXRlZCI6MTcxNzg1MTQyODE5NCwidXNlcnR5cGUiOiJJTlZFU1RPUiIsImV4cCI6MTcxNzg1ODYyOCwic3ViZm9hY2NvdW50IjpudWxsfQ.4KpCJ_gPoUBM9CFKMhcuM4oOLToztRoErE5h0u9ymAzdtrWSNTK-JE_yovEo7mc7Q675tpePk7u_SxcVQeuHSQ",
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0"
+    }
+
+    cbonds_headers = {
+        "CP-TOKEN": "CfDJ8PdG7etAbaxFiQmMzHBRJ_2sVlLjgLxmoDfJYkf4DjW5bvnDJ0jm-UsdJfuS_hLAbVTiQZ6psIzlBXVnVRyOdnpSEnchf4U3itTQaXkg_SWahIllAkNVKSTpv8oCL2MHaaU5QdRL5O4XLxBstm040Mw",
+        "Cookie": "CP-TOKEN-COOKIE=CfDJ8PdG7etAbaxFiQmMzHBRJ_28XYX8SzsfoJ67GJY4CpOXn0po4J_zwHwacvfZGnL6YMsmSjVkPXSeTwdIvfm4leybvQZlFf_PArnsx2G_XyFPV2-xh29T4u6MvCspQOkBPe96tjg9GSN9O7JX4AoO5sQ",
+    }
