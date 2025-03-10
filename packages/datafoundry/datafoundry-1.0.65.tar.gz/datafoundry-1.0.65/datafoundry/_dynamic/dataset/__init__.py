e4b0afa9efb4c709a8e2d1f93de43c289bcd594f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:05:10 +0000

    Add package datafoundry/1.0.65/datafoundry-1.0.65.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.65.tar.gz/datafoundry-1.0.65/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,26 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .datasettest_67ca9b4e72e1d762ef9be120 import DatasetTest
+
+from .gamemetric_67cd469c75d7d2c3e145214e import gamemetric
+
+from .mktvng2025_67cd75faf87b225450e953a2 import mktvng2025
+
+from .hainv4test22121_67cd9cc3f87b225450e953a5 import hainv4test22121
+
+from .hainv4db1999_67cd9ffcf87b225450e953a7 import hainv4db1999
+
+
+__all__ = [
+    
+    "DatasetTest",
+    
+    "gamemetric",
+    
+    "mktvng2025",
+    
+    "hainv4test22121",
+    
+    "hainv4db1999"
+    
+]
---
+++ packages/datafoundry/datafoundry-1.0.65.tar.gz/datafoundry-1.0.65/datafoundry/_dynamic/dataset/hainv4db1999_67cd9ffcf87b225450e953a7.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class hainv4db1999(DatasetBase):
+    """
+    Database: hainv4db1999
+    Description: abc
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
