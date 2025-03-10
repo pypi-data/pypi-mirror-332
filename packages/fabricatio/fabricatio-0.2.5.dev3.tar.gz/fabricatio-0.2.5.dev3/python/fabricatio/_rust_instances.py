0d49feff0551892f0e8b071fa8ec0c9d541e8812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:45 +0000

    Add package fabricatio/0.2.5.dev3/fabricatio-0.2.5.dev3.tar.gz

---
+++ packages/fabricatio/fabricatio-0.2.5.dev3.tar.gz/fabricatio-0.2.5.dev3/python/fabricatio/_rust_instances.py
@@ -0,0 +1,10 @@
+"""Some necessary instances."""
+
+from fabricatio._rust import TemplateManager
+from fabricatio.config import configs
+
+template_manager = TemplateManager(
+    template_dirs=configs.templates.template_dir,
+    suffix=configs.templates.template_suffix,
+    active_loading=configs.templates.active_loading,
+)
