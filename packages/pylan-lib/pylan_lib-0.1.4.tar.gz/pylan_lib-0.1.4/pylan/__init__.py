7b36b3091bb99ba890beff8c25af6c9095da0521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:37 +0000

    Add package pylan-lib/0.1.4/pylan_lib-0.1.4.tar.gz

---
+++ packages/pylan-lib/pylan_lib-0.1.4.tar.gz/pylan_lib-0.1.4/pylan/__init__.py
@@ -0,0 +1,5 @@
+from pylan.item import *  # noqa: F403
+from pylan.patterns.add import *  # noqa: F403
+from pylan.patterns.divide import *  # noqa: F403
+from pylan.patterns.multiply import *  # noqa: F403
+from pylan.patterns.subtract import *  # noqa: F403
---
+++ packages/pylan-lib/pylan_lib-0.1.4.tar.gz/pylan_lib-0.1.4/setup.py
@@ -0,0 +1,37 @@
+"""Python setup.py for pylan package"""
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
+
+setup(
+    name="pylan-lib",
+    version=os.environ["VERSION"],
+    description="Python library for time based planning.",
+    url="https://github.com/TimoKats/pylan",
+    long_description=read("misc/docs.md"),
+    long_description_content_type="text/markdown",
+    author="Timo Kats",
+    packages=find_packages(exclude=[".github"]),
+    install_requires=read_requirements("requirements.txt"),
+)
