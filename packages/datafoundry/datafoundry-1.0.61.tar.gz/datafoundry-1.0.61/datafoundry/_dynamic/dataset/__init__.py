ca5a0f014e512508ce20a09258fa584c6488c68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:50 +0000

    Add package datafoundry/1.0.61/datafoundry-1.0.61.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.61.tar.gz/datafoundry-1.0.61/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,18 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .datasettest_67ca9b4e72e1d762ef9be120 import DatasetTest
+
+from .gamemetric_67cd469c75d7d2c3e145214e import gamemetric
+
+from .mktvng2025_67cd75faf87b225450e953a2 import mktvng2025
+
+
+__all__ = [
+    
+    "DatasetTest",
+    
+    "gamemetric",
+    
+    "mktvng2025"
+    
+]
