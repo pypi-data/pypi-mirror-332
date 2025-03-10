765138984f92c1285fc2d36f5fb44122d484c286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:26 +0000

    Add package datafoundry/1.0.93/datafoundry-1.0.93-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.93-py3-none-any.whl/datafoundry/_dynamic/dataset/haitestdataset_67cddadac02ec2e05423809b.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class haitestdataset(DatasetBase):
+    """
+    Database: haitestdataset
+    Description: abc
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
