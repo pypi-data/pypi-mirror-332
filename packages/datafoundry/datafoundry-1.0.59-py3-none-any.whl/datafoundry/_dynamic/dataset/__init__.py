8e4c4de0a26c52342ea9f08ed9b3828e06e72d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:46 +0000

    Add package datafoundry/1.0.59/datafoundry-1.0.59-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.59-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,22 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .datasettest_67ca9b4e72e1d762ef9be120 import DatasetTest
+
+from .hainv4test22121_67cc9020dfc1b464c7c61d41 import hainv4test22121
+
+from .gamemetric_67cd469c75d7d2c3e145214e import gamemetric
+
+from .mktvng2025_67cd75faf87b225450e953a2 import mktvng2025
+
+
+__all__ = [
+    
+    "DatasetTest",
+    
+    "hainv4test22121",
+    
+    "gamemetric",
+    
+    "mktvng2025"
+    
+]
