d4123525ce5d9fae38e57407c49a54b6215a7360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:56 +0000

    Add package datafoundry/1.0.75/datafoundry-1.0.75-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.75-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,14 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .gamemetrics1_67cda656f87b225450e953aa import gamemetrics1
+
+from .vnghainv4_67cdab53f87b225450e953ad import vnghainv4
+
+
+__all__ = [
+    
+    "gamemetrics1",
+    
+    "vnghainv4"
+    
+]
