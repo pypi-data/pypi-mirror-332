f6c51203af85fff45213a391e9a7fc96881cde4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:42 +0000

    Add package revo-snd/0.1.0/revo_snd-0.1.0-cp312-cp312-musllinux_1_2_i686.whl

---
+++ packages/revo-snd/revo_snd-0.1.0-cp312-cp312-musllinux_1_2_i686.whl/revo_snd-0.1.0.data/data/revo_snd/engine/OS_Sound.py
@@ -0,0 +1,13 @@
+import revo_snd.engine._snd.OS_SeqPlayer as _OS_seq
+
+WII_PHYSICAL_CHN_NUM    = 96
+WII_MIN_CHN             = 0
+WII_MAX_CHN             = WII_MIN_CHN + WII_PHYSICAL_CHN_NUM
+
+STRM_SND_HANDLE_CHN_MAX = 32
+STRM_FILE_MAX_CHN       = STRM_SND_HANDLE_CHN_MAX >> 2  # => 16
+
+SEQ_BASE_NOTE = _OS_seq.SEQ_BASE_NOTE
+
+OS_PlaySeq  = _OS_seq.OS_SeqHandle_Play
+OS_PlaySeq2 = _OS_seq.OS_SeqHandle_Play2
