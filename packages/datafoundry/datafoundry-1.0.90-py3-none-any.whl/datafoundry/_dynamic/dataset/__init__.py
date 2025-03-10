525f05d77e5b5481361e9573faab89a3e51502a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:56 +0000

    Add package datafoundry/1.0.90/datafoundry-1.0.90-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.90-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,14 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .infrastructure_db_67cdcc9916e2eaa728b1882d import infrastructure_db
+
+from .ird_db_v1_67cdccf416e2eaa728b1882e import ird_db_v1
+
+
+__all__ = [
+    
+    "infrastructure_db",
+    
+    "ird_db_v1"
+    
+]
