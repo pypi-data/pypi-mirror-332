77e4a632698191e65d252d9fccd8f4273c7ebcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:48 +0000

    Add package datafoundry/1.0.82/datafoundry-1.0.82.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.82.tar.gz/datafoundry-1.0.82/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,18 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .abc_67cdb61ff87b225450e953af import abc
+
+from .xyz_67cdb631f87b225450e953b0 import xyz
+
+from .mnq_67cdc8f916e2eaa728b1882b import mnq
+
+
+__all__ = [
+    
+    "abc",
+    
+    "xyz",
+    
+    "mnq"
+    
+]
---
+++ packages/datafoundry/datafoundry-1.0.82.tar.gz/datafoundry-1.0.82/datafoundry/_dynamic/dataset/mnq_67cdc8f916e2eaa728b1882b.py
@@ -0,0 +1,29 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class mnq(DatasetBase):
+    """
+    Database: mnq
+    Description: mnq
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class mnq(TableBase):
+        """
+        Database: mnq
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "mnq"
+
+        @classmethod
+        def get_table_name(cls):
+            return "mnq"
+    
+# --- gen code block ---
