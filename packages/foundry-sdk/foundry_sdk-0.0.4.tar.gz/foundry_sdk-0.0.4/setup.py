b345df7b909ab41b6e0fb83c4ecd75f88f0df602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:57 +0000

    Add package foundry-sdk/0.0.4/foundry_sdk-0.0.4.tar.gz

---
+++ packages/foundry-sdk/foundry_sdk-0.0.4.tar.gz/foundry_sdk-0.0.4/setup.py
@@ -0,0 +1,14 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "requirements.txt")) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="foundry-sdk",
+    version="0.0.4",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+)
