2bd53858f7f203a8e2a77d0fa3ebb68e7c41a7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:52 +0000

    Add package datafoundry/1.0.73/datafoundry-1.0.73.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.73.tar.gz/datafoundry-1.0.73/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,14 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .gamemetrics1_67cda656f87b225450e953aa import gamemetrics1
+
+from .hainv4dataset_67cda897f87b225450e953ac import hainv4dataset
+
+
+__all__ = [
+    
+    "gamemetrics1",
+    
+    "hainv4dataset"
+    
+]
