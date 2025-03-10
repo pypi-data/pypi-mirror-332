56c2bc57fa0f042a186a07e9ddd356d0775829e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:05 +0000

    Add package basedmypy/2.10.0/basedmypy-2.10.0-cp310-cp310-win_amd64.whl

---
+++ packages/basedmypy/basedmypy-2.10.0-cp310-cp310-win_amd64.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "1.15.0"
+based_version_info = VersionInfo(major=2, minor=10, patch=0, release_level='final', iteration=0, mypy_version='1.15.0', mypy_release_level='final')
+__based_version__ = "2.10.0"
