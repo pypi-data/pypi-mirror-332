deaa2e082a7ed60a992841aff4477c136349f597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:46 +0000

    Add package datafoundry/1.0.53/datafoundry-1.0.53-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.53-py3-none-any.whl/datafoundry/_dynamic/dataset/hainv4testreload_67cd4c3f75d7d2c3e1452150.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class hainv4testreload(DatasetBase):
+    """
+    Database: hainv4testreload
+    Description: None
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
