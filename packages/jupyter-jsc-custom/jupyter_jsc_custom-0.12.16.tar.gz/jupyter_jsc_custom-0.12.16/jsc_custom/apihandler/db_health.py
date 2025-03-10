e4635813cb246fe7c0b9708eae007d2f4be6b015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:46 +0000

    Add package jupyter-jsc-custom/0.12.16/jupyter_jsc_custom-0.12.16.tar.gz

---
+++ packages/jupyter-jsc-custom/jupyter_jsc_custom-0.12.16.tar.gz/jupyter_jsc_custom-0.12.16/jsc_custom/apihandler/db_health.py
@@ -0,0 +1,11 @@
+from jupyterhub.apihandlers import APIHandler
+from jupyterhub.orm import Role
+
+
+class DBHealthAPIHandler(APIHandler):
+    async def get(self):
+        Role.find(self.db, "any")
+        self.set_status(200)
+
+
+default_handlers.append((r"/api/health_db", DBHealthAPIHandler))
