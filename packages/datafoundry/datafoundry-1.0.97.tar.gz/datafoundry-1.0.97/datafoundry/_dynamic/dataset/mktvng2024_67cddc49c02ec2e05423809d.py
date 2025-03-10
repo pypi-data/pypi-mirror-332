c264a95de50a1634ab4ee2d52e7507e905cf2aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:28 +0000

    Add package datafoundry/1.0.97/datafoundry-1.0.97.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.97.tar.gz/datafoundry-1.0.97/datafoundry/_dynamic/dataset/mktvng2024_67cddc49c02ec2e05423809d.py
@@ -0,0 +1,29 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class mktvng2024(DatasetBase):
+    """
+    Database: mktvng2024
+    Description: mkt vng 2024
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class pipelineusedata(TableBase):
+        """
+        Database: mktvng2024
+        Description: pipeline use data        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "mktvng2024"
+
+        @classmethod
+        def get_table_name(cls):
+            return "pipelineusedata"
+    
+# --- gen code block ---
