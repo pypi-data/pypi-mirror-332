6c4f2f898ebcdee5d162508d4c53f9822d8f3140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:52 +0000

    Add package datafoundry/1.0.73/datafoundry-1.0.73-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.73-py3-none-any.whl/datafoundry/_dynamic/dataset/hainv4dataset_67cda897f87b225450e953ac.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class hainv4dataset(DatasetBase):
+    """
+    Database: hainv4dataset
+    Description: abcccc
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
