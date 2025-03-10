9c12144b4fdfe18d55f06270ab9c1d1ecbdcc499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 03:04:57 +0000

    Add package datafoundry/1.0.57/datafoundry-1.0.57-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.57-py3-none-any.whl/datafoundry/_dynamic/dataset/mktvng2025_67cd75faf87b225450e953a2.py
@@ -0,0 +1,29 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class mktvng2025(DatasetBase):
+    """
+    Database: mktvng2025
+    Description: mkt vng 2025
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class pipelinerawdata(TableBase):
+        """
+        Database: mktvng2025
+        Description: pipeline raw data        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "mktvng2025"
+
+        @classmethod
+        def get_table_name(cls):
+            return "pipelinerawdata"
+    
+# --- gen code block ---
