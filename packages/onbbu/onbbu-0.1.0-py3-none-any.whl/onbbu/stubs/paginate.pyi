80ae8708e8d36ad381b750184980e907d12f318e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:57 +0000

    Add package onbbu/0.1.0/onbbu-0.1.0-py3-none-any.whl

---
+++ packages/onbbu/onbbu-0.1.0-py3-none-any.whl/onbbu/stubs/paginate.pyi
@@ -0,0 +1,17 @@
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+U = TypeVar('U')
+
+class PaginateDTO:
+    page: int
+    limit: int
+
+class PaginateOutputDTO(Generic[T]):
+    page: int
+    limit: int
+    total: int
+    total_page: int
+    data: list[T]
+
+def calculate_total_pages(total: int, limit: int) -> int: ...
