2dad8d68b37e7719556f1a9f1ded2da4bc1df41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:44 +0000

    Add package datafoundry/1.0.72/datafoundry-1.0.72.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.72.tar.gz/datafoundry-1.0.72/datafoundry/_dynamic/source/postgresql/sourcetest.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class sourcetest(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67cc70bedfc1b464c7c61d3e"
+# --- render code block -----
