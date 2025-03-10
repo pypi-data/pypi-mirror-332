04ae8474b8b1e98dea0078db6396274d1f4de073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:24 +0000

    Add package psi4-step/2025.3.9/psi4_step-2025.3.9-py2.py3-none-any.whl

---
+++ packages/psi4-step/psi4_step-2025.3.9-py2.py3-none-any.whl/psi4_step/__init__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+psi4_step
+A step for Psi4 in a SEAMM flowchart
+"""
+
+# Bring up the classes so that they appear to be directly in
+# the psi4_step package.
+
+from .psi4_metadata import methods  # noqa: F401
+from .psi4_metadata import dft_functionals  # noqa: F401
+from .psi4_metadata import optimization_methods  # noqa: F401
+from .psi4_metadata import optimization_convergence  # noqa: F401
+from .psi4_metadata import metadata  # noqa: F401
+
+from .psi4 import Psi4  # noqa: F401
+from .psi4_parameters import Psi4Parameters  # noqa: F401
+from .psi4_step import Psi4Step  # noqa: F401
+from .tk_psi4 import TkPsi4  # noqa: F401
+
+from .initialization import Initialization  # noqa: F401
+from .initialization_parameters import InitializationParameters  # noqa: F401
+from .initialization_step import InitializationStep  # noqa: F401
+from .tk_initialization import TkInitialization  # noqa: F401
+
+from .energy import Energy  # noqa: F401
+from .energy_parameters import EnergyParameters  # noqa: F401
+from .energy_step import EnergyStep  # noqa: F401
+from .tk_energy import TkEnergy  # noqa: F401
+
+from .optimization import Optimization  # noqa: F401
+from .optimization_parameters import OptimizationParameters  # noqa: F401
+from .optimization_step import OptimizationStep  # noqa: F401
+from .tk_optimization import TkOptimization  # noqa: F401
+
+from .thermochemistry_step import ThermochemistryStep  # noqa: F401
+from .thermochemistry import Thermochemistry  # noqa: F401
+from .thermochemistry_parameters import ThermochemistryParameters  # noqa: F401
+from .tk_thermochemistry import TkThermochemistry  # noqa: F401
+
+# Handle versioneer
+from ._version import get_versions
+
+__author__ = """Paul Saxe"""
+__email__ = "psaxe@molssi.org"
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
---
+++ packages/psi4-step/psi4_step-2025.3.9-py2.py3-none-any.whl/psi4_step/initialization_parameters.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""Global control parameters for Psi4"""
+
+import logging
+import seamm
+
+logger = logging.getLogger(__name__)
+
+
+class InitializationParameters(seamm.Parameters):
+    """The control parameters for initializing Psi4"""
+
+    parameters = {
+        "basis": {
+            "default": "6-31G**",
+            "kind": "enumeration",
+            "default_units": "",
+            "enumeration": (
+                "6-31G",
+                "6-31G(d)",
+                "6-31G(d,p)",
+                "6-31+G",
+                "6-31+G(d)",
+                "6-31+G(d,p)",
+                "6-311G",
+                "6-311G(d)",
+                "6-311G(d,p)",
+                "6-311+G",
+                "6-311+G(d)",
+                "6-311+G(d,p)",
+                "cc-pVDZ",
+                "cc-pVTZ",
+                "cc-pVQZ",
+                "def2-SV(P)",
+                "def2-SVP",
+                "def2-TZVP",
+                "def2-TZVPP",
+                "def2-QZVP",
+                "def2-QZVPP",
+            ),
+            "format_string": "s",
+            "description": "Basis:",
+            "help_text": ("The basis set to use."),
+        },
+        "symmetry_tolerance": {
+            "default": "0.00001",
+            "kind": "float",
+            "default_units": None,
+            "enumeration": tuple(),
+            "format_string": ".1e",
+            "description": "Symmetry tolerance:",
+            "help_text": "The tolerance used when determining the symmetry.",
+        },
+    }
+
+    def __init__(self, defaults={}, data=None):
+        """Initialize the instance, by default from the default
+        parameters given in the class"""
+
+        super().__init__(
+            defaults={**InitializationParameters.parameters, **defaults}, data=data
+        )
