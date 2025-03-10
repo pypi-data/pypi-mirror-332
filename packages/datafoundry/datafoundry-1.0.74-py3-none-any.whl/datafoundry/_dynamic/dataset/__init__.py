0d1fbad5216b25472ccb7ad208737452f0311dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:52 +0000

    Add package datafoundry/1.0.74/datafoundry-1.0.74-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.74-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,10 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .gamemetrics1_67cda656f87b225450e953aa import gamemetrics1
+
+
+__all__ = [
+    
+    "gamemetrics1"
+    
+]
---
+++ packages/datafoundry/datafoundry-1.0.74-py3-none-any.whl/datafoundry/_dynamic/source/postgresql/gameanalyze.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class gameanalyze(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67cd480775d7d2c3e145214f"
+# --- render code block -----
