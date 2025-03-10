ca631c4daa67e492a23980b782ecac6d172c858e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 05:06:11 +0000

    Add package project-euler-aid/1.0.0/project_euler_aid-1.0.0-py3-none-any.whl

---
+++ packages/project-euler-aid/project_euler_aid-1.0.0-py3-none-any.whl/project_euler_aid/__init__.py
@@ -0,0 +1,23 @@
+"""
+Utility functions for math problems
+====================================
+
+project-euler-aid is a package providing utility functions for solving math problems using python
+
+"""
+
+from .project_euler_aid import (
+    get_total_positive_divisors,
+    get_nth_prime,
+    sum_of_primes_until_n,
+    sum_of_even_fibonacci_terms,
+    largest_prime_factor,
+    reverse_number,
+    getPrime_less_than_or_equal_to_n,
+    get_count_of_prime_factors,
+    smallest_positive_number_divisible_by_all_numbers_till_n,
+    search_sum_of_pythogorean_triple,
+    square_sum_difference,
+    get_max_product_of_n_adjacent_digits,
+    get_nth_triangle_number,
+)
