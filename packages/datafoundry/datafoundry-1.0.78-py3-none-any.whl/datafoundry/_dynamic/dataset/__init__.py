b9c904aaf300c73380361141b9e665d4228089ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:57 +0000

    Add package datafoundry/1.0.78/datafoundry-1.0.78-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.78-py3-none-any.whl/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,10 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .vnghainv42_67cdac0af87b225450e953ae import vnghainv42
+
+
+__all__ = [
+    
+    "vnghainv42"
+    
+]
---
+++ packages/datafoundry/datafoundry-1.0.78-py3-none-any.whl/datafoundry/_dynamic/dataset/vnghainv42_67cdac0af87b225450e953ae.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class vnghainv42(DatasetBase):
+    """
+    Database: vnghainv42
+    Description: abc
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
