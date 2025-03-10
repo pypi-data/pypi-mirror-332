76834298eca7c1fa4dbcd201ff31bde4752129a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:52 +0000

    Add package datafoundry/1.0.74/datafoundry-1.0.74.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.74.tar.gz/datafoundry-1.0.74/datafoundry/_dynamic/source/postgresql/hainv4testsource.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class hainv4testsource(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67cd9fa8f87b225450e953a6"
+# --- render code block -----
