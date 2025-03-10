03ffa760e2585a2dddc07bff69448de0164e8ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:04:56 +0000

    Add package datafoundry/1.0.87/datafoundry-1.0.87.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.87.tar.gz/datafoundry-1.0.87/datafoundry/_dynamic/dataset/infrastructure_db_67cdcc9916e2eaa728b1882d.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class infrastructure_db(DatasetBase):
+    """
+    Database: infrastructure_db
+    Description: testing
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
