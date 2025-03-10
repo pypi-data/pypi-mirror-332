ad42633b9e319ea0bc0743c38b110b5b4b8ad040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 05:06:04 +0000

    Add package chatbot-analysis/0.8/chatbot_analysis-0.8.tar.gz

---
+++ packages/chatbot-analysis/chatbot_analysis-0.8.tar.gz/chatbot_analysis-0.8/setup.py
@@ -0,0 +1,24 @@
+#from setuptools import setup, find_packages
+
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as f:
+    description = f.read()
+setup(
+    name="chatbot_analysis",
+    version="0.8",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=2.0.0",
+        "transformers>=4.10.0",
+        "pandas>=1.5.3",
+        "tabulate>=0.9.0",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': [
+            'chatbot-cli = chatbot_analysis.cli:main',  # Define your CLI command and the function to call
+        ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
