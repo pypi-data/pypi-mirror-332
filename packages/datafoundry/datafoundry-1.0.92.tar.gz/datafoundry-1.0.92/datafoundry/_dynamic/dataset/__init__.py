8cf269e88751e8890b11c40a9f5150ff5a229146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 04:05:08 +0000

    Add package datafoundry/1.0.92/datafoundry-1.0.92.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.92.tar.gz/datafoundry-1.0.92/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,14 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .infrastructure_db_67cdcc9916e2eaa728b1882d import infrastructure_db
+
+from .test_edit_db_67cdcf0716e2eaa728b1882f import test_edit_db
+
+
+__all__ = [
+    
+    "infrastructure_db",
+    
+    "test_edit_db"
+    
+]
---
+++ packages/datafoundry/datafoundry-1.0.92.tar.gz/datafoundry-1.0.92/datafoundry/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,10 @@
+# --- render code block -----
+
+from .vng_marketing_2024 import vng_marketing_2024
+
+__all__ = [
+    
+    "vng_marketing_2024"
+    
+]
+# --- render code block -----
