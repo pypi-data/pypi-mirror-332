84e24a395f0c20ef06d2f8aaebc45fac0530ceb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-10 05:06:21 +0000

    Add package django-formset/1.7.4/django_formset-1.7.4.tar.gz

---
+++ packages/django-formset/django_formset-1.7.4.tar.gz/django_formset-1.7.4/formset/static/formset/js/FormDialog-XTJRBFAC.js
@@ -0,0 +1,1 @@
+import{a,b}from"./chunk-H6AGPGI2.js";import"./chunk-DFR323EV.js";import"./chunk-OU45VCVM.js";import"./chunk-ULT7T2WM.js";import"./chunk-JC2BH436.js";export{a as FormDialogBase,b as FormDialogElement};
