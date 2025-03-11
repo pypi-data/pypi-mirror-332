ca68e95e553104865fdae61b45b3c0baa6e49641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:18 +0000

    Add package bytedance-ui-detection-driver/0.0.1/bytedance_ui_detection_driver-0.0.1.tar.gz

---
+++ packages/bytedance-ui-detection-driver/bytedance_ui_detection_driver-0.0.1.tar.gz/bytedance_ui_detection_driver-0.0.1/bytedance_ui_detection_driver/__init__.py
@@ -0,0 +1,2 @@
+# bytedance_ui_detection_driver - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedance-ui-detection-driver/bytedance_ui_detection_driver-0.0.1.tar.gz/bytedance_ui_detection_driver-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_ui_detection_driver",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_ui_detection_driver",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_ui_detection_driver.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
