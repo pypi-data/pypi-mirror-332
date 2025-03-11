b30678f87347ca79e5ff4d12ebf0057fbea4983f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:13:58 +0000

    Add package time-it-profiler/0.0.2/time_it_profiler-0.0.2.tar.gz

---
+++ packages/time-it-profiler/time_it_profiler-0.0.2.tar.gz/time_it_profiler-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="time-it-profiler",  # Your package name
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=[],
+    author="Brian Vess",
+    author_email="brianvess@icloud.com",
+    description="A lightweight Python decorator for measuring execution time and system resource usage.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/brianvess/time_it.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
