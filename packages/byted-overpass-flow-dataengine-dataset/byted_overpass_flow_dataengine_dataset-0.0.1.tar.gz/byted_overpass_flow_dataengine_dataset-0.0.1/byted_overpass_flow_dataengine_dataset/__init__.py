d2b8fee50ee0dd69d87b438555fcf37e6ef2681c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:06:59 +0000

    Add package byted-overpass-flow-dataengine-dataset/0.0.1/byted_overpass_flow_dataengine_dataset-0.0.1.tar.gz

---
+++ packages/byted-overpass-flow-dataengine-dataset/byted_overpass_flow_dataengine_dataset-0.0.1.tar.gz/byted_overpass_flow_dataengine_dataset-0.0.1/byted_overpass_flow_dataengine_dataset/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_flow_dataengine_dataset - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-flow-dataengine-dataset/byted_overpass_flow_dataengine_dataset-0.0.1.tar.gz/byted_overpass_flow_dataengine_dataset-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_flow_dataengine_dataset",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_flow_dataengine_dataset",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_flow_dataengine_dataset.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
