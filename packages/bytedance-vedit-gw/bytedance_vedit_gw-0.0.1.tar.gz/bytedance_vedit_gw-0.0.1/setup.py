ccd6595cd9fa2a0d9b3fd4c4ee574575331b1def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:07:12 +0000

    Add package bytedance-vedit-gw/0.0.1/bytedance_vedit_gw-0.0.1.tar.gz

---
+++ packages/bytedance-vedit-gw/bytedance_vedit_gw-0.0.1.tar.gz/bytedance_vedit_gw-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_vedit_gw",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_vedit_gw",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_vedit_gw.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
