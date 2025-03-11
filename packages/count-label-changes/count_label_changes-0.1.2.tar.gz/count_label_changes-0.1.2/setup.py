2c4828a0c0c4f2eeaddbb82bfbb3d84fecfaf95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:07:19 +0000

    Add package count-label-changes/0.1.2/count_label_changes-0.1.2.tar.gz

---
+++ packages/count-label-changes/count_label_changes-0.1.2.tar.gz/count_label_changes-0.1.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='count-label-changes',
+    version='0.1.2',
+    py_modules=['plugin_module'],
+
+    install_requires=[
+        'nellie',
+        'pandas',
+    ],
+
+    entry_points={
+        'nellie.plugins': [
+            'Count Label Changes = plugin_module:count_label_changes',
+        ],
+    },
+
+    author='Austin E. Y. T. Lefebvre',
+    author_email='austin.e.lefebvre+nellie@gmail.com',
+    description='A plugin for Nellie that counts label changes. One type of typically used fission and fusion metric.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/aelefebv/nellie-plugin-fission-fusion',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
