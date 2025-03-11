dce742f7aaa076fe13378df4a9a4e607331bc381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:14:52 +0000

    Add package wujing/1.9.0/wujing-1.9.0.tar.gz

---
+++ packages/wujing/wujing-1.9.0.tar.gz/wujing-1.9.0/push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eux
+
+uv publish --username=__token__ --password=pypi-AgEIcHlwaS5vcmcCJGE2ZjVjZmFmLWMwYjItNDA0OC1hZWQyLTdjMjIyYmJlNzJlMgACDlsxLFsid3VqaW5nIl1dAAIsWzIsWyI0NGUwY2QxNC00ODM0LTQzN2MtYTkyNi1kZjNjYTk3OGEyNzUiXV0AAAYg3bKB8OQrrwlW5W0SjMf1AHEfffHx97hZN0LV9P1vZ0M
