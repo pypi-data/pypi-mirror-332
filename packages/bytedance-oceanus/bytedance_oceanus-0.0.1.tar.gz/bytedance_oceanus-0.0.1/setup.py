83ca7e21e166994f465b733b595f05d1bfb65328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:06:58 +0000

    Add package bytedance-oceanus/0.0.1/bytedance_oceanus-0.0.1.tar.gz

---
+++ packages/bytedance-oceanus/bytedance_oceanus-0.0.1.tar.gz/bytedance_oceanus-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_oceanus",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_oceanus",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_oceanus.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
