bdab65eeaeab12af8cef24705e53e2f8566a8fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 11:06:02 +0000

    Add package ontol/2.10.6/ontol-2.10.6.tar.gz

---
+++ packages/ontol/ontol-2.10.6.tar.gz/ontol-2.10.6/src/ontol/__init__.py
@@ -0,0 +1,38 @@
+from .oast import (
+    Ontology,
+    Term,
+    Function,
+    Relationship,
+    Meta,
+    FunctionArgument,
+    RelationshipType,
+    FunctionAttributes,
+    RelationshipAttributes,
+    RelationshipDirection,
+    TermAttributes,
+)
+from .parser import Parser
+from .plantuml import PlantUML
+from .serializer import JSONSerializer
+from .retranslator import Retranslator
+from .cli import CLI
+
+
+__all__ = (
+    'Ontology',
+    'Term',
+    'Function',
+    'Relationship',
+    'FunctionAttributes',
+    'RelationshipAttributes',
+    'RelationshipDirection',
+    'TermAttributes',
+    'Meta',
+    'FunctionArgument',
+    'RelationshipType',
+    'Parser',
+    'PlantUML',
+    'JSONSerializer',
+    'Retranslator',
+    'CLI',
+)
