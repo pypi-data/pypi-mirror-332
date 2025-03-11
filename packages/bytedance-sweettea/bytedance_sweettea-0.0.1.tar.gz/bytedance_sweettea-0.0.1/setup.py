c8381669af4f892c85ad2f723d9c593f87a8aa7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:07:01 +0000

    Add package bytedance-sweettea/0.0.1/bytedance_sweettea-0.0.1.tar.gz

---
+++ packages/bytedance-sweettea/bytedance_sweettea-0.0.1.tar.gz/bytedance_sweettea-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_sweettea",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_sweettea",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_sweettea.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
