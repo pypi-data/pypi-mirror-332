67860dffa3d857149535de2de6d81f5a0d995d43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:05:53 +0000

    Add package django-geojson/4.2.0/django_geojson-4.2.0.tar.gz

---
+++ packages/django-geojson/django_geojson-4.2.0.tar.gz/django_geojson-4.2.0/setup.py
@@ -0,0 +1,43 @@
+import os
+from io import open
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='django-geojson',
+    version='4.2.0',
+    author='Mathieu Leplatre',
+    author_email='mathieu.leplatre@makina-corpus.com',
+    url='https://github.com/makinacorpus/django-geojson',
+    download_url="http://pypi.python.org/pypi/django-geojson/",
+    description="Serve vectorial map layers with Django",
+    long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
+                     open(os.path.join(here, 'CHANGES'), encoding='utf-8').read(),
+    license='LPGL, see LICENSE file.',
+    install_requires=[
+        'Django>=4.2',
+    ],
+    extras_require={
+        'field': ['django-leaflet>=0.12'],
+        'docs': ['sphinx', 'sphinx-autobuild'],
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=['Topic :: Utilities',
+                 'Natural Language :: English',
+                 'Operating System :: OS Independent',
+                 'Intended Audience :: Developers',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Development Status :: 5 - Production/Stable',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11',
+                 'Programming Language :: Python :: 3.12',
+                 'Programming Language :: Python :: 3.13',
+    ],
+)
