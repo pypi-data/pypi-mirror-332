71b94dc74cf8c875d1700fa88a9f48494aa40f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:57 +0000

    Add package byted-overpass-caijing-doupay-channel-admin/0.0.1/byted_overpass_caijing_doupay_channel_admin-0.0.1.tar.gz

---
+++ packages/byted-overpass-caijing-doupay-channel-admin/byted_overpass_caijing_doupay_channel_admin-0.0.1.tar.gz/byted_overpass_caijing_doupay_channel_admin-0.0.1/byted_overpass_caijing_doupay_channel_admin/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_caijing_doupay_channel_admin - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-caijing-doupay-channel-admin/byted_overpass_caijing_doupay_channel_admin-0.0.1.tar.gz/byted_overpass_caijing_doupay_channel_admin-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_caijing_doupay_channel_admin",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_caijing_doupay_channel_admin",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_caijing_doupay_channel_admin.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
