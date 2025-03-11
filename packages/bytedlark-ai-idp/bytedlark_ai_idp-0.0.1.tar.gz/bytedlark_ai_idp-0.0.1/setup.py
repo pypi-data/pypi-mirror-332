f488631e5081b302766f2ce545f332ee54d8b598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:06:03 +0000

    Add package bytedlark-ai-idp/0.0.1/bytedlark_ai_idp-0.0.1.tar.gz

---
+++ packages/bytedlark-ai-idp/bytedlark_ai_idp-0.0.1.tar.gz/bytedlark_ai_idp-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedlark_ai_idp",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedlark_ai_idp",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedlark_ai_idp.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
