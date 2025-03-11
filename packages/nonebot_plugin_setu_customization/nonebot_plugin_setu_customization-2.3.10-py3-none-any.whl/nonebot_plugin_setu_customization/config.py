cf8e3f10009c2d826eb67bd7ecac945ea2a7debd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:50 +0000

    Add package nonebot_plugin_setu_customization/2.3.10/nonebot_plugin_setu_customization-2.3.10-py3-none-any.whl

---
+++ packages/nonebot_plugin_setu_customization/nonebot_plugin_setu_customization-2.3.10-py3-none-any.whl/nonebot_plugin_setu_customization/config.py
@@ -0,0 +1,105 @@
+from asyncio import Lock
+from json import dump, load
+from os import listdir, makedirs, path
+from typing import Dict, List, Optional, Set
+
+from nonebot import get_driver, get_plugin_config
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    # 图图命令CD时间（秒）
+    tutu_cooldown: int = 3
+    # 危险图库，危险图库的图片无法在群聊发送
+    tutu_danger_gallery: List[str] = []
+    # 本地图片库的路径
+    tutu_local_api_path: str = "data/tutu_local_img_lib/"
+    # 插件数据文件名
+    tutu_data_filename: str = "tutu_data.json"
+    # 是否默认全部接口走代理，如果为False，则需要url末尾追加“代理翻转”才会走代理
+    tutu_http_proxy_default: bool = True
+    # http代理地址，如 http://127.0.0.1:1234
+    tutu_http_proxy: Optional[str] = None
+    # pixiv图片反代地址 备选 https://i.pixiv.re/ 、 https://i.pixiv.cat/ 、 https://i.loli.best/ 、 pimg.rem.asia 、 https://c.jitsu.top/
+    tutu_pixiv_proxy: Optional[str] = None
+    # 限定哪个bot响应，填bot的qq号，限定群聊只有这个bot响应，不填则均响应
+    tutu_bot_id: Optional[int] = None
+
+
+class Var:
+    # 图库接口   图库名：api列表
+    gallery_list: Dict[str, List[str]] = {}
+    # 本地图库   文件名：图片url列表
+    local_imgs: Dict[str, List[str]] = {}
+    # 图图白名单群列表
+    group_list: Set[int] = set()
+    # 请求头
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "Sec-Ch-Ua": '"Not(A:Brand";v="99", "Microsoft Edge";v="133", "Chromium";v="133"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "Windows",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0",
+    }
+    # 消息响应锁
+    msg_lock: Lock = Lock()
+    received_msg_id_set: set[int] = set()
+
+
+driver = get_driver()
+var = Var()
+pc = get_plugin_config(Config)
+
+
+def read_data():
+    """
+    读取配置文件
+    """
+    with open(f"data/{pc.tutu_data_filename}", "r", encoding="utf-8") as r:
+        tmp_data = load(r)
+        for group_id in tmp_data[0]:
+            var.group_list.add(group_id)
+        var.gallery_list = tmp_data[1]
+
+
+def save_data():
+    """
+    保存配置文件
+    """
+    with open(f"data/{pc.tutu_data_filename}", "w", encoding="utf-8") as w:
+        dump(
+            [
+                list(var.group_list),
+                var.gallery_list,
+            ],
+            w,
+            indent=4,
+            ensure_ascii=False,
+        )
+
+
+def load_local_api():
+    """
+    读取本地图片库
+    """
+    var.local_imgs.clear()
+    for file_name in listdir(pc.tutu_local_api_path):
+        with open(pc.tutu_local_api_path + file_name, "r", encoding="utf-8") as r:
+            api_list_lines = r.readlines()
+        var.local_imgs[file_name] = [line.rstrip() for line in api_list_lines]
+
+
+@driver.on_startup
+async def on_startup():
+    """
+    启动时执行
+    """
+    if not path.exists(pc.tutu_local_api_path):
+        makedirs(pc.tutu_local_api_path)
+
+    if path.exists(f"data/{pc.tutu_data_filename}"):
+        read_data()
+
+    load_local_api()
---
+++ packages/nonebot_plugin_setu_customization/nonebot_plugin_setu_customization-2.3.10-py3-none-any.whl/nonebot_plugin_setu_customization/data_handle.py
@@ -0,0 +1,180 @@
+from json import dumps, loads
+from random import choice
+from re import search
+from typing import Optional, Tuple, Union
+from urllib.parse import unquote, urljoin
+
+from aiohttp import ClientSession, ClientTimeout
+from nonebot.adapters.onebot.v11 import MessageSegment as MS
+from nonebot.matcher import Matcher
+
+from .config import pc, var
+
+
+def url_diy_replace(img_url: str) -> str:
+    """
+    图片url自定义替换，可根据自己需求自己改
+    """
+    # pixiv图片缩小尺寸以及pixiv反代地址
+    if "/img-original/img/" in img_url:
+        img_url_group = img_url.replace("//", "").split("/")
+        img_url_group.pop(0)
+        if pc.tutu_pixiv_proxy:
+            img_url = pc.tutu_pixiv_proxy + "/" + "/".join(img_url_group)
+        else:
+            img_url = "https://i.pixiv.re/" + "/".join(img_url_group)
+
+        # 缩小图片大小
+        img_url = img_url.replace("/img-original/img/", "/img-master/img/")
+        ext = img_url.split(".")[-1]
+        img_url = img_url.replace(f".{ext}", "_master1200.jpg")
+
+    return unquote(img_url)
+
+
+def extract_img_url(text: str) -> Optional[str]:
+    # 判断有没有original关键字，有就找原图
+    if '"original"' in text:
+        original_exists = True
+    else:
+        original_exists = False
+
+    # 尝试反序列化，如果序列化成功再变带缩进的序列号字符串，方便正则找url
+    try:
+        tmp_data = dumps(loads(text), indent=4, ensure_ascii=False)
+        if original_exists:
+            img_url = search(
+                r"original.+(?P<MSG>(?:http)?s?:?\\?/\\?/[^\"]*)", tmp_data
+            )
+        else:
+            img_url = search(r"(?P<MSG>(?:http)?s?:?\\?/\\?/[^\"]*)", tmp_data)
+    except:
+        img_url = search(r"(?P<MSG>(?:http)?s?:?\\?/\\?/[^\"]*)", text)
+
+    try:
+        if not img_url:
+            raise IndexError
+        else:
+            # 去掉反斜杠
+            img_url = img_url.group("MSG").replace("\\", "")
+            # 没有协议头补上https
+            if img_url.startswith("//"):
+                img_url = "https:" + img_url
+            return img_url
+    except IndexError:
+        return None
+
+
+def get_proxy(api_url: str) -> Optional[str]:
+    if pc.tutu_http_proxy:
+        if pc.tutu_http_proxy_default:
+            http_proxy = None if "代理翻转" in api_url else pc.tutu_http_proxy
+        else:
+            http_proxy = pc.tutu_http_proxy if "代理翻转" in api_url else None
+
+    else:
+        http_proxy = None
+
+    return http_proxy
+
+
+async def get_img(api_url: str) -> Tuple[bool, Union[str, bytes], str]:
+    """
+    向API发起请求，获取返回的图片url
+    返回   请求成功失败, 图片地址, debug信息
+    """
+
+    if "本地图库" in api_url:
+        filename = api_url[4:].replace("代理翻转", "")
+        if filename not in var.local_imgs:
+            return False, "", f"本地图库{filename}不存在"
+
+        img = choice(var.local_imgs[filename])
+        return True, img, ""
+
+    try:
+        async with ClientSession(headers=var.headers) as session:
+            async with session.get(
+                api_url.replace("代理翻转", ""),
+                allow_redirects=False,
+                ssl=False,
+                proxy=get_proxy(api_url),
+                timeout=ClientTimeout(connect=5, total=12),
+            ) as resp:
+                resp_status = resp.status
+
+                if resp_status > 400:
+                    return (
+                        False,
+                        "",
+                        f"{api_url}\n响应异常，\n响应码：{resp_status}",
+                    )
+
+                if 300 <= resp_status < 400:
+                    img = resp.headers["location"]
+                    if "//" not in img[:10]:
+                        img = urljoin(api_url, img)
+                    return True, url_diy_replace(img), ""
+
+                # 判断 Content-Type
+                content_type = resp.headers.get("Content-Type", "")
+                if "image" in content_type:
+                    img = await resp.read()
+                    return True, img, ""
+
+                img = extract_img_url(await resp.text())
+                if not img:
+                    return (
+                        False,
+                        "",
+                        f"{api_url}\n找不到图片地址，\n响应码：{resp_status}",
+                    )
+
+                return True, url_diy_replace(img), ""
+
+    except Exception as e:
+        return False, "", f"{api_url}\n请求出错：{repr(e)}"
+
+
+async def send_img(matcher: Matcher, api_url: str, img: Union[str, bytes]):
+    img_url = img if isinstance(img, str) else api_url
+    try:
+        if isinstance(img, str):
+            async with ClientSession(headers=var.headers) as session:
+                async with session.get(
+                    img,
+                    proxy=get_proxy(api_url),
+                    ssl=False,
+                    timeout=ClientTimeout(connect=5, total=12),
+                ) as resp:
+                    if resp.status != 200:
+                        await matcher.send(
+                            f"图片下载出错，响应码：{resp.status}\n接口：{api_url}\n图片地址：{img_url}"
+                        )
+                        return
+
+                    img_bytes = await resp.read()
+                    if len(img_bytes) == 0:
+                        raise Exception("响应数据为空")
+                    await matcher.send(MS.image(img_bytes))
+
+        else:
+            if len(img) == 0:
+                raise Exception("响应数据为空")
+            await matcher.send(MS.image(img))
+
+    except Exception as e:
+        await matcher.send(
+            f"图片下载出错：{repr(e)}\n接口：{api_url}\n图片地址：{img_url}"
+        )
+
+
+async def check_api(matcher: Matcher, api_url: str, img_num: int = 0):
+    success, img, debug_info = await get_img(api_url)
+    if not success:
+        await matcher.send(f"接口解析图片失败：{api_url}\n{debug_info}")
+        return
+
+    if img_num == 0:
+        await matcher.send("接口解析图片成功，测试发送图片")
+    await send_img(matcher, api_url, img)
