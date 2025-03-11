60d7dd09ee441c2c5caf3b573d199808720ce72b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:28 +0000

    Add package datafoundry/1.0.117/datafoundry-1.0.117.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.117.tar.gz/datafoundry-1.0.117/datafoundry/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,19 @@
+# --- render code block -----
+
+from .VngMarketing2024 import VngMarketing2024
+from .VngMarketingSourceV2 import VngMarketingSourceV2
+from .VngMarketingSourceV3 import VngMarketingSourceV3
+from .VngPgV2 import VngPgV2
+
+__all__ = [
+    
+    "VngMarketing2024",
+    
+    "VngMarketingSourceV2",
+    
+    "VngMarketingSourceV3",
+    
+    "VngPgV2"
+    
+]
+# --- render code block -----
