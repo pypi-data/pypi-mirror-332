0689be4043d5c411d4bdff69986ab90a56d7a180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:06:22 +0000

    Add package bytedance-account-tech/0.0.1/bytedance_account_tech-0.0.1.tar.gz

---
+++ packages/bytedance-account-tech/bytedance_account_tech-0.0.1.tar.gz/bytedance_account_tech-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_account_tech",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_account_tech",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_account_tech.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
