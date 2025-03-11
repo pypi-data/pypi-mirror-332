29da755e8199feab1b93c5ebf45df22b6a4ec217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:22 +0000

    Add package datafoundry/1.0.122/datafoundry-1.0.122.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.122.tar.gz/datafoundry-1.0.122/datafoundry/_dynamic/dataset/InfrastructureDb_67cdcc9916e2eaa728b1882d.py
@@ -0,0 +1,81 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class InfrastructureDb(DatasetBase):
+    """
+    Database: InfrastructureDb
+    Description: testing
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+    class CampaignDb(TableBase):
+        """
+        Database: InfrastructureDb
+        Description: campaign db testing        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "infrastructuredb"
+
+        @classmethod
+        def get_table_name(cls):
+            return "campaigndb"
+    
+    class Marketing(TableBase):
+        """
+        Database: InfrastructureDb
+        Description: vng marketing dataset        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "infrastructuredb"
+
+        @classmethod
+        def get_table_name(cls):
+            return "marketing"
+    
+    class MarketingV2(TableBase):
+        """
+        Database: InfrastructureDb
+        Description: marketing        
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "infrastructuredb"
+
+        @classmethod
+        def get_table_name(cls):
+            return "marketingv2"
+    
+    class MarketingV3(TableBase):
+        """
+        Database: InfrastructureDb
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "infrastructuredb"
+
+        @classmethod
+        def get_table_name(cls):
+            return "marketingv3"
+    
+    class MarketingV4(TableBase):
+        """
+        Database: InfrastructureDb
+        Description:         
+        """
+        @classmethod
+        def get_db_name(cls):
+            return "infrastructuredb"
+
+        @classmethod
+        def get_table_name(cls):
+            return "marketingv4"
+    
+# --- gen code block ---
