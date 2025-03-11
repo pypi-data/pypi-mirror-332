d396e61134ecc21ade6a6efeac8ab7260e404363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:27 +0000

    Add package fair-kmeans-ashwinram/0.1.0/fair_kmeans_ashwinram-0.1.0.tar.gz

---
+++ packages/fair-kmeans-ashwinram/fair_kmeans_ashwinram-0.1.0.tar.gz/fair_kmeans_ashwinram-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fair_kmeans_ashwinram",  # Package name
+    version="0.1.0",  # Initial version
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[],  # Dependencies (if any)
+    author="Ashwin Ram Venkataraman",
+    author_email="ashwinram232@gmail.com",
+    description="FAIR K-MEANS",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
