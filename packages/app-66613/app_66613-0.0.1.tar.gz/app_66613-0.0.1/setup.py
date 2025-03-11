aa85b923a21bc32c374d8ca5911f76fa0fa2b3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:06:13 +0000

    Add package app-66613/0.0.1/app_66613-0.0.1.tar.gz

---
+++ packages/app-66613/app_66613-0.0.1.tar.gz/app_66613-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='app_66613',  # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='PowerMa1ner',
+    author_email='romankomarov154@gmail.com',
+    description='app_66613',
+    packages=setuptools.find_packages(),
+)
