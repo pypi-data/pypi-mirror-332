bcca311612f3791b3d834b6e2a342dae839bdabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 12:05:50 +0000

    Add package llemacs-python/0.1.0/llemacs_python-0.1.0-py3-none-any.whl

---
+++ packages/llemacs-python/llemacs_python-0.1.0-py3-none-any.whl/llemacs-python/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Timestamp: "2025-03-11 08:55:16 (ywatanabe)"
+# File: /home/ywatanabe/proj/llemacs/llemacs-python/src/llemacs-python/__init__.py
+
+__THIS_FILE__ = "/home/ywatanabe/proj/llemacs/llemacs-python/src/llemacs-python/__init__.py"
+
+import os
+
+__copyright__ = "Copyright (C) 2025 Yusuke Watanabe"
+__version__ = "0.1.0"
+__license__ = "MIT"
+__author__ = "Yusuke Watanabe"
+__author_email__ = "ywatanabe@alumni.u-tokyo.ac.jp"
+__url__ = "https://github.com/ywatanabe1989/llemacs-python"
+
+# EOF
