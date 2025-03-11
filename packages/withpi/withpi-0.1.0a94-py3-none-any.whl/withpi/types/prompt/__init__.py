d059862fd1343d9e2283cb8fb65e9345100ec252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:14:00 +0000

    Add package withpi/0.1.0a94/withpi-0.1.0a94-py3-none-any.whl

---
+++ packages/withpi/withpi-0.1.0a94-py3-none-any.whl/withpi/types/prompt/__init__.py
@@ -0,0 +1,10 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .optimize_list_params import OptimizeListParams as OptimizeListParams
+from .optimize_list_response import OptimizeListResponse as OptimizeListResponse
+from .optimize_cancel_response import OptimizeCancelResponse as OptimizeCancelResponse
+from .optimize_start_job_params import OptimizeStartJobParams as OptimizeStartJobParams
+from .prompt_optimization_status import PromptOptimizationStatus as PromptOptimizationStatus
+from .optimize_stream_messages_response import OptimizeStreamMessagesResponse as OptimizeStreamMessagesResponse
