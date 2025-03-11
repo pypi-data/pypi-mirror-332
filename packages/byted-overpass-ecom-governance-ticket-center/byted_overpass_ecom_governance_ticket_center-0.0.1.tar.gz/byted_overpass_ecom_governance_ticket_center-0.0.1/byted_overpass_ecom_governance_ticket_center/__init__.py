792a8a3ffbc748001d3064e136c8c371cda175d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:55 +0000

    Add package byted-overpass-ecom-governance-ticket-center/0.0.1/byted_overpass_ecom_governance_ticket_center-0.0.1.tar.gz

---
+++ packages/byted-overpass-ecom-governance-ticket-center/byted_overpass_ecom_governance_ticket_center-0.0.1.tar.gz/byted_overpass_ecom_governance_ticket_center-0.0.1/byted_overpass_ecom_governance_ticket_center/__init__.py
@@ -0,0 +1,2 @@
+# byted_overpass_ecom_governance_ticket_center - A simple package
+__version__ = "0.0.1"
---
+++ packages/byted-overpass-ecom-governance-ticket-center/byted_overpass_ecom_governance_ticket_center-0.0.1.tar.gz/byted_overpass_ecom_governance_ticket_center-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_ecom_governance_ticket_center",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_ecom_governance_ticket_center",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_ecom_governance_ticket_center.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
