004ea7ed7012ce1d3273991c9febe7915da831e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:06:43 +0000

    Add package aiootp/0.23.16/aiootp-0.23.16.tar.gz

---
+++ packages/aiootp/aiootp-0.23.16.tar.gz/aiootp-0.23.16/tests/test_aiootp.py
@@ -0,0 +1,31 @@
+# This file is part of aiootp:
+# a high-level async cryptographic anonymity library to scale, simplify,
+# & automate privacy best practices for secure data & identity processing,
+# communication, & storage.
+#
+# Licensed under the AGPLv3: https://www.gnu.org/licenses/agpl-3.0.html
+# Copyright © 2019-2021 Gonzo Investigative Journalism Agency, LLC
+#            <gonzo.development@protonmail.ch>
+#           © 2019-2025 Ricchi (Richard) Machado <rmlibre@riseup.net>
+# All rights reserved.
+#
+
+
+from conftest import *
+from test_hypothesis_strategies import *
+from test_typing import *
+from test_paths import *
+from test_exceptions import *
+from test_commons import *
+from test_asynchs import *
+from test_gentools import *
+from test_generics import *
+from test_permutations import *
+from test_randoms import *
+from test_ciphers import *
+from test_keygens import *
+from test_databases import *
+from test_time_to_live import *
+
+
+__all__ = sorted({n for n in globals() if n.lower().startswith("test")})
