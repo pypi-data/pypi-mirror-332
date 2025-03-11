270f6924046d9d5ee9ee0e62b41d27b29345bf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:28 +0000

    Add package datafoundry/1.0.118/datafoundry-1.0.118.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.118.tar.gz/datafoundry-1.0.118/datafoundry/_dynamic/source/postgresql/VngPgV2.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class VngPgV2(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67ce976c57fdaddab2882ff8"
+# --- render code block -----
---
+++ packages/datafoundry/datafoundry-1.0.118.tar.gz/datafoundry-1.0.118/datafoundry/_dynamic/source/postgresql/VngPgV3.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class VngPgV3(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67ce979957fdaddab2882ff9"
+# --- render code block -----
