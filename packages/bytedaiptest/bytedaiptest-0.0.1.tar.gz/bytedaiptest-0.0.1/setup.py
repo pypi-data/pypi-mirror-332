46c33a82c404e703b5a00c1d4592ae014fd2d931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:10 +0000

    Add package bytedaiptest/0.0.1/bytedaiptest-0.0.1.tar.gz

---
+++ packages/bytedaiptest/bytedaiptest-0.0.1.tar.gz/bytedaiptest-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedaiptest",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedaiptest",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedaiptest.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
