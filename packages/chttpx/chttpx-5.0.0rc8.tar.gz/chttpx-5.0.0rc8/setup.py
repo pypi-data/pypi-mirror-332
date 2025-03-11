7918eeee31925e1b489b51da0c852b9dceac145c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:06:14 +0000

    Add package chttpx/5.0.0rc8/chttpx-5.0.0rc8.tar.gz

---
+++ packages/chttpx/chttpx-5.0.0rc8.tar.gz/chttpx-5.0.0rc8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='chttpx',
+    versioning='dev',
+    setup_requires='setupmeta',
+    packages=['chttpx'],
+    install_requires=[
+        'httpx',
+        'truststore',
+    ],
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'cli2-chttpx-example = chttpx.example:cli.entry_point',
+        ],
+    },
+)
