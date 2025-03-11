73b3b63b9da065989792406aaf105968572d5f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:06:41 +0000

    Add package geos5fp/1.0.7/geos5fp-1.0.7-py3-none-any.whl

---
+++ packages/geos5fp/geos5fp-1.0.7-py3-none-any.whl/geos5fp/geos5fp.py
@@ -0,0 +1,8 @@
+from .constants import *
+from .exceptions import *
+from .geos5fp_granule import GEOS5FPGranule
+from .geos5fp_connection import GEOS5FPConnection
+
+__author__ = 'Gregory H. Halverson'
+
+GEOS5FP = GEOS5FPConnection
