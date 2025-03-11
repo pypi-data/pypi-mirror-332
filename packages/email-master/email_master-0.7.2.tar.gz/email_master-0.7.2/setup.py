ebd3ad9388f12d12e55b77aa17f8c3e36acde658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:43 +0000

    Add package email-master/0.7.2/email_master-0.7.2.tar.gz

---
+++ packages/email-master/email_master-0.7.2.tar.gz/email_master-0.7.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+with open('./README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='email-master',
+    packages=['email_master', 'email_master.eml', 'email_master.msg', 'email_master.pgp'],
+    version='0.7.2',
+    description='Master Email Parsing Package',
+    author='Swimlane',
+    author_email="info@swimlane.com",
+    long_description=long_description,
+    install_requires=[
+        "pendulum>=2.1.2",
+        "compressed-rtf==1.0.5",
+        "extract_msg==0.23.2",
+        "PGPy==0.6.0"
+    ],
+    keywords=['utilities', 'email', 'parsing', 'eml', 'msg'],
+    classifiers=[],
+)
