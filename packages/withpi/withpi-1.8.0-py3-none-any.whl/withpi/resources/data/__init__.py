24f0b2dfb39e13952758e61c14e3cd623f10f3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:06:42 +0000

    Add package withpi/1.8.0/withpi-1.8.0-py3-none-any.whl

---
+++ packages/withpi/withpi-1.8.0-py3-none-any.whl/withpi/resources/data/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .data import (
+    DataResource,
+    AsyncDataResource,
+    DataResourceWithRawResponse,
+    AsyncDataResourceWithRawResponse,
+    DataResourceWithStreamingResponse,
+    AsyncDataResourceWithStreamingResponse,
+)
+from .generate_inputs import (
+    GenerateInputsResource,
+    AsyncGenerateInputsResource,
+    GenerateInputsResourceWithRawResponse,
+    AsyncGenerateInputsResourceWithRawResponse,
+    GenerateInputsResourceWithStreamingResponse,
+    AsyncGenerateInputsResourceWithStreamingResponse,
+)
+from .generate_examples import (
+    GenerateExamplesResource,
+    AsyncGenerateExamplesResource,
+    GenerateExamplesResourceWithRawResponse,
+    AsyncGenerateExamplesResourceWithRawResponse,
+    GenerateExamplesResourceWithStreamingResponse,
+    AsyncGenerateExamplesResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "GenerateInputsResource",
+    "AsyncGenerateInputsResource",
+    "GenerateInputsResourceWithRawResponse",
+    "AsyncGenerateInputsResourceWithRawResponse",
+    "GenerateInputsResourceWithStreamingResponse",
+    "AsyncGenerateInputsResourceWithStreamingResponse",
+    "GenerateExamplesResource",
+    "AsyncGenerateExamplesResource",
+    "GenerateExamplesResourceWithRawResponse",
+    "AsyncGenerateExamplesResourceWithRawResponse",
+    "GenerateExamplesResourceWithStreamingResponse",
+    "AsyncGenerateExamplesResourceWithStreamingResponse",
+    "DataResource",
+    "AsyncDataResource",
+    "DataResourceWithRawResponse",
+    "AsyncDataResourceWithRawResponse",
+    "DataResourceWithStreamingResponse",
+    "AsyncDataResourceWithStreamingResponse",
+]
