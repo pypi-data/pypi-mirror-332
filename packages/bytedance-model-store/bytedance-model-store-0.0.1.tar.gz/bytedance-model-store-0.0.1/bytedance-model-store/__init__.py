f7107338f95395771b8f94eed84725592045b943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:10 +0000

    Add package bytedance-model-store/0.0.1/bytedance-model-store-0.0.1.tar.gz

---
+++ packages/bytedance-model-store/bytedance-model-store-0.0.1.tar.gz/bytedance-model-store-0.0.1/bytedance-model-store/__init__.py
@@ -0,0 +1,2 @@
+# bytedance-model-store - A simple package
+__version__ = "0.0.1"
---
+++ packages/bytedance-model-store/bytedance-model-store-0.0.1.tar.gz/bytedance-model-store-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance-model-store",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance-model-store",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance-model-store.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
