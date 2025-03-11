0fee5d77290352395e4f8ae12272996665ddaf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:56 +0000

    Add package bytedbytegraph-consumer/0.0.1/bytedbytegraph_consumer-0.0.1.tar.gz

---
+++ packages/bytedbytegraph-consumer/bytedbytegraph_consumer-0.0.1.tar.gz/bytedbytegraph_consumer-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedbytegraph_consumer",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedbytegraph_consumer",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedbytegraph_consumer.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
