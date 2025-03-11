1e3dc9d73102810d83a0dba9117ec24ce27a87c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:17 +0000

    Add package coc.py/3.9.1/coc_py-3.9.1-py3-none-any.whl

---
+++ packages/coc.py/coc_py-3.9.1-py3-none-any.whl/coc/enums.py
@@ -0,0 +1,379 @@
+"""
+MIT License
+
+Copyright (c) 2019-2020 mathsman5133
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from enum import Enum
+
+
+class ExtendedEnum(Enum):
+    """An Enum class that allows for the `__str__` method to be implemented."""
+    def __str__(self):
+        return self.in_game_name
+
+    def __eq__(self, other):
+        """Check if the enum is equal to another enum or a string."""
+        if isinstance(other, Enum):
+            return self.value == other.value
+        elif isinstance(other, str):
+            return str(self.name) == other or str(self.value) == other
+        return False
+
+    @property
+    def in_game_name(self) -> str:
+        raise NotImplementedError
+
+    @classmethod
+    def values(cls):
+        return list(map(lambda c: c.value, cls))
+
+    @classmethod
+    def names(cls):
+        return list(map(lambda c: c.name, cls))
+
+
+class PlayerHouseElementType(ExtendedEnum):
+    """Enum to map the type of element of the player house."""
+
+    ground = "ground"
+    roof = "roof"
+    foot = "foot"  # API docs say this exists, but unable to find it anywhere. Looks like this means `walls`
+    deco = "decoration"
+    walls = "walls"
+
+    @property
+    def in_game_name(self) -> str:
+        """Get a neat client-facing string value for the element type."""
+        lookup = {"ground": "Ground", "roof": "Roof", "foot": "Foot", "decoration": "Decoration", "walls": "Walls"}
+        return lookup[self.value]
+
+
+class Role(ExtendedEnum):
+    """Enum to map a player's role in the clan."""
+
+    member = "member"
+    elder = "admin"
+    co_leader = "coLeader"
+    leader = "leader"
+
+    @property
+    def in_game_name(self) -> str:
+        """Get a neat client-facing string value for the role."""
+        lookup = {"member": "Member", "admin": "Elder", "coLeader": "Co-Leader", "leader": "Leader"}
+        return lookup[self.value]
+
+
+class WarRound(ExtendedEnum):
+    previous_war = 0
+    current_war = 1
+    current_preparation = 2
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def in_game_name(self) -> str:
+        lookup = ["Previous War", "Current War", "Current Preparation"]
+        return lookup[self.value]
+
+
+class BattleModifier(ExtendedEnum):
+    """Enum to map the type of battle modifiers."""
+    none = "none"
+    hard_mode = "hardMode"
+
+    @property
+    def in_game_name(self) -> str:
+        """Get a neat client-facing string value for the battle modifier."""
+        lookup = {"none": "None", "hardMode": "Hard Mode"}
+        return lookup[self.value]
+
+
+class WarState(ExtendedEnum):
+    """Enum to map the state of the war.
+    Compared to the api docs a few states are missing, but those were never observed in the wild."""
+    not_in_war = "notInWar"
+    preparation = "preparation"
+    in_war = "inWar"
+    war_ended = "warEnded"
+
+    @property
+    def in_game_name(self) -> str:
+        """Get a neat client-facing string value for the war state."""
+        lookup = {"notInWar": "Not in War", "preparation": "Preparation", "inWar": "In War", "warEnded": "War Ended"}
+        return lookup[self.value]
+
+
+class WarResult(ExtendedEnum):
+    """Enum to map the result of the war"""
+    win = "win"
+    lose = "lose"
+    tie = "tie"
+
+    @property
+    def in_game_name(self) -> str:
+        """Get a neat client-facing string value for the war state."""
+        lookup = {"win": "Win", "lose": "Lose", "tie": "Tie"}
+        return lookup[self.value]
+
+
+class Resource(ExtendedEnum):
+    elixir = "Elixir"
+    builder_elixir = "Elixir2"
+    dark_elixir = "DarkElixir"
+    gold = "Gold"
+    builder_gold = "Gold2"
+    shiny_ore = "CommonOre"
+    glowy_ore = "RareOre"
+    starry_ore = "EpicOre"
+
+    @property
+    def in_game_name(self) -> str:
+        """Get a neat client-facing string value for the resource."""
+        lookup = {"Elixir": "Elixir", "Elixir2": "Builder Elixir",
+                  "DarkElixir": "Dark Elixir", "Gold": "Gold", "Gold2": "Builder Gold",
+                  "CommonOre": "Shiny Ore", "RareOre": "Glowy Ore", "EpicOre": "Starry Ore"}
+        return lookup[self.value]
+
+
+ELIXIR_TROOP_ORDER = [
+    "Barbarian",
+    "Archer",
+    "Giant",
+    "Goblin",
+    "Wall Breaker",
+    "Balloon",
+    "Wizard",
+    "Healer",
+    "Dragon",
+    "P.E.K.K.A",
+    "Baby Dragon",
+    "Miner",
+    "Electro Dragon",
+    "Yeti",
+    "Dragon Rider",
+    "Electro Titan",
+    "Root Rider",
+    "Thrower"
+]
+
+
+DARK_ELIXIR_TROOP_ORDER = [
+    "Minion",
+    "Hog Rider",
+    "Valkyrie",
+    "Golem",
+    "Witch",
+    "Lava Hound",
+    "Bowler",
+    "Ice Golem",
+    "Headhunter",
+    "Apprentice Warden",
+    "Druid",
+]
+
+SIEGE_MACHINE_ORDER = [
+    "Wall Wrecker",
+    "Battle Blimp",
+    "Stone Slammer",
+    "Siege Barracks",
+    "Log Launcher",
+    "Flame Flinger",
+    "Battle Drill",
+    "Troop Launcher"
+]
+
+SUPER_TROOP_ORDER = [
+    "Super Barbarian",
+    "Super Archer",
+    "Super Giant",
+    "Sneaky Goblin",
+    "Super Wall Breaker",
+    "Rocket Balloon",
+    "Super Wizard",
+    "Super Dragon",
+    "Inferno Dragon",
+    "Super Minion",
+    "Super Valkyrie",
+    "Super Witch",
+    "Ice Hound",
+    "Super Bowler",
+    "Super Miner",
+    "Super Hog Rider",
+]
+
+HV_TROOP_ORDER = ELIXIR_TROOP_ORDER + DARK_ELIXIR_TROOP_ORDER
+HOME_TROOP_ORDER = HV_TROOP_ORDER + SIEGE_MACHINE_ORDER
+
+
+BUILDER_TROOPS_ORDER = [
+    "Raged Barbarian",
+    "Sneaky Archer",
+    "Boxer Giant",
+    "Beta Minion",
+    "Bomber",
+    "Baby Dragon",
+    "Cannon Cart",
+    "Night Witch",
+    "Drop Ship",
+    "Power P.E.K.K.A",
+    "Hog Glider",
+    "Electrofire Wizard",
+]
+
+
+ELIXIR_SPELL_ORDER = [
+    "Lightning Spell",
+    "Healing Spell",
+    "Rage Spell",
+    "Jump Spell",
+    "Freeze Spell",
+    "Clone Spell",
+    "Invisibility Spell",
+    "Recall Spell",
+    "Revive Spell"
+]
+
+
+DARK_ELIXIR_SPELL_ORDER = [
+    "Poison Spell",
+    "Earthquake Spell",
+    "Haste Spell",
+    "Skeleton Spell",
+    "Bat Spell",
+    "Overgrowth Spell",
+]
+
+
+SPELL_ORDER = ELIXIR_SPELL_ORDER + DARK_ELIXIR_SPELL_ORDER
+
+HOME_BASE_HERO_ORDER = ["Barbarian King", "Archer Queen", "Minion Prince", "Grand Warden", "Royal Champion"]
+BUILDER_BASE_HERO_ORDER = ["Battle Machine", "Battle Copter"]
+HERO_ORDER = HOME_BASE_HERO_ORDER + BUILDER_BASE_HERO_ORDER
+
+PETS_ORDER = [
+    "L.A.S.S.I",
+    "Electro Owl",
+    "Mighty Yak",
+    "Unicorn",
+    "Frosty",
+    "Diggy",
+    "Poison Lizard",
+    "Phoenix",
+    "Spirit Fox",
+    "Angry Jelly",
+]
+
+EQUIPMENT = [
+    "Barbarian Puppet",
+    "Rage Vial",
+    "Archer Puppet",
+    "Invisibility Vial",
+    "Eternal Tome",
+    "Life Gem",
+    "Seeking Shield",
+    "Royal Gem",
+    "Earthquake Boots",
+    "Vampstache",
+    "Giant Arrow",
+    "Healer Puppet",
+    "Rage Gem",
+    "Healing Tome",
+    "Giant Gauntlet",
+    "Frozen Arrow",
+    "Hog Rider Puppet",
+    "Haste Vial",
+    "Fireball",
+    "Spiky Ball",
+    "Rocket Spear",
+    "Lavaloon Puppet",
+    "Magic Mirror",
+    "Henchmen Puppet",
+    "Dark Orb",
+    "Electro Boots",
+    "Snake Bracelet",
+    "Metal Pants"
+]
+
+ACHIEVEMENT_ORDER = [
+    # Home Base
+    "Keep Your Account Safe!",
+    "Bigger & Better",
+    "Discover New Troops",
+    "Bigger Coffers",
+    "Gold Grab",
+    "Elixir Escapade",
+    "Heroic Heist",
+    "Well Seasoned",
+    "Nice and Tidy",
+    "Empire Builder",
+    "Clan War Wealth",
+    "Friend in Need",
+    "Sharing is caring",
+    "Siege Sharer",
+    "War Hero",
+    "War League Legend",
+    "Games Champion",
+    "Unbreakable",
+    "Sweet Victory!",
+    "Conqueror",
+    "League All-Star",
+    "Humiliator",
+    "Not So Easy This Time",
+    "Union Buster",
+    "Bust This!",
+    "Wall Buster",
+    "Mortar Mauler",
+    "X-Bow Exterminator",
+    "Firefighter",
+    "Anti-Artillery",
+    "Shattered and Scattered",
+    "Counterspell",
+    "Monolith Masher",
+    "Get those Goblins!",
+    "Get those other Goblins!",
+    "Get even more Goblins!",
+    "Dragon Slayer",
+    "Ungrateful Child",
+    "Superb Work",
+    "Supercharger",
+
+    # Builder Base
+    "Master Engineering",
+    "Hidden Treasures",
+    "High Gear",
+    "Next Generation Model",
+    "Un-Build It",
+    "Champion Builder",
+    
+    # Clan Capital
+    "Aggressive Capitalism",
+    "Most Valuable Clanmate",
+]
+
+UNRANKED_LEAGUE_DATA = {
+    "id": 29000000,
+    "name": "Unranked",
+    "iconUrls": {
+        "small": "https://api-assets.clashofclans.com/leagues/72/e--YMyIexEQQhE4imLoJcwhYn6Uy8KqlgyY3_kFV6t4.png",
+        "tiny": "https://api-assets.clashofclans.com/leagues/36/e--YMyIexEQQhE4imLoJcwhYn6Uy8KqlgyY3_kFV6t4.png",
+    },
+}
---
+++ packages/coc.py/coc_py-3.9.1-py3-none-any.whl/coc/hero.py
@@ -0,0 +1,354 @@
+import orjson
+
+from typing import TYPE_CHECKING, Dict, List, Optional, Type
+from pathlib import Path
+
+from .abc import DataContainer, DataContainerHolder
+from .miscmodels import try_enum
+from .utils import UnitStat
+
+from .enums import Resource
+
+if TYPE_CHECKING:
+    from .miscmodels import TimeDelta
+
+HERO_FILE_PATH = Path(__file__).parent.joinpath(Path("static/heroes.json"))
+PET_FILE_PATH = Path(__file__).parent.joinpath(Path("static/pets.json"))
+EQUIPMENT_FILE_PATH = Path(__file__).parent.joinpath(Path("static/equipment.json"))
+
+
+class Hero(DataContainer):
+    """
+    Represents a Hero object as returned by the API, optionally
+    filled with game data.
+
+    Attributes
+    ----------
+    id: :class:`int`
+        The hero's unique ID.
+    name: :class:`str`
+        The hero's name.
+    range: :class:`int`
+        The hero's attack range.
+    dps: :class:`int`
+        The hero's Damage Per Second (DPS).
+    hitpoints: :class:`int`
+        The number of hitpoints the troop has at this level.
+    ground_target: :class:`bool`
+        Whether the hero is ground-targetting. The Grand Warden is classified as ground targetting always.
+    speed: :class:`int`
+        The hero's speed.
+    upgrade_cost: :class:`int`
+        The amount of resources required to upgrade the hero to the next level.
+    upgrade_resource: :class:`Resource`
+        The type of resource used to upgrade this hero.
+    upgrade_time: :class:`TimeDelta`
+        The time taken to upgrade this hero to the next level.
+    ability_time: :class:`int`
+        The number of milliseconds the hero's ability lasts for.
+    required_th_level: :class:`int`
+        The minimum required townhall to unlock this level of the hero.
+    regeneration_time: :class:`TimeDelta`
+        The time required for this hero to regenerate after being "knocked out".
+    equipment: :class:`List[Equipment]`
+        a list of the equipment currently used by this hero
+    is_loaded: :class:`bool`
+        Whether the API data has been loaded for this hero.
+    level: :class:`int`
+        The hero's level
+    max_level: :class:`int`
+        The max level for this hero.
+    village: :class:`str`
+        Either ``home`` or ``builderBase``, indicating which village this hero belongs to.
+    """
+    name: str
+    level: int
+    max_level: int
+    village: str
+    is_active: bool
+    equipment: List["Equipment"]
+
+    id: int
+    range: int
+    dps: int
+    hitpoints: int
+    ground_target: bool
+    speed: int
+    upgrade_cost: int
+    upgrade_resource: "Resource"
+    upgrade_time: "TimeDelta"
+    ability_time: int
+    required_th_level: int
+    regeneration_time: "TimeDelta"
+    is_loaded: bool = False
+
+    def __init__(self, data, townhall):
+        # super().__init__ call fails, hence copy & pasted the parent init
+        self.name: str = data["name"]
+        self.level: int = data["level"]
+        self.max_level: int = data["maxLevel"]
+        self.village: str = data["village"]
+        self.is_active: bool = data.get("superTroopIsActive")
+
+        self._townhall = townhall
+
+        # copies for a static hash
+        self.__name = data['name']
+        self.__level = data['level']
+        self.__village = data['village']
+        self.__is_active = data.get("superTroopIsActive")
+
+        # end of copy & pasted init
+
+        equipment = [try_enum(Equipment, equipment, townhall=townhall) for equipment in data.get('equipment', [])]
+        self.equipment = [eq for eq in equipment if eq is not None]
+
+    def __repr__(self):
+        attrs = [
+            ("name", self.name),
+            ("id", self.id),
+        ]
+        return "<%s %s>" % (
+            self.__class__.__name__, " ".join("%s=%r" % t for t in attrs),)
+
+    @property
+    def is_max_for_townhall(self) -> bool:
+        """:class:`bool`: Returns whether the hero is the max level for the player's townhall level."""
+        if self.is_max:
+            return True
+
+        return self._townhall < self.__class__.required_th_level[self.level]
+
+    @classmethod
+    def get_max_level_for_townhall(cls, townhall):
+        """Get the maximum level for a hero for a given townhall level.
+
+        Parameters
+        ----------
+        townhall
+            The townhall level to get the maximum hero level for.
+
+        Returns
+        --------
+        :class:`int`
+            The maximum spell level.
+
+        """
+        return max(i for i, th in enumerate(cls.required_th_level, start=1) if th <= townhall)
+
+
+class HeroHolder(DataContainerHolder):
+    items: List[Type[Hero]] = []
+    item_lookup: Dict[str, Type[Hero]]
+
+    FILE_PATH = HERO_FILE_PATH
+    data_object = Hero
+
+
+class Pet(DataContainer):
+    """Represents a Pet object as returned by the API, optionally filled with game data.
+
+    Attributes
+    ----------
+    id: :class:`int`
+        The pet's unique ID.
+    name: :class:`str`
+        The pet's name.
+    range: :class:`int`
+        The pet's attack range.
+    dps: :class:`int`
+        The pet's Damage Per Second (DPS).
+    ground_target: :class:`bool`
+        Whether the pet is ground-targetting.
+    hitpoints: :class:`int`
+        The number of hitpoints the troop has at this level.
+    speed: :class:`int`
+        The pet's speed.
+    upgrade_cost: :class:`int`
+        The amount of resources required to upgrade the pet to the next level.
+    upgrade_resource: :class:`Resource`
+        The type of resource used to upgrade this pet.
+    upgrade_time: :class:`TimeDelta`
+        The time taken to upgrade this pet to the next level.
+    is_loaded: :class:`bool`
+        Whether the API data has been loaded for this pet.
+    level: :class:`int`
+        The pet's level
+    max_level: :class:`int`
+        The max level for this pet.
+    village: :class:`str`
+        Either ``home`` or ``builderBase``, indicating which village this pet belongs to.
+    required_th_level: :class:`int`
+        The minimum required townhall to unlock this level of the pet.
+    """
+    name: str
+    level: int
+    max_level: int
+    village: str
+    is_active: bool
+
+    id: int
+    range: int
+    dps: int
+    hitpoints: int
+    ground_target: bool
+    speed: int
+    upgrade_cost: int
+    upgrade_resource: "Resource"
+    upgrade_time: "TimeDelta"
+    is_loaded: bool = False
+    required_th_level: int
+
+    def __repr__(self):
+        attrs = [
+            ("name", self.name),
+            ("id", self.id),
+        ]
+        return "<%s %s>" % (
+            self.__class__.__name__, " ".join("%s=%r" % t for t in attrs),)
+
+    @property
+    def is_max_for_townhall(self) -> bool:
+        """:class:`bool`: Returns whether the hero pet is the max level for the player's townhall level."""
+        if self.is_max:
+            return True
+
+        return self._townhall < self.__class__.required_th_level[self.level]
+
+    @classmethod
+    def get_max_level_for_townhall(cls, townhall):
+        """Get the maximum level for a hero pet for a given townhall level.
+
+        Parameters
+        ----------
+        townhall
+            The townhall level to get the maximum hero pet level for.
+
+        Returns
+        --------
+        :class:`int`
+            The maximum spell level.
+
+        """
+        return max(i for i, th in enumerate(cls.required_th_level, start=1) if th <= townhall)
+
+
+class PetHolder(DataContainerHolder):
+    items: List[Type[Pet]] = []
+    item_lookup: Dict[str, Type[Pet]]
+
+    FILE_PATH = PET_FILE_PATH
+    data_object = Pet
+
+
+class Equipment(DataContainer):
+    """Represents a hero equipment object as returned by the API
+
+        Attributes
+        ----------
+        name: :class:`str`
+            The equipment's name.
+        level: :class:`int`
+            The equipment's level
+        max_level: :class:`int`
+            The max level for this equipment.
+        village: :class:`str`
+            Either ``home`` or ``builderBase``, indicating which village this equipment belongs to.
+    """
+    name: str
+    level: int
+    max_level: int
+    village: str
+
+    @classmethod
+    def _load_json_meta(cls, json_meta, id, name, smithy_to_townhall):
+        cls.id = int(id)
+        cls.name = name
+        cls.smithy_to_townhall = smithy_to_townhall
+
+        cls._json_meta = json_meta
+        smithy_levels = json_meta.get("RequiredBlacksmithLevel")
+        levels_available = [key for key in json_meta.keys() if key.isnumeric()]
+        cls.levels_available = levels_available
+
+        cls.smithy_level = try_enum(UnitStat, smithy_levels)
+        cls.level = cls.smithy_level and UnitStat(range(1, len(cls.smithy_level) + 1))
+        cls.hero_level = try_enum(UnitStat,
+                                  [json_meta.get(level).get("RequiredCharacterLevel") for level in levels_available])
+        cls.speed = try_enum(UnitStat, [json_meta.get(level).get("Speed") for level in levels_available])
+        cls.hitpoints = try_enum(UnitStat, [json_meta.get(level).get("HitPoints") for level in levels_available])
+        cls.attack_range = try_enum(UnitStat, [json_meta.get(level).get("AttackRange") for level in levels_available])
+        cls.dps = try_enum(UnitStat, [json_meta.get(level).get("DPS") for level in levels_available])
+        cls.heal = try_enum(UnitStat, [json_meta.get(level).get("HealOnActivation") for level in levels_available])
+
+        # hacky way to translate internal hero names to English
+        hero = json_meta.get('AllowedCharacters', '').strip(';')
+        hero_map = {"Warrior Princess": "Royal Champion", "Minion Hero": "Minion Prince"}
+        cls.hero = hero_map.get(hero, hero)
+
+        costs = [(int(el) for el in str(cost).split(';')) for cost in
+                 [json_meta.get(level).get('UpgradeCosts') for level in levels_available] if cost]
+
+        resources = [(Resource(el.strip()) for el in resource.split(';')) for resource in
+                     [json_meta.get(level).get('UpgradeResources', '') for level in levels_available]]
+
+        cls.upgrade_cost = try_enum(UnitStat, [[(c, r) for c, r in zip(cost, resource)]
+                                               for cost, resource in zip(costs, resources)])
+
+        cls._is_home_village = True  # todo: update with json key if they add builder base equipment
+        cls.village = "home" if cls._is_home_village else "builderBase"
+
+        cls.is_loaded = True
+        return cls
+
+
+class EquipmentHolder(DataContainerHolder):
+    items: List[Type[Equipment]] = []
+    item_lookup: Dict[str, Type[Equipment]]
+
+    FILE_PATH = EQUIPMENT_FILE_PATH
+    data_object = Equipment
+
+    def _load_json(self, english_aliases, lab_to_townhall):
+        id = 3000
+        with open(EQUIPMENT_FILE_PATH, 'rb') as fp:
+            equipment_data = orjson.loads(fp.read())
+
+        for supercell_name, equipment_meta in equipment_data.items():
+            if not equipment_meta.get("TID"):
+                continue
+
+            # ignore deprecated content
+            if equipment_meta.get("Deprecated") or equipment_meta.get("DisableProduction"):
+                continue
+
+            new_equipment: Type[Equipment] = type('Equipment', Equipment.__bases__, dict(Equipment.__dict__))
+            new_equipment._load_json_meta(
+                equipment_meta,
+                id=id,
+                name=english_aliases[equipment_meta.get("TID")],
+                smithy_to_townhall=lab_to_townhall,
+            )
+            id += 1
+            self.items.append(new_equipment)
+            self.item_lookup[new_equipment.name] = new_equipment
+
+        self.is_loaded = True
+
+    def load(self, data, townhall: int, default: "Equipment" = Equipment, load_game_data: bool = None
+             ) -> Equipment:
+        if load_game_data is True:
+            try:
+                equipment = self.item_lookup[data["name"]]
+            except KeyError:
+                equipment = default
+        else:
+            equipment = default
+
+        return equipment(data=data, townhall=townhall)
+
+    def get(self, name, home_village=True) -> Optional[Type[Equipment]]:
+        try:
+            return self.item_lookup[name]
+        except KeyError:
+            return None
