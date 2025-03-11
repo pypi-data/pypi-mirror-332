69d7092bb6e3e7150abb38ce4ed4bfbe0703a58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:06:07 +0000

    Add package torchrl/0.7.2/torchrl-0.7.2-cp313-cp313-win_amd64.whl

---
+++ packages/torchrl/torchrl-0.7.2-cp313-cp313-win_amd64.whl/torchrl/envs/transforms/__init__.py
@@ -0,0 +1,69 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .gym_transforms import EndOfLifeTransform
+from .r3m import R3MTransform
+from .rb_transforms import MultiStepTransform
+from .rlhf import KLRewardTransform
+from .transforms import (
+    ActionDiscretizer,
+    ActionMask,
+    AutoResetEnv,
+    AutoResetTransform,
+    BatchSizeTransform,
+    BinarizeReward,
+    BurnInTransform,
+    CatFrames,
+    CatTensors,
+    CenterCrop,
+    ClipTransform,
+    Compose,
+    Crop,
+    DeviceCastTransform,
+    DiscreteActionProjection,
+    DoubleToFloat,
+    DTypeCastTransform,
+    ExcludeTransform,
+    FiniteTensorDictCheck,
+    FlattenObservation,
+    FrameSkipTransform,
+    GrayScale,
+    gSDENoise,
+    Hash,
+    InitTracker,
+    LineariseRewards,
+    NoopResetEnv,
+    ObservationNorm,
+    ObservationTransform,
+    PermuteTransform,
+    PinMemoryTransform,
+    RandomCropTensorDict,
+    RemoveEmptySpecs,
+    RenameTransform,
+    Resize,
+    Reward2GoTransform,
+    RewardClipping,
+    RewardScaling,
+    RewardSum,
+    SelectTransform,
+    SignTransform,
+    SqueezeTransform,
+    Stack,
+    StepCounter,
+    TargetReturn,
+    TensorDictPrimer,
+    TimeMaxPool,
+    Tokenizer,
+    ToTensorImage,
+    TrajCounter,
+    Transform,
+    TransformedEnv,
+    UnaryTransform,
+    UnsqueezeTransform,
+    VecGymEnvTransform,
+    VecNorm,
+)
+from .vc1 import VC1Transform
+from .vip import VIPRewardTransform, VIPTransform
