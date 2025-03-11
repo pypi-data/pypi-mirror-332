de70b1413a9d7b9a8cf6a699d550dd0277a52b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:05:47 +0000

    Add package llm2dict/0.0.2/llm2dict-0.0.2-py3-none-any.whl

---
+++ packages/llm2dict/llm2dict-0.0.2-py3-none-any.whl/llm2dict/__init__.py
@@ -0,0 +1,3 @@
+from .llm2dict import llm2dict,LLM_API,add_user,add_system,add_assistant,validate_and_process_nested_dict
+
+__all__ = ['llm2dict','LLM_API', 'add_user','add_system','add_assistant','validate_and_process_nested_dict']  # 可选，明确导出内容
