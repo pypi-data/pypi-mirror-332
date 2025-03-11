d338737d3a42558852020e291003564bad1ef32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:57 +0000

    Add package airoframework/0.1.5/airoframework-0.1.5.tar.gz

---
+++ packages/airoframework/airoframework-0.1.5.tar.gz/airoframework-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="airoframework",
+    version="0.1.5",
+    packages=find_packages(),
+    include_package_data=True,  
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "airoframework=airoframework.cli:create_project",
+        ],
+    },
+    package_data={
+        "airoframework": ["template/**/*"],  
+    },
+)
