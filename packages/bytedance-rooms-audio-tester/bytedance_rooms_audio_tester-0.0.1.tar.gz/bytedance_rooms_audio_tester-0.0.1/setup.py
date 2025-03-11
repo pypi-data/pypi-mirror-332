376ffc7e2cadd699fdd62a5914b55a597cbf3930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 12:05:10 +0000

    Add package bytedance-rooms-audio-tester/0.0.1/bytedance_rooms_audio_tester-0.0.1.tar.gz

---
+++ packages/bytedance-rooms-audio-tester/bytedance_rooms_audio_tester-0.0.1.tar.gz/bytedance_rooms_audio_tester-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_rooms_audio_tester",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_rooms_audio_tester",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_rooms_audio_tester.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
