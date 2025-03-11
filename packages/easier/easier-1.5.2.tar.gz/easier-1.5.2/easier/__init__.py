f4e7add7bfbe27650cd7fb1bf6354e1be8f44fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 11:06:10 +0000

    Add package easier/1.5.2/easier-1.5.2.tar.gz

---
+++ packages/easier/easier-1.5.2.tar.gz/easier-1.5.2/easier/__init__.py
@@ -0,0 +1,25 @@
+# flake8: noqa
+from .api import API, Importable
+
+# This object is loaded with descriptors that lazy import items from modules
+tools = API()
+
+# This list will hold all the items the package will export
+dir_list = []
+
+
+# We don't actually define any of the import in the namespace.
+# We rely on this function to lazy load them
+def __getattr__(name):
+    return getattr(tools, name)
+
+
+# We cycle over the api items and add their names to the exportable namespace
+for att_name, att_obj in API.__dict__.items():
+    if isinstance(att_obj, Importable):
+        dir_list.append(att_name)
+
+
+# (I)Python looks at this function to determine what is exported
+def __dir__():
+    return dir_list
