7718a328436a03a3dc3fd8355065682f4452432d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:07:03 +0000

    Add package maze-dataset/1.2.0/maze_dataset-1.2.0-py3-none-any.whl

---
+++ packages/maze-dataset/maze_dataset-1.2.0-py3-none-any.whl/maze_dataset/tokenization/__init__.py
@@ -0,0 +1,52 @@
+"""turning a maze into text
+
+- `MazeTokenizerModular` is the new recommended way to do this as of 1.0.0
+- legacy `TokenizationMode` enum and `MazeTokenizer` class for supporting existing code
+- a whole lot of helper classes and functions
+
+"""
+
+from maze_dataset.tokenization.maze_tokenizer import (
+	AdjListTokenizers,
+	CoordTokenizers,
+	EdgeGroupings,
+	EdgePermuters,
+	EdgeSubsets,
+	MazeTokenizer,
+	MazeTokenizerModular,
+	PathTokenizers,
+	PromptSequencers,
+	StepSizes,
+	StepTokenizers,
+	TargetTokenizers,
+	TokenizationMode,
+	_TokenizerElement,
+	get_tokens_up_to_path_start,
+)
+
+# we don't sort alphabetically on purpose, we sort by the type
+__all__ = [
+	# submodules
+	"all_tokenizers",
+	"maze_tokenizer",
+	"save_hashes",
+	# modular maze tokenization components
+	"TokenizationMode",
+	"_TokenizerElement",
+	"MazeTokenizerModular",
+	"PromptSequencers",
+	"CoordTokenizers",
+	"AdjListTokenizers",
+	"EdgeGroupings",
+	"EdgePermuters",
+	"EdgeSubsets",
+	"TargetTokenizers",
+	"StepSizes",
+	"StepTokenizers",
+	"PathTokenizers",
+	# helpers
+	"coord_str_to_tuple",
+	"get_tokens_up_to_path_start",
+	# old tokenizer
+	"MazeTokenizer",
+]
