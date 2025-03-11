cc72f52f9b238febac9f64ce1094a08d27efc787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:05:54 +0000

    Add package bytedance-dataset/0.0.1/bytedance_dataset-0.0.1.tar.gz

---
+++ packages/bytedance-dataset/bytedance_dataset-0.0.1.tar.gz/bytedance_dataset-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_dataset",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_dataset",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_dataset.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
