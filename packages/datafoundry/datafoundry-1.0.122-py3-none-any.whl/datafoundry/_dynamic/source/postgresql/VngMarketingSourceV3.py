fc83326f389ab50fd838f3386b0b93a697fc0ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:21 +0000

    Add package datafoundry/1.0.122/datafoundry-1.0.122-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.122-py3-none-any.whl/datafoundry/_dynamic/source/postgresql/VngMarketingSourceV3.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class VngMarketingSourceV3(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67ce7f6f63c7f6c1881cb7c7"
+# --- render code block -----
