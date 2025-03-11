677e7d84ef20461af1bd713637b5682a06460156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:07:12 +0000

    Add package byted-overpass-toutiao-kitex-rgo-multiservice/0.0.1/byted_overpass_toutiao_kitex_rgo_multiservice-0.0.1.tar.gz

---
+++ packages/byted-overpass-toutiao-kitex-rgo-multiservice/byted_overpass_toutiao_kitex_rgo_multiservice-0.0.1.tar.gz/byted_overpass_toutiao_kitex_rgo_multiservice-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_toutiao_kitex_rgo_multiservice",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_toutiao_kitex_rgo_multiservice",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_toutiao_kitex_rgo_multiservice.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
