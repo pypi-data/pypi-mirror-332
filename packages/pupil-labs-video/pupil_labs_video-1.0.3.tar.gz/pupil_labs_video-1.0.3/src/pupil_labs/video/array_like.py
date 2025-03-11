3dbee7e4ad565b2cbcaac74e296bb4c19e72b789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:22 +0000

    Add package pupil-labs-video/1.0.3/pupil_labs_video-1.0.3.tar.gz

---
+++ packages/pupil-labs-video/pupil_labs_video-1.0.3.tar.gz/pupil_labs_video-1.0.3/src/pupil_labs/video/array_like.py
@@ -0,0 +1,20 @@
+from typing import (
+    Iterator,
+    Protocol,
+    TypeVar,
+    overload,
+)
+
+ArrayLikeItemType = TypeVar("ArrayLikeItemType", covariant=True)
+
+
+class ArrayLike(Protocol[ArrayLikeItemType]):
+    def __len__(self) -> int: ...
+    @overload
+    def __getitem__(self, key: int, /) -> ArrayLikeItemType: ...
+    @overload
+    def __getitem__(self, key: slice, /) -> "ArrayLike[ArrayLikeItemType]": ...
+    def __getitem__(
+        self, key: int | slice, /
+    ) -> ArrayLikeItemType | "ArrayLike[ArrayLikeItemType]": ...
+    def __iter__(self) -> Iterator[ArrayLikeItemType]: ...
