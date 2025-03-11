fddc509017f83d9d78111f7074cba2a445c92e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:36 +0000

    Add package dota098745/0.0.1/dota098745-0.0.1.tar.gz

---
+++ packages/dota098745/dota098745-0.0.1.tar.gz/dota098745-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name= 'dota098745', # Имя пакета
+    version= '0.0.1', # Версия пакета
+    author= 'Laptev Artem',
+    author_email = 'Artemlaptev05@mail.ru' ,
+    description= 'My Package' ,
+    packages=setuptools.find_packages(),
+)
