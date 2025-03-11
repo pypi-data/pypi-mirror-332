02b6a71491ed0f541bde115731fc237305b657a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:07:32 +0000

    Add package flake8-tergeo/0.0.1/flake8_tergeo-0.0.1.tar.gz

---
+++ packages/flake8-tergeo/flake8_tergeo-0.0.1.tar.gz/flake8_tergeo-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flake8-tergeo',
+    version='0.0.1',
+    description='PoC',
+    long_description=open('README.md').read(),  # Remove CHANGELOG.txt
+    long_description_content_type='text/markdown',  # Specify Markdown format
+    url='https://github.com/sjnscythe',
+    author='scythe abhi',
+    author_email='scytheabhi97@gmail.com',
+    license='MIT',
+    keywords='PoC',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
