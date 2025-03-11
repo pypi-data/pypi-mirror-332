280dbb36439ad91b3458e272c3f506579397a6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:06:15 +0000

    Add package billybob-123/0.0.1/billybob_123-0.0.1.tar.gz

---
+++ packages/billybob-123/billybob_123-0.0.1.tar.gz/billybob_123-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='billybob_123', # Имя пакета
+    version='0.0.1', # Версия пакета
+    author='Your Name',
+    author_email='vadimus.05ev@mail.ru',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
