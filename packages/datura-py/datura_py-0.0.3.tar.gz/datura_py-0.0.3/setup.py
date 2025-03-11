5365f20f25c47bc2deb1b8411ad8fd20c3feab8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:46 +0000

    Add package datura-py/0.0.3/datura_py-0.0.3.tar.gz

---
+++ packages/datura-py/datura_py-0.0.3.tar.gz/datura_py-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datura_py",
+    version="0.0.3",
+    description="A Python SDK for interacting with the Datura API service.",
+    author="Datura",
+    author_email="",
+    license="MIT",
+    package_data={"datura_py": ["py.typed"]},
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["requests", "typing-extensions"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
