541a47ce5c4d25912d15ffc867c10444107fcf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 11:06:02 +0000

    Add package collective.js.galleria/1.7.0/collective_js_galleria-1.7.0-py3-none-any.whl

---
+++ packages/collective.js.galleria/collective_js_galleria-1.7.0-py3-none-any.whl/collective/js/galleria/setuphandlers.py
@@ -0,0 +1,22 @@
+from Products.CMFPlone.interfaces import INonInstallable
+from zope.interface import implementer
+
+
+@implementer(INonInstallable)
+class HiddenProfiles:
+    def getNonInstallableProducts(self):
+        """Hide the upgrades package from prefs_install_products_form."""
+        return ["collective.js.galleria.upgrades"]
+
+    def getNonInstallableProfiles(self):
+        """Hide profiles from site-creation and quickinstaller."""
+        return ["collective.js.galleria:uninstall"]
+
+
+def post_install(context):
+    """Post install script"""
+
+
+def uninstall(context):
+    """Uninstall script"""
+    # Do something at the end of the uninstallation of this package.
---
+++ packages/collective.js.galleria/collective_js_galleria-1.7.0-py3-none-any.whl/collective/js/galleria/testing.py
@@ -0,0 +1,44 @@
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.testing.zope import WSGI_SERVER
+
+import collective.js.galleria
+
+
+class Fixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        self.loadZCML(package=collective.js.galleria)
+        self.loadZCML(name="tests/configure.zcml", package=collective.js.galleria)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, "collective.js.galleria:default")
+
+
+FIXTURE = Fixture()
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(FIXTURE,),
+    name="collective.js.galleria:Integration",
+)
+
+
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(FIXTURE, WSGI_SERVER),
+    name="collective.js.galleria:Functional",
+)
+
+
+COLLECTIVE_JS_GALLERIA_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        WSGI_SERVER,
+    ),
+    name="CollectiveJsGalleriaLayer:AcceptanceTesting",
+)
