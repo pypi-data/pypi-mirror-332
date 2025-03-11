536470644ea86658c54431691f50ad4155e3de67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:40 +0000

    Add package color-config/0.1.0/color_config-0.1.0-py3-none-any.whl

---
+++ packages/color-config/color_config-0.1.0-py3-none-any.whl/color_config/__init__.py
@@ -0,0 +1,11 @@
+from colorama import Fore, Style
+
+COLORS = {
+    "header": Fore.YELLOW,
+    "prompt": Fore.CYAN,
+    "success": Fore.GREEN,
+    "error": Fore.RED,
+    "info": Fore.BLUE,
+    "highlight": Fore.MAGENTA,
+    "reset": Style.RESET_ALL
+}
