868a852060499b8160dab24ef0d400975833a5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:27 +0000

    Add package fmu-tools/1.19.2/fmu_tools-1.19.2.tar.gz

---
+++ packages/fmu-tools/fmu_tools-1.19.2.tar.gz/fmu_tools-1.19.2/docs/index.rst
@@ -0,0 +1,34 @@
+Welcome to fmu-tools' documentation!
+======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   fmudesign
+   qcforward
+   qcproperties
+   qcreset
+   generate_petro_jobs_for_field_update
+   properties
+   domain_conversion
+   create_rft_ertobs
+   rename_rms_scripts
+   rms_import_localmodule
+   generate_bw_per_facies
+   update_petro_real
+   export_and_import_fields
+   utilities
+   rmsvolumetrics2csv
+   ensembles
+   contributing
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/fmu-tools/fmu_tools-1.19.2.tar.gz/fmu_tools-1.19.2/src/fmu/tools/rms/__init__.py
@@ -0,0 +1,25 @@
+"""
+Initialize modules for use in RMS
+"""
+
+from .generate_bw_per_facies import create_bw_per_facies
+from .generate_petro_jobs_for_field_update import (
+    main as generate_petro_jobs,
+)
+from .import_localmodules import import_localmodule
+from .update_petro_real import (
+    export_initial_field_parameters,
+    import_updated_field_parameters,
+    update_petro_real as update_petro_parameters,
+)
+from .volumetrics import rmsvolumetrics_txt2df
+
+__all__ = [
+    "rmsvolumetrics_txt2df",
+    "import_localmodule",
+    "generate_petro_jobs",
+    "create_bw_per_facies",
+    "update_petro_parameters",
+    "import_updated_field_parameters",
+    "export_initial_field_parameters",
+]
