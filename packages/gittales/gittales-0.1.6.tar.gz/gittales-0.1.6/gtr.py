83bdd1dae72cf7f979ede2a1010e88987105130c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:50 +0000

    Add package gittales/0.1.6/gittales-0.1.6.tar.gz

---
+++ packages/gittales/gittales-0.1.6.tar.gz/gittales-0.1.6/gtr.py
@@ -0,0 +1,11 @@
+# /// script
+# dependencies = [
+#   "gittales",
+# ]
+# ///
+
+# Simple wrapper to run GitTales directly with uv
+from gittales.cli import main
+
+if __name__ == "__main__":
+    main()
