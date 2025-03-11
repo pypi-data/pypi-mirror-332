673ecc37293457670e6359825a0b2c200947b8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:07:38 +0000

    Add package tomassi-memegen/0.1.1/tomassi_memegen-0.1.1.tar.gz

---
+++ packages/tomassi-memegen/tomassi_memegen-0.1.1.tar.gz/tomassi_memegen-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tomassi-memegen',
+    version='0.1.1',
+    description='A module to create memes with text on images',
+    author='Tomáš Ivan',
+    author_email='tomas.ivan.it@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow', 
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'create-meme=memegen.meme:create_meme',
+        ],
+    },
+)
