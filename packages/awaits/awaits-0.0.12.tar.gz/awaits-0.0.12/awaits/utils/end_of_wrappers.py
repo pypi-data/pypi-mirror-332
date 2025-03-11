ad51ff4e691283fa5f2726e07ced67e3493e7b6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:13:52 +0000

    Add package awaits/0.0.12/awaits-0.0.12.tar.gz

---
+++ packages/awaits/awaits-0.0.12.tar.gz/awaits-0.0.12/awaits/utils/end_of_wrappers.py
@@ -0,0 +1,14 @@
+from typing import Tuple, Callable, Any, Union
+
+from awaits.errors import IncorrectUseOfTheDecoratorError
+
+
+def end_of_wrappers(args: Tuple[Callable[..., Any], ...], wrapper: Callable[[Callable[..., Any]], Callable[..., Any]]) -> Union[Callable[[Callable[..., Any]], Callable[..., Any]], Callable[..., Any]]:
+    """
+    Определяем, как вызван декоратор - как фабрика декораторов (т. е. без позиционных аргументов) или как непосредственный декоратор.
+    """
+    if not len(args):
+        return wrapper
+    elif len(args) == 1 and callable(args[0]):
+        return wrapper(args[0])
+    raise IncorrectUseOfTheDecoratorError('You used the awaitable decorator incorrectly. Read the documentation.')
