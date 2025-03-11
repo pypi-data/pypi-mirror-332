5a8e921ce30e59ddc1db625d3b3cf8d2af1e01fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:17 +0000

    Add package bytedance-genai-utils/0.0.1/bytedance_genai_utils-0.0.1.tar.gz

---
+++ packages/bytedance-genai-utils/bytedance_genai_utils-0.0.1.tar.gz/bytedance_genai_utils-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_genai_utils",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_genai_utils",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_genai_utils.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
