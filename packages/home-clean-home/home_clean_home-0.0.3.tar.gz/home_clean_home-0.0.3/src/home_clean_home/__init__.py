99b83d9ca1d5bafb42a0bbee973382739ac91eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:18 +0000

    Add package home-clean-home/0.0.3/home_clean_home-0.0.3.tar.gz

---
+++ packages/home-clean-home/home_clean_home-0.0.3.tar.gz/home_clean_home-0.0.3/src/home_clean_home/__init__.py
@@ -0,0 +1,12 @@
+from . import server
+import asyncio
+
+
+def main():
+    """Main entry point for the package."""
+    print("__INIT__ main script")
+    asyncio.run(server.main())
+
+
+# Optionally expose other important items at package level
+__all__ = ["main", "server"]
