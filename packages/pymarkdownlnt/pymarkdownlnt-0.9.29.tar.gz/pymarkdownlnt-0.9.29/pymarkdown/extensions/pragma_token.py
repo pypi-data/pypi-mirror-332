de118e76e0e34dda93930b5434bfca20f44e3143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:17 +0000

    Add package pymarkdownlnt/0.9.29/pymarkdownlnt-0.9.29.tar.gz

---
+++ packages/pymarkdownlnt/pymarkdownlnt-0.9.29.tar.gz/pymarkdownlnt-0.9.29/pymarkdown/extensions/pragma_token.py
@@ -0,0 +1,440 @@
+"""
+Module to provide for linter instructions that can be embedded within the document.
+"""
+
+import logging
+from typing import Dict, List, Optional, Set, Tuple
+
+from application_properties import ApplicationPropertiesFacade
+from typing_extensions import Protocol
+
+from pymarkdown.container_blocks.parse_block_pass_properties import (
+    ParseBlockPassProperties,
+)
+from pymarkdown.extension_manager.extension_impl import ExtensionDetails
+from pymarkdown.extension_manager.extension_manager_constants import (
+    ExtensionManagerConstants,
+)
+from pymarkdown.extension_manager.parser_extension import ParserExtension
+from pymarkdown.general.constants import Constants
+from pymarkdown.general.parser_helper import ParserHelper
+from pymarkdown.general.parser_logger import ParserLogger
+from pymarkdown.general.position_marker import PositionMarker
+from pymarkdown.plugin_manager.found_plugin import FoundPlugin
+from pymarkdown.tokens.markdown_token import MarkdownToken, MarkdownTokenClass
+from pymarkdown.transform_gfm.transform_state import TransformState
+from pymarkdown.transform_markdown.markdown_transform_context import (
+    RegisterHtmlTransformHandlersProtocol,
+    RegisterMarkdownTransformHandlersProtocol,
+)
+
+POGGER = ParserLogger(logging.getLogger(__name__))
+
+
+# pylint: disable=too-few-public-methods
+class LogPragmaFailureProtocol(Protocol):
+    """
+    Protocol to specify a function that allows failures to be reported.
+    """
+
+    def __call__(
+        self, scan_file: str, line_number: int, pragma_error: str
+    ) -> None: ...  # pragma: no cover
+
+
+# pylint: enable=too-few-public-methods
+
+
+class PragmaExtension(ParserExtension):
+    """
+    Extension to implement the pragma extensions.
+    """
+
+    def get_identifier(self) -> str:
+        """
+        Get the identifier associated with this extension.
+        """
+        return "linter-pragmas"
+
+    def get_details(self) -> ExtensionDetails:
+        """
+        Get the details for the extension.
+        """
+        return ExtensionDetails(
+            extension_id=self.get_identifier(),
+            extension_name="Pragma Linter Instructions",
+            extension_description="Allows parsing of instructions for the linter.",
+            extension_enabled_by_default=True,
+            extension_version="0.5.0",
+            extension_interface_version=ExtensionManagerConstants.EXTENSION_INTERFACE_VERSION_BASIC,
+            extension_url="https://pymarkdown.readthedocs.io/en/latest/extensions/pragmas/",
+            extension_configuration=None,
+        )
+
+    def apply_configuration(
+        self, extension_specific_facade: ApplicationPropertiesFacade
+    ) -> None:
+        """
+        Apply any configuration required by the extension.
+        """
+        _ = extension_specific_facade
+
+    @staticmethod
+    def look_for_pragmas(
+        position_marker: PositionMarker,
+        line_to_parse: str,
+        container_depth: int,
+        extracted_whitespace: Optional[str],
+        parser_properties: ParseBlockPassProperties,
+    ) -> bool:
+        """
+        Look for a pragma in the current line.
+        """
+
+        POGGER.debug("look_for_pragmas - >$<", line_to_parse)
+        POGGER.debug("look_for_pragmas - ws >$<", extracted_whitespace)
+        if (
+            not container_depth
+            and not extracted_whitespace
+            and (
+                line_to_parse.startswith(PragmaToken.pragma_prefix)
+                or line_to_parse.startswith(PragmaToken.pragma_alternate_prefix)
+            )
+        ):
+            was_extended_prefix = line_to_parse.startswith(
+                PragmaToken.pragma_alternate_prefix
+            )
+
+            start_index, _ = ParserHelper.extract_spaces(
+                line_to_parse,
+                len(
+                    PragmaToken.pragma_alternate_prefix
+                    if was_extended_prefix
+                    else PragmaToken.pragma_prefix
+                ),
+            )
+            remaining_line = (
+                line_to_parse[start_index:].rstrip(Constants.ascii_whitespace).lower()
+            )
+            if remaining_line.startswith(
+                PragmaToken.pragma_title
+            ) and remaining_line.endswith(PragmaToken.pragma_suffix):
+                index_number = (
+                    -position_marker.line_number
+                    if was_extended_prefix
+                    else position_marker.line_number
+                )
+                parser_properties.pragma_lines[index_number] = line_to_parse
+                POGGER.debug("pragma $ extracted - >$<", index_number, line_to_parse)
+                return True
+        POGGER.debug("pragma not extracted - >$<", line_to_parse)
+        return False
+
+    # pylint: disable=too-many-arguments
+    @staticmethod
+    def compile_single_pragma(
+        scan_file: str,
+        next_line_number: int,
+        pragma_lines: Dict[int, str],
+        all_ids: Dict[str, FoundPlugin],
+        document_pragmas: Dict[int, Set[str]],
+        document_pragma_ranges: List[Tuple[int, int, Set[str]]],
+        log_pragma_failure: LogPragmaFailureProtocol,
+    ) -> None:
+        """
+        Compile a single pragma line, validating it before adding it to the dictionary of pragmas.
+        """
+        if next_line_number > 0:
+            prefix_length = len(PragmaToken.pragma_prefix)
+            actual_line_number = next_line_number
+        else:
+            prefix_length = len(PragmaToken.pragma_alternate_prefix)
+            actual_line_number = -next_line_number
+
+        line_after_prefix = pragma_lines[next_line_number][prefix_length:]
+        after_whitespace_index, _ = ParserHelper.extract_spaces_verified(
+            line_after_prefix, 0
+        )
+        after_whitespace_index, _ = ParserHelper.extract_spaces_verified(
+            line_after_prefix, after_whitespace_index + len(PragmaToken.pragma_title)
+        )
+        command_data = line_after_prefix[
+            after_whitespace_index : -len(PragmaToken.pragma_suffix)
+        ]
+        after_command_index, command = ParserHelper.extract_until_spaces_verified(
+            command_data, 0
+        )
+        command = command.lower()
+        if not command:
+            log_pragma_failure(
+                scan_file,
+                actual_line_number,
+                "Inline configuration specified without command.",
+            )
+        elif command == "disable-next-line":
+            PragmaExtension.__handle_disable_next_line(
+                command_data,
+                after_command_index,
+                log_pragma_failure,
+                scan_file,
+                actual_line_number,
+                document_pragmas,
+                all_ids,
+                command,
+            )
+        elif command == "disable-num-lines":
+            PragmaExtension.__handle_disable_num_lines(
+                command_data,
+                after_command_index,
+                log_pragma_failure,
+                scan_file,
+                actual_line_number,
+                document_pragma_ranges,
+                all_ids,
+                command,
+            )
+        else:
+            log_pragma_failure(
+                scan_file,
+                actual_line_number,
+                f"Inline configuration command '{command}' not understood.",
+            )
+
+    # pylint: enable=too-many-arguments
+
+    # pylint: disable=too-many-arguments
+    @staticmethod
+    def __handle_disable_next_line(
+        command_data: str,
+        after_command_index: int,
+        log_pragma_failure: LogPragmaFailureProtocol,
+        scan_file: str,
+        actual_line_number: int,
+        document_pragmas: Dict[int, Set[str]],
+        all_ids: Dict[str, FoundPlugin],
+        command: str,
+    ) -> None:
+        ids_to_disable = command_data[after_command_index:].split(",")
+        processed_ids = set()
+        for next_id in ids_to_disable:
+            next_id = next_id.strip(" ").lower()
+            if not next_id:
+                log_pragma_failure(
+                    scan_file,
+                    actual_line_number,
+                    f"Inline configuration command '{command}' specified a plugin with a blank id.",
+                )
+            elif next_id in all_ids:
+                normalized_id = all_ids[next_id].plugin_id
+                processed_ids.add(normalized_id)
+            else:
+                log_pragma_failure(
+                    scan_file,
+                    actual_line_number,
+                    f"Inline configuration command '{command}' unable to find a plugin with the id '{next_id}'.",
+                )
+
+        if processed_ids:
+            document_pragmas[actual_line_number + 1] = processed_ids
+
+    # pylint: enable=too-many-arguments
+    # pylint: disable=too-many-arguments
+
+    @staticmethod
+    def __handle_disable_num_lines_parse(
+        command_data: str,
+        after_command_index: int,
+        log_pragma_failure: LogPragmaFailureProtocol,
+        scan_file: str,
+        actual_line_number: int,
+        command: str,
+    ) -> Tuple[bool, int, Optional[int]]:
+        after_space_index, _ = ParserHelper.extract_spaces_verified(
+            command_data, after_command_index
+        )
+        if after_space_index == len(command_data):
+            log_pragma_failure(
+                scan_file,
+                actual_line_number,
+                f"Inline configuration command '{command}' was not followed by a count and a list of plugin ids to temporarily disable.",
+            )
+            return False, -1, None
+        after_num_index, extracted_number = ParserHelper.extract_until_spaces_verified(
+            command_data, after_space_index
+        )
+
+        try:
+            count_value = int(extracted_number)
+        except ValueError:
+            count_value = -1
+        if count_value < 1:
+            log_pragma_failure(
+                scan_file,
+                actual_line_number,
+                f"Inline configuration command '{command}' specified a count '{extracted_number}' that is not a valid positive integer.",
+            )
+            return False, -1, None
+
+        after_number_index, _ = ParserHelper.extract_spaces_verified(
+            command_data, after_num_index
+        )
+        if after_number_index == len(command_data):
+            log_pragma_failure(
+                scan_file,
+                actual_line_number,
+                f"Inline configuration command '{command}' and its count were not followed by a list of plugin ids to temporarily disable.",
+            )
+            return False, -1, None
+        return True, after_number_index, count_value
+
+    # pylint: enable=too-many-arguments
+
+    # pylint: disable=too-many-arguments, too-many-locals
+    @staticmethod
+    def __handle_disable_num_lines(
+        command_data: str,
+        after_command_index: int,
+        log_pragma_failure: LogPragmaFailureProtocol,
+        scan_file: str,
+        actual_line_number: int,
+        document_pragma_ranges: List[Tuple[int, int, Set[str]]],
+        all_ids: Dict[str, FoundPlugin],
+        command: str,
+    ) -> None:
+        (
+            is_ok,
+            after_number_index,
+            count_value,
+        ) = PragmaExtension.__handle_disable_num_lines_parse(
+            command_data,
+            after_command_index,
+            log_pragma_failure,
+            scan_file,
+            actual_line_number,
+            command,
+        )
+        if not is_ok:
+            return
+
+        ids_to_disable = command_data[after_number_index:].split(",")
+        processed_ids = set()
+        for next_id in ids_to_disable:
+            next_id = next_id.strip(" ").lower()
+            if not next_id:
+                log_pragma_failure(
+                    scan_file,
+                    actual_line_number,
+                    f"Inline configuration command '{command}' specified a plugin with a blank id.",
+                )
+            elif next_id in all_ids:
+                normalized_id = all_ids[next_id].plugin_id
+                processed_ids.add(normalized_id)
+            else:
+                log_pragma_failure(
+                    scan_file,
+                    actual_line_number,
+                    f"Inline configuration command '{command}' unable to find a plugin with the id '{next_id}'.",
+                )
+
+        if processed_ids:
+            assert count_value is not None
+            pragma_tuple = (
+                actual_line_number + 1,
+                actual_line_number + count_value,
+                processed_ids,
+            )
+            document_pragma_ranges.append(pragma_tuple)
+
+    # pylint: enable=too-many-arguments, too-many-locals
+
+
+class PragmaToken(MarkdownToken):
+    """
+    Token that contains the pragmas for the document.
+    """
+
+    pragma_prefix = "<!--"
+    pragma_alternate_prefix = "<!---"
+    pragma_title = "pyml "
+    pragma_suffix = "-->"
+
+    def __init__(self, pragma_lines: Dict[int, str]) -> None:
+        self.__pragma_lines = pragma_lines
+
+        MarkdownToken.__init__(
+            self,
+            MarkdownToken._token_pragma,
+            MarkdownTokenClass.SPECIAL,
+            is_extension=True,
+            extra_data="",
+        )
+        self.__compose_extra_data_field()
+
+    def __compose_extra_data_field(self) -> None:
+        """
+        Compose the object's self.extra_data field from the local object's variables.
+        """
+        serialized_pragmas = "".join(
+            f";{next_line_number}:{self.__pragma_lines[next_line_number]}"
+            for next_line_number in self.__pragma_lines
+        )
+        self._set_extra_data(serialized_pragmas[1:])
+
+    @staticmethod
+    def get_markdown_token_type() -> str:
+        """
+        Get the type of markdown token for rehydration purposes.
+        """
+        return MarkdownToken._token_pragma
+
+    @property
+    def pragma_lines(self) -> Dict[int, str]:
+        """
+        Returns the pragma lines for the document.
+        """
+        return self.__pragma_lines
+
+    def adjust_pragma_line_number(
+        self, initial_line_number: int, new_line_number: int
+    ) -> None:
+        """Perform an adjustment to the line number of a given pragma."""
+        old_pragma = self.__pragma_lines[initial_line_number]
+        del self.__pragma_lines[initial_line_number]
+        self.__pragma_lines[new_line_number] = old_pragma
+        self.__compose_extra_data_field()
+
+    def register_for_markdown_transform(
+        self,
+        registration_function: RegisterMarkdownTransformHandlersProtocol,
+    ) -> None:
+        """
+        Register any rehydration handlers for leaf markdown tokens.
+        """
+
+        # Note, because the Pragma token contains every pragma contained
+        # within the file, this is handled globally in TransformToMarkdown's
+        # __handle_pragma_processing.
+        _ = registration_function
+
+    @staticmethod
+    def register_for_html_transform(
+        register_handlers: RegisterHtmlTransformHandlersProtocol,
+    ) -> None:
+        """
+        Register any functions required to generate HTML from the tokens.
+        """
+        register_handlers(
+            PragmaToken,
+            PragmaToken.__handle_pragma_token,
+            None,
+        )
+
+    @staticmethod
+    def __handle_pragma_token(
+        output_html: str,
+        next_token: MarkdownToken,
+        transform_state: TransformState,
+    ) -> str:
+        _ = (transform_state, next_token)
+
+        return output_html
---
+++ packages/pymarkdownlnt/pymarkdownlnt-0.9.29.tar.gz/pymarkdownlnt-0.9.29/pymarkdown/general/parser_state.py
@@ -0,0 +1,303 @@
+"""
+Module to provide for an encapsulation of the high level state of the parser.
+"""
+
+from __future__ import annotations
+
+import copy
+from typing import List, Optional, Tuple, cast
+
+from typing_extensions import Protocol
+
+from pymarkdown.container_blocks.parse_block_pass_properties import (
+    ParseBlockPassProperties,
+)
+from pymarkdown.general.position_marker import PositionMarker
+from pymarkdown.general.requeue_line_info import RequeueLineInfo
+from pymarkdown.tokens.block_quote_markdown_token import BlockQuoteMarkdownToken
+from pymarkdown.tokens.markdown_token import MarkdownToken
+from pymarkdown.tokens.stack_token import ListStackToken, StackToken
+
+
+# pylint: disable=too-few-public-methods
+class CloseOpenBlocksProtocol(Protocol):
+    """
+    Protocol to provide typing for the close open blocks function.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __call__(
+        self,
+        parser_state: ParserState,
+        destination_array: Optional[List[MarkdownToken]] = None,
+        only_these_blocks: Optional[List[type]] = None,
+        include_block_quotes: bool = False,
+        include_lists: bool = False,
+        until_this_index: int = -1,
+        caller_can_handle_requeue: bool = False,
+        requeue_reset: bool = False,
+        was_forced: bool = False,
+    ) -> Tuple[List[MarkdownToken], Optional[RequeueLineInfo]]: ...  # pragma: no cover
+
+    # pylint: enable=too-many-arguments
+
+
+# pylint: enable=too-few-public-methods
+
+
+# pylint: disable=too-few-public-methods
+class HandleBlankLineProtocol(Protocol):
+    """
+    Protocol to provide typing for the blank line function.
+    """
+
+    def __call__(
+        self,
+        parser_state: ParserState,
+        input_line: str,
+        from_main_transform: bool,
+        position_marker: Optional[PositionMarker] = None,
+    ) -> Tuple[
+        Optional[List[MarkdownToken]], Optional[RequeueLineInfo]
+    ]: ...  # pragma: no cover
+
+
+# pylint: enable=too-few-public-methods
+
+
+# pylint: disable=too-many-instance-attributes
+class ParserState:
+    """
+    Class to provide for an encapsulation of the high level state of the parser.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        token_stack: List[StackToken],
+        token_document: List[MarkdownToken],
+        close_open_blocks_fn: CloseOpenBlocksProtocol,
+        handle_blank_line_fn: HandleBlankLineProtocol,
+        parse_properties: ParseBlockPassProperties,
+    ) -> None:
+        (
+            self.__token_stack,
+            self.__token_document,
+            self.__close_open_blocks_fn,
+            self.__handle_blank_line_fn,
+        ) = (token_stack, token_document, close_open_blocks_fn, handle_blank_line_fn)
+
+        self.__no_para_start_if_empty: bool = False
+        self.__last_block_quote_stack_token: Optional[StackToken] = None
+        self.__last_block_quote_markdown_token_index: Optional[int] = None
+        self.__copy_of_last_block_quote_markdown_token: Optional[
+            BlockQuoteMarkdownToken
+        ] = None
+        self.__original_stack_depth: int = 0
+        self.__original_document_depth: int = 0
+        self.__original_line_to_parse: Optional[str] = None
+        self.__same_line_container_tokens: Optional[List[MarkdownToken]] = None
+        self.nested_list_start: Optional[ListStackToken] = None
+        self.copy_of_token_stack: List[StackToken] = []
+        self.block_copy: List[Optional[MarkdownToken]] = []
+        self.parse_properties = parse_properties
+
+    # pylint: enable=too-many-arguments
+
+    @property
+    def token_stack(self) -> List[StackToken]:
+        """
+        Stack array containing the currently active stack tokens.
+        """
+        return self.__token_stack
+
+    @property
+    def token_document(self) -> List[MarkdownToken]:
+        """
+        Document array containing any Markdown tokens.
+        """
+        return self.__token_document
+
+    @property
+    def close_open_blocks_fn(self) -> CloseOpenBlocksProtocol:
+        """
+        Function to handle the closing of blocks.
+        """
+        return self.__close_open_blocks_fn
+
+    @property
+    def handle_blank_line_fn(self) -> HandleBlankLineProtocol:
+        """
+        Function to handle blank lines.
+        """
+        return self.__handle_blank_line_fn
+
+    @property
+    def same_line_container_tokens(self) -> Optional[List[MarkdownToken]]:
+        """
+        State of the container tokens at the start of the leaf processing.
+        """
+        return self.__same_line_container_tokens
+
+    @property
+    def last_block_quote_stack_token(self) -> Optional[StackToken]:
+        """
+        Last block quote token.
+        """
+        return self.__last_block_quote_stack_token
+
+    @property
+    def last_block_quote_markdown_token_index(self) -> Optional[int]:
+        """
+        Index of the last block quote token.
+        """
+        return self.__last_block_quote_markdown_token_index
+
+    @property
+    def copy_of_last_block_quote_markdown_token(
+        self,
+    ) -> Optional[BlockQuoteMarkdownToken]:
+        """
+        Copy of the last block quote markdown token, before any changes.
+        """
+        return self.__copy_of_last_block_quote_markdown_token
+
+    @property
+    def original_line_to_parse(self) -> Optional[str]:
+        """
+        Line to parse, before any processing occurs.
+        """
+        return self.__original_line_to_parse
+
+    @property
+    def original_stack_depth(self) -> int:
+        """
+        Length of the token_stack array at the start of the line.
+        """
+        return self.__original_stack_depth
+
+    @property
+    def original_document_depth(self) -> int:
+        """
+        Length of the token_document array at the start of the line.
+        """
+        return self.__original_document_depth
+
+    @property
+    def no_para_start_if_empty(self) -> bool:
+        """
+        Whether to start a paragraph if the owning list item was empty.
+        """
+        return self.__no_para_start_if_empty
+
+    def find_last_block_quote_on_stack(self) -> int:
+        """
+        Finds the index of the last block quote on the stack (from the end).
+        If no block quotes are found, 0 is returned.
+        """
+        last_stack_index = len(self.token_stack) - 1
+        while (
+            not self.token_stack[last_stack_index].is_document
+            and not self.token_stack[last_stack_index].is_block_quote
+        ):
+            last_stack_index -= 1
+        return last_stack_index
+
+    def find_last_list_block_on_stack(self) -> int:
+        """
+        Finds the index of the last list block on the stack (from the end).
+        If no block quotes are found, 0 is returned.
+        """
+        last_stack_index = len(self.token_stack) - 1
+        while (
+            not self.token_stack[last_stack_index].is_document
+            and not self.token_stack[last_stack_index].is_list
+        ):
+            last_stack_index -= 1
+        return last_stack_index
+
+    def find_last_container_on_stack(self) -> int:
+        """
+        Finds the index of the last container on the stack (from the end).
+        If no container tokens are found, 0 is returned.
+        """
+        last_stack_index = len(self.token_stack) - 1
+        while not self.token_stack[last_stack_index].is_document and not (
+            self.token_stack[last_stack_index].is_block_quote
+            or self.token_stack[last_stack_index].is_list
+        ):
+            last_stack_index -= 1
+        return last_stack_index
+
+    def count_of_block_quotes_on_stack(self) -> int:
+        """
+        Helper method to count the number of block quotes currently on the stack.
+        """
+
+        return sum(
+            bool(next_item_on_stack.is_block_quote)
+            for next_item_on_stack in self.token_stack
+        )
+
+    def mark_start_information(self, position_marker: PositionMarker) -> None:
+        """
+        Mark the start of processing this line of information.  A lot of
+        this information is to allow a requeue to occur, if needed.
+        """
+        (
+            self.__original_line_to_parse,
+            self.__original_stack_depth,
+            self.__original_document_depth,
+            self.__no_para_start_if_empty,
+        ) = (
+            position_marker.text_to_parse[:],
+            len(self.token_stack),
+            len(self.token_document),
+            False,
+        )
+
+        last_stack_index = self.find_last_block_quote_on_stack()
+
+        (
+            self.__last_block_quote_stack_token,
+            self.__last_block_quote_markdown_token_index,
+        ) = (None, None)
+        self.__copy_of_last_block_quote_markdown_token = None
+        if not self.token_stack[last_stack_index].is_document:
+            self.__last_block_quote_stack_token = self.token_stack[last_stack_index]
+            markdown_token = self.token_stack[last_stack_index].matching_markdown_token
+            assert (
+                markdown_token is not None
+            ), "Always start with a container or leaf token, that has a matching markdown token."
+            self.__last_block_quote_markdown_token_index = self.token_document.index(
+                markdown_token
+            )
+            self.__copy_of_last_block_quote_markdown_token = cast(
+                BlockQuoteMarkdownToken,
+                copy.deepcopy(
+                    self.token_document[self.__last_block_quote_markdown_token_index]
+                ),
+            )
+
+    def mark_for_leaf_processing(
+        self, container_level_tokens: List[MarkdownToken]
+    ) -> None:
+        """
+        Set things up for leaf processing.
+        """
+        self.__same_line_container_tokens = container_level_tokens
+
+    def clear_after_leaf_processing(self) -> None:
+        """
+        Reset things after leaf processing.
+        """
+        self.__same_line_container_tokens = None
+
+    def set_no_para_start_if_empty(self) -> None:
+        """
+        Set the member variable to true.
+        """
+        self.__no_para_start_if_empty = True
+
+
+# pylint: enable=too-many-instance-attributes
