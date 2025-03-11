9daa68f1600d3f9e3884b741bdc7f6d6d1dc6929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:05:56 +0000

    Add package f-task-5-test/0.0.1/f_task_5_test-0.0.1.tar.gz

---
+++ packages/f-task-5-test/f_task_5_test-0.0.1.tar.gz/f_task_5_test-0.0.1/f_task_5_test/de.py
@@ -0,0 +1,4 @@
+def f2():
+	print('f2')
+	a = 2
+	print(a + 10)
---
+++ packages/f-task-5-test/f_task_5_test-0.0.1.tar.gz/f_task_5_test-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+	name='f_task_5_test', # Имя пакета
+	version='0.0.1', # Версия пакета
+	author='Michail',
+	author_email='mi.ershov.2017@yandex.ru',
+	description='My Package',
+	packages=setuptools.find_packages(),
+)
