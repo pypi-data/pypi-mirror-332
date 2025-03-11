3f66bfc9019e4491b9ed853bf4e514de987c3176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:06:19 +0000

    Add package cosmotalker/1.3.1/cosmotalker-1.3.1-py3-none-any.whl

---
+++ packages/cosmotalker/cosmotalker-1.3.1-py3-none-any.whl/cosmotalker/__init__.py
@@ -0,0 +1,7 @@
+from .get import get
+from .get import get_data
+from .spacex import spacex
+from .search import search
+from .apod import apod
+from .celestrak import celestrak
+from .c import c
---
+++ packages/cosmotalker/cosmotalker-1.3.1-py3-none-any.whl/cosmotalker/data/__init__.py
@@ -0,0 +1,1 @@
+# This makes 'data' a Python package
