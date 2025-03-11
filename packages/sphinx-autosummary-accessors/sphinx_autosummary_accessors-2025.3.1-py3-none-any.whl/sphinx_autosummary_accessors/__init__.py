78575aa60ef3439bff4e03e36582405a553de7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:05:54 +0000

    Add package sphinx-autosummary-accessors/2025.3.1/sphinx_autosummary_accessors-2025.3.1-py3-none-any.whl

---
+++ packages/sphinx-autosummary-accessors/sphinx_autosummary_accessors-2025.3.1-py3-none-any.whl/sphinx_autosummary_accessors/__init__.py
@@ -0,0 +1,37 @@
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
+from sphinx_autosummary_accessors import autosummary
+from sphinx_autosummary_accessors.documenters import (
+    AccessorAttributeDocumenter,
+    AccessorCallableDocumenter,
+    AccessorDocumenter,
+    AccessorMethodDocumenter,
+)
+from sphinx_autosummary_accessors.templates import templates_path  # noqa: F401
+
+try:
+    __version__ = version("sphinx-autosummary-accessors")
+except Exception:
+    __version__ = "999"
+
+
+def add_autosummary_create_documenter(func):
+    import sphinx.ext.autosummary
+
+    sphinx.ext.autosummary.Autosummary.create_documenter = func
+
+
+def setup(app):
+    app.setup_extension("sphinx.ext.autosummary")
+
+    app.add_autodocumenter(AccessorDocumenter)
+    app.add_autodocumenter(AccessorAttributeDocumenter)
+    app.add_autodocumenter(AccessorMethodDocumenter)
+    app.add_autodocumenter(AccessorCallableDocumenter)
+
+    add_autosummary_create_documenter(autosummary.create_documenter_from_template)
+
+    return {"version": __version__, "parallel_read_safe": True}
