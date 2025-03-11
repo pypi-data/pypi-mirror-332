06ea818d008de0cc5f7f0b6c05fecf0526643cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:48 +0000

    Add package eamcvd/1.3/eamcvd-1.3-py3-none-any.whl

---
+++ packages/eamcvd/eamcvd-1.3-py3-none-any.whl/eamcvd/__init__.py
@@ -0,0 +1,16 @@
+def copyright():
+    print('''Copyright (c) 2001-2024 Python Software Foundation.
+All Rights Reserved.
+
+Copyright (c) 2000 BeOpen.com.
+All Rights Reserved.
+
+Copyright (c) 1995-2001 Corporation for National Research Initiatives.
+All Rights Reserved.
+
+Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam.
+All Rights Reserved.
+
+Copyright (C) 2021-2025 The EAMCVD Team. MIT License''')
+
+print(copyright())
