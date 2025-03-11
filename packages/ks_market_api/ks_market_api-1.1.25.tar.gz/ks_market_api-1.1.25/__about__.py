ff6a0b6a3cabdc841a3c11d26c5527d9b4c00974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:12 +0000

    Add package ks_market_api/1.1.25/ks_market_api-1.1.25.tar.gz

---
+++ packages/ks_market_api/ks_market_api-1.1.25.tar.gz/ks_market_api-1.1.25/__about__.py
@@ -0,0 +1,8 @@
+# __version__="1.1.5" # 初始化
+# __version__="1.1.6" # 修正api
+# __version__="1.1.7" # 修正api
+# __version__="1.1.8" # 修正api
+# __version__="1.1.9" # 修正api
+# __version__="1.1.10" # 修正api
+# __version__="1.1.24" # 修正api
+__version__="1.1.25" # 增加close
