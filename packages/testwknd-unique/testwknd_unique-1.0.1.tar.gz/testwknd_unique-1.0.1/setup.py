cc03fc5491e75adb5e972c9404346ecec58a4641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:40 +0000

    Add package testwknd-unique/1.0.1/testwknd_unique-1.0.1.tar.gz

---
+++ packages/testwknd-unique/testwknd_unique-1.0.1.tar.gz/testwknd_unique-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testwknd_unique",  # Change to a unique name
+    version="1.0.1",
+    description="I'm on my way",
+    author="1nfinity",
+    author_email="edmond.nistor723@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
