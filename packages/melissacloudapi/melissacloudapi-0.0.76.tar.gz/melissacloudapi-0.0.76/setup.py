a57e84cf35f4827a915382a06d763ebeab82b577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:07:12 +0000

    Add package melissacloudapi/0.0.76/melissacloudapi-0.0.76.tar.gz

---
+++ packages/melissacloudapi/melissacloudapi-0.0.76.tar.gz/melissacloudapi-0.0.76/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='melissacloudapi',
+    version='0.0.76',
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here
+)
