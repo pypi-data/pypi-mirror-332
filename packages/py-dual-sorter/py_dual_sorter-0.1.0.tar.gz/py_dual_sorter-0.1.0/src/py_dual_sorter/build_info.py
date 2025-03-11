0763f80218287b97b6c207deabc85c301a0f4e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:14:30 +0000

    Add package py-dual-sorter/0.1.0/py_dual_sorter-0.1.0.tar.gz

---
+++ packages/py-dual-sorter/py_dual_sorter-0.1.0.tar.gz/py_dual_sorter-0.1.0/src/py_dual_sorter/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.10.16'
+    os_name = 'posix:ubuntu'
+    version = '0.1.0'
+    git_sha = '282ef25660386b4755a1e60c9360b7f54b8afeb5'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
