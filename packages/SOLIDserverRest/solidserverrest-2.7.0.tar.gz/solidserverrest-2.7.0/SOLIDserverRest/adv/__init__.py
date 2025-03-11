bc61db42c3cabc45bd63aeb4b37cc176ecc805da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:33 +0000

    Add package SOLIDserverRest/2.7.0/solidserverrest-2.7.0.tar.gz

---
+++ packages/SOLIDserverRest/solidserverrest-2.7.0.tar.gz/solidserverrest-2.7.0/SOLIDserverRest/adv/__init__.py
@@ -0,0 +1,31 @@
+# -*- Mode: Python; python-indent-offset: 4 -*-
+#
+# Time-stamp: <2021-08-24 16:39:48 alex>
+#
+
+"""
+init for advanced functions
+"""
+
+from .sds import SDS
+from .base import Base
+from .class_params import ClassParams
+from .space import Space
+from .device import Device
+from .devif import DeviceInterface
+from .network import Network
+from .ipaddress import IpAddress
+from .dns import DNS
+from .dns_zone import DNS_zone
+from .dns_view import DNS_view
+from .dns_record import (
+    DNS_record,
+    NAPTRRecord,
+    TXTRecord,
+    ARecord,
+    AAAARecord,
+    CNAMERecord,
+    MXRecord,
+    NSRecord
+)
+from .device_tools import list_devices
