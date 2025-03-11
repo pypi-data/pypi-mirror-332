2b8a28bd84aa42af0c964fa74e913cfb2323e3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:26 +0000

    Add package haplo/0.18.1/haplo-0.18.1.tar.gz

---
+++ packages/haplo/haplo-0.18.1.tar.gz/haplo-0.18.1/docs/source/analysis_tools/index.md
@@ -0,0 +1,8 @@
+# Analysis tools
+
+```{toctree}
+:caption: 'Contents:'
+:maxdepth: 2
+
+converting_mcmc_output_files_to_xarray_zarr
+```
---
+++ packages/haplo/haplo-0.18.1.tar.gz/haplo-0.18.1/quick.py
@@ -0,0 +1,4 @@
+from haplo.internal.constantinos_kalapotharakos_format import \
+    combine_constantinos_kalapotharakos_split_mcmc_output_files_to_xarray_zarr
+
+combine_constantinos_kalapotharakos_split_mcmc_output_files_to_xarray_zarr(,,
