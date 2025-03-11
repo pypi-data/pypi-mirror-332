431754d59b344996f1bf59f2112bd006bf3b7511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:17 +0000

    Add package kiba-core/0.5.3.dev20/kiba_core-0.5.3.dev20-py3-none-any.whl

---
+++ packages/kiba-core/kiba_core-0.5.3.dev20-py3-none-any.whl/core/api/api_respnse.py
@@ -0,0 +1,9 @@
+import typing
+
+import orjson
+from starlette.responses import JSONResponse
+
+
+class KibaJSONResponse(JSONResponse):
+    def render(self, content: typing.Any) -> bytes:  # type: ignore[explicit-any]
+        return orjson.dumps(content)
