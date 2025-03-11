e3ebcf83b0c5e417800115c6ba74ea1ffe58553b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:07:01 +0000

    Add package bytedance-byteair-api-base/0.0.1/bytedance_byteair_api_base-0.0.1.tar.gz

---
+++ packages/bytedance-byteair-api-base/bytedance_byteair_api_base-0.0.1.tar.gz/bytedance_byteair_api_base-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_byteair_api_base",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_byteair_api_base",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_byteair_api_base.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
