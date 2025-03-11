e6dbc31ca256c14780413585386eed7db022d9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:58 +0000

    Add package pyjabber/0.2.1/pyjabber-0.2.1.tar.gz

---
+++ packages/pyjabber/pyjabber-0.2.1.tar.gz/pyjabber-0.2.1/pyjabber/network/ConnectionManager.py
@@ -0,0 +1,224 @@
+import asyncio
+import re
+from typing import List, Optional
+from asyncio import Transport
+from typing import Dict, Union, Tuple
+from loguru import logger
+
+from pyjabber.stream.JID import JID
+from pyjabber.utils import Singleton
+
+PeerList = Dict[
+    Tuple[str, int],
+    Tuple[Optional[JID], Transport]
+]
+
+
+# EXAMPLE
+# peerKeys = {
+#     ("0.0.0.0", "50000"): ("demo@localhost/12341234", <Transport>),
+#     ("0.0.0.0", "50001"): ("test@localhos/43214321", <Transport>)
+# }
+
+
+class ConnectionManager(metaclass=Singleton):
+    def __init__(self) -> None:
+
+        self._peerList: PeerList = {}
+        self._remoteList = {}
+
+    ###################
+    ### LOCAL BOUND ###
+    ###################
+
+    def connection(self, peer: Tuple[str, int], transport: Transport = None) -> None:
+        """
+            Store a new connection, without jid or transport.
+            Those will be added in the future with the set_jid method.
+
+            :param peer: The peer value in the tuple format ({IP}, {PORT})
+            :param transport: The transport object associated to the connection
+        """
+        if peer not in self._peerList:
+            self._peerList[peer] = (None, transport)
+
+    def disconnection(self, peer: Tuple[str, int]) -> None:
+        """
+            Remove a stored connection, and fires the DisconnectEvent
+
+            :param peer: The peer value in the tuple format ({IP}, {PORT})
+        """
+
+        try:
+            self._peerList.pop(peer)
+        except KeyError:
+            logger.warning(f"{peer} not present in the online list")
+
+    def close(self, peer: Tuple[str, int]) -> None:
+        """
+            Closes a connection by sending a '</stream:stream> message' and
+            deletes it from the peers list
+
+            :param peer: The peer value in the tuple format ({IP}, {PORT})
+        """
+        try:
+            _, buffer = self._peerList.pop(peer)
+            buffer.write('</stream:stream>'.encode())
+            self.disconnection(peer)
+        except KeyError as e:
+            logger.error(f"{peer} not present in the online list")
+
+    def get_users_connected(self) -> PeerList:
+        """
+            Return all the users connected
+
+            :return: peerList
+        """
+        return self._peerList
+
+    def get_buffer(self, jid: JID) -> List[Tuple[JID, Transport]]:
+        """
+            Get all the available buffers associated with a jid.
+
+            If the jid is in the format username@domain/resource, the function
+            will only return one buffer or none.
+
+            If the jid is in the format username@domain, the function
+            will return a list of the buffers for each resource available.
+
+            Both cases return a list.
+
+            :param jid: The jid to get the buffers for. It can be a full jid or a bare jid
+            :return: (JID, TRANSPORT) tuple
+        """
+        if jid.domain is None:
+            logger.error("JID must have, at least, user and domain")
+            return []
+
+        if jid.resource:
+            return [(jid_stored, buffer) for jid_stored, buffer, _ in self._peerList.values() if str(jid) == jid_stored]
+        else:
+            return [(jid_stored, buffer) for jid_stored, buffer in self._peerList.values() if
+                    re.match(f"{str(jid)}/*", str(jid_stored))]
+
+    ###########
+    ### JID ###
+    ###########
+
+    def get_jid(self, peer: Tuple[str, int]) -> Union[JID, None]:
+        """
+            Return the jid associated with the peername
+
+            :param peer: The peer value in the tuple format ({IP}, {PORT})
+        """
+        try:
+            return self._peerList[peer][0]
+        except KeyError:
+            return None
+
+    def set_jid(self, peer: Tuple[str, int], jid: JID, transport: Transport = None) -> None:
+        """
+            Set/update the jid of a registered connection.
+
+            An optional transport argument can be provided, in order to
+            set/update the stored buffer
+
+            :param peer: The peer value in the tuple format ({IP}, {PORT})
+            :param jid: The jid to set/update
+            :param transport: Transport to use
+        """
+        try:
+            _, old_transport = self._peerList[peer]
+            self._peerList[peer] = (jid, transport or old_transport)
+        except KeyError:
+            logger.error(f"Unable to find {peer} during jid/transport update")
+
+    def update_resource(self, peer: Tuple[str, int], resource: str):
+        try:
+            self._peerList[peer][0].resource = resource
+        except KeyError:
+            logger.warning(f"Unable to find {peer} during resource update")
+
+    ###########################################################################
+    ############################# REMOTE SERVER ###############################
+    ###########################################################################
+
+    def connection_server(self, peer, host, transport) -> None:
+        """
+            Store a new connection, without jid or transport.
+        """
+        if peer not in self._remoteList:
+            self._remoteList[peer] = {
+                peerKeys.JID: host,
+                peerKeys.TRANSPORT: None
+            }
+
+    def disconnection_server(self, peer) -> None:
+        """
+        Remove a present server connection in the list
+        i.e. EOF recived, or TCP connection lost
+        """
+        try:
+            self._remoteList.pop(peer)
+        except KeyError:
+            logger.error(f"{peer} not present in the online list")
+
+    def get_server_buffer(self, host: str) -> Union[Tuple[str, Transport], None]:
+        """
+            Return the buffer associated with the given host
+            :return: (<HOST>, <TRANSPORT>) tuple
+        """
+        if self.check_server_stream_available(host):
+            key = next((k for k, v in self._remoteList.items() if v.get(peerKeys.JID) == host), None)
+            return self._remoteList[key][peerKeys.JID], self._remoteList[key][peerKeys.TRANSPORT]
+
+        if self.check_server_present_in_list(host):
+            return
+
+        self._task_s2s(host)
+
+    def get_server_host(self, peer: Tuple[str, int]):
+        """
+            Return the host associated with the given peer.
+            :return: Hostname
+        """
+        try:
+            return self._remoteList[peer]["host"]
+        except KeyError:
+            return None
+
+    def set_server_transport(self, peer: Tuple[str, int], transport: Transport) -> Union[None, bool]:
+        """
+            Set/update the transport of a registered server connection.
+
+            :param peer: The peer value in the tuple format ({IP}, {PORT})
+            :param transport: Transport to use
+        """
+        try:
+            self._remoteList[peer][peerKeys.TRANSPORT] = transport
+        except KeyError:
+            return False
+
+    def set_server_host(self, peer: Tuple[str, int], host: str):
+        """
+            Set/update the host of a registered server connection.
+
+            :param peer: The peer value in the tuple format ({IP}, {PORT})
+            :param host: New host name
+        """
+        try:
+            self._remoteList[peer]["host"] = host
+        except KeyError:
+            return False
+
+    def check_server_stream_available(self, host) -> bool:
+        for k, v in self._remoteList.items():
+            if v[peerKeys.JID] == host:
+                return v[peerKeys.TRANSPORT] is not None
+
+    def check_server_present_in_list(self, host) -> bool:
+        try:
+            if [v for v in self._remoteList.values() if v[self.JID] == host]:
+                return True
+        except KeyError:
+            return False
---
+++ packages/pyjabber/pyjabber-0.2.1.tar.gz/pyjabber-0.2.1/pyjabber/stream/StanzaHandler.py
@@ -0,0 +1,89 @@
+import re
+import xml.etree.ElementTree as ET
+
+from loguru import logger
+
+from pyjabber.features.presence.PresenceFeature import Presence
+from pyjabber.network.ConnectionManager import ConnectionManager
+from pyjabber.stream.JID import JID
+from pyjabber.stream.QueueMessage import QueueMessage
+from pyjabber.plugins.PluginManager import PluginManager
+
+
+class InternalServerError(Exception):
+    pass
+
+
+class StanzaHandler:
+    def __init__(self, host, buffer) -> None:
+        self._host = host
+        self._buffer = buffer
+        self._connections = ConnectionManager()
+        self._queue_message = QueueMessage()
+
+        self._peername = buffer.get_extra_info('peername')
+        self._jid = self._connections.get_jid(self._peername)
+
+        self._pluginManager = PluginManager(self._jid)
+        self._presenceManager = Presence()
+
+        self._functions = {
+            "{jabber:client}iq": self.handle_iq,
+            "{jabber:client}message": self.handle_msg,
+            "{jabber:client}presence": self.handle_pre
+        }
+
+    def feed(self, element: ET.Element):
+        try:
+            self._functions[element.tag](element)
+        except (KeyError, InternalServerError) as e:
+            logger.error(e)
+            logger.error(f"INTERNAL SERVER ERROR WITH {self._peername}. CLOSING CONNECTION FOR SERVER STABILITY")
+            raise InternalServerError
+
+    def handle_iq(self, element: ET.Element):
+        """
+            Process the iq stanza with the PluginManager (PM) class
+            If the feature/XEP requested is not available, the PM instance
+            will send a
+
+            :param element: The stanza in the ElementTree format
+        """
+        res = self._pluginManager.feed(element)
+        if res:
+            self._buffer.write(res)
+
+    def handle_msg(self, element: ET.Element):
+        """
+            Router the message to the client
+
+            If the destination client is a user of a remote server, it will queue the message into the QueueMessage
+            object and try to connect to the remote server
+
+            :param element: the message in the ElementTree format
+        """
+        jid = JID(element.attrib["to"])
+
+        if re.match(fr'^[a-zA-Z0-9._%+-]+@{re.escape(self._host)}$', jid.bare()):
+            for _, buffer in self._connections.get_buffer(JID(jid.bare())):
+                buffer.write(ET.tostring(element))
+
+        else:
+            pass
+            # the s2s feature is currently disabled due to bad implementation
+            # Future version of the server will fix that
+
+            # server_buffer = self._connections.get_server_buffer(jid.bare())
+            # if server_buffer:
+            #     server_buffer[-1].write(ET.tostring(element))
+            #
+            # else:
+            #     self._queue_message.enqueue(jid.domain, ET.tostring(element))
+
+    def handle_pre(self, element: ET.Element):
+        """
+            Handle the presences stanzas
+        """
+        res = self._presenceManager.feed(self._jid, element)
+        if res:
+            self._buffer.write(res)
