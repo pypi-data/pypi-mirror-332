e95ba63e9bfbd35db1d07187ffe0b8b8efb5dea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:17 +0000

    Add package codeforlife/0.25.19/codeforlife-0.25.19.tar.gz

---
+++ packages/codeforlife/codeforlife-0.25.19.tar.gz/codeforlife-0.25.19/codeforlife/user/serializers/klass.py
@@ -0,0 +1,50 @@
+"""
+© Ocado Group
+Created on 20/01/2024 at 11:28:29(+00:00).
+"""
+
+from rest_framework import serializers
+
+from ...serializers import ModelSerializer
+from ..models import Class
+from ..models import User as RequestUser
+
+# pylint: disable=missing-class-docstring
+# pylint: disable=too-many-ancestors
+
+
+class ClassSerializer(ModelSerializer[RequestUser, Class]):
+    id = serializers.CharField(
+        source="access_code",
+        read_only=True,
+    )
+
+    read_classmates_data = serializers.BooleanField(
+        source="classmates_data_viewable",
+        read_only=True,
+    )
+
+    receive_requests_until = serializers.DateTimeField(
+        source="accept_requests_until",
+        read_only=True,
+    )
+
+    school = serializers.IntegerField(
+        source="teacher.school.id",
+        read_only=True,
+    )
+
+    class Meta:
+        model = Class
+        fields = [
+            "name",
+            "id",
+            "teacher",
+            "school",
+            "read_classmates_data",
+            "receive_requests_until",
+        ]
+        extra_kwargs = {
+            "name": {"read_only": True},
+            "teacher": {"read_only": True},
+        }
