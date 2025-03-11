12eca4c6a4220d29a3e4b113e16912038d1d1baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:45 +0000

    Add package zmbus-sdk/0.1.6/zmbus_sdk-0.1.6.tar.gz

---
+++ packages/zmbus-sdk/zmbus_sdk-0.1.6.tar.gz/zmbus_sdk-0.1.6/zimasabus_sdk/__init__.py
@@ -0,0 +1,11 @@
+"""
+Zimasa Bus SDK - A Python SDK for interacting with Zimasa medical systems.
+"""
+
+# Import main classes for easier access
+from zimasabus_sdk.zimasa.appointments import AppointmentService
+from zimasabus_sdk.zimasa.followups import FollowUpService
+from zimasabus_sdk.zmsystem import ZmSystem, ZmSystemEnums
+
+# Define version
+__version__ = "0.1.6"
