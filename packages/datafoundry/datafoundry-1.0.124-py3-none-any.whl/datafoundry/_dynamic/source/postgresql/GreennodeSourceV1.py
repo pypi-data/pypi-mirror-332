4f907cb911340c609958f2e82bf93b80598c783b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:05:36 +0000

    Add package datafoundry/1.0.124/datafoundry-1.0.124-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.124-py3-none-any.whl/datafoundry/_dynamic/source/postgresql/GreennodeSourceV1.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class GreennodeSourceV1(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67ce9c4157fdaddab2882fff"
+# --- render code block -----
