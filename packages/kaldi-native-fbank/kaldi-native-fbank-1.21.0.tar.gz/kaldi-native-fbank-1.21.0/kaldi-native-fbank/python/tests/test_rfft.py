f26aba679730ec817dbeac231c4c1e938c09309e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:14:07 +0000

    Add package kaldi-native-fbank/1.21.0/kaldi-native-fbank-1.21.0.tar.gz

---
+++ packages/kaldi-native-fbank/kaldi-native-fbank-1.21.0.tar.gz/kaldi-native-fbank-1.21.0/kaldi-native-fbank/python/tests/test_rfft.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#
+# Copyright (c)  2021-2023  Xiaomi Corporation (authors: Fangjun Kuang)
+
+
+import torch
+
+import kaldi_native_fbank as knf
+
+
+def test_rfft():
+    t = torch.tensor([-1, 1.5, 10, 3, -2, 9, 11, 8])
+    N = t.size(0)
+    r = torch.fft.rfft(t)
+    assert len(r) == N // 2 + 1, (len(r), N // 2 + 1)
+
+    real = r.real
+    imag = r.imag
+    print(r)
+
+    k = t.tolist()
+    rfft = knf.Rfft(N)
+
+    p = rfft.compute(k)
+    print(p)
+
+    assert abs(p[0] - real[0]) < 1e-5, (p[0], real[0])
+    assert imag[0] == 0, imag[0]
+
+    assert abs(p[1] - real[-1]) < 1e-5, (p[1], real[-1])
+    assert imag[-1] == 0, imag[-1]
+
+    for i in range(1, N // 2):
+        assert abs(p[2 * i] - real[i]) < 1e-5, (p[2 * i], real[i])
+        # Note: the imaginary part is multiplied by negative 1
+        assert abs(p[2 * i + 1] - -1 * imag[i]) < 1e-5, (p[2 * i + 1], imag[i])
+
+
+def main():
+    test_rfft()
+
+
+if __name__ == "__main__":
+    main()
