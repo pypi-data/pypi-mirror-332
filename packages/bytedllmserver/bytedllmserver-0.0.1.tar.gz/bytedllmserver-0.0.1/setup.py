c671e9e08d87445b4b515bae8b4d15a4db94f054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:06:03 +0000

    Add package bytedllmserver/0.0.1/bytedllmserver-0.0.1.tar.gz

---
+++ packages/bytedllmserver/bytedllmserver-0.0.1.tar.gz/bytedllmserver-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedllmserver",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedllmserver",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedllmserver.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
