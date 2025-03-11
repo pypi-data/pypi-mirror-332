f005f581bbee6033854238dd2b5c827a8a986571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:06:05 +0000

    Add package nesegodnapp-123/0.0.1/nesegodnapp_123-0.0.1.tar.gz

---
+++ packages/nesegodnapp-123/nesegodnapp_123-0.0.1.tar.gz/nesegodnapp_123-0.0.1/nesegodnapp_123/__init__.py
@@ -0,0 +1,2 @@
+from .First import *
+from .Second import *
---
+++ packages/nesegodnapp-123/nesegodnapp_123-0.0.1.tar.gz/nesegodnapp_123-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='nesegodnapp_123', # Имя пакета
+    version='0.0.1', # Версия пакета
+    author='Your Name',
+    author_email='mambas32584@gamil.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
