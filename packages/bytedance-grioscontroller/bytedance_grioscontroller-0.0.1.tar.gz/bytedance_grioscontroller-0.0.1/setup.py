d2fa2100fd783bc5ff201356ffd91aa6f62e4874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:36 +0000

    Add package bytedance-grioscontroller/0.0.1/bytedance_grioscontroller-0.0.1.tar.gz

---
+++ packages/bytedance-grioscontroller/bytedance_grioscontroller-0.0.1.tar.gz/bytedance_grioscontroller-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_grioscontroller",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_grioscontroller",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_grioscontroller.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
