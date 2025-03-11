b67922bc6b1adfd7d87308284837add119fd60b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 11:06:01 +0000

    Add package bayes-sizer/1.0.2/bayes_sizer-1.0.2.tar.gz

---
+++ packages/bayes-sizer/bayes_sizer-1.0.2.tar.gz/bayes_sizer-1.0.2/tests/test_calculator.py
@@ -0,0 +1,8 @@
+import pytest
+from bayessizer.calculator import bayesian_sample_size
+
+def test_bayesian_sample_size_simple():
+    """Simple test to verify Bayesian sample size function runs without excessive iterations."""
+    sample_size = bayesian_sample_size(prior_a=2, prior_b=2, min_effect=0.05, power=0.8, loss_threshold=0.1)
+    assert isinstance(sample_size, int)
+    assert sample_size > 0
