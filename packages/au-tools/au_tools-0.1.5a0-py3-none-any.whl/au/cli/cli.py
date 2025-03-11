62c8af8d1421d9bfd17b394a30b936c0af961bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:29 +0000

    Add package au-tools/0.1.5a0/au_tools-0.1.5a0-py3-none-any.whl

---
+++ packages/au-tools/au_tools-0.1.5a0-py3-none-any.whl/au/cli/cli.py
@@ -0,0 +1,38 @@
+import click
+from au.click import AliasedGroup
+from importlib.metadata import version, PackageNotFoundError
+
+from .assignment.cli import assignment
+from .python.cli import python
+from .sql.cli import sql
+
+
+MODULE_NAME = "au-tools"
+try:
+    VERSION = version(MODULE_NAME)
+except PackageNotFoundError:
+    VERSION = "undetermined"
+
+
+# @click.command(cls=SubdirGroup, file=__file__, module=__package__)
+@click.version_option(VERSION)
+@click.group(cls=AliasedGroup)
+def main():
+    """
+    au - GitHub Classroom Automation Tools
+
+    The gold standard for managing GitHub Classroom assignments at scale. These
+    tools help to automate the workflows required to create, administer,
+    evaluate, and provide feedback for assignments.
+
+    Homepage: https://ptyork.github.io/au
+    """
+
+
+main.add_command(assignment)
+main.add_command(python)
+main.add_command(sql)
+
+
+if __name__ == "__main__":
+    main()
