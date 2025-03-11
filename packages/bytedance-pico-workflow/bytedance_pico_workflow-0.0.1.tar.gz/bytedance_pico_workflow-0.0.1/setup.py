7fd0057dc47b166effefe9dc1ab32c9527f2b96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:45 +0000

    Add package bytedance-pico-workflow/0.0.1/bytedance_pico_workflow-0.0.1.tar.gz

---
+++ packages/bytedance-pico-workflow/bytedance_pico_workflow-0.0.1.tar.gz/bytedance_pico_workflow-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_pico_workflow",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_pico_workflow",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_pico_workflow.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
