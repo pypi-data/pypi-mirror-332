b5c3448149fae97ca3729cdd2d389d502d8aa326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:46 +0000

    Add package hack4u-slowie-version/0.1.1/hack4u_slowie_version-0.1.1.tar.gz

---
+++ packages/hack4u-slowie-version/hack4u_slowie_version-0.1.1.tar.gz/hack4u_slowie_version-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hack4u_slowie_version",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="CMJ",
+    description="Biblioteca para conultar cursos de Hack4u",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://hack4u.io"
+
+
+)
+
