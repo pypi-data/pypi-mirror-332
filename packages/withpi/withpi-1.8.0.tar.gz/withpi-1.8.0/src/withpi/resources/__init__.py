6118585e864abf758e32667cf1ce7faa76979a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:06:43 +0000

    Add package withpi/1.8.0/withpi-1.8.0.tar.gz

---
+++ packages/withpi/withpi-1.8.0.tar.gz/withpi-1.8.0/src/withpi/resources/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .rag import (
+    RagResource,
+    AsyncRagResource,
+    RagResourceWithRawResponse,
+    AsyncRagResourceWithRawResponse,
+    RagResourceWithStreamingResponse,
+    AsyncRagResourceWithStreamingResponse,
+)
+from .data import (
+    DataResource,
+    AsyncDataResource,
+    DataResourceWithRawResponse,
+    AsyncDataResourceWithRawResponse,
+    DataResourceWithStreamingResponse,
+    AsyncDataResourceWithStreamingResponse,
+)
+from .model import (
+    ModelResource,
+    AsyncModelResource,
+    ModelResourceWithRawResponse,
+    AsyncModelResourceWithRawResponse,
+    ModelResourceWithStreamingResponse,
+    AsyncModelResourceWithStreamingResponse,
+)
+from .prompt import (
+    PromptResource,
+    AsyncPromptResource,
+    PromptResourceWithRawResponse,
+    AsyncPromptResourceWithRawResponse,
+    PromptResourceWithStreamingResponse,
+    AsyncPromptResourceWithStreamingResponse,
+)
+from .scorers import (
+    ScorersResource,
+    AsyncScorersResource,
+    ScorersResourceWithRawResponse,
+    AsyncScorersResourceWithRawResponse,
+    ScorersResourceWithStreamingResponse,
+    AsyncScorersResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "DataResource",
+    "AsyncDataResource",
+    "DataResourceWithRawResponse",
+    "AsyncDataResourceWithRawResponse",
+    "DataResourceWithStreamingResponse",
+    "AsyncDataResourceWithStreamingResponse",
+    "ModelResource",
+    "AsyncModelResource",
+    "ModelResourceWithRawResponse",
+    "AsyncModelResourceWithRawResponse",
+    "ModelResourceWithStreamingResponse",
+    "AsyncModelResourceWithStreamingResponse",
+    "PromptResource",
+    "AsyncPromptResource",
+    "PromptResourceWithRawResponse",
+    "AsyncPromptResourceWithRawResponse",
+    "PromptResourceWithStreamingResponse",
+    "AsyncPromptResourceWithStreamingResponse",
+    "ScorersResource",
+    "AsyncScorersResource",
+    "ScorersResourceWithRawResponse",
+    "AsyncScorersResourceWithRawResponse",
+    "ScorersResourceWithStreamingResponse",
+    "AsyncScorersResourceWithStreamingResponse",
+    "RagResource",
+    "AsyncRagResource",
+    "RagResourceWithRawResponse",
+    "AsyncRagResourceWithRawResponse",
+    "RagResourceWithStreamingResponse",
+    "AsyncRagResourceWithStreamingResponse",
+]
