96a2ca37140fe5e484bc27a7f5e98b0e9fcce816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:24 +0000

    Add package EasySoccerData/0.0.3/easysoccerdata-0.0.3-py3-none-any.whl

---
+++ packages/EasySoccerData/easysoccerdata-0.0.3-py3-none-any.whl/esd/sofascore/__init__.py
@@ -0,0 +1,33 @@
+"""
+Sofascore types module necessary for the client.
+"""
+
+from .client import SofascoreClient
+from .types import (
+    EntityType,
+    Event,
+    Team,
+    Player,
+    MatchStats,
+    PlayerLineup,
+    TeamLineup,
+    Lineups,
+    TeamColor,
+    Category,
+    Tournament,
+)
+
+__all__ = [
+    "SofascoreClient",
+    "EntityType",
+    "Event",
+    "Team",
+    "Player",
+    "MatchStats",
+    "PlayerLineup",
+    "TeamLineup",
+    "Lineups",
+    "TeamColor",
+    "Category",
+    "Tournament",
+]
