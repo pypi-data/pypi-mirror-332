b5f084eae574f00fd2e85ab913d0f8119332d07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:06:21 +0000

    Add package otauto/0.1.0/otauto-0.1.0.tar.gz

---
+++ packages/otauto/otauto-0.1.0.tar.gz/otauto-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# 读取 requirements.txt 中的依赖项
+def parse_requirements(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return [line.strip() for line in f if line and not line.startswith('#')]
+
+requirements = parse_requirements('requirements.txt')
+
+setup(
+    name='otauto',
+    version='0.1.0',
+    author='ordtie',
+    author_email='283044916@qq.com',
+    description='vnc窗口操作工具',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    install_requires=requirements,  # 引用从 requirements.txt 中读取的依赖
+)
