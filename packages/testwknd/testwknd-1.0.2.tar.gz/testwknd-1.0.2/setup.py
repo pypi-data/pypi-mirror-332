d2a142b6237a7e89eb62374f56faf1c10637ebb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:48 +0000

    Add package testwknd/1.0.2/testwknd-1.0.2.tar.gz

---
+++ packages/testwknd/testwknd-1.0.2.tar.gz/testwknd-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testwknd",  # Change back to testwknd
+    version="1.0.2",
+    description="I'm on my way",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="1nfinity",
+    author_email="edmond.nistor723@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
