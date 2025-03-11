3e8ae68ede3e11ffb040cc4d50337f5923c050a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:07:34 +0000

    Add package pycocotools-holi/2.1/pycocotools_holi-2.1.tar.gz

---
+++ packages/pycocotools-holi/pycocotools_holi-2.1.tar.gz/pycocotools_holi-2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+import numpy as np
+
+# To compile and install locally run "python setup.py build_ext --inplace"
+# To install library to Python site-packages run "python setup.py build_ext install"
+
+ext_modules = [
+    Extension(
+        'pycocotools._mask',
+        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs = [np.get_include(), '../common'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+    )
+]
+
+setup(
+    name='pycocotools_holi',
+    packages=['pycocotools_holi'],
+    package_dir = {'pycocotools_holi': 'pycocotools'},
+    install_requires=[
+        'setuptools>=18.0',
+        'cython<=0.29.36',
+        'matplotlib>=2.1.0'
+    ],
+    version='2.1',
+    ext_modules= ext_modules
+)
