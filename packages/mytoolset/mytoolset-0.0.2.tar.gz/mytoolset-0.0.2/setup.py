ed4cd3d8090b41fab6c680eda8954bc69421bf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:06:21 +0000

    Add package mytoolset/0.0.2/mytoolset-0.0.2.tar.gz

---
+++ packages/mytoolset/mytoolset-0.0.2.tar.gz/mytoolset-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytoolset',
+    version='0.0.2',
+    description='wget & pydockerNpygit2 env is my tool set',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/mytoolset',
+    packages=find_packages(exclude=[]),
+    install_requires=['wget', 'dockerNpygit2'],
+    keywords=['py', 'docker', 'pydocker', 'and', 'N', 'n', '&', 'pygit', 'git', 'pygit2', 'pydockerNpygit2', 'dockerNpygit2', 'docker&git', 'wget', 'pywget', 'my', 'tooo', 'set', 'mytool', 'mytoolset', 'toolset', 'tools', 'mytools'],
+    python_requires='>=3.4',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
