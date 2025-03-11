c72abf0e05f4046c88fa4bd235d08e31c1857fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:12 +0000

    Add package datafoundry/1.0.111/datafoundry-1.0.111.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.111.tar.gz/datafoundry-1.0.111/datafoundry/_dynamic/dataset/IrdDbV2_67ce627463c7f6c1881cb7b8.py
@@ -0,0 +1,16 @@
+from datafoundry._core.dataset import DatasetBase
+from datafoundry._core.dataset.table_core import TableBase
+
+
+# --- gen code block ---
+class IrdDbV2(DatasetBase):
+    """
+    Database: IrdDbV2
+    Description: testing
+   
+    """
+    def __init__(self):
+        pass
+        
+    
+# --- gen code block ---
---
+++ packages/datafoundry/datafoundry-1.0.111.tar.gz/datafoundry-1.0.111/datafoundry/_dynamic/dataset/__init__.py
@@ -0,0 +1,18 @@
+# This is an auto-generated file. DO NOT EDIT.
+
+from .InfrastructureDb_67cdcc9916e2eaa728b1882d import InfrastructureDb
+
+from .Mktvng2024_67cddc49c02ec2e05423809d import Mktvng2024
+
+from .IrdDbV2_67ce627463c7f6c1881cb7b8 import IrdDbV2
+
+
+__all__ = [
+    
+    "InfrastructureDb",
+    
+    "Mktvng2024",
+    
+    "IrdDbV2"
+    
+]
