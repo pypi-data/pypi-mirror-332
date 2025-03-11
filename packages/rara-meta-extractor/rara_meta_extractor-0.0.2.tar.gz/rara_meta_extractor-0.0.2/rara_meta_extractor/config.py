17a9dc8b66561600e0d66367c8fff056a5a7c91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:29 +0000

    Add package rara-meta-extractor/0.0.2/rara_meta_extractor-0.0.2.tar.gz

---
+++ packages/rara-meta-extractor/rara_meta_extractor-0.0.2.tar.gz/rara_meta_extractor-0.0.2/rara_meta_extractor/config.py
@@ -0,0 +1,17 @@
+from rara_meta_extractor.utils import load_txt, load_json
+import os
+
+SIMPLE_FIELDS_FILE_PATH = os.path.join("rara_meta_extractor", "data", "simple_fields.txt")
+RESTRICTED_FIELDS_FILE_PATH = os.path.join("rara_meta_extractor", "data", "fields.json")
+
+SIMPLE_FIELDS = load_txt(SIMPLE_FIELDS_FILE_PATH, to_list=True)
+RESTRICTED_FIELDS = load_json(RESTRICTED_FIELDS_FILE_PATH)
+
+DEFAULT_INSTRUCTIONS = "This is a conversation between user and an information extractor \
+whose sole purpose is to extract {0}. NB! Do not output any \
+additional information or specifications!"
+
+DEFAULT_CONFIG = {
+    "fields": RESTRICTED_FIELDS,
+    "instructions": DEFAULT_INSTRUCTIONS
+}
