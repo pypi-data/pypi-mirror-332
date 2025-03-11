036164cc164759e8b97a355c003d7ce02a197af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 08:06:04 +0000

    Add package bytedance-primus-torch-io/0.0.1/bytedance_primus_torch_io-0.0.1.tar.gz

---
+++ packages/bytedance-primus-torch-io/bytedance_primus_torch_io-0.0.1.tar.gz/bytedance_primus_torch_io-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_primus_torch_io",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_primus_torch_io",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_primus_torch_io.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
