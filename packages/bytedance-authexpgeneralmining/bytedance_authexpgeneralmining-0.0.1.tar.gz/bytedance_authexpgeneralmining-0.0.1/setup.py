b820823d5f42466df54f94fd2acc98a778a4cd96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 11:05:58 +0000

    Add package bytedance-authexpgeneralmining/0.0.1/bytedance_authexpgeneralmining-0.0.1.tar.gz

---
+++ packages/bytedance-authexpgeneralmining/bytedance_authexpgeneralmining-0.0.1.tar.gz/bytedance_authexpgeneralmining-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_authexpgeneralmining",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_authexpgeneralmining",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_authexpgeneralmining.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
