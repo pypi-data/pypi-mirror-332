f899704a7f9a237cf71c2874801776d64ad97b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:56 +0000

    Add package test-pioneer/0.1.0/test_pioneer-0.1.0-py3-none-any.whl

---
+++ packages/test-pioneer/test_pioneer-0.1.0-py3-none-any.whl/test_pioneer/logging/loggin_instance.py
@@ -0,0 +1,27 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+class TestPioneerHandler(RotatingFileHandler):
+
+    def __init__(self, filename: str = "TestPioneer.log", mode="w",
+                 maxBytes: int = 1073741824, backupCount: int = 0):
+        super().__init__(filename=filename, mode=mode, maxBytes=maxBytes, backupCount=backupCount)
+        self.formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+        self.setLevel(logging.DEBUG)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        super().emit(record)
+
+test_pioneer_logger = logging.getLogger("TestPioneer")
+
+
+def step_log_check(enable_logging: bool = False, logger: logging.Logger = None,
+                   level: str = "info", message: str = None):
+    if enable_logging and logger:
+        logger_level = {
+            "info": logger.info,
+            "error": logger.error,
+        }.get(level, None)
+        if logger_level is not None:
+            logger_level(message)
