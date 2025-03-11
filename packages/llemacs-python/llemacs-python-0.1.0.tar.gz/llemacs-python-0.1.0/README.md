f294f0f91fc671b1b534b77390babd2938e82e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 12:05:51 +0000

    Add package llemacs-python/0.1.0/llemacs-python-0.1.0.tar.gz

---
+++ packages/llemacs-python/llemacs-python-0.1.0.tar.gz/llemacs-python-0.1.0/README.md
@@ -0,0 +1,9 @@
+<!-- ---
+!-- Timestamp: 2025-03-11 08:51:01
+!-- Author: ywatanabe
+!-- File: /home/ywatanabe/proj/llemacs/llemacs-py/README.md
+!-- --- -->
+
+# LLEmacs-Python
+
+<!-- EOF -->
---
+++ packages/llemacs-python/llemacs-python-0.1.0.tar.gz/llemacs-python-0.1.0/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Timestamp: "2025-03-11 08:57:01 (ywatanabe)"
+# File: /home/ywatanabe/proj/llemacs/llemacs-python/setup.py
+
+__THIS_FILE__ = "/home/ywatanabe/proj/llemacs/llemacs-python/setup.py"
+
+import re
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+################################################################################
+PACKAGE_NAME = "llemacs-python"
+PACKAGES = find_packages(where="src")
+DESCRIPTION = "Python Utilities for LLEmacs"
+KEYWORDS = ["llemacs", "llm", "agent"]
+CLASSIFIERS = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+]
+################################################################################
+
+root_dir = path.abspath(path.dirname(__file__))
+
+def _requirements():
+    return [
+        name.rstrip()
+        for name in open(path.join(root_dir, "requirements.txt")).readlines()
+        if "--no-deps" not in name
+    ]
+
+with open(path.join(root_dir, "src", PACKAGE_NAME, "__init__.py")) as f:
+    init_text = f.read()
+    version = re.search(
+        r"__version__\s*=\s*[\'\"](.+?)[\'\"]", init_text
+    ).group(1)
+    license = re.search(
+        r"__license__\s*=\s*[\'\"](.+?)[\'\"]", init_text
+    ).group(1)
+    author = re.search(r"__author__\s*=\s*[\'\"](.+?)[\'\"]", init_text).group(
+        1
+    )
+    author_email = re.search(
+        r"__author_email__\s*=\s*[\'\"](.+?)[\'\"]", init_text
+    ).group(1)
+    url = re.search(r"__url__\s*=\s*[\'\"](.+?)[\'\"]", init_text).group(1)
+
+assert version
+assert license
+assert author
+assert author_email
+assert url
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    package_dir={"": "src"},
+    name=PACKAGE_NAME,
+    packages=PACKAGES,
+    version=version,
+    license=license,
+    install_requires=_requirements(),
+    author=author,
+    author_email=author_email,
+    url=url,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
+    python_requires=">=3.8",
+)
+
+# EOF
