fce084369d9b1e96b198d5c0e7441074af6cf277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:14:34 +0000

    Add package antitesting/0.0.12/antitesting-0.0.12-py3-none-any.whl

---
+++ packages/antitesting/antitesting-0.0.12-py3-none-any.whl/antitesting/proxy_module.py
@@ -0,0 +1,15 @@
+import sys
+from types import ModuleType
+
+from antitesting.state import collector
+from antitesting.collector import DisabledTestsCollector
+
+
+class ProxyModule(sys.modules[__name__].__class__):  # type: ignore[misc]
+    def __call__(self, *args: str) -> None:
+        for file_name in args:
+            collector.cannibalize(DisabledTestsCollector(file_name))
+
+    @property
+    def antitesting(self) -> ModuleType:
+        return self
