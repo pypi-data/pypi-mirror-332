fb610c522069584a14467fa415825ba76817f538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:06:22 +0000

    Add package custom-torchtitan/0.0.2.1/custom_torchtitan-0.0.2.1.tar.gz

---
+++ packages/custom-torchtitan/custom_torchtitan-0.0.2.1.tar.gz/custom_torchtitan-0.0.2.1/torchtitan/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# Copyright (c) Meta Platforms, Inc. All Rights Reserved.
+
+# Import to register Float8Converter.
+import torchtitan.components.float8  # noqa: F401
+
+# Import the built-in models here so that the corresponding register_model_spec()
+# will be called.
+# import torchtitan.models  # noqa: F401
