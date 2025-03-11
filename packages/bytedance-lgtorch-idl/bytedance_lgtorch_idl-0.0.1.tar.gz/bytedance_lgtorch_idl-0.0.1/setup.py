b7f75ad5ae8ecc3507a28355f552afb0df783d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:15 +0000

    Add package bytedance-lgtorch-idl/0.0.1/bytedance_lgtorch_idl-0.0.1.tar.gz

---
+++ packages/bytedance-lgtorch-idl/bytedance_lgtorch_idl-0.0.1.tar.gz/bytedance_lgtorch_idl-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_lgtorch_idl",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_lgtorch_idl",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_lgtorch_idl.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
