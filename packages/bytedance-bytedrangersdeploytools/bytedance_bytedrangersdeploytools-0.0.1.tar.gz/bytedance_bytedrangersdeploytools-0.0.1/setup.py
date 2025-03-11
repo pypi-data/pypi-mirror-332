a8973f03ebbe0b947c3d435b6f5dc1f81ed60286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:45 +0000

    Add package bytedance-bytedrangersdeploytools/0.0.1/bytedance_bytedrangersdeploytools-0.0.1.tar.gz

---
+++ packages/bytedance-bytedrangersdeploytools/bytedance_bytedrangersdeploytools-0.0.1.tar.gz/bytedance_bytedrangersdeploytools-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_bytedrangersdeploytools",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_bytedrangersdeploytools",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_bytedrangersdeploytools.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
