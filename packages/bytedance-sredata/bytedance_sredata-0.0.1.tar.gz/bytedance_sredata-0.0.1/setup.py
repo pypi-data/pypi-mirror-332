f6b483e18f1bcee1ad18211a62181ff32a0dfaa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:17 +0000

    Add package bytedance-sredata/0.0.1/bytedance_sredata-0.0.1.tar.gz

---
+++ packages/bytedance-sredata/bytedance_sredata-0.0.1.tar.gz/bytedance_sredata-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_sredata",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_sredata",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_sredata.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
