540d0602e9a50e360585e1c65848ae04f100ab14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:07:18 +0000

    Add package hdl-registers/7.1.1/hdl_registers-7.1.1-py3-none-any.whl

---
+++ packages/hdl-registers/hdl_registers-7.1.1-py3-none-any.whl/hdl_registers/generator/reserved_keywords.py
@@ -0,0 +1,23 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl-registers project, an HDL register generator fast enough to run
+# in real time.
+# https://hdl-registers.com
+# https://github.com/hdl-registers/hdl-registers
+# --------------------------------------------------------------------------------------------------
+
+from hdl_registers.generator.c.reserved_keywords import RESERVED_C_KEYWORDS
+from hdl_registers.generator.cpp.reserved_keywords import RESERVED_CPP_KEYWORDS
+from hdl_registers.generator.html.reserved_keywords import RESERVED_HTML_KEYWORDS
+from hdl_registers.generator.python.reserved_keywords import RESERVED_PYTHON_KEYWORDS
+from hdl_registers.generator.vhdl.reserved_keywords import RESERVED_VHDL_KEYWORDS
+
+# All reserved keywords from all target languages.
+RESERVED_KEYWORDS = (
+    RESERVED_C_KEYWORDS
+    | RESERVED_CPP_KEYWORDS
+    | RESERVED_HTML_KEYWORDS
+    | RESERVED_PYTHON_KEYWORDS
+    | RESERVED_VHDL_KEYWORDS
+)
