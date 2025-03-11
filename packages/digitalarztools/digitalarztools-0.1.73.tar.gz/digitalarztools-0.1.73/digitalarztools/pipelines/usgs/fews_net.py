56919f482b28554b7796777290cc3fe8f9673654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:06:05 +0000

    Add package digitalarztools/0.1.73/digitalarztools-0.1.73.tar.gz

---
+++ packages/digitalarztools/digitalarztools-0.1.73.tar.gz/digitalarztools-0.1.73/digitalarztools/pipelines/usgs/fews_net.py
@@ -0,0 +1,10 @@
+
+
+class FEWSNet(object):
+    """
+    https://earlywarning.usgs.gov/fews/datadownloads/Global/CHIRPS%202.0
+    """
+
+    @staticmethod
+    def download_ndvi_data():
+        pass
