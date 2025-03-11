6feded9e6149b01ba8aba7899335c18a1e20cd2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:28 +0000

    Add package datafoundry/1.0.118/datafoundry-1.0.118-py3-none-any.whl

---
+++ packages/datafoundry/datafoundry-1.0.118-py3-none-any.whl/datafoundry/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,22 @@
+# --- render code block -----
+
+from .VngMarketing2024 import VngMarketing2024
+from .VngMarketingSourceV2 import VngMarketingSourceV2
+from .VngMarketingSourceV3 import VngMarketingSourceV3
+from .VngPgV2 import VngPgV2
+from .VngPgV3 import VngPgV3
+
+__all__ = [
+    
+    "VngMarketing2024",
+    
+    "VngMarketingSourceV2",
+    
+    "VngMarketingSourceV3",
+    
+    "VngPgV2",
+    
+    "VngPgV3"
+    
+]
+# --- render code block -----
