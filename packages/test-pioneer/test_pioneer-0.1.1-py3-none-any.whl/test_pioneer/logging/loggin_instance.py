8cde53866908bbf76eeaf1b60c0367096d18c8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:48 +0000

    Add package test-pioneer/0.1.1/test_pioneer-0.1.1-py3-none-any.whl

---
+++ packages/test-pioneer/test_pioneer-0.1.1-py3-none-any.whl/test_pioneer/logging/loggin_instance.py
@@ -0,0 +1,34 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+from IPython.core.usage import default_banner
+
+logging.root.setLevel(logging.DEBUG)
+test_pioneer_logger = logging.getLogger("TestPioneer")
+formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
+
+class TestPioneerHandler(RotatingFileHandler):
+
+    def __init__(self, filename: str = "TestPioneer.log", mode="w",
+                 maxBytes: int = 1073741824, backupCount: int = 0):
+        super().__init__(filename=filename, mode=mode, maxBytes=maxBytes, backupCount=backupCount)
+        self.formatter = formatter
+        self.setLevel(logging.DEBUG)
+
+    def emit(self, record: logging.LogRecord) -> None:
+        super().emit(record)
+
+
+def step_log_check(enable_logging: bool = False, logger: logging.Logger = None,
+                   level: str = "info", message: str = None):
+    if enable_logging and logger:
+        logger_level = {
+            "info": logger.info,
+            "error": logger.error,
+        }.get(level, None)
+        if logger_level is not None:
+            logger_level(message)
+
+default_file_handler = TestPioneerHandler()
+test_pioneer_logger.addHandler(default_file_handler)
