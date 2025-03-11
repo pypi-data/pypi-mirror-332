74252103cbb407c35fc1b06e4e9fd20ef0bf63f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:13:57 +0000

    Add package m4a2text/0.1.12/m4a2text-0.1.12.tar.gz

---
+++ packages/m4a2text/m4a2text-0.1.12.tar.gz/m4a2text-0.1.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="m4a2text",
+    version="0.1.12",
+    description="Convert M4A audio files to WAV and transcribe speech to text using Azure Speech API.",
+    author="Ian Park",
+    author_email="ianolpx@gmail.com",
+    url="https://github.com/ianolpx/m4a2text",
+    # packages=find_packages(),
+    py_modules=["m4a2text"],  # ✅ 단일 모듈 지정 (패키지가 아니라 단순 모듈)
+    install_requires=[
+        "ffmpeg-python",
+        "azure-cognitiveservices-speech",
+    ],
+    entry_points={
+        "console_scripts": [
+            "m4a2text=m4a2text.speech_converter:convert_m4a_to_wav",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
