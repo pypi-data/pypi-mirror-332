11a317398681ddbbd3d905980fa3c297e096163b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:06:15 +0000

    Add package qt-parameters/1.0.2/qt_parameters-1.0.2.tar.gz

---
+++ packages/qt-parameters/qt_parameters-1.0.2.tar.gz/qt_parameters-1.0.2/qt_parameters/__init__.py
@@ -0,0 +1,29 @@
+from .box import CollapsibleBox
+from .tabdata import TabDataParameter
+from .widgets import (
+    ParameterWidget,
+    IntParameter,
+    FloatParameter,
+    StringParameter,
+    PathParameter,
+    ComboParameter,
+    EnumParameter,
+    BoolParameter,
+    MultiIntParameter,
+    MultiFloatParameter,
+    PointParameter,
+    PointFParameter,
+    SizeParameter,
+    SizeFParameter,
+    ColorParameter,
+    Label,
+)
+from .editor import (
+    ParameterToolTip,
+    ParameterLabel,
+    ParameterForm,
+    ParameterEditor,
+)
+
+
+__version__ = '1.0.2'
