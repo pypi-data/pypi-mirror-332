dc48403f84c9b8bb5d226ce83d36fc9956285f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:45 +0000

    Add package doc-page-extractor/0.0.4/doc_page_extractor-0.0.4.tar.gz

---
+++ packages/doc-page-extractor/doc_page_extractor-0.0.4.tar.gz/doc_page_extractor-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="doc-page-extractor",
+  version="0.0.4",
+  author="Tao Zeyu",
+  author_email="i@taozeyu.com",
+  url="https://github.com/Moskize91/doc-page-extractor",
+  description="doc page extractor can identify text and format in images and return structured data.",
+  packages=find_packages(),
+  long_description=open("./README.md", encoding="utf8").read(),
+  long_description_content_type="text/markdown",
+  install_requires=[
+    "opencv-python>=4.11.0,<5.0",
+    "pillow>=10.3,<11.0",
+    "shapely>=2.0.0,<3.0",
+    "transformers>=4.48.0,<5.0",
+    "doclayout_yolo>=0.0.3",
+    "paddleocr==2.9.0", # https://github.com/Moskize91/doc-page-extractor/issues/3
+  ],
+)
