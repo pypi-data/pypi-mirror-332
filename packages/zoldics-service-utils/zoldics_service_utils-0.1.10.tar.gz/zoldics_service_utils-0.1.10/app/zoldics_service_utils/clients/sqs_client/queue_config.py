f804c7575b36d8da3e766fbddca1674a5c721b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 11:05:47 +0000

    Add package zoldics-service-utils/0.1.10/zoldics_service_utils-0.1.10.tar.gz

---
+++ packages/zoldics-service-utils/zoldics_service_utils-0.1.10.tar.gz/zoldics_service_utils-0.1.10/app/zoldics_service_utils/clients/sqs_client/queue_config.py
@@ -0,0 +1,22 @@
+from enum import StrEnum
+from typing import Dict
+
+"""
+Configuration:
+- When running locally, mock the queue names in a .env file.
+
+Example:
+<env>
+embedding-service=yourname-embedding-service
+evaluation-service=yourname-evaluation-service
+</env>
+"""
+
+
+class QUEUE_NAMES(StrEnum):
+    APP_001_AI_SERVICE = "app001-ai-service"
+
+
+QUEUE_CONFIG: Dict[str, Dict[str, bool]] = {
+    QUEUE_NAMES.APP_001_AI_SERVICE: dict(fifo=False),
+}
