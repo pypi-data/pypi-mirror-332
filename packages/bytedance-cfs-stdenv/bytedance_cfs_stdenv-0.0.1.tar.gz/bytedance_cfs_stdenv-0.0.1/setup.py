4863f71684a71a42731c4a8e6472802a61185367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:57 +0000

    Add package bytedance-cfs-stdenv/0.0.1/bytedance_cfs_stdenv-0.0.1.tar.gz

---
+++ packages/bytedance-cfs-stdenv/bytedance_cfs_stdenv-0.0.1.tar.gz/bytedance_cfs_stdenv-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_cfs_stdenv",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_cfs_stdenv",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_cfs_stdenv.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
