b553b0056b47f5d502d6545c64f764a6d9ebcbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:27 +0000

    Add package fkjghdkjghlDhbsli86432/0.0.1/fkjghdkjghlDhbsli86432-0.0.1.tar.gz

---
+++ packages/fkjghdkjghlDhbsli86432/fkjghdkjghlDhbsli86432-0.0.1.tar.gz/fkjghdkjghlDhbsli86432-0.0.1/fkjghdkjghlDhbsli86432/two.py
@@ -0,0 +1,5 @@
+import math
+
+def sin_squared(x):
+    print(math.sin(x) ** 2)
+sin_squared(math.pi / 4)
---
+++ packages/fkjghdkjghlDhbsli86432/fkjghdkjghlDhbsli86432-0.0.1.tar.gz/fkjghdkjghlDhbsli86432-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='fkjghdkjghlDhbsli86432', # Имя пакета
+    version='0.0.1', # Версия пакета
+    author='Dasha',
+    author_email='d_knishenko@mail.ru',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
