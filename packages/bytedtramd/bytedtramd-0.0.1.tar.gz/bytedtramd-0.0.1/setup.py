f8312fa552311de0c7deaeb9a079c2bc9c10f822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 17:06:40 +0000

    Add package bytedtramd/0.0.1/bytedtramd-0.0.1.tar.gz

---
+++ packages/bytedtramd/bytedtramd-0.0.1.tar.gz/bytedtramd-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedtramd",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedtramd",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedtramd.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
