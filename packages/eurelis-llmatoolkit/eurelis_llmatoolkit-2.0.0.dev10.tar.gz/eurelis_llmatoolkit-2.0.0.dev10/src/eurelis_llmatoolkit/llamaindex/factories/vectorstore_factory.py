bb1ce76566ce22df3c6891baaf2f5af15bff7987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:40 +0000

    Add package eurelis-llmatoolkit/2.0.0.dev10/eurelis_llmatoolkit-2.0.0.dev10.tar.gz

---
+++ packages/eurelis-llmatoolkit/eurelis_llmatoolkit-2.0.0.dev10.tar.gz/eurelis_llmatoolkit-2.0.0.dev10/src/eurelis_llmatoolkit/llamaindex/factories/vectorstore_factory.py
@@ -0,0 +1,39 @@
+class VectorStoreFactory:
+    @staticmethod
+    def create_vector_store(config: dict):
+        provider = config["provider"]
+
+        if provider == "MongoDB":
+            import pymongo
+            from llama_index.vector_stores.mongodb import MongoDBAtlasVectorSearch
+
+            client = pymongo.MongoClient(config["url"])
+
+            # FIXME : ValueError: Must specify MONGODB_URI via env variable if not directly passing in client.
+            return MongoDBAtlasVectorSearch(
+                client,
+                db_name=config["db_name"],
+                collection_name=config["collection_name"],
+                vector_index_name=config["vector_index_name"],
+            )
+
+        if provider == "Chroma":
+            import chromadb
+            from llama_index.vector_stores.chroma import ChromaVectorStore
+
+            mode = config.get("mode", "ephemeral")
+
+            if mode == "ephemeral":
+                client = chromadb.Client()
+            elif mode == "persistent":
+                client = chromadb.PersistentClient(config["path"])
+
+            # check if the collection exists
+            if config["collection_name"] in [c.name for c in client.list_collections()]:
+                collection = client.get_collection(config["collection_name"])
+            else:
+                collection = client.create_collection(config["collection_name"])
+
+            return ChromaVectorStore(client=client, chroma_collection=collection)
+
+        raise ValueError(f"Vector store provider {provider} is not supported.")
