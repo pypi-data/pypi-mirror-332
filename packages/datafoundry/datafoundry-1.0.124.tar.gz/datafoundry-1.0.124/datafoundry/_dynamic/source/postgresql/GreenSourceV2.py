43edc303f7bcf7fa7d4d93b05dd00063f272cd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:05:36 +0000

    Add package datafoundry/1.0.124/datafoundry-1.0.124.tar.gz

---
+++ packages/datafoundry/datafoundry-1.0.124.tar.gz/datafoundry-1.0.124/datafoundry/_dynamic/source/postgresql/GreenSourceV2.py
@@ -0,0 +1,11 @@
+# --- render code block -----
+from datafoundry._core.postgres_core import PostgresqlBase
+
+class GreenSourceV2(PostgresqlBase):
+    @classmethod
+    def get_uuid(cls) -> str:
+        """
+        initialize credential for the class
+        """
+        return "67ce9f0b57fdaddab2883001"
+# --- render code block -----
---
+++ packages/datafoundry/datafoundry-1.0.124.tar.gz/datafoundry-1.0.124/datafoundry/_dynamic/source/postgresql/__init__.py
@@ -0,0 +1,25 @@
+# --- render code block -----
+
+from .VngMarketing2024 import VngMarketing2024
+from .VngMarketingSourceV2 import VngMarketingSourceV2
+from .VngMarketingSourceV3 import VngMarketingSourceV3
+from .GreennodeSourceV1 import GreennodeSourceV1
+from .GreenSourceV2 import GreenSourceV2
+from .GreenNodeSource import GreenNodeSource
+
+__all__ = [
+    
+    "VngMarketing2024",
+    
+    "VngMarketingSourceV2",
+    
+    "VngMarketingSourceV3",
+    
+    "GreennodeSourceV1",
+    
+    "GreenSourceV2",
+    
+    "GreenNodeSource"
+    
+]
+# --- render code block -----
