a84c81db7a170ad22831b6d9cecb8cf9d5dd1483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:35 +0000

    Add package pupok-123/0.0.1/pupok_123-0.0.1.tar.gz

---
+++ packages/pupok-123/pupok_123-0.0.1.tar.gz/pupok_123-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name='pupok_123', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Tatyana',
+    author_email='tatyanka.nadtochieva@mail.ru',
+    description='PUPOK',
+    packages=setuptools.find_packages(),
+ )
