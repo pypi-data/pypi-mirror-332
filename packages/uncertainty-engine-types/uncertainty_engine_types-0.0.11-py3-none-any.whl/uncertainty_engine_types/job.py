e7c4776ffecb46c6c41513761dc3217fd8e4c8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 09:06:20 +0000

    Add package uncertainty-engine-types/0.0.11/uncertainty_engine_types-0.0.11-py3-none-any.whl

---
+++ packages/uncertainty-engine-types/uncertainty_engine_types-0.0.11-py3-none-any.whl/uncertainty_engine_types/job.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class JobStatus(Enum):
+    PENDING = "pending"
+    RUNNING = "running"
+    COMPLETED = "completed"
+    FAILED = "failed"
+
+
+class JobInfo(BaseModel):
+    status: JobStatus
+    message: Optional[str] = None
+    inputs: dict
+    outputs: Optional[dict] = None
