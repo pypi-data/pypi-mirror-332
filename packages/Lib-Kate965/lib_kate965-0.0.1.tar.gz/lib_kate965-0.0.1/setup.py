fa809c2458d393635a029941a28d3620e5cb42ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 06:05:59 +0000

    Add package Lib-Kate965/0.0.1/lib_kate965-0.0.1.tar.gz

---
+++ packages/Lib-Kate965/lib_kate965-0.0.1.tar.gz/lib_kate965-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name='Lib_Kate965', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Your Name',
+    author_email='rom.1405@mail.ru',
+    description='Lib_Kate',
+    packages=setuptools.find_packages(),
+    )
