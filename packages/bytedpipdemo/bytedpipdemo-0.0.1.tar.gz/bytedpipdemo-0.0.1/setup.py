06a77ea055219dd69269caa6800101bca8ade295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 04:05:09 +0000

    Add package bytedpipdemo/0.0.1/bytedpipdemo-0.0.1.tar.gz

---
+++ packages/bytedpipdemo/bytedpipdemo-0.0.1.tar.gz/bytedpipdemo-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedpipdemo",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedpipdemo",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedpipdemo.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
