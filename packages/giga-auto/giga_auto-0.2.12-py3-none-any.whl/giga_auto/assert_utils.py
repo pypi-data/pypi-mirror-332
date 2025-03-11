bded759dee2784f9ef53d702af4ad1ccd7094262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 03:14:56 +0000

    Add package giga-auto/0.2.12/giga_auto-0.2.12-py3-none-any.whl

---
+++ packages/giga-auto/giga_auto-0.2.12-py3-none-any.whl/giga_auto/assert_utils.py
@@ -0,0 +1,45 @@
+class AssertUtils:
+
+    @staticmethod
+    def assert_equal(actual, expected, msg=None):
+        """
+        断言两个值相等
+        """
+        assert actual == expected, f"{msg or ''}\nAssert Equal Failed:\nExpected:{expected}\nActual:{actual}"
+
+    @staticmethod
+    def assert_not_equal(actual, expected, msg=None):
+        """
+        断言两个值不相等
+        """
+        assert actual != expected, f"{msg or ''}\nAssert Not Equal Failed: Expected:{expected},Actual:{actual}"
+
+    @staticmethod
+    def assert_in(expect_text, actual_text, msg=None):
+        """
+        断言actual在expected中
+        """
+        assert expect_text in actual_text, f"{msg or ''} \nAssert In Failed:\nExpected:{expect_text}"
+
+    @staticmethod
+    def assert_not_in(expect_text, actual_text, msg=None):
+        """
+        断言actual不在expected中
+        """
+        assert expect_text not in actual_text, f"{msg or ''} \nAssert Not In Failed"
+
+    @staticmethod
+    def assert_not_none(actual, msg=None):
+        assert actual is not None, f"{msg or ''} \nAssert Not None Failed:\nActual:{actual}"
+
+    @staticmethod
+    def assert_is_none(actual, msg=None):
+        assert actual is None, f"{msg or ''} \nAssert Not None Failed:\nActual:{actual}"
+
+    @staticmethod
+    def assert_true(actual, msg=None):
+        assert actual == True, f"{msg or ''} \nAssert True Failed:\nActual:{actual}"
+
+    @staticmethod
+    def assert_false(actual, msg=None):
+        assert actual == False, f"{msg or ''} \nAssert False Failed:\nActual:{actual}"
