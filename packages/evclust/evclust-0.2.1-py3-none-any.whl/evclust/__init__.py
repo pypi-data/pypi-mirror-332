977d63b01c8c3a4336c25c51576a65ba99094934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 05:05:54 +0000

    Add package evclust/0.2.1/evclust-0.2.1-py3-none-any.whl

---
+++ packages/evclust/evclust-0.2.1-py3-none-any.whl/evclust/__init__.py
@@ -0,0 +1,10 @@
+
+"""
+This module is used to initialise the package.
+It contains imports that must be available at package level.
+"""
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("evclust")
+
