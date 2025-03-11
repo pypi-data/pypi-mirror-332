b8186bf6690e372f810b353d7c3fe1a5b8062d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 07:05:28 +0000

    Add package zmbus-sdk/0.1.5/zmbus_sdk-0.1.5.tar.gz

---
+++ packages/zmbus-sdk/zmbus_sdk-0.1.5.tar.gz/zmbus_sdk-0.1.5/zimasabus_sdk/zimasa/__init__.py
@@ -0,0 +1,8 @@
+"""
+Zimasa module for healthcare-related services.
+"""
+
+from zimasabus_sdk.zimasa.appointments import AppointmentService
+from zimasabus_sdk.zimasa.followups import FollowUpService
+
+__all__ = ["AppointmentService", "FollowUpService"]
