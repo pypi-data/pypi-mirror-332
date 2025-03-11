25d799fe3b5c73b8fa5b3dcd9e2d17f8705c07e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 15:07:10 +0000

    Add package bytedance-roi-statsmodel/0.0.1/bytedance_roi_statsmodel-0.0.1.tar.gz

---
+++ packages/bytedance-roi-statsmodel/bytedance_roi_statsmodel-0.0.1.tar.gz/bytedance_roi_statsmodel-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bytedance_roi_statsmodel",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named bytedance_roi_statsmodel",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/bytedance_roi_statsmodel.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
