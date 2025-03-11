a10024d5b11cef14a3de02624cf5c467bec2fd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-11 16:05:15 +0000

    Add package byted-overpass-life-alliance-task-fulfilment-base/0.0.1/byted_overpass_life_alliance_task_fulfilment_base-0.0.1.tar.gz

---
+++ packages/byted-overpass-life-alliance-task-fulfilment-base/byted_overpass_life_alliance_task_fulfilment_base-0.0.1.tar.gz/byted_overpass_life_alliance_task_fulfilment_base-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="byted_overpass_life_alliance_task_fulfilment_base",
+    version="0.0.1",
+    packages=find_packages(),
+    description="A simple package named byted_overpass_life_alliance_task_fulfilment_base",
+    author="artifact",
+    author_email="artifact@example.com",
+    url="https://github.com/artifact/byted_overpass_life_alliance_task_fulfilment_base.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
